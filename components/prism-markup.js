1cd590fd83b1888b7d875b159f226b757393cdab
Jacob Rask <jacob@jacobrask.net>
2012-10-26 22:57:06 +0200

    Convert all language grammars to a two dimensional array format
    Faster in all tested browsers. See http://jsperf.com/prismjs/4

--- components/prism-markup.js
+++ components/prism-markup.js
@@ -1,40 +1,38 @@
-Prism.languages.markup = {
-	'comment': /&lt;!--[\w\W]*?--(&gt;|&gt;)/g,
-	'prolog': /&lt;\?.+?\?&gt;/,
-	'doctype': /&lt;!DOCTYPE.+?&gt;/,
-	'cdata': /&lt;!\[CDATA\[[\w\W]+?]]&gt;/i,
-	'tag': {
+Prism.languages.markup = [
+	[ 'comment', /&lt;!--[\w\W]*?--(&gt;|&gt;)/g ],
+	[ 'prolog', /&lt;\?.+?\?&gt;/ ],
+	[ 'doctype', /&lt;!DOCTYPE.+?&gt;/ ],
+	[ 'cdata', /&lt;!\[CDATA\[[\w\W]+?]]&gt;/i ],
+	[ 'tag', {
 		pattern: /&lt;\/?[\w:-]+\s*[\w\W]*?&gt;/gi,
-		inside: {
-			'tag': {
+		inside: [
+			[ 'tag', {
 				pattern: /^&lt;\/?[\w:-]+/i,
-				inside: {
-					'punctuation': /^&lt;\/?/,
-					'namespace': /^[\w-]+?:/
-				}
-			},
-			'attr-value': {
+				inside: [
+					[ 'punctuation', /^&lt;\/?/ ],
+					[ 'namespace', /^[\w-]+?:/ ]
+				]
+			} ],
+			[ 'attr-value', {
 				pattern: /=(('|")[\w\W]*?(\2)|[^\s>]+)/gi,
-				inside: {
-					'punctuation': /=/g
-				}
-			},
-			'punctuation': /\/?&gt;/g,
-			'attr-name': {
+				inside: [
+					[ 'punctuation', /=/g ]
+				]
+			} ],
+			[ 'punctuation', /\/?&gt;/g ],
+			[ 'attr-name', {
 				pattern: /[\w:-]+/g,
-				inside: {
-					'namespace': /^[\w-]+?:/
-				}
-			}
-			
-		}
-	},
-	'entity': /&amp;#?[\da-z]{1,8};/gi
-};
+				inside: [
+					[ 'namespace', /^[\w-]+?:/ ]
+				]
+			} ]
+		]
+	} ],
+	[ 'entity', /&amp;#?[\da-z]{1,8};/gi ]
+];
 
 // Plugin to make entity title show the real entity, idea by Roman Komarov
 Prism.hooks.add('wrap', function(env) {
-
 	if (env.type === 'entity') {
 		env.attributes['title'] = env.content.replace(/&amp;/, '&');
 	}
