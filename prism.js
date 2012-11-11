1cdfcb840b2e3eb995ccf0337516e899cbeb491a
Lea Verou <lea@verou.me>
2012-11-11 04:25:56 +0200

    Fixed #37

--- prism.js
+++ prism.js
@@ -443,7 +443,7 @@ Prism.languages.clike = {
 	'string': /("|')(\\?.)*?\1/g,
 	'keyword': /\b(if|else|while|do|for|return|in|instanceof|function|new|try|catch|finally|null|break|continue)\b/g,
 	'boolean': /\b(true|false)\b/g,
-	'number': /\b-?(0x)?[\da-f]*\.?\d+\b/g,
+	'number': /\b-?(0x)?\d*\.?[\da-f]+\b/g,
 	'operator': /[-+]{1,2}|!|=?&lt;|=?&gt;|={1,2}|(&amp;){1,2}|\|?\||\?|\*|\//g,
 	'ignore': /&(lt|gt|amp);/gi,
 	'punctuation': /[{}[\];(),.:]/g
@@ -454,7 +454,8 @@ Prism.languages.clike = {
 ********************************************** */
 
 Prism.languages.javascript = Prism.languages.extend('clike', {
-	'keyword': /\b(var|let|if|else|while|do|for|return|in|instanceof|function|new|with|typeof|try|catch|finally|null|break|continue)\b/g
+	'keyword': /\b(var|let|if|else|while|do|for|return|in|instanceof|function|new|with|typeof|try|catch|finally|null|break|continue)\b/g,
+	'number': /\b(-?(0x)?\d*\.?[\da-f]+|NaN|-?Infinity)\b/g,
 });
 
 Prism.languages.insertBefore('javascript', 'keyword', {
