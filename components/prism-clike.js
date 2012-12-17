ad010eac22ca42eae28363c627f8b6bae92eda52
mseeboeck <marcus.seeboeck@chello.at>
2012-12-18 00:18:43 +0100

    Changed regex Prism.languages.clike.number
    Changes:
    * Variable names matching /[a-f]+/ are no longer treated as numbers.
    * Added support for upper case and scientific notation.
    * Also changed corresponding files in /components.
    
    Minor issues:
    * No support for type suffixes (e.g. 300f for float).
    * Leading decimal point is marked up as 'punctuation', instead as part of
    * the number (e.g. .1; 0.1 is matched correctly).

--- components/prism-clike.js
+++ components/prism-clike.js
@@ -6,7 +6,7 @@ Prism.languages.clike = {
 	'string': /("|')(\\?.)*?\1/g,
 	'keyword': /\b(if|else|while|do|for|return|in|instanceof|function|new|try|catch|finally|null|break|continue)\b/g,
 	'boolean': /\b(true|false)\b/g,
-	'number': /\b-?(0x)?\d*\.?[\da-f]+\b/g,
+	'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?)\b/g,
 	'operator': /[-+]{1,2}|!|=?&lt;|=?&gt;|={1,2}|(&amp;){1,2}|\|?\||\?|\*|\//g,
 	'ignore': /&(lt|gt|amp);/gi,
 	'punctuation': /[{}[\];(),.:]/g
