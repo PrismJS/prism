9b8d5114c7525c4e6231258e2988f41fe22f5186
Miles Johnson <mileswjohnson@gmail.com>
2013-05-03 16:10:27 -0700

    Added pseudo, hexcode, number, function to CSS Based on Issue #54

--- prism.js
+++ prism.js
@@ -402,10 +402,18 @@ Prism.languages.css = {
 	'comment': /\/\*[\w\W]*?\*\//g,
 	'atrule': /@[\w-]+?(\s+[^;{]+)?(?=\s*{|\s*;)/gi,
 	'url': /url\((["']?).*?\1\)/gi,
-	'selector': /[^\{\}\s][^\{\}]*(?=\s*\{)/g,
-	'property': /(\b|\B)[a-z-]+(?=\s*:)/ig,
+	'selector': {
+		pattern: /[^\{\}\s][^\{\}]*(?=\s*\{)/g,
+		inside: {
+			'pseudo': /:\w+/g
+		}
+	},
 	'string': /("|')(\\?.)*?\1/g,
+	'property': /(\b|\B)[a-z-]+(?=\s*:)/ig,
 	'important': /\B!important\b/gi,
+	'hexcode': /#[0-9a-f]{3,6}/gi,
+	'number': /\d+/g,
+	'function': /(attr|calc|cross-fade|cycle|element|hsl|hsla|image|lang|linear-gradient|matrix|matrix3d|perspective|radial-gradient|repeating-linear-gradient|repeating-radial-gradient|rgb|rgba|rotate|rotatex|rotatey|rotatez|rotate3d|scale|scalex|scaley|scalez|scale3d|skew|skewx|skewy|steps|translate|translatex|translatey|translatez|translate3d|url|var)/ig,
 	'ignore': /&(lt|gt|amp);/gi,
 	'punctuation': /[\{\};:]/g
 };
