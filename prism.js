a1daa2e039e311a0a808396daed023350a86d2b6
Miles Johnson <mileswjohnson@gmail.com>
2013-05-03 11:44:35 -0700

    Added instanceof for class lookup

--- prism.js
+++ prism.js
@@ -437,7 +437,7 @@ Prism.languages.clike = {
 	},
 	'string': /("|')(\\?.)*?\1/g,
 	'class-name': {
-		pattern: /(class|interface|extends|implements|trait) [a-z0-9_\.]+/ig,
+		pattern: /(class|interface|extends|implements|trait|instanceof) [a-z0-9_\.]+/ig,
 		lookbehind: true
 	},
 	'keyword': /\b(if|else|while|do|for|return|in|instanceof|function|new|try|catch|finally|null|break|continue)\b/g,
