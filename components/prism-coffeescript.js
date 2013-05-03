39175b7c53026d726bad767a9ed4ab9638f2d3ec
Miles Johnson <mileswjohnson@gmail.com>
2013-05-03 12:44:24 -0700

    Generated new min files

--- components/prism-coffeescript.js
+++ components/prism-coffeescript.js
@@ -1,7 +1,7 @@
 Prism.languages.coffeescript = Prism.languages.extend('javascript', {
 	'block-comment': /([#]{3}\s*\r?\n(.*\s*\r*\n*)\s*?\r?\n[#]{3})/g,
 	'comment': /(\s|^)([#]{1}[^#^\r^\n]{2,}?(\r?\n|$))/g,
-	'keyword': /\b(this|window|delete|class|extends|namespace|extend|ar|let|if|else|while|do|for|each|of|return|in|instanceof|new|with|typeof|try|catch|finally|null|undefined|break|continue)\b/g,
+	'keyword': /\b(this|window|delete|class|extends|namespace|extend|ar|let|if|else|while|do|for|each|of|return|in|instanceof|new|with|typeof|try|catch|finally|null|undefined|break|continue)\b/g
 });
 
 Prism.languages.insertBefore('coffeescript', 'keyword', {
@@ -12,6 +12,5 @@ Prism.languages.insertBefore('coffeescript', 'keyword', {
 			'operator': /[-+]{1,2}|!|=?&lt;|=?&gt;|={1,2}|(&amp;){1,2}|\|?\||\?|\*|\//g
 		}
 	},
-
 	'attr-name': /[_?a-z-|A-Z-]+(\s*:)| @[_?$?a-z-|A-Z-]+(\s*)| /g
 });
