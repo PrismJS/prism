943a0cd469eca325501584821e1a39a0a994f145
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-05-10 07:17:33 +0100

    updated groovy syntax to take advantage of language argument now being passed to wrap callback

--- components/prism-groovy.js
+++ components/prism-groovy.js
@@ -1,13 +1,10 @@
 Prism.languages.groovy = Prism.languages.extend('clike', {
 	'keyword': /\b(as|def|in|abstract|assert|boolean|break|byte|case|catch|char|class|const|continue|default|do|double|else|enum|extends|final|finally|float|for|goto|if|implements|import|instanceof|int|interface|long|native|new|package|private|protected|public|return|short|static|strictfp|super|switch|synchronized|this|throw|throws|transient|try|void|volatile|while)\b/g,
+	'string': /("""|''')[\W\w]*?\1|("|'|\/)[\W\w]*?\2/g,
 	'number': /\b0b[01_]+\b|\b0x[\da-f_]+(\.[\da-f_p\-]+)?\b|\b[\d_]+(\.[\d_]+[e]?[\d]*)?[glidf]\b|[\d_]+(\.[\d_]+)?\b/gi,
 	'operator': /={0,2}~|\?\.|\*?\.@|\.&amp;|\.(?=\w)|\.{2}(&lt;)?(?=\w)|-&gt;|\?:|[-+]{1,2}|!|&lt;=&gt;|(&gt;){1,3}|(&lt;){1,2}|={1,2}|(&amp;){1,2}|\|{1,2}|\?|\*{1,2}|\/|\^|%/g,
 	'punctuation': /\.+|[{}[\];(),:$]/g,
 	'annotation': /@\w+/
-});
-
-Prism.languages.insertBefore('groovy', 'string', {
-	'gstring': /("""|''')[\W\w]*?\1|("|'|\/)[\W\w]*?\2/g
 });
 
 Prism.languages.insertBefore('groovy', 'punctuation', {
@@ -15,7 +12,7 @@ Prism.languages.insertBefore('groovy', 'punctuation', {
 });
 
 Prism.hooks.add('wrap', function(env) {
-	if (env.type === 'gstring') {
+	if (env.language === 'groovy' && env.type === 'string') {
 		var delimiter = env.content[0];
 
 		if (delimiter != "'") {
@@ -26,12 +23,8 @@ Prism.hooks.add('wrap', function(env) {
 					inside: Prism.languages.groovy
 				}
 			});
-		}
 
-		switch (delimiter) {
-			case "'": env.classes = ['token', 'string']; break;
-			case '"': env.classes = ['token', 'string', 'gstring']; break;
-			case '/': env.classes = ['token', 'string', 'regex']; break;
+			env.classes.push(delimiter === '/' ? 'regex' : 'gstring');
 		}
 	}
 });
