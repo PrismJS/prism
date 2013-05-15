9bc70c5eab1ebd2614db7295a5f5705193921b61
Miles Johnson <mileswjohnson@gmail.com>
2013-05-15 10:44:42 -0700

    Moved CSS rules to an extras file

--- prism.js
+++ prism.js
@@ -408,18 +408,10 @@ Prism.languages.css = {
 	'comment': /\/\*[\w\W]*?\*\//g,
 	'atrule': /@[\w-]+?(\s+[^;{]+)?(?=\s*{|\s*;)/gi,
 	'url': /url\((["']?).*?\1\)/gi,
-	'selector': {
-		pattern: /[^\{\}\s][^\{\}]*(?=\s*\{)/g,
-		inside: {
-			'pseudo': /:[-a-z0-9]+/g
-		}
-	},
-	'string': /("|')(\\?.)*?\1/g,
+	'selector': /[^\{\}\s][^\{\}]*(?=\s*\{)/g,
 	'property': /(\b|\B)[a-z-]+(?=\s*:)/ig,
+	'string': /("|')(\\?.)*?\1/g,
 	'important': /\B!important\b/gi,
-	'hexcode': /#[0-9a-f]{3,6}/gi,
-	'number': /[0-9%\.]+/g,
-	'function': /(attr|calc|cross-fade|cycle|element|hsl|hsla|image|lang|linear-gradient|matrix|matrix3d|perspective|radial-gradient|repeating-linear-gradient|repeating-radial-gradient|rgb|rgba|rotate|rotatex|rotatey|rotatez|rotate3d|scale|scalex|scaley|scalez|scale3d|skew|skewx|skewy|steps|translate|translatex|translatey|translatez|translate3d|url|var)/ig,
 	'ignore': /&(lt|gt|amp);/gi,
 	'punctuation': /[\{\};:]/g
 };
@@ -438,6 +430,20 @@ if (Prism.languages.markup) {
 		}
 	});
 }
+
+Prism.languages.css.selector = {
+	pattern: /[^\{\}\s][^\{\}]*(?=\s*\{)/g,
+	inside: {
+		'pseudo': /::?[-a-z0-9]+/g
+	}
+};
+
+Prism.languages.insertBefore('css', 'ignore', {
+	'hexcode': /#[0-9a-f]{3,6}/gi,
+	'entity': /\\[\da-f]{1,8}/gi,
+	'number': /[0-9%\.]+/g,
+	'function': /(attr|calc|cross-fade|cycle|element|hsl|hsla|image|lang|linear-gradient|matrix|matrix3d|perspective|radial-gradient|repeating-linear-gradient|repeating-radial-gradient|rgb|rgba|rotate|rotatex|rotatey|rotatez|rotate3d|scale|scalex|scaley|scalez|scale3d|skew|skewx|skewy|steps|translate|translatex|translatey|translatez|translate3d|url|var)/ig
+});
 
 /* **********************************************
      Begin prism-clike.js
