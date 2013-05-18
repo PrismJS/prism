1318f799dd766e7605f528680a5576efe7402776
Miles Johnson <mileswjohnson@gmail.com>
2013-05-17 17:03:49 -0700

    Improved selector patterns

--- components/prism-css-extras.js
+++ components/prism-css-extras.js
@@ -1,14 +1,13 @@
 Prism.languages.css.selector = {
 	pattern: /[^\{\}\s][^\{\}]*(?=\s*\{)/ig,
 	inside: {
-		'pseudo': /::?[-a-z0-9]+/ig,
-		'selector-class': /\.[-a-z0-9]+/ig,
-		'selector-id': /#[-a-z0-9]+/ig
+		'pseudo': /::?[-a-z0-9\(\)+-]+/ig,
+		'selector-class': /\.[-a-z0-9_:\.]+/ig,
+		'selector-id': /#[-a-z0-9_:\.]+/ig
 	}
 };
 
 Prism.languages.insertBefore('css', 'ignore', {
-	'pseudo': /::?[-a-z0-9]+/g,
 	'hexcode': /#[0-9a-f]{3,6}/gi,
 	'number': /[0-9%\.]+/g,
 	'function': /(attr|calc|cross-fade|cycle|element|hsl|hsla|image|lang|linear-gradient|matrix|matrix3d|perspective|radial-gradient|repeating-linear-gradient|repeating-radial-gradient|rgb|rgba|rotate|rotatex|rotatey|rotatez|rotate3d|scale|scalex|scaley|scalez|scale3d|skew|skewx|skewy|steps|translate|translatex|translatey|translatez|translate3d|url|var)/ig
--- prism.js
+++ prism.js
@@ -432,15 +432,16 @@ if (Prism.languages.markup) {
 }
 
 Prism.languages.css.selector = {
-	pattern: /[^\{\}\s][^\{\}]*(?=\s*\{)/g,
+	pattern: /[^\{\}\s][^\{\}]*(?=\s*\{)/ig,
 	inside: {
-		'pseudo': /::?[-a-z0-9]+/g
+		'pseudo': /::?[-a-z0-9\(\)+-]+/ig,
+		'selector-class': /\.[-a-z0-9_:\.]+/ig,
+		'selector-id': /#[-a-z0-9_:\.]+/ig
 	}
 };
 
 Prism.languages.insertBefore('css', 'ignore', {
 	'hexcode': /#[0-9a-f]{3,6}/gi,
-	'entity': /\\[\da-f]{1,8}/gi,
 	'number': /[0-9%\.]+/g,
 	'function': /(attr|calc|cross-fade|cycle|element|hsl|hsla|image|lang|linear-gradient|matrix|matrix3d|perspective|radial-gradient|repeating-linear-gradient|repeating-radial-gradient|rgb|rgba|rotate|rotatex|rotatey|rotatez|rotate3d|scale|scalex|scaley|scalez|scale3d|skew|skewx|skewy|steps|translate|translatex|translatey|translatez|translate3d|url|var)/ig
 });
