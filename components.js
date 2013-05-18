97d57c65d4a5cb6b3f01903bd42fe6d67819557d
Lea Verou <lea@verou.me>
2013-05-19 00:35:29 +0300

    Split the components registration object into its own file

---
+++ components.js
@@ -0,0 +1,94 @@
+var components = {
+	core: {
+		meta: {
+			path: 'components/prism-core.js',
+			option: 'mandatory'
+		},
+		'core': 'Core'
+	},
+	themes: {
+		meta: {
+			path: '{id}.css',
+			link: 'index.html?theme={id}',
+			exclusive: true
+		},
+		'prism': {
+			title: 'Default',
+			option: 'default'
+		},
+		'prism-dark': 'Dark',
+		'prism-funky': 'Funky',
+		'okaidia': 'Okaidia'
+	},
+	languages: {
+		meta: {
+			path: 'components/prism-{id}'
+		},
+		'markup': {
+			title: 'Markup',
+			option: 'default'
+		},
+		'css': {
+			title: 'CSS',
+			option: 'default'
+		},
+		'clike': {
+			title: 'C-like',
+			option: 'default'
+		},
+		'javascript': {
+			title: 'JavaScript',
+			option: 'default',
+			require: 'clike'
+		},
+		'java' : {
+			title: 'Java',
+			require: 'clike'
+		},
+		'php' : {
+			title: 'PHP',
+			require: 'clike'
+		},
+		'coffeescript': {
+			title: 'CoffeeScript',
+			require: 'javascript'
+		},
+		'scss': {
+			title: 'Sass (Scss)',
+			require: 'css'
+		},
+		'bash' : {
+			title: 'Bash',
+			require: 'clike'
+		},
+		'c': {
+			title: 'C',
+			require: 'clike'
+		},
+		'cpp': {
+			title: 'C++',
+			require: 'c'
+		},
+		'python': {
+			title: 'Python'
+		},
+		'sql': {
+			title: 'SQL'
+		},
+		'groovy': {
+			title: 'Groovy',
+			require: 'clike'
+		}
+	},
+	plugins: {
+		meta: {
+			path: 'plugins/{id}/prism-{id}',
+			link: 'plugins/{id}/',
+			hasCSS: true
+		},
+		'line-highlight': 'Line Highlight',
+		'show-invisibles': 'Show Invisibles',
+		'autolinker': 'Autolinker',
+		'wpd': 'WebPlatform Docs'
+	}
+};
