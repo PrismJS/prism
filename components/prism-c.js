2aeed5ccb58d42873ea75dd44835a132bed2b46d
Fabian Giesen <ryg@gmx.net>
2013-07-25 19:19:15 -0700

    Refine C number regexp.
    - Exponent may contain '+', not just '-'.
    - Numbers may have "float" (f), "unsigned" (u), "long" (l)
      or "long long" (ll) suffixes.
    
    The set of suffixes described by this regexp is somewhat larger
    than what a real C compiler would accept.

--- components/prism-c.js
+++ components/prism-c.js
@@ -1,6 +1,7 @@
 Prism.languages.c = Prism.languages.extend('clike', {
 	'keyword': /\b(asm|typeof|inline|auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)\b/g,
-	'operator': /[-+]{1,2}|!=?|&lt;{1,2}=?|&gt;{1,2}=?|\-&gt;|={1,2}|\^|~|%|(&amp;){1,2}|\|?\||\?|\*|\//g
+	'operator': /[-+]{1,2}|!=?|&lt;{1,2}=?|&gt;{1,2}=?|\-&gt;|={1,2}|\^|~|%|(&amp;){1,2}|\|?\||\?|\*|\//g,
+	'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee][+-]?\d+)?)[fFuUlL]*\b/g
 });
 
 Prism.languages.insertBefore('c', 'keyword', {
