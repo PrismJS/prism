e694d6a153973382745821eae1622c0312f4be8c
valipour <valipour@gmail.com>
2013-10-26 20:56:49 +0100

    add component file for gherkin language

---
+++ components/prism-gherkin.js
@@ -0,0 +1,13 @@
+// TODO:
+// 		- Support for outline parameters
+// 		- Support for tables
+
+Prism.languages.gherkin = {
+	'comment': {
+		pattern: /(^|[^\\])(\/\*[\w\W]*?\*\/|((#)|(\/\/)).*?(\r?\n|$))/g,
+		lookbehind: true
+	},
+	'string': /("|')(\\?.)*?\1/g,
+	'atrule': /\b(And|Given|When|Then|In order to|As an|I want to|As a)\b/g,
+	'keyword': /\b(Scenario Outline|Scenario|Feature|Background|Story)\b/g,
+};
