d0806c82c3a774ec903c640187d8b93806327ea0
Daniel Wachsstock <d.wachss@prodigy.net>
2013-12-09 10:40:05 -0600

    New line numbering plugin that uses ::before rather than a new element

--- plugins/line-numbers/prism-line-numbers.js
+++ plugins/line-numbers/prism-line-numbers.js
@@ -2,7 +2,7 @@ Prism.hooks.add('before-insert', function(env){
 	var el = env.element;
 	if (!(el.hasAttribute('data-linenumber'))) return;
 	var startNumber = parseInt(el.getAttribute('data-linenumber'))||0;
-	el.style.counterReset = getComputedStyle(el).counterReset.replace(/\d+/, startNumber-1);
+	el.style.counterReset = getComputedStyle(el).counterReset.replace(/-?\d+/, startNumber-1);
 	var line = '<span class=line >', endline = '</span>';
 	env.highlightedCode = line + env.highlightedCode.split('\n').join(endline+'\n'+line) + endline;
 });
