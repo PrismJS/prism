444c60f947d166ebae9c69e81d8e11c94c5d934a
Thomas Ingram <code@ravinggenius.com>
2014-05-10 19:32:08 -0400

    Recognize boolean, date, time, datetime, number

--- components/prism-rip.js
+++ components/prism-rip.js
@@ -3,5 +3,11 @@ Prism.languages.rip = {
 
 	'builtin': /\b(@|System)\b/g,
 
-	'boolean': /\b(true|false)\b/g
+	'boolean': /\b(true|false)\b/g,
+
+	'date': /\b\d{4}-\d{2}-\d{2}\b/g,
+	'time': /\b\d{2}:\d{2}:\d{2}\b/g,
+	'datetime': /\b\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\b/g,
+
+	'number': /[+-]?(?:(?:\d+\.\d+)|(?:\d+))/g
 };
