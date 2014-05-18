8115e6409a29223977ffdaa68ba6baf08e560c34
Eugene Platonov <jozic@ua.fm>
2014-05-17 21:32:55 -0400

    add Scala support to prism

---
+++ components/prism-scala.js
@@ -0,0 +1,8 @@
+Prism.languages.scala = Prism.languages.extend('java', {
+	'keyword': /(<-|=>)|\b(abstract|case|catch|class|def|do|else|extends|final|finally|for|forSome|if|implicit|import|lazy|match|new|null|object|override|package|private|protected|return|sealed|self|super|this|throw|trait|try|type|val|var|while|with|yield)\b/g,
+	'builtin': /\b(String|Int|Long|Short|Byte|Boolean|Double|Float|Char|Any|AnyRef|AnyVal|Unit|Nothing)\b/g,
+	'number': /\b0x[\da-f]*\.?[\da-f\-]+\b|\b\d*\.?\d+[e]?[\d]*[dfl]?\b/gi,
+	'symbol': /'([^\d\s]\w*)/g,
+	'string': /(""")[\W\w]*?\1|("|\/)[\W\w]*?\2|('.')/g
+});
+delete Prism.languages.scala['class-name','function'];
