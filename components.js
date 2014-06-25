ecb20c18627527802a0f2e92575405d97096d6cb
Tzu-ping Chung <uranusjr@gmail.com>
2014-06-25 22:06:06 +0800

    Add objc to components

--- components.js
+++ components.js
@@ -156,6 +156,11 @@ var components = {
 			"title": "Swift",
 			"require": "clike",
 			"owner": "chrischares"
+		},
+		"objc": {
+			"title": "Objective-C",
+			"require": "c",
+			"owner": "uranusjr"
 		}
 	},
 	"plugins": {
