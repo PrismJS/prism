8beaa6fb85f43ad8124ec687a891e95a75be4db7
Avi Aryan <avi.aryan123@gmail.com>
2014-07-13 13:26:00 +0530

    fixed another bug with ini highlight

--- components/prism-ini.js
+++ components/prism-ini.js
@@ -1,11 +1,11 @@
 Prism.languages.ini= {
 	'comment': /^\s*;.*$/gm,
 	'important': /\[.*?\]/gm,
-	'constant': /^\s*[^\W]+?(?=\=)/gm,
+	'constant': /^\s*[^\s\=]+?(?=[ \t]*\=)/gm,
 	'attr-value': {
 		pattern: /\=.*/gm, 
 		inside: {
-			'punctuation': /[\=]/g
+			'punctuation': /^[\=]/g
 		}
 	}
 };
