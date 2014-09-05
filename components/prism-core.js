08037e5b419f8d35167aea24d368174d1cefe98a
Andreas Rohner <andreas.rohner@gmx.net>
2014-09-05 11:01:02 +0200

    Fix for DFS bug with new pattern array feature
    This patch implements support for the new pattern array feature in the
    Prism.languages.DFS function and fixes a bug in the autolinker plugin.
    
    It adds an optional parameter to the callback of Prism.languages.DFS,
    which contains the type of the current object as oposed to the key in
    the parent. In most cases both key and type are exactly the same. Only
    if the parent is an array the key will contain the index number and the
    type will contain the attribute name of the array in the parent object.
    The key can be used to replace the object in the parent and the type can
    be used to provide the necessary context.

--- components/prism-core.js
+++ components/prism-core.js
@@ -96,12 +96,16 @@ var _ = self.Prism = {
 		},
 
 		// Traverse a language definition with Depth First Search
-		DFS: function(o, callback) {
+		DFS: function(o, callback, type) {
 			for (var i in o) {
-				callback.call(o, i, o[i]);
+				if (o.hasOwnProperty(i)) {
+					callback.call(o, i, o[i], type || i);
 
-				if (_.util.type(o) === 'Object') {
-					_.languages.DFS(o[i], callback);
+					if (_.util.type(o[i]) === 'Object') {
+						_.languages.DFS(o[i], callback);
+					} else if (_.util.type(o[i]) === 'Array') {
+						_.languages.DFS(o[i], callback, i);
+					}
 				}
 			}
 		}
