122d13e9839283509c2f8744a171c4bc7c42b9bd
CupOfTea696 <cupoftea696@gmail.com>
2014-09-22 20:42:39 +0200

    Merging JSONP definition with JSON
    JSONP alias added to JSON.

--- components/prism-json.js
+++ components/prism-json.js
@@ -2,8 +2,16 @@ Prism.languages.json = {
     'property': /"(\b|\B)[\w-]+"(?=\s*:)/ig,
     'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?)\b/g,
     'string': /"(?!:)(\\?[^'"])*?"(?!:)/g,
-    'punctuation': /[{}[\],]/g,
+    'function': {
+		pattern: /[a-z0-9_]+\(/ig,
+		inside: {
+			punctuation: /\(/
+		}
+	},
+    'punctuation': /[{}[\]);,]/g,
     'operator': /:/g,
     'boolean': /\b(true|false)\b/gi,
     'null': /\bnull\b/gi,
 };
+
+Prism.languages.jsonp;
--- components/prism-jsonp.js
+++
@@ -1,9 +0,0 @@
-Prism.languages.jsonp = Prism.languages.extend('json',{
-    'function': {
-		pattern: /[a-z0-9_]+\(/ig,
-		inside: {
-			punctuation: /\(/
-		}
-	},
-    'punctuation': /[{}[\]);,]/g,
-});
