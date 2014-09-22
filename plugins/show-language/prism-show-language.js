13c31e37c761b0e3b39d53491f225e562a3b5724
CupOfTea696 <cupoftea696@gmail.com>
2014-09-22 20:05:36 +0200

    Removed version aliases completely

--- plugins/show-language/prism-show-language.js
+++ plugins/show-language/prism-show-language.js
@@ -1,9 +1,3 @@
-Prism.languages.css3 = Prism.languages.css;
-Prism.languages.html5 = Prism.languages.html;
-Prism.languages.php4 = Prism.languages.php;
-Prism.languages.php5 = Prism.languages.php;
-Prism.languages.php6 = Prism.languages.php;
-
 (function(){
 
 if (!self.Prism) {
