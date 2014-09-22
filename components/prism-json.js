754de7818c90ee0e3895af89f53ba08ba51fd53c
CupOfTea696 <cupoftea696@gmail.com>
2014-09-22 19:35:15 +0200

    Punctuation improvements

--- components/prism-json.js
+++ components/prism-json.js
@@ -2,7 +2,7 @@ Prism.languages.json = {
     'property': /"(\b|\B)[\w-]+"(?=\s*:)/ig,
     'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?)\b/g,
     'string': /"(?!:)(\\?[^'"])*?"(?!:)/g,
-    'punctuation': /[\{\};:]/g,
+    'punctuation': /[{}[\]:,]/g,
     'boolean': /\b(true|false)\b/gi,
     'null': /\bnull\b/gi,
 };
