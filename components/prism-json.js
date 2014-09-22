ba9c2b614514e4f70269bc5fad0da97dd38acc20
CupOfTea696 <cupoftea696@gmail.com>
2014-09-22 19:44:52 +0200

    Added operator

--- components/prism-json.js
+++ components/prism-json.js
@@ -2,7 +2,8 @@ Prism.languages.json = {
     'property': /"(\b|\B)[\w-]+"(?=\s*:)/ig,
     'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?)\b/g,
     'string': /"(?!:)(\\?[^'"])*?"(?!:)/g,
-    'punctuation': /[{}[\]:,]/g,
+    'punctuation': /[{}[\],]/g,
+    'operator': /:/g,
     'boolean': /\b(true|false)\b/gi,
     'null': /\bnull\b/gi,
 };
