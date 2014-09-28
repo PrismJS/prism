bd63cab22d167bd463d1cf8c8542e86e6586d6ac
CupOfTea696 <cupoftea696@gmail.com>
2014-09-28 11:09:37 +0200

    Added string & punctuation to css.selector.inside

--- components/prism-css-extras.js
+++ components/prism-css-extras.js
@@ -4,7 +4,9 @@ Prism.languages.css.selector = {
 		'pseudo-element': /:(?:after|before|first-letter|first-line|selection)|::[-\w]+/g,
 		'pseudo-class': /:[-\w]+(?:\(.*\))?/g,
 		'class': /\.[-:\.\w]+/g,
-		'id': /#[-:\.\w]+/g
+		'id': /#[-:\.\w]+/g,
+        'punctuation': /[,\[\]=~\|\^\$\*]/g,
+		'string': /("|')(\\?.)*?\1/g
 	}
 };
 
