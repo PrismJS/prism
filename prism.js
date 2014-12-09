f22f3249341d778fc30ab24ad3ed385949c12017
Golmote <golmote@hotmail.com>
2014-12-09 20:53:41 +0100

    Keep prism.js up to date

--- prism.js
+++ prism.js
@@ -494,6 +494,17 @@ Prism.languages.css = {
 	'string': /("|')(\\?.)*?\1/g,
 	'important': /\B!important\b/gi,
 	'punctuation': /[\{\};:]/g,
+	'color': [
+		{
+			pattern: /#(?:[0-9a-f]{3}){1,2}/ig,
+			alias: 'builtin'
+		},
+		{
+			pattern: /(?:rgb|hsl)a?\(\s*\d+\s*(?:,\s*(?:\d+(?:\.\d*)?|\.\d+)%?\s*)+\)/,
+			alias: 'builtin'
+		}
+	],
+	'number': /(?:\d+(?:\.\d*)?|\.\d+)(?:%|ch|cm|deg|dpcm|dpi|dppx|ex|grad|k?Hz|in|mm|m?s|pc|pt|px|rad|r?em|turn|vh|vw|vmin|vmax)?/g,
 	'function': /[-a-z0-9]+(?=\()/ig
 };
 
