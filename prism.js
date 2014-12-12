41699c3f79f817d80517fbe935b28ac3423fc408
Nathan Friedly <nathan@nfriedly.com>
2014-12-12 14:27:55 -0500

    adding start and end line support to file highlighter, with example. Plays nice with line-numbers plugin.

--- prism.js
+++ prism.js
@@ -625,26 +625,39 @@ Array.prototype.slice.call(document.querySelectorAll('pre[data-src]')).forEach(f
 	var src = pre.getAttribute('data-src');
 	var extension = (src.match(/\.(\w+)$/) || [,''])[1];
 	var language = Extensions[extension] || extension;
-	
+
+	var start = 0, end;
+	if (pre.hasAttribute('data-start')) {
+		start = (parseInt(pre.getAttribute('data-start'), 10) - 1);
+		start = Math.max(start, 0);
+	}
+	if (pre.hasAttribute('data-end')) {
+		end = parseInt(pre.getAttribute('data-end'), 10);
+	}
+
 	var code = document.createElement('code');
 	code.className = 'language-' + language;
-	
+
 	pre.textContent = '';
-	
+
 	code.textContent = 'Loading…';
-	
+
 	pre.appendChild(code);
-	
+
 	var xhr = new XMLHttpRequest();
-	
+
 	xhr.open('GET', src, true);
 
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4) {
-			
+
 			if (xhr.status < 400 && xhr.responseText) {
-				code.textContent = xhr.responseText;
-			
+				if (start || end) {
+					code.textContent = xhr.responseText.split('\n').slice(start, end).join('\n');
+				} else {
+					code.textContent = xhr.responseText;
+				}
+
 				Prism.highlightElement(code);
 			}
 			else if (xhr.status >= 400) {
@@ -655,7 +668,7 @@ Array.prototype.slice.call(document.querySelectorAll('pre[data-src]')).forEach(f
 			}
 		}
 	};
-	
+
 	xhr.send(null);
 });
 
