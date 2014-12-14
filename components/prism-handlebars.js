4a81e9d3693d7e174075961a6e514223fb980896
Golmote <golmote@hotmail.com>
2014-12-14 23:59:57 +0100

    Rewrote Handlebars for better intergration with markup, css, js

--- components/prism-handlebars.js
+++ components/prism-handlebars.js
@@ -1,37 +1,39 @@
-Prism.languages.handlebars = {
-	'expression': {
-		pattern: /\{\{\{[\w\W]+?\}\}\}|\{\{[\w\W]+?\}\}/g,
-		inside: {
-			'comment': {
-				pattern: /(\{\{)![\w\W]*(?=\}\})/g,
-				lookbehind: true
-			},
-			'delimiter': {
-				pattern: /^\{\{\{?|\}\}\}?$/ig,
-				alias: 'punctuation'
-			},
-			'string': /(["'])(\\?.)+?\1/g,
-			'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?)\b/g,
-			'boolean': /\b(true|false)\b/g,
-			'block': {
-				pattern: /^(\s*~?\s*)[#\/]\w+/ig,
-				lookbehind: true,
-				alias: 'keyword'
-			},
-			'brackets': {
-				pattern: /\[[^\]]+\]/,
-				inside: {
-					punctuation: /\[|\]/g,
-					variable: /[\w\W]+/g
-				}
-			},
-			'punctuation': /[!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~]/g,
-			'variable': /[^!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~]+/g
-		}
-	}
-};
+(function(Prism) {
 
-if (Prism.languages.markup) {
+	Prism.languages.handlebars = Prism.languages.extend('markup', {});
+
+	var grammar = {
+		'handlebars': {
+			pattern: /\{\{\{[\w\W]+?\}\}\}|\{\{[\w\W]+?\}\}/g,
+			inside: {
+				'comment': {
+					pattern: /(\{\{)![\w\W]*(?=\}\})/g,
+					lookbehind: true
+				},
+				'delimiter': {
+					pattern: /^\{\{\{?|\}\}\}?$/ig,
+					alias: 'punctuation'
+				},
+				'string': /(["'])(\\?.)+?\1/g,
+				'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?)\b/g,
+				'boolean': /\b(true|false)\b/g,
+				'block': {
+					pattern: /^(\s*~?\s*)[#\/]\w+/ig,
+					lookbehind: true,
+					alias: 'keyword'
+				},
+				'brackets': {
+					pattern: /\[[^\]]+\]/,
+					inside: {
+						punctuation: /\[|\]/g,
+						variable: /[\w\W]+/g
+					}
+				},
+				'punctuation': /[!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~]/g,
+				'variable': /[^!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~]+/g
+			}
+		}
+	};
 
 	// Tokenize all inline Handlebars expressions that are wrapped in {{ }} or {{{ }}}
 	// This allows for easy Handlebars + markup highlighting
@@ -59,32 +61,17 @@ if (Prism.languages.markup) {
 	});
 
 	// Re-insert the tokens after highlighting
+	// and highlight them with defined grammar
 	Prism.hooks.add('after-highlight', function(env) {
 		if (env.language !== 'handlebars') {
 			return;
 		}
 
 		for (var i = 0, t; t = env.tokenStack[i]; i++) {
-			env.highlightedCode = env.highlightedCode.replace('___HANDLEBARS' + (i + 1) + '___', Prism.highlight(t, env.grammar, 'handlebars'));
+			env.highlightedCode = env.highlightedCode.replace('___HANDLEBARS' + (i + 1) + '___', Prism.highlight(t, grammar, 'handlebars'));
 		}
 
 		env.element.innerHTML = env.highlightedCode;
 	});
 
-	// Wrap tokens in classes that are missing them
-	Prism.hooks.add('wrap', function(env) {
-		if (env.language === 'handlebars' && env.type === 'markup') {
-			env.content = env.content.replace(/(___HANDLEBARS[0-9]+___)/g, "<span class=\"token handlebars\">$1</span>");
-		}
-	});
-
-	// Add the rules before all others
-	Prism.languages.insertBefore('handlebars', 'expression', {
-		'markup': {
-			pattern: /<[^?]\/?(.*?)>/g,
-			inside: Prism.languages.markup
-		},
-		'handlebars': /___HANDLEBARS[0-9]+___/g
-	});
-}
-
+}(Prism));
