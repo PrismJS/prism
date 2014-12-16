45f2b828278d90f79453e1e685b319d476f2da8e
Golmote <golmote@hotmail.com>
2014-12-16 21:28:20 +0100

    Update components

--- components.js
+++ components.js
@@ -206,6 +206,10 @@ var components = {
 		"handlebars": {
 			"title": "Handlebars",
 			"owner": "Golmote"
+		},
+		"actionscript": {
+			"title": "ActionScript",
+			"owner": "Golmote"
 		}
 	},
 	"plugins": {
