f22c391161ba3828c52b34e0122fdebd1d8e3268
Golmote <golmote@hotmail.com>
2014-12-19 09:14:54 +0100

    Add Erlang language

--- components.js
+++ components.js
@@ -206,6 +206,10 @@ var components = {
 		"handlebars": {
 			"title": "Handlebars",
 			"owner": "Golmote"
+		},
+		"erlang": {
+			"title": "Erlang",
+			"owner": "Golmote"
 		}
 	},
 	"plugins": {
