dabc5d0532fbe4f98b69df01ff388cd97fbccd70
Golmote <golmote@hotmail.com>
2014-12-20 09:33:00 +0100

    Add LOLCODE language

--- components.js
+++ components.js
@@ -206,6 +206,10 @@ var components = {
 		"handlebars": {
 			"title": "Handlebars",
 			"owner": "Golmote"
+		},
+		"lolcode": {
+			"title": "LOLCODE",
+			"owner": "Golmote"
 		}
 	},
 	"plugins": {
