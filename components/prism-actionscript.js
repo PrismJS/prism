68858669570e0f3dd0415a02b3c5e2fef751269e
Golmote <golmote@hotmail.com>
2014-12-20 13:36:18 +0100

    Merge all keywords in one regexp

--- components/prism-actionscript.js
+++ components/prism-actionscript.js
@@ -3,13 +3,7 @@
  */
 
 Prism.languages.actionscript = Prism.languages.extend('javascript',  {
-	'keyword': [
-		// Lexical keywords
-		// Intentionally ignore true and false that are already handled as booleans
-		/\b(?:as|break|case|catch|class|const|default|delete|do|else|extends|finally|for|function|if|implements|import|in|instanceof|interface|internal|is|native|new|null|package|private|protected|public|return|super|switch|this|throw|try|typeof|use|var|void|while|with)\b/,
-		// Syntactic keywords
-		/\b(?:each|dynamic|final|get|include|namespace|native|override|set|static)\b/
-	],
+	'keyword': /\b(?:as|break|case|catch|class|const|default|delete|do|else|extends|finally|for|function|if|implements|import|in|instanceof|interface|internal|is|native|new|null|package|private|protected|public|return|super|switch|this|throw|try|typeof|use|var|void|while|with|dynamic|each|final|get|include|namespace|native|override|set|static)\b/,
 	'number': /\b-?(\d*\.?\d+([Ee][+-]?\d+)?|NaN|-?Infinity)\b/g,
 	'operator': /(?:[+\-*\/%^]|&&?|\|\|?|<<?|>>?>?|[!=]=)=?|[=~?@]/,
 	'function': /(?!\d)[a-z0-9_$]+(?=\()/ig
