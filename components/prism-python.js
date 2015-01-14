344212878c0e36dba2588bdbbd23926dbad98399
Golmote <golmote@hotmail.com>
2015-01-14 21:56:42 +0100

    Fixed operators in Python

--- components/prism-python.js
+++ components/prism-python.js
@@ -7,7 +7,7 @@ Prism.languages.python= {
 	'keyword' : /\b(as|assert|break|class|continue|def|del|elif|else|except|exec|finally|for|from|global|if|import|in|is|lambda|pass|print|raise|return|try|while|with|yield)\b/g,
 	'boolean' : /\b(True|False)\b/g,
 	'number' : /\b-?(0[box])?(?:[\da-f]+\.?\d*|\.\d+)(?:e[+-]?\d+)?j?\b/gi,
-	'operator' : /[-+]{1,2}|=?<|=?>|!|={1,2}|&{1,2}|\|?\||\?|\*|\/|~|\^|%|\b(or|and|not)\b/g,
+	'operator' : /[-+]{1,2}|<=?|<=?|!|={1,2}|&{1,2}|\|?\||\?|\*|\/|~|\^|%|\b(or|and|not)\b/g,
 	'punctuation' : /[{}[\];(),.:]/g
 };
 
