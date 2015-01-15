813b301f8dc460670af4af9a2c0b76a776a78a9b
cdagnino <cdagnino@users.noreply.github.com>
2015-01-15 14:01:54 -0600

    Delete ignore and remove encoded entities in Julia

--- components/prism-julia.js
+++ components/prism-julia.js
@@ -7,7 +7,6 @@ Prism.languages.julia= {
 	'keyword' : /\b(abstract|baremodule|begin|bitstype|break|catch|ccall|const|continue|do|else|elseif|end|export|finally|for|function|global|if|immutable|import|importall|let|local|macro|module|print|println|quote|return|try|type|typealias|using|while)\b/g,
 	'boolean' : /\b(true|false)\b/g,
 	'number' : /\b-?(0[box])?(?:[\da-f]+\.?\d*|\.\d+)(?:e[+-]?\d+)?j?\b/gi,
-	'operator' : /[-+]{1,2}|=?&lt;|=?&gt;|!|={1,2}|(&){1,2}|(&amp;){1,2}|\|?\||\?|\*|\/|~|\^|%|\b(or|and|not)\b/g,
-	'ignore' : /&(lt|gt|amp);/gi,
+	'operator' : /[-+]{1,2}|=?<|=?>|!|={1,2}|&{1,2}|\|?\||\?|\*|\/|~|\^|%|\b(or|and|not)\b/g,
 	'punctuation' : /[{}[\];(),.:]/g
 };
