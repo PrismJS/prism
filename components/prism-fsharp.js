2664796c7959c13150cf4328d6a07b972b09e6e4
Simon Reynolds <simon.reynolds7@gmail.com>
2015-02-07 16:44:50 +0000

    Removed newline consumption in single line comment and split numbers regex to handle hex, binary, integer and decimal numbers separately

--- components/prism-fsharp.js
+++ components/prism-fsharp.js
@@ -5,12 +5,25 @@ Prism.languages.fsharp = Prism.languages.extend('clike', {
 			lookbehind: true
 		},
 		{
-			pattern: /(^|[^\\:])\/\/.*?(\r?\n|$)/g,
+			pattern: /(^|[^\\:])\/\/.*/g,
 			lookbehind: true
 		}
 	],
 	'keyword': /\b(abstract|and|as|assert|base|begin|class|default|delegate|do|done|downcast|downto|elif|else|end|exception|extern|false|finally|for|fun|function|global|if|in|inherit|inline|interface|internal|lazy|let|let!|match|member|module|mutable|namespace|new|not|null|of|open|or|override|private|public|rec|return|return!|select|static|struct|then|to|true|try|type|upcast|use|use!|val|void|when|while|with|yield|yield!|asr|land|lor|lsl|lsr|lxor|mod|sig|atomic|break|checked|component|const|constraint|constructor|continue|eager|event|external|fixed|functor|include|method|mixin|object|parallel|process|protected|pure|sealed|tailcall|trait|virtual|volatile)\b/g,
 	'string': /@?("""|"|')((\\|\n)?.)*?\1(B)?/g,
 	'preprocessor': /^\s*#.*/gm,
-	'number': /\b-?(0x)?(\d[a-fA-F]?)*\.?((\d\.?[a-fA-F]?)(y|uy|s|us|l|ul|un|L|UL|F|f|lf|LF|I|M|m))?\b/g
+	'number': [
+		{
+			pattern: /\b-?(0x((\d*)|([a-fA-F]*))*(un|LF)?)\b/g
+		},
+		{
+			pattern: /\b-?(0b((0|1))*(y|uy)?)\b/g
+		},
+		{
+			pattern: /\b-?(\d*\.(f|\d)?\d*(f|F|m|M|((E|e)(\+|-)?\d*))?)\b/g
+		},
+		{
+			pattern: /\b-?(\d{1,}(y|uy|s|us|l|u|ul|L|UL|I)?)\b/g
+		}
+	]
 });
