9020d78f89a0fbb92e0605d1fe165129df0e168c
Justin Beckwith <justin.beckwith@gmail.com>
2015-05-25 21:34:42 -0700

    add label keyword to docker highlighter

--- components/prism-docker.js
+++ components/prism-docker.js
@@ -1,6 +1,6 @@
 Prism.languages.docker = {
 	'keyword': {
-		pattern: /(^\s*)(?:ONBUILD|FROM|MAINTAINER|RUN|EXPOSE|ENV|ADD|COPY|VOLUME|USER|WORKDIR|CMD|ENTRYPOINT)(?=\s)/mi,
+		pattern: /(^\s*)(?:ONBUILD|FROM|MAINTAINER|RUN|EXPOSE|ENV|ADD|COPY|VOLUME|USER|WORKDIR|CMD|LABEL|ENTRYPOINT)(?=\s)/mi,
 		lookbehind: true
 	},
 	'string': /("|')(\\\n|\\?.)*?\1/,
