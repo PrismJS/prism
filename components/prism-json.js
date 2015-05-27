db367b0bb1f7741a04590eaea3b5138864f7803d
CupOfTea696 <cupoftea696@gmail.com>
2015-05-27 16:26:58 +0200

    JSON numbers in string fix

--- components/prism-json.js
+++ components/prism-json.js
@@ -1,7 +1,7 @@
 Prism.languages.json = {
     'property': /"(\b|\B)[\w-]+"(?=\s*:)/ig,
-    'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?)\b/g,
     'string': /"(?!:)(\\?[^'"])*?"(?!:)/g,
+    'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?)\b/g,
     'function': {
 		pattern: /[a-z0-9_]+\(/ig,
 		inside: {
