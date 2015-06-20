d3b4cd540d027c78e4f69e88ab887dcdabe75eb5
Golmote <golmote@hotmail.com>
2015-06-20 20:06:39 +0200

    Improved regex performance of C-like strings and JS regexps

--- components/prism-javascript.js
+++ components/prism-javascript.js
@@ -6,7 +6,7 @@ Prism.languages.javascript = Prism.languages.extend('clike', {
 
 Prism.languages.insertBefore('javascript', 'keyword', {
 	'regex': {
-		pattern: /(^|[^/])\/(?!\/)(\[.+?]|\\.|[^/\r\n])+\/[gimyu]{0,5}(?=\s*($|[\r\n,.;})]))/,
+		pattern: /(^|[^/])\/(?!\/)(\[.+?]|\\.|[^/\\\r\n])+\/[gimyu]{0,5}(?=\s*($|[\r\n,.;})]))/,
 		lookbehind: true
 	}
 });
