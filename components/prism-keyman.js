1c8a841957164e91217c437aebbf3af954645d9b
Marc Durdin <marc@keyman.com>
2015-06-27 22:46:21 +1000

    Add match and nomatch structural keywords

--- components/prism-keyman.js
+++ components/prism-keyman.js
@@ -3,7 +3,7 @@ Prism.languages.keyman = {
 	'function': /\[\s*((CTRL|SHIFT|ALT|LCTRL|RCTRL|LALT|RALT)\s+)*(([T|K|U]_[a-z0-9_]+)|(".+")|('.+'))\s*\]/i,  // virtual key
 	'string': /("|')((?!\1).)*\1/,
 	'keyword': /\b(any|beep|call|context|deadkey|dk|if|index|notany|nul|outs|return|reset|save|set|store|use)\b/i,  // rule keywords
-	'atrule': /\b(ansi|begin|unicode|group|(using keys))\b/i,   // structural keywords
+	'atrule': /\b(ansi|begin|unicode|group|(using keys)|match|nomatch)\b/i,   // structural keywords
 	'bold': [   // header statements
 		/\&(baselayout|bitmap|capsononly|capsalwaysoff|shiftfreescaps|copyright|ethnologuecode|hotkey|includecodes|keyboardversion|kmw_embedcss|kmw_embdjs|kmw_helpfile|kmw_helptext|kmw_rtl|language|layer|layoutfile|message|mnemoniclayout|name|oldcharposmatching|platform|targets|version|visualkeyboard|windowslanguages)\b/i,
 		/\b(bitmap|bitmaps|(caps on only)|(caps always off)|(shift frees caps)|copyright|hotkey|language|layout|message|name|version)\b/i
