fd88018b9ed706c9415d40c8d0c4b6b56cdbeed4
RUSLAN LÓPEZ CARRO <scherzo_16@hotmail.com>
2015-07-09 04:58:45 -0500

    Adding some documentation

--- components/prism-java.js
+++ components/prism-java.js
@@ -1,4 +1,4 @@
-Prism.languages.java = Prism.languages.extend('clike', {
+Prism.languages.java = Prism.languages.extend('clike', { //this language definition reuses all definitions from clike syntax file
 	'keyword': /\b(abstract|continue|for|new|switch|assert|default|goto|package|synchronized|boolean|do|if|private|this|break|double|implements|protected|throw|byte|else|import|public|throws|case|enum|instanceof|return|transient|catch|extends|int|short|try|char|final|interface|static|void|class|finally|long|strictfp|volatile|const|float|native|super|while)\b/,
 	'number': /\b0b[01]+\b|\b0x[\da-f]*\.?[\da-fp\-]+\b|\b\d*\.?\d+[e]?[\d]*[df]\b|\b\d*\.?\d+\b/i,
 	'operator': {
