adcbc4fedefcae794878308c03270b36941f6598
RUSLAN LÓPEZ CARRO <scherzo_16@hotmail.com>
2015-07-11 04:36:58 -0500

    Update prism-java.js

--- components/prism-java.js
+++ components/prism-java.js
@@ -5,7 +5,7 @@ Prism.languages.java = Prism.languages.extend('clike', {
 	// a word consisting of 0x an optional letter from a to f or digit perhaps one dot at least one letter from a to f or digit or
 	// a word consisting of an optional digit perhaps one dot at least one digit perhaps the letter e a + or - symbol, optional digits and perhaps one of the letters d f l or
 	// a word consisting of an optional digit perhaps one dot at least one digit
-	'number': /\b0b[01]+\b|\b0x[\da-f]*\.?[\da-fp\-]+\b|\b\d+\.?\d*(e|E)?([\+]|[\-])?[\d]*[dfl]*\b|\b\d*\.?\d+\b/i,
+	'number': /\b0b[01]+\b|\b0x[\da-f]*\.?[\da-fp\-]+\b|\b\d+\.?\d*e?[+-]?[\d]*[dfl]*\b|\b\d*\.?\d+\b/i,
 	'operator': {
 		pattern: /(^|[^\.])(?:\+=|\+\+?|-=|--?|!=?|<{1,2}=?|>{1,3}=?|==?|&=|&&?|\|=|\|\|?|\?|\*=?|\/=?|%=?|\^=?|:|~)/m,
 		lookbehind: true
