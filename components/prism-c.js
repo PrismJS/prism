dcce1a788b2ed1671e0b77ec859a0d29cecc0f40
Golmote <golmote@hotmail.com>
2015-07-12 08:21:31 +0200

    C: Removed string pattern (now that C-like supports escaped new lines) + simplified operators regexp

--- components/prism-c.js
+++ components/prism-c.js
@@ -1,8 +1,6 @@
 Prism.languages.c = Prism.languages.extend('clike', {
-	// allow for c multiline strings
-	'string': /("|')([^\n\\\1]|\\.|\\\r*\n)*?\1/,
 	'keyword': /\b(asm|typeof|inline|auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)\b/,
-	'operator': /[-+]{1,2}|!=?|<{1,2}=?|>{1,2}=?|\->|={1,2}|\^|~|%|&{1,2}|\|?\||\?|\*|\//
+	'operator': /\-[>-]?|\+\+?|!=?|<<?=?|>>?=?|==?|&&?|\|?\||[~^%?*\/]/
 });
 
 Prism.languages.insertBefore('c', 'string', {
