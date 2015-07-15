f05e0797648e816d38c86edaead9095c71cfdbf7
Jannik Zschiesche <jz@becklyn.com>
2015-07-15 20:01:39 +0200

    Updated simplification of token stream
    It now strips empty values

--- tests/helper/token-stream-transformer.js
+++ tests/helper/token-stream-transformer.js
@@ -3,43 +3,34 @@
 
 module.exports = {
 	/**
-	 * Simplifies the token stream to ease the matching with the expected token stream
+	 * Simplifies the token stream to ease the matching with the expected token stream.
 	 *
-	 * @param {string} tokenStream
+	 * * Strings are kept as-is
+	 * * In arrays each value is transformed individually
+	 * * Values that are empty (empty arrays or strings only containing whitespace)
+	 *
+	 *
+	 * @param {Array} tokenStream
 	 * @returns {Array.<string[]|Array>}
 	 */
 	simplify: function (tokenStream) {
-		// First filter all top-level non-objects as non-objects are not-identified tokens
-		//
-		// we don't want to filter them in the lower levels as we want to support nested content-structures
-		return tokenStream.filter(
-			function (token) {
-				return (typeof token === "object");
-			}
-		).map(this.simplifyRecursively.bind(this));
-	},
-
-
-	/**
-	 * Walks the token stream and recursively simplifies it
-	 *
-	 * @private
-	 * @param {Array|{type: string, content: *}|string} token
-	 * @returns {Array|string}
-	 */
-	simplifyRecursively: function (token)
-	{
-		if (Array.isArray(token))
+		if (Array.isArray(tokenStream))
 		{
-			return token.map(this.simplifyRecursively.bind(this));
+			return tokenStream
+				.map(this.simplify.bind(this))
+				.filter(function(value)
+				{
+					return !(Array.isArray(value) && !value.length) && !(typeof value === "string" && !value.trim().length);
+				}
+			);
 		}
-		else if (typeof token === "object")
+		else if (typeof tokenStream === "object")
 		{
-			return [token.type, this.simplifyRecursively(token.content)];
+			return [tokenStream.type, this.simplify(tokenStream.content)];
 		}
 		else
 		{
-			return token;
+			return tokenStream;
 		}
 	}
 };
