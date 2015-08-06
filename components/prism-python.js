3ded4f2c5356f6505e1ca0956b2954e465eb1e2c
Yury Selivanov <yselivanov@sprymix.com>
2015-08-06 18:37:13 -0400

    python: Add async/await and @ operator (introduced in 3.5)

--- components/prism-python.js
+++ components/prism-python.js
@@ -1,4 +1,4 @@
-Prism.languages.python= { 
+Prism.languages.python= {
 	'comment': {
 		pattern: /(^|[^\\])#.*?(\r?\n|$)/,
 		lookbehind: true
@@ -8,9 +8,9 @@ Prism.languages.python= {
 		pattern: /((^|\s)def[ \t]+)([a-zA-Z_][a-zA-Z0-9_]*(?=\())/g,
 		lookbehind: true
 	},
-	'keyword' : /\b(as|assert|break|class|continue|def|del|elif|else|except|exec|finally|for|from|global|if|import|in|is|lambda|pass|print|raise|return|try|while|with|yield)\b/,
+	'keyword' : /\b(as|assert|async|await|break|class|continue|def|del|elif|else|except|exec|finally|for|from|global|if|import|in|is|lambda|pass|print|raise|return|try|while|with|yield)\b/,
 	'boolean' : /\b(True|False)\b/,
 	'number' : /\b-?(0[bo])?(?:(\d|0x[a-f])[\da-f]*\.?\d*|\.\d+)(?:e[+-]?\d+)?j?\b/i,
-	'operator' : /[-+]|<=?|>=?|!|={1,2}|&{1,2}|\|?\||\?|\*|\/|~|\^|%|\b(or|and|not)\b/,
+	'operator' : /[-+]|<=?|>=?|!|={1,2}|&{1,2}|\|?\||\?|\*|\/|@|~|\^|%|\b(or|and|not)\b/,
 	'punctuation' : /[{}[\];(),.:]/
 };
