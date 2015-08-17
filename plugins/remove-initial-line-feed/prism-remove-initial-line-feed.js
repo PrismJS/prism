b8d098e1d420715366302274c511ab4aeaeeb2fe
Golmote <golmote@hotmail.com>
2015-08-17 09:03:06 +0200

    Remove the behaviour of Remove initial line feed plugin (remove by default, allow to keep with a CSS class)

--- plugins/remove-initial-line-feed/prism-remove-initial-line-feed.js
+++ plugins/remove-initial-line-feed/prism-remove-initial-line-feed.js
@@ -1,11 +1,11 @@
 Prism.hooks.add('before-highlight', function(env) {
 	if (env.code) {
 		var pre = env.element.parentNode;
-		var clsReg = /\s*\bremove-initial-line-feed\b\s*/;
+		var clsReg = /\s*\bkeep-initial-line-feed\b\s*/;
 		if (
 			pre && pre.nodeName.toLowerCase() === 'pre' &&
-			// Apply only if the <pre> or the <code> have the class
-			(clsReg.test(pre.className) || clsReg.test(env.element.className))
+			// Apply only if nor the <pre> or the <code> have the class
+			(!clsReg.test(pre.className) && !clsReg.test(env.element.className))
 		) {
 			env.code = env.code.replace(/^(?:\r?\n|\r)/, '');
 		}
