aa757f6c5f4d1a9a170928ece8f1c162c9a289e8
Golmote <golmote@hotmail.com>
2015-08-21 19:05:38 +0200

    C-like: add word boundary before class-name prefixes

--- prism.js
+++ prism.js
@@ -551,7 +551,7 @@ Prism.languages.clike = {
 	],
 	'string': /("|')(\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
 	'class-name': {
-		pattern: /((?:(?:class|interface|extends|implements|trait|instanceof|new)\s+)|(?:catch\s+\())[a-z0-9_\.\\]+/i,
+		pattern: /((?:\b(?:class|interface|extends|implements|trait|instanceof|new)\s+)|(?:catch\s+\())[a-z0-9_\.\\]+/i,
 		lookbehind: true,
 		inside: {
 			punctuation: /(\.|\\)/
