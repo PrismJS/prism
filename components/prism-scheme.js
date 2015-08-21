3120ff73ab57b356785e6e9b67ade55039e926f5
Golmote <golmote@hotmail.com>
2015-08-21 07:47:59 +0200

    Scheme: Add missing lookbehind on number pattern. Fix #702

--- components/prism-scheme.js
+++ components/prism-scheme.js
@@ -1,24 +1,23 @@
 Prism.languages.scheme = {
-    'boolean' : /#(t|f){1}/,
-    'comment' : /;.*/,
-    'keyword' : {
+	'boolean' : /#(t|f){1}/,
+	'comment' : /;.*/,
+	'keyword' : {
 		pattern : /([(])(define(-syntax|-library|-values)?|(case-)?lambda|let(-values|(rec)?(\*)?)?|else|if|cond|begin|delay|delay-force|parameterize|guard|set!|(quasi-)?quote|syntax-rules)/,
 		lookbehind : true
-    },
-    'builtin' : {
+	},
+	'builtin' : {
 		pattern :  /([(])(cons|car|cdr|null\?|pair\?|boolean\?|eof-object\?|char\?|procedure\?|number\?|port\?|string\?|vector\?|symbol\?|bytevector\?|list|call-with-current-continuation|call\/cc|append|abs|apply|eval)\b/,
 		lookbehind : true
-    },
-    'string' :  /(["])(?:(?=(\\?))\2.)*?\1|'[^('|\s)]+/, //thanks http://stackoverflow.com/questions/171480/regex-grabbing-values-between-quotation-marks
-    'number' : /(\s|\))[-+]?[0-9]*\.?[0-9]+((\s*)[-+]{1}(\s*)[0-9]*\.?[0-9]+i)?/,
-    'operator': /(\*|\+|\-|%|\/|<=|=>|>=|<|=|>)/,
-    'function' : {
+	},
+	'string' :  /(["])(?:(?=(\\?))\2.)*?\1|'[^('|\s)]+/, //thanks http://stackoverflow.com/questions/171480/regex-grabbing-values-between-quotation-marks
+	'number' : {
+		pattern: /(\s|\))[-+]?[0-9]*\.?[0-9]+((\s*)[-+]{1}(\s*)[0-9]*\.?[0-9]+i)?/,
+		lookbehind: true
+	},
+	'operator': /(\*|\+|\-|%|\/|<=|=>|>=|<|=|>)/,
+	'function' : {
 		pattern : /([(])[^(\s|\))]*\s/,
 		lookbehind : true
-    },
-    'punctuation' : /[()]/
+	},
+	'punctuation' : /[()]/
 };
-
-    
-
-    
