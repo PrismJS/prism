068704a2f69d409bf8f26b62988998a60080f3ad
Golmote <golmote@hotmail.com>
2015-08-31 09:06:27 +0200

    Scheme: Fixes and simplifications

--- components/prism-scheme.js
+++ components/prism-scheme.js
@@ -1,22 +1,25 @@
 Prism.languages.scheme = {
-	'boolean' : /#(t|f){1}/,
 	'comment' : /;.*/,
+	'string' :  /"(?:[^"\\\r\n]|\\.)*?"|'[^'\s]+/, //thanks http://stackoverflow.com/questions/171480/regex-grabbing-values-between-quotation-marks
 	'keyword' : {
-		pattern : /([(])(define(-syntax|-library|-values)?|(case-)?lambda|let(-values|(rec)?(\*)?)?|else|if|cond|begin|delay|delay-force|parameterize|guard|set!|(quasi-)?quote|syntax-rules)/,
+		pattern : /(\()(?:define(?:-syntax|-library|-values)?|(?:case-)?lambda|let(?:\*|rec)?(?:-values)?|else|if|cond|begin|delay(?:-force)?|parameterize|guard|set!|(?:quasi-)?quote|syntax-rules)/,
 		lookbehind : true
 	},
 	'builtin' : {
-		pattern :  /([(])(cons|car|cdr|null\?|pair\?|boolean\?|eof-object\?|char\?|procedure\?|number\?|port\?|string\?|vector\?|symbol\?|bytevector\?|list|call-with-current-continuation|call\/cc|append|abs|apply|eval)\b/,
+		pattern :  /(\()(?:(?:cons|car|cdr|list|call-with-current-continuation|call\/cc|append|abs|apply|eval)\b|null\?|pair\?|boolean\?|eof-object\?|char\?|procedure\?|number\?|port\?|string\?|vector\?|symbol\?|bytevector\?)/,
 		lookbehind : true
 	},
-	'string' :  /(["])(?:(?=(\\?))\2.)*?\1|'[^('|\s)]+/, //thanks http://stackoverflow.com/questions/171480/regex-grabbing-values-between-quotation-marks
 	'number' : {
-		pattern: /(\s|\))[-+]?[0-9]*\.?[0-9]+((\s*)[-+]{1}(\s*)[0-9]*\.?[0-9]+i)?/,
+		pattern: /(\s|\))[-+]?[0-9]*\.?[0-9]+(?:\s*[-+]\s*[0-9]*\.?[0-9]+i)?\b/,
 		lookbehind: true
 	},
-	'operator': /(\*|\+|\-|%|\/|<=|=>|>=|<|=|>)/,
+	'boolean' : /#[tf]/,
+	'operator': {
+		pattern: /(\()(?:[-+*%\/]|[<>]=?|=>?)/,
+		lookbehind: true
+	},
 	'function' : {
-		pattern : /([(])[^(\s|\))]*\s/,
+		pattern : /(\()[^\s()]*(?=\s)/,
 		lookbehind : true
 	},
 	'punctuation' : /[()]/
