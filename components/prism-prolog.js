48da80ab2da776d824beb33239b4d28a48e089bf
Golmote <golmote@hotmail.com>
2015-09-09 08:42:27 +0200

    Prolog: Handle \r\n + add comment to explain operators

--- components/prism-prolog.js
+++ components/prism-prolog.js
@@ -5,12 +5,13 @@ Prism.languages.prolog = {
 		/\/\*[\s\S]*?\*\//
 	],
 	// Depending on the implementation, strings may allow escaped newlines and quote-escape
-	'string': /(["'])(?:\1\1|\\[\s\S]|(?!\1).)*\1/,
+	'string': /(["'])(?:\1\1|\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
 	'builtin': /\b(?:fx|fy|xf[xy]?|yfx?)\b/,
 	'variable': /\b[A-Z_]\w*/,
 	// FIXME: Should we list all null-ary predicates (not followed by a parenthesis) like halt, trace, etc.?
 	'function': /\b[a-z]\w*(?:(?=\()|\/\d+)/,
 	'number': /\b\d+\.?\d*/,
+	// Custom operators are allowed
 	'operator': /[:\\=><\-?*@\/;+^|!$.]+|\b(?:is|mod|not|xor)\b/,
 	'punctuation': /[(){}\[\],]/
 };
