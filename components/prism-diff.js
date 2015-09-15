3669034d75d5c90740e149c6915c511bf949ff75
Golmote <golmote@hotmail.com>
2015-09-15 08:25:00 +0200

    Add tests for diff

--- components/prism-diff.js
+++ components/prism-diff.js
@@ -1,11 +1,11 @@
 Prism.languages.diff = {
 	'coord': [
 		// Match all kinds of coord lines (prefixed by "+++", "---" or "***").
-		/^(\*{3}|-{3}|\+{3}).*$/m,
+		/^(?:\*{3}|-{3}|\+{3}).*$/m,
 		// Match "@@ ... @@" coord lines in unified diff.
 		/^@@.*@@$/m,
 		// Match coord lines in normal diff (starts with a number).
-		/^\d+.*$/m,
+		/^\d+.*$/m
 	],
 
 	// Match inserted and deleted lines. Support both +/- and >/< styles.
@@ -14,7 +14,7 @@ Prism.languages.diff = {
 
 	// Match "different" lines (prefixed with "!") in context diff.
 	'diff': {
-		'pattern': /^\!(?!\!).+$/m,
+		'pattern': /^!(?!!).+$/m,
 		'alias': 'important'
 	}
-}
+};
