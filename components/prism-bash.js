89b1f857d83dc8fb3f754222af1b9ef98a3c37a9
Andreas Rohner <andreas.rohner@gmx.net>
2015-10-01 12:21:46 +0200

    Use a proper alias for the Bash-Shebang
    This patch introduces a proper alias for the Bash-Shebang.
    Additionally the variables are changed from the class "property"
    to "variable". The tests also need to be updated.

--- components/prism-bash.js
+++ components/prism-bash.js
@@ -1,4 +1,8 @@
 (function(Prism) {
+	var bashVars = {
+		variable: /\$(?:[a-zA-Z0-9_#\?\-\*!@]+|\{[^}]+\})/
+	};
+
 	Prism.languages.bash = Prism.languages.extend('clike', {
 		'comment': {
 			pattern: /(^|[^"{\\])#.*/,
@@ -7,10 +11,7 @@
 		'string': {
 			//allow multiline string
 			pattern: /("|')(\\?[\s\S])*?\1/,
-			inside: {
-				//'property' class reused for bash variables
-				'property': /\$([a-zA-Z0-9_#\?\-\*!@]+|\{[^\}]+\})/
-			}
+			inside: bashVars
 		},
 		// Redefined to prevent highlighting of numbers in filenames
 		'number': {
@@ -22,12 +23,12 @@
 		'keyword': /\b(if|then|else|elif|fi|for|break|continue|while|in|case|function|select|do|done|until|echo|exit|return|set|declare)\b/
 	});
 
-	Prism.languages.insertBefore('bash', 'keyword', {
-		//'property' class reused for bash variables
-		'property': /\$([a-zA-Z0-9_#\?\-\*!@]+|\{[^}]+\})/
-	});
+	Prism.languages.insertBefore('bash', 'keyword', bashVars);
 	Prism.languages.insertBefore('bash', 'comment', {
-		//shebang must be before comment, 'important' class from css reused
-		'important': /^#!\s*\/bin\/bash|^#!\s*\/bin\/sh/
+		// Shebang must be before comment
+		shebang: {
+			pattern: /^#!\s*\/bin\/bash|^#!\s*\/bin\/sh/,
+			alias: 'important'
+		}
 	});
 })(Prism);
