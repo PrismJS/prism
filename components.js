e25da0da6755fbe09cbe07b81113ed65d6271e88
Golmote <golmote@hotmail.com>
2015-10-15 08:55:07 +0200

    Add support for Haxe

--- components.js
+++ components.js
@@ -219,6 +219,11 @@ var components = {
 			"title": "Haskell",
 			"owner": "bholst"
 		},
+		"haxe": {
+			"title": "Haxe",
+			"require": "clike",
+			"owner": "Golmote"
+		},
 		"http": {
 			"title": "HTTP",
 			"owner": "danielgtaylor"
