13cc02c00c999e055b4f6dbcbc42c11561862a23
mAAdhaTTah <jamesorodig@gmail.com>
2016-03-20 10:19:29 -0400

    Load Clipboard.js from CDN if not present

--- plugins/copy-to-clipboard/prism-copy-to-clipboard.js
+++ plugins/copy-to-clipboard/prism-copy-to-clipboard.js
@@ -15,34 +15,56 @@
 		Clipboard = require('clipboard');
 	}
 
-	if (!Clipboard) {
-		console.warn('Clipboard.js not loaded.');
+	var callbacks = [];
 
-		return;
+	if (!Clipboard) {
+		var script = document.createElement('script');
+		var head = document.querySelector('head');
+
+		script.onload = function() {
+			Clipboard = window.Clipboard;
+
+			if (Clipboard) {
+				while (callbacks.length) {
+					callbacks.pop()();
+				}
+			}
+		};
+
+		script.src = 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.8/clipboard.min.js';
+		head.appendChild(script);
 	}
 
 	Prism.plugins.toolbar.registerButton(function (env) {
 		var linkCopy = document.createElement('a');
 		linkCopy.textContent = 'Copy';
 
-		var clip = new Clipboard(linkCopy, {
-			'text': function () {
-				return env.code;
-			}
-		});
-
-		clip.on('success', function() {
-			linkCopy.textContent = 'Copied!';
-
-			resetText();
-		});
-		clip.on('error', function () {
-			linkCopy.textContent = 'Press Ctrl+C to copy';
-
-			resetText();
-		});
+		if (!Clipboard) {
+			callbacks.push(registerClipboard);
+		} else {
+			registerClipboard();
+		}
 
 		return linkCopy;
+
+		function registerClipboard() {
+			var clip = new Clipboard(linkCopy, {
+				'text': function () {
+					return env.code;
+				}
+			});
+
+			clip.on('success', function() {
+				linkCopy.textContent = 'Copied!';
+
+				resetText();
+			});
+			clip.on('error', function () {
+				linkCopy.textContent = 'Press Ctrl+C to copy';
+
+				resetText();
+			});
+		}
 
 		function resetText() {
 			setTimeout(function () {
