4b1698bb200272dc87f5cd9135a37e14d96b0293
wayward710 <mrscake@gmail.com>
2016-04-01 02:08:35 -0500

    More improvements to Bro

--- components/prism-bro.js
+++ components/prism-bro.js
@@ -13,55 +13,69 @@ Prism.languages.bro = {
     'keyword': [
         /\b(break|next|continue)\b/,
         /\b(alarm|using|of|add|delete)\b/,
-		/\b(default|export|event)\b/,
-        /\b(print|redef|return|schedule)\b/,
+		/\b(export)\b/,
+        /\b(print|return|schedule)\b/,
 		/\b(when|timeout)\b/,
 		/\b(addr|any|bool|count)\b/,
 		/\b(double|enum)\b/,
 		/\b(file|int|interval)\b/,
-		/\b(pattern)\b/,
+		/\b(pattern|opaque)\b/,
 		/\b(port|record|set)\b/,
 		/\b(string|subnet|table)\b/,
 		/\b(time|vector)\b/,
-		/\b(for|if|else)\b/,
-		/\b(day|hr|min|sec|usec)s\b/
-		    
-        
+		/\b(for|if|else)\b/,      
 	],
-	'operator': /--?|\+\+?|!=?=?|<=?|>=?|==?=?|&&?|\|\|?|\?|\*|\/|~|\^|%/,
 	
-	'function': 
+	'operator': /--?|\+\+?|!=?=?|<=?|>=?|==?=?|&&|\|\|?|\?|\*|\/|~|\^|%/,
+	
+	'function': [
 	{
-		pattern: /function [a-z0-9_]+/i,
+		pattern: /function [a-zA-Z0-9_]+(::[a-zA-Z0-9_]+)?/i,
 		inside: {
 			keyword: /function/
 		}
 	},
 	
+	{
+		pattern: /hook [a-zA-Z0-9_]+(::[a-zA-Z0-9_]+)?/i,
+		inside: {
+			keyword: /hook/
+		}
+	},
+	
+	{
+		pattern: /event [a-zA-Z0-9_]+(::[a-zA-Z0-9_]+)?/i,
+		inside: {
+			keyword: /event/
+		}
+	}
+	],
+	
 	'variable':	
 	[{
-		pattern: /global [a-z0-9_]+/i,
+		pattern: /global [a-zA-Z0-9_]+/i,
 		inside: {
 			keyword: /global/
 		}
 	},
 	{
-		pattern: /local [a-z0-9_]+/i,
+		pattern: /local [a-zA-Z0-9_]+/i,
 		inside: {
 			keyword: /local/
 		}
 	}],
 	
 	
-	'number': [     
+	'number': [       
 	     /\b-?(?:0x[\da-f]+|\d*\.?\d+(?:e[+-]?\d+)?)\b/i,
 	     
-     ],
+    ],
     
     'italic':  /\b(TODO|FIXME|XXX)\b/,
 	'punctuation': /[{}[\];(),.:]/,
 	
-	'builtin': [
+	'builtin': 
+	[
 	     /@load\s+/,
 	     /@load-sigs\s+/,
 	     /@load-plugin\s+/,
@@ -73,12 +87,14 @@ Prism.languages.bro = {
 	     /@else\s+/,
 	     /@endif\s+/,
 	     /@DIR\s+/,
-	     /@FILENAME\s+/
+	     /@FILENAME\s+/,
+	     {pattern: /&(redef|priority|log|optional|default|add_func|delete_func|expire_func|read_expire|write_expire|create_expire|synchronized|persistent|rotate_interval|rotate_size|encrypt|raw_output|mergeable|group|error_handler|type_column)/,
+	     }
 	],
-	
+		     
 	'constant': 
 	{
-		pattern: /const [a-z0-9_]+/i,
+		pattern: /const [a-zA-Z0-9_]+/i,
 		inside: {
 			keyword: /const/
 		}
