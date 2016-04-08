b8275198db0cb655ae476b0341e6a22a91008993
wayward710 <mrscake@gmail.com>
2016-04-07 23:50:03 -0500

    Fixed whitespaces, reordered tokens, used Gulp to min JS

--- components/prism-bro.js
+++ components/prism-bro.js
@@ -2,23 +2,21 @@ Prism.languages.bro = {
 
 	'comment': {
 		pattern: /(^|[^\\$])#.*/,
-		lookbehind: true
+		lookbehind: true,
+			inside: {
+				'italic':  /\b(TODO|FIXME|XXX)\b/
+		}
 	},
 
 	'string': /(["'])(\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
 
 	'boolean': /\b(T|F)\b/,
 
-	'keyword': 
-		/\b(break|next|continue|alarm|using|of|add|delete|export|print|return|schedule|when|timeout|addr|any|bool|count|double|enum|file|int|interval|pattern|opaque|port|record|set|string|subnet|table|time|vector|for|if|else|in|module)\b/,                     
-	            
-	'operator': /--?|\+\+?|!=?=?|<=?|>=?|==?=?|&&|\|\|?|\?|\*|\/|~|\^|%/,
-
 	'function': {
-	    pattern: /(?:function|hook|event) [a-zA-Z0-9_]+(::[a-zA-Z0-9_]+)?/,
-	    inside: {
-	        keyword: /^(?:function|hook|event)/
-	    }
+		pattern: /(?:function|hook|event) [a-zA-Z0-9_]+(::[a-zA-Z0-9_]+)?/,
+		inside: {
+			keyword: /^(?:function|hook|event)/
+		}
 	},
 
 	'variable':	{
@@ -28,13 +26,8 @@ Prism.languages.bro = {
 		}
 	},
 
-	'number': /\b-?(?:0x[\da-f]+|\d*\.?\d+(?:e[+-]?\d+)?)\b/i,
-
-	'italic':  /\b(TODO|FIXME|XXX)\b/,
-	'punctuation': /[{}[\];(),.:]/,
-
-	'builtin': 
-	    /(@(load(-(sigs|plugin))?|unload|prefixes|ifn?def|else|(end)?if|DIR|FILENAME))|(&?(redef|priority|log|optional|default|add_func|delete_func|expire_func|read_expire|write_expire|create_expire|synchronized|persistent|rotate_interval|rotate_size|encrypt|raw_output|mergeable|group|error_handler|type_column))/,           
+	'builtin':
+		/(@(load(-(sigs|plugin))?|unload|prefixes|ifn?def|else|(end)?if|DIR|FILENAME))|(&?(redef|priority|log|optional|default|add_func|delete_func|expire_func|read_expire|write_expire|create_expire|synchronized|persistent|rotate_interval|rotate_size|encrypt|raw_output|mergeable|group|error_handler|type_column))/,
 
 	'constant': {
 		pattern: /const [a-zA-Z0-9_]+/i,
@@ -42,4 +35,13 @@ Prism.languages.bro = {
 			keyword: /const/
 		}
 	},
+
+	'keyword': 
+		/\b(break|next|continue|alarm|using|of|add|delete|export|print|return|schedule|when|timeout|addr|any|bool|count|double|enum|file|int|interval|pattern|opaque|port|record|set|string|subnet|table|time|vector|for|if|else|in|module|function)\b/,
+
+	'operator': /--?|\+\+?|!=?=?|<=?|>=?|==?=?|&&|\|\|?|\?|\*|\/|~|\^|%/,
+
+	'number': /\b-?(?:0x[\da-f]+|\d*\.?\d+(?:e[+-]?\d+)?)\b/i,
+
+	'punctuation': /[{}[\];(),.:]/
 };
