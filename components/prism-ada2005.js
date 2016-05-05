84a4a437bc01cf75f64513ba8ef769424d091067
Luke A. Guest <laguest@archeia.com>
2016-05-05 13:07:46 +0100

    Fixed plugin name.

--- components/prism-ada2005.js
+++ components/prism-ada2005.js
@@ -3,6 +3,6 @@
  * Written Luke A. Guest
     'string': /(\'|\")(\\?.)*?\1/,
  */
-Prism.languages.ada_2005 = Prism.languages.extend('ada95', {
+Prism.languages.ada2005 = Prism.languages.extend('ada95', {
     'keyword': /\b(abort|abs|abstract|accept|access|aliased|all|and|array|at|begin|body|case|constant|declare|delay|delta|digits|do|else|new|return|elsif|end|entry|exception|exit|for|function|generic|goto|if|in|interface|is|limited|loop|mod|not|null|of|others|out|overriding|package|pragma|private|procedure|protected|raise|range|record|rem|renames|requeue|reverse|select|separate|subtype|synchronized|tagged|task|terminate|then|type|until|use|when|while|with|xor)\b/i,
 });
