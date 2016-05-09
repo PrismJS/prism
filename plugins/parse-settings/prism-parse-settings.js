21ef141ee0a7b98aad0eb92a3553b34b6a1c5c4a
Andreas Rohner <andreas.rohner@gmx.net>
2016-05-09 07:58:28 +0200

    Implement plugin settings using specifications with type and default values

--- plugins/parse-settings/prism-parse-settings.js
+++ plugins/parse-settings/prism-parse-settings.js
@@ -4,58 +4,94 @@ if (typeof self === 'undefined' || !self.Prism || !self.document) {
 	return;
 }
 
-var lang = /\blang(?:uage)?-(\w+)\b/i;
-
-function hasData(node, i) {
-	return i === 0 || (i == 1 && node.nodeName.toLowerCase() === 'pre') ||
-			node.hasAttribute('data-prism') || lang.test(node.className);
-}
-
-function extractAttrs(prev, node) {
-	return prev.concat(Array.prototype.slice.call(node.attributes));
-}
-
-function addClasses(settings, name) {
-	if (name.indexOf('no-') === 0) {
-		settings[name.substring(3)] = false;
-	} else {
-		settings[name] = true;
+var assign = Object.assign || function (obj1, obj2) {
+	for (var name in obj2) {
+		if (obj2.hasOwnProperty(name))
+			obj1[name] = obj2[name];
 	}
+	return obj1;
+}
+
+var specs = {},
+    reloadSettings = false;
+
+Prism.plugins.ParseSettings = {
+	declare: function(pluginSpecs) {
+		assign(specs, pluginSpecs);
+		reloadSettings = true;
+	}
+};
+
+function loadSettings(parent) {
+	var settings = {};
+
+	do {
+		for (var i = 0, attrs = parent.attributes; i < attrs.length; ++i) {
+			var name = attrs[i].nodeName,
+			    val = attrs[i].nodeValue;
+
+			if (name === 'class') {
+				for (var j = 0, classes = val.split(/\s+/); j < classes.length; ++j) {
+					name = classes[j];
+					if (name.indexOf('no-') === 0) {
+						name = name.substring(3);
+						if (specs[name])
+							settings[name] = false;
+					} else {
+						if (specs[name])
+							settings[name] = true;
+					}
+				}
+			} else if (name.indexOf('data-') === 0) {
+				name = name.substring(5);
+				if (specs[name])
+					settings[name] = val;
+			}
+		}
+	} while ((parent = parent.parentNode) && parent.hasAttribute);
 
 	return settings;
 }
 
-function addAttrs(settings, attr) {
-	var name = attr.nodeName.toLowerCase(),
-	    value = attr.nodeValue;
+function applySpecs(settings, specs) {
+	var out = {};
 
-	if (name === "class") {
-		value.toLowerCase().split(/\s+/).reduce(addClasses, settings)
-	} else if (name.indexOf('data-') === 0) {
-		if (!value || value.toLowerCase() === 'true') {
-			value = true;
-		} else if (value.toLowerCase() === 'false') {
-			value = false;
+	for (var name in specs) {
+		var spec = specs[name] || {},
+		    value = settings[name] || spec['default'];
+
+		switch(spec.type) {
+		case 'int':
+			value = parseInt(value);
+			break;
+		case 'boolean':
+		case 'bool':
+			value = (value === 'false' || value === 'no') ? false : (value === '') ? true : !!value;
+			break;
+		case 'number':
+		case 'float':
+			value = parseFloat(value);
+			break;
 		}
 
-		settings[name.substring(5)] = value;
+		if (value || value === false || value === 0) {
+			out[name] = value;
+		}
 	}
-	return settings;
+
+	return out;
+}
+
+function getSettings(pluginSpecs) {
+	if (reloadSettings) {
+		this.rawSettings = loadSettings(this.element);
+	}
+
+	return applySpecs(this.rawSettings || {}, pluginSpecs || specs);
 }
 
 Prism.hooks.add('before-highlight', function (env) {
-	if (env.settings)
-		return;
-
-	var parents = [],
-	    parent = env.element;
-
-	do {
-		parents.push(parent);
-	} while ((parent = parent.parentNode) && parent.hasAttribute);
-
-	env.settings = parents.filter(hasData).reverse().
-						reduce(extractAttrs, []).reduce(addAttrs, {});
+	env.getSettings = getSettings;
 });
 
 }());
