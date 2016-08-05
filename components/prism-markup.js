2b926a150efad52cdb2a0a43a8250b07be552776
Valtteri Laitinen <valtlai@users.noreply.github.com>
2016-08-05 03:07:56 +0300

    Make doctype case insensetive

--- components/prism-markup.js
+++ components/prism-markup.js
@@ -1,7 +1,7 @@
 Prism.languages.markup = {
 	'comment': /<!--[\w\W]*?-->/,
 	'prolog': /<\?[\w\W]+?\?>/,
-	'doctype': /<!DOCTYPE[\w\W]+?>/,
+	'doctype': /<!DOCTYPE[\w\W]+?>/i,
 	'cdata': /<!\[CDATA\[[\w\W]*?]]>/i,
 	'tag': {
 		pattern: /<\/?(?!\d)[^\s>\/=$<]+(?:\s+[^\s>\/=]+(?:=(?:("|')(?:\\\1|\\?(?!\1)[\w\W])*\1|[^\s'">=]+))?)*\s*\/?>/i,
