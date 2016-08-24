02cc166ae94d42a1f15081fc30aafbb95ac677fc
Scott Helme <scotthelme@hotmail.com>
2016-08-24 22:26:41 +0100

    Updates based on feedback.

--- components/prism-csp.js
+++ components/prism-csp.js
@@ -11,15 +11,15 @@
 
 Prism.languages.csp = {
 	'directive':  {
-             pattern: /\b(base-uri|block-all-mixed-content|child-src|connect-src|default-src|disown-opener|font-src|form-action|frame-ancestors|frame-src|img-src|manifest-src|media-src|object-src|plugin-types|referrer|reflected-xss|report-to|report-uri |require-sri-for|sandbox|script-src|style-src|upgrade-insecure-requests|worker-src)\b/i,
+             pattern: /\b(?:base-uri|block-all-mixed-content|(?:child|connect|default|font|frame|img|manifest|media|object|script|style|worker)-src|disown-opener|form-action|frame-ancestors|plugin-types|referrer|reflected-xss|report-to|report-uri |require-sri-for|sandbox|upgrade-insecure-requests)\b/i,
              alias: 'keyword'
         },
 	'safe': {
-            pattern: /('self'|'none'|'strict-dynamic'|('nonce-|'sha256-)[a-zA-Z0-9]{1,}('))/,
+            pattern: /'(?:self|none|strict-dynamic|(nonce-|sha256-)[a-zA-Z0-9]+)'/,
             alias: 'selector'
         },
 	'unsafe': {
-            pattern: /('unsafe-inline'|'unsafe-eval'|'unsafe-hashed-attributes'|\*)/,
+            pattern: /(?:'unsafe-inline'|'unsafe-eval'|'unsafe-hashed-attributes'|\*)/,
             alias: 'function'
         }
 };
