561bceb5f251b80a57100a542aff264e3441fb4c
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-02-08 13:04:02 +0530

    fixes #1061 JSX spread operator break (#1094)
    * fixes #1061 JSX spread operator break
    
    * simplify jsx tag regexp

--- components/prism-jsx.js
+++ components/prism-jsx.js
@@ -3,9 +3,19 @@
 var javascript = Prism.util.clone(Prism.languages.javascript);
 
 Prism.languages.jsx = Prism.languages.extend('markup', javascript);
-Prism.languages.jsx.tag.pattern= /<\/?[\w\.:-]+\s*(?:\s+[\w\.:-]+(?:=(?:("|')(\\?[\w\W])*?\1|[^\s'">=]+|(\{[\w\W]*?\})))?\s*)*\/?>/i;
+Prism.languages.jsx.tag.pattern= /<\/?[\w\.:-]+\s*(?:\s+(?:[\w\.:-]+(?:=(?:("|')(\\?[\w\W])*?\1|[^\s'">=]+|(\{[\w\W]*?\})))?|\{\.{3}\w+\})\s*)*\/?>/i;
 
 Prism.languages.jsx.tag.inside['attr-value'].pattern = /=[^\{](?:('|")[\w\W]*?(\1)|[^\s>]+)/i;
+
+Prism.languages.insertBefore('inside', 'attr-name', {
+	'spread': {
+		pattern: /\{\.{3}\w+\}/,
+		inside: {
+			'punctuation': /\{|\}|\./,
+			'attr-value': /\w+/
+		}
+	}
+}, Prism.languages.jsx.tag);
 
 var jsxExpression = Prism.util.clone(Prism.languages.jsx);
 
