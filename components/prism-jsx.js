867ea42877cbdfb8b8d3645b1ccbf93308157d9f
Golmote <yann.logan@live.fr>
2017-03-02 08:26:05 +0100

    JSX: Fix highlighting of attributes containing spaces

--- components/prism-jsx.js
+++ components/prism-jsx.js
@@ -5,7 +5,7 @@ var javascript = Prism.util.clone(Prism.languages.javascript);
 Prism.languages.jsx = Prism.languages.extend('markup', javascript);
 Prism.languages.jsx.tag.pattern= /<\/?[\w\.:-]+\s*(?:\s+(?:[\w\.:-]+(?:=(?:("|')(\\?[\w\W])*?\1|[^\s'">=]+|(\{[\w\W]*?\})))?|\{\.{3}\w+\})\s*)*\/?>/i;
 
-Prism.languages.jsx.tag.inside['attr-value'].pattern = /=[^\{](?:('|")[\w\W]*?(\1)|[^\s>]+)/i;
+Prism.languages.jsx.tag.inside['attr-value'].pattern = /=(?!\{)(?:('|")[\w\W]*?(\1)|[^\s>]+)/i;
 
 Prism.languages.insertBefore('inside', 'attr-name', {
 	'spread': {
