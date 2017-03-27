e8ad09186c554a459e86bcef3b812d82c777d58f
Bruno Bernardo <Bigsby@users.noreply.github.com>
2017-03-27 23:38:34 +0100

    Add =begin =end comments

--- components/prism-ruby.js
+++ components/prism-ruby.js
@@ -6,7 +6,10 @@
  */
 (function(Prism) {
 	Prism.languages.ruby = Prism.languages.extend('clike', {
-		'comment': /#(?!\{[^\r\n]*?\}).*/,
+		'comment': [
+			/#[^\r\n]*(\r?\n|$)/g,
+			/^=begin\n(.*\n)+^=end/gm
+		],
 		'keyword': /\b(alias|and|BEGIN|begin|break|case|class|def|define_method|defined|do|each|else|elsif|END|end|ensure|false|for|if|in|module|new|next|nil|not|or|raise|redo|require|rescue|retry|return|self|super|then|throw|true|undef|unless|until|when|while|yield)\b/
 	});
 
