d57812c1815ef5448e112987d03356c719bba867
Valtteri Laitinen <valtlai@users.noreply.github.com>
2017-05-23 23:45:15 +0300

    Support CSS #RRGGBBAA
    Add support for CSS #RRGGBBAA notation.

--- components/prism-css-extras.js
+++ components/prism-css-extras.js
@@ -10,7 +10,7 @@ Prism.languages.css.selector = {
 };
 
 Prism.languages.insertBefore('css', 'function', {
-	'hexcode': /#[\da-f]{3,6}/i,
+	'hexcode': /#[\da-f]{3,8}/i,
 	'entity': /\\[\da-f]{1,8}/i,
 	'number': /[\d%\.]+/
 });
