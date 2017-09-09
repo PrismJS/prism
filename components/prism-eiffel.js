2e32fc7eba86e881d73ef7022846d36bd02a2b7e
Golmote <yann.logan@live.fr>
2017-09-09 10:51:00 +0200

    Eiffel: Make strings greedy and update tests and example

--- components/prism-eiffel.js
+++ components/prism-eiffel.js
@@ -1,14 +1,22 @@
 Prism.languages.eiffel = {
+	'comment': /--.*/,
 	'string': [
 		// Aligned-verbatim-strings
-		/"([^[]*)\[[\s\S]+?\]\1"/,
+		{
+			pattern: /"([^[]*)\[[\s\S]+?\]\1"/,
+			greedy: true
+		},
 		// Non-aligned-verbatim-strings
-		/"([^{]*)\{[\s\S]+?\}\1"/,
+		{
+			pattern: /"([^{]*)\{[\s\S]+?\}\1"/,
+			greedy: true
+		},
 		// Single-line string
-		/"(?:%\s+%|%"|.)*?"/
+		{
+			pattern: /"(?:%\s+%|%"|.)*?"/,
+			greedy: true
+		}
 	],
-	// (comments including quoted strings not supported)
-	'comment': /--.*/,
 	// normal char | special char | char code
 	'char': /'(?:%'|.)+?'/,
 	'keyword': /\b(?:across|agent|alias|all|and|attached|as|assign|attribute|check|class|convert|create|Current|debug|deferred|detachable|do|else|elseif|end|ensure|expanded|export|external|feature|from|frozen|if|implies|inherit|inspect|invariant|like|local|loop|not|note|obsolete|old|once|or|Precursor|redefine|rename|require|rescue|Result|retry|select|separate|some|then|undefine|until|variant|Void|when|xor)\b/i,
