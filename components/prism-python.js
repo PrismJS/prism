b9cb32f3585922af887374454ae06989ad700d31
Erin Wild <eewild@gmail.com>
2017-10-12 11:36:04 -0400

    Add Python builtin function highlighting

--- components/prism-python.js
+++ components/prism-python.js
@@ -1,4 +1,4 @@
-Prism.languages.python= {
+Prism.languages.python = {
 	'triple-quoted-string': {
 		pattern: /"""[\s\S]+?"""|'''[\s\S]+?'''/,
 		alias: 'string'
@@ -11,7 +11,7 @@ Prism.languages.python= {
 		pattern: /("|')(?:\\\\|\\?[^\\\r\n])*?\1/,
 		greedy: true
 	},
-	'function' : {
+	'function': {
 		pattern: /((?:^|\s)def[ \t]+)[a-zA-Z_][a-zA-Z0-9_]*(?=\()/g,
 		lookbehind: true
 	},
@@ -19,9 +19,10 @@ Prism.languages.python= {
 		pattern: /(\bclass\s+)[a-z0-9_]+/i,
 		lookbehind: true
 	},
-	'keyword' : /\b(?:as|assert|async|await|break|class|continue|def|del|elif|else|except|exec|finally|for|from|global|if|import|in|is|lambda|pass|print|raise|return|try|while|with|yield)\b/,
-	'boolean' : /\b(?:True|False)\b/,
-	'number' : /\b-?(?:0[bo])?(?:(?:\d|0x[\da-f])[\da-f]*\.?\d*|\.\d+)(?:e[+-]?\d+)?j?\b/i,
-	'operator' : /[-+%=]=?|!=|\*\*?=?|\/\/?=?|<[<=>]?|>[=>]?|[&|^~]|\b(?:or|and|not)\b/,
-	'punctuation' : /[{}[\];(),.:]/
+	'keyword': /\b(?:as|assert|async|await|break|class|continue|def|del|elif|else|except|exec|finally|for|from|global|if|import|in|is|lambda|pass|print|raise|return|try|while|with|yield)\b/,
+	'builtin':/\b(?:\_\_import\_\_|abs|all|any|apply|basestring|bin|bool|buffer|bytearray|callable|chr|classmethod|cmp|coerce|compile|complex|delattr|dict|dir|divmod|enumerate|eval|execfile|file|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|int|intern|isinstance|issubclass|iter|len|list|locals|long|map|max|memoryview|min|next|object|oct|open|ord|pow|print|property|range|raw_input|reduce|reload|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|unichr|unicode|vars|xrange|zip)\b/,
+	'boolean': /\b(?:True|False)\b/,
+	'number': /\b-?(?:0[bo])?(?:(?:\d|0x[\da-f])[\da-f]*\.?\d*|\.\d+)(?:e[+-]?\d+)?j?\b/i,
+	'operator': /[-+%=]=?|!=|\*\*?=?|\/\/?=?|<[<=>]?|>[=>]?|[&|^~]|\b(?:or|and|not)\b/,
+	'punctuation': /[{}[\];(),.:]/
 };
