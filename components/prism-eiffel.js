e881fe3f66836ebd430995d6b6969bf87edcb1a8
Golmote <yann.logan@live.fr>
2017-10-22 14:31:37 +0200

    Eiffel: Regexp optimisation

--- components/prism-eiffel.js
+++ components/prism-eiffel.js
@@ -13,17 +13,17 @@ Prism.languages.eiffel = {
 		},
 		// Single-line string
 		{
-			pattern: /"(?:%\s+%|%"|.)*?"/,
+			pattern: /"(?:%\s+%|%.|[^%"\r\n])*"/,
 			greedy: true
 		}
 	],
 	// normal char | special char | char code
-	'char': /'(?:%'|.)+?'/,
+	'char': /'(?:%.|[^%'\r\n])+'/,
 	'keyword': /\b(?:across|agent|alias|all|and|attached|as|assign|attribute|check|class|convert|create|Current|debug|deferred|detachable|do|else|elseif|end|ensure|expanded|export|external|feature|from|frozen|if|implies|inherit|inspect|invariant|like|local|loop|not|note|obsolete|old|once|or|Precursor|redefine|rename|require|rescue|Result|retry|select|separate|some|then|undefine|until|variant|Void|when|xor)\b/i,
 	'boolean': /\b(?:True|False)\b/i,
 	// Convention: class-names are always all upper-case characters
 	'class-name': {
-		'pattern': /\b[A-Z][\dA-Z_]*\b/g,
+		'pattern': /\b[A-Z][\dA-Z_]*\b/,
 		'alias': 'builtin'
 	},
 	'number': [
