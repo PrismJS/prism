f2b13f6c8098413ca073c5851d6eeba97177f9f1
Ales Tsurko <ales.tsurko@gmail.com>
2017-12-23 18:17:06 +0300

    Io: fix strings and operators, add tests

--- components/prism-io.js
+++ components/prism-io.js
@@ -13,22 +13,19 @@ Prism.languages.io = {
             lookbehind: true
         }
     ],
-    'string': [
-        {
-            pattern: /(")(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
-            greedy: true
-        },
-        {
-            pattern: /(""")"?[\s\S]+?"?\1/,
-            greedy: true
-        }
-    ], 
-    'class-name': {
-        pattern: /\b(?:_*\p{Lu}+[\p{L}_0-9]*)\b/
+    'triple-quoted-string': {
+        pattern: /"""(?:\\.|(?!""")[^\\])*"""/,
+        greedy: true,
+        alias: 'string'
+    },
+    'string': {
+        pattern: /"(?:\\.|(?!")[^\\\r\n])*"/,
+        greedy: true
     },
     'keyword': /\b(?:activate|activeCoroCount|asString|block|break|catch|clone|collectGarbage|compileString|continue|do|doFile|doMessage|doString|else|elseif|exit|for|foreach|forward|getSlot|getEnvironmentVariable|hasSlot|if|ifFalse|ifNil|ifNilEval|ifTrue|isActive|isNil|isResumable|list|message|method|parent|pass|pause|perform|performWithArgList|print|println|proto|raise|raiseResumable|removeSlot|resend|resume|schedulerSleepSeconds|self|sender|setSchedulerSleepSeconds|setSlot|shallowCopy|slotNames|super|system|then|thisBlock|thisContext|call|try|type|uniqueId|updateSlot|wait|while|write|yield)\b/,
-    'type':/\b(?:Array|AudioDevice|AudioMixer|Block|Box|Buffer|CFunction|CGI|Color|Curses|DBM|DNSResolver|DOConnection|DOProxy|DOServer|Date|Directory|Duration|DynLib|Error|Exception|FFT|File|Fnmatch|Font|Future|GL|GLE|GLScissor|GLU|GLUCylinder|GLUQuadric|GLUSphere|GLUT|Host|Image|Importer|LinkList|List|Lobby|Locals|MD5|MP3Decoder|MP3Encoder|Map|Message|Movie|Notification|Number|Object|OpenGL|Point|Protos|Regex|SGML|SGMLElement|SGMLParser|SQLite|Server|Sequence|ShowMessage|SleepyCat|SleepyCatCursor|Socket|SocketManager|Sound|Soup|Store|String|Tree|UDPSender|UPDReceiver|URL|User|Warning|WeakLink|true|false|nil|Random|BigNum|Sequence)\b/,
-    'number': /\b-?(?:0x[\da-f]+|0o[0-7]+|\d*\.?\d+(?:e[+-]?\d+)?)\b/i,
-    'operator': /:?:?=|==|\!=|\|\||&&?|>=?|<=?|\*\*?|\*=?|\/=?|%=?|\+=?|-=?|\^=?|>>=?|<<=?|return|and|or|not|@@?|\?\??|\.\./,
+    'builtin':/\b(?:Array|AudioDevice|AudioMixer|Block|Box|Buffer|CFunction|CGI|Color|Curses|DBM|DNSResolver|DOConnection|DOProxy|DOServer|Date|Directory|Duration|DynLib|Error|Exception|FFT|File|Fnmatch|Font|Future|GL|GLE|GLScissor|GLU|GLUCylinder|GLUQuadric|GLUSphere|GLUT|Host|Image|Importer|LinkList|List|Lobby|Locals|MD5|MP3Decoder|MP3Encoder|Map|Message|Movie|Notification|Number|Object|OpenGL|Point|Protos|Regex|SGML|SGMLElement|SGMLParser|SQLite|Server|Sequence|ShowMessage|SleepyCat|SleepyCatCursor|Socket|SocketManager|Sound|Soup|Store|String|Tree|UDPSender|UPDReceiver|URL|User|Warning|WeakLink|Random|BigNum|Sequence)\b/,
+    'boolean': /\b(?:true|false|nil)\b/,
+    'number': /\b-?(?:0x[\da-f]+|\d*\.?\d+(?:e-?\d+)?)\b/i,
+    'operator': /==|\!=|\*=|\/=|%=|\+=|-=|\^=|>>?=?|<<?=?|&=|\|=|:?:?=|\+\+?|--?|\*\*?|\/\/?|%|\|\|?|&&?|return|and|or|not|@@?|\?\??|\.\./,
     'punctuation': /[{}[\];(),.:]/
 };
