17904473f88ac0f4e50bf05b7f4493a594749513
ilias <mail@ilias.xyz>
2017-12-25 00:11:14 +0100

    Simplify patterns and fix a few things inherited from haskell

--- components/prism-elm.js
+++ components/prism-elm.js
@@ -3,16 +3,26 @@ Prism.languages.elm = {
 		pattern: /(^|[^-!#$%*+=?&@|~.:<>^\\\/])(--[^-!#$%*+=?&@|~.:<>^\\\/].*|{-[\s\S]*?-})/m,
 		lookbehind: true
 	},
-	char: /'([^\\']|\\([abfnrtv\\"'&]|\^[A-Z@[\]\^_]|NUL|SOH|STX|ETX|EOT|ENQ|ACK|BEL|BS|HT|LF|VT|FF|CR|SO|SI|DLE|DC1|DC2|DC3|DC4|NAK|SYN|ETB|CAN|EM|SUB|ESC|FS|GS|RS|US|SP|DEL|\d+|o[0-7]+|x[0-9a-fA-F]+))'/,
-	string: {
-		pattern: /"([^\\"]|\\([abfnrtv\\"'&]|\^[A-Z@[\]\^_]|NUL|SOH|STX|ETX|EOT|ENQ|ACK|BEL|BS|HT|LF|VT|FF|CR|SO|SI|DLE|DC1|DC2|DC3|DC4|NAK|SYN|ETB|CAN|EM|SUB|ESC|FS|GS|RS|US|SP|DEL|\d+|o[0-7]+|x[0-9a-fA-F]+)|\\\s+\\)*"/,
+	char: {
+		pattern: /'([^\\'\r\n]|\\([abfnrtv\\']|\d+|x[0-9a-fA-F]+))'/,
 		greedy: true
 	},
+	string: [
+		{
+			// Multiline strings are wrapped in triple ". Quotes may appear unescaped.
+			pattern: /"""((.|\s)*?(?="""))"""/,
+			greedy: true
+		},
+		{
+			pattern: /"([^\\"\r\n]|\\([abfnrtv\\"]|\d+|x[0-9a-fA-F]+))*"/,
+			greedy: true
+		}
+	],
 	import_statement: {
 		// The imported or hidden names are not included in this import
 		// statement. This is because we want to highlight those exactly like
 		// we do for the names in the program.
-		pattern: /(\r?\n|\r|^)\s*import\s+([A-Z][_a-zA-Z0-9]*)(\.[A-Z][_a-zA-Z0-9]*)*(\s+as\s+([A-Z][_a-zA-Z0-9]*)(\.[A-Z][_a-zA-Z0-9]*)*)?(\s+exposing\s+)?/m,
+		pattern: /^\s*import\s+[A-Z]\w*(\.[A-Z]\w*)*(\s+as\s+([A-Z]\w*)(\.[A-Z]\w*)*)?(\s+exposing\s+)?/m,
 		inside: {
 			keyword: /\b(import|as|exposing)\b/
 		}
@@ -20,16 +30,18 @@ Prism.languages.elm = {
 	keyword: /\b(alias|as|case|else|exposing|if|in|infixl|infixr|let|module|of|then|type)\b/,
 	// These are builtin variables only. Constructors are highlighted later as a constant.
 	builtin: /\b(abs|acos|always|asin|atan|atan2|ceiling|clamp|compare|cos|curry|degrees|e|flip|floor|fromPolar|identity|isInfinite|isNaN|logBase|max|min|negate|never|not|pi|radians|rem|round|sin|sqrt|tan|toFloat|toPolar|toString|truncate|turns|uncurry|xor)\b/,
-	// decimal integers and floating point numbers | octal integers | hexadecimal integers
+	// decimal integers and floating point numbers | hexadecimal integers
 	number: /\b(\d+(\.\d+)?(e[+-]?\d+)?|0x[0-9a-f]+)\b/i,
 	// Most of this is needed because of the meaning of a single '.'.
 	// If it stands alone freely, it is the function composition.
 	// It may also be a separator between a module name and an identifier => no
 	// operator. If it comes together with other special characters it is an
 	// operator too.
-	operator: /\s\.\s|[-!#$%*+=?&@|~.:<>^\\\/]*\.[-!#$%*+=?&@|~.:<>^\\\/]+|[-!#$%*+=?&@|~.:<>^\\\/]+\.[-!#$%*+=?&@|~.:<>^\\\/]*|[-!#$%*+=?&@|~:<>^\\\/]+|`([A-Z][_a-zA-Z0-9']*\.)*[_a-z][_a-zA-Z0-9']*`/,
+	// Valid operator characters in 0.18: +-/*=.$<>:&|^?%#@~!
+	// Ref: https://groups.google.com/forum/#!msg/elm-dev/0AHSnDdkSkQ/E0SVU70JEQAJ
+	operator: /\s\.\s|[+\-/*=.$<>:&|^?%#@~!]{2,}|[+\-/*=$<>:&|^?%#@~!]/,
 	// In Elm, nearly everything is a variable, do not highlight these.
-	hvariable: /\b([A-Z][_a-zA-Z0-9]*\.)*[_a-z][_a-zA-Z0-9]*\b/,
-	constant: /\b([A-Z][_a-zA-Z0-9]*\.)*[A-Z][_a-zA-Z0-9]*\b/,
-	punctuation: /[{}[\];(),.:]/
+	hvariable: /\b([A-Z]\w*\.)*[a-z]\w*\b/,
+	constant: /\b([A-Z]\w*\.)*[A-Z]\w*\b/,
+	punctuation: /[{}[\]|(),.:]/
 };
