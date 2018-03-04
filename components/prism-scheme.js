51bff80ad8de5d29141de25f8531aae4b8c4061c
Golmote <yann.logan@live.fr>
2018-03-04 18:42:41 +0100

    Scheme: Don't highlight first number of a list as a function. Fix #1331

--- components/prism-scheme.js
+++ components/prism-scheme.js
@@ -13,7 +13,7 @@ Prism.languages.scheme = {
 		lookbehind : true
 	},
 	'number' : {
-		pattern: /(\s|\))[-+]?\d*\.?\d+(?:\s*[-+]\s*\d*\.?\d+i)?\b/,
+		pattern: /(\s|[()])[-+]?\d*\.?\d+(?:\s*[-+]\s*\d*\.?\d+i)?\b/,
 		lookbehind: true
 	},
 	'boolean' : /#[tf]/,
