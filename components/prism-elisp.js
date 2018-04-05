589243723af30559d91dbf6a853db49e17c8ed8f
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-04-05 10:07:00 -0500

    add semicolons elisp

--- components/prism-elisp.js
+++ components/prism-elisp.js
@@ -2,32 +2,32 @@
 // simple form
 // e.g. (interactive ... or (interactive)
 function simple_form(name) {
-	return new RegExp("(\\()" + name + "(?=[\\s\\)])")
+	return new RegExp('(\\()' + name + '(?=[\\s\\)])');
 }
 // booleans and numbers
 function primitive(pattern) {
-	return new RegExp("([\\s([])" + pattern + "(?=[\\s)])")
+	return new RegExp('([\\s([])' + pattern + '(?=[\\s)])');
 }
 
 // Patterns in regular expressions
 
 // Symbol name. See https://www.gnu.org/software/emacs/manual/html_node/elisp/Symbol-Type.html
 // & and : are excluded as they are usually used for special purposes
-var symbol = "[-+*/_~!@$%^=<>{}\\w]+"
+var symbol = '[-+*/_~!@$%^=<>{}\\w]+';
 // symbol starting with & used in function arguments
-var marker = "&" + symbol
+var marker = '&' + symbol;
 // Open parenthesis for look-behind
-var par = "(\\()"
-var endpar = "(?=\\))"
+var par = '(\\()';
+var endpar = '(?=\\))';
 // End the pattern with look-ahead space
-var space = "(?=\\s)"
+var space = '(?=\\s)';
 
 var language = {
 	// Three or four semicolons are considered a heading.
 	// See https://www.gnu.org/software/emacs/manual/html_node/elisp/Comment-Tips.html
 	heading: {
 		pattern: /;;;.*/,
-		alias: ["comment", "title"],
+		alias: ['comment', 'title'],
 	},
 	comment: /;.*/,
 	string: {
@@ -35,57 +35,57 @@ var language = {
 		greedy: true,
 		inside: {
 			argument: /[-A-Z]+(?=[.,\s])/,
-			symbol: new RegExp("`" + symbol + "'"),
+			symbol: new RegExp('`' + symbol + "'"),
 		},
 	},
-	"quoted-symbol": {
+	'quoted-symbol': {
 		pattern: new RegExp("#?'" + symbol),
-		alias: ["variable", "symbol"],
+		alias: ['variable', 'symbol'],
 	},
-	"lisp-property": {
-		pattern: new RegExp(":" + symbol),
-		alias: "property",
+	'lisp-property': {
+		pattern: new RegExp(':' + symbol),
+		alias: 'property',
 	},
 	splice: {
-		pattern: new RegExp(",@?" + symbol),
-		alias: ["symbol", "variable"],
+		pattern: new RegExp(',@?' + symbol),
+		alias: ['symbol', 'variable'],
 	},
 	keyword: [
 		{
 			pattern: new RegExp(
 				par +
-					"(?:(?:lexical-)?let\\*?|(?:cl-)?letf|if|when|while|unless|cons|cl-loop|and|or|not|cond|setq|error|message|null|require|provide|use-package)" +
+					'(?:(?:lexical-)?let\\*?|(?:cl-)?letf|if|when|while|unless|cons|cl-loop|and|or|not|cond|setq|error|message|null|require|provide|use-package)' +
 					space
 			),
 			lookbehind: true,
 		},
 		{
 			pattern: new RegExp(
-				par + "(?:for|do|collect|return|finally|append|concat|in|by)" + space
+				par + '(?:for|do|collect|return|finally|append|concat|in|by)' + space
 			),
 			lookbehind: true,
 		},
 	],
 	declare: {
-		pattern: simple_form("declare"),
+		pattern: simple_form('declare'),
 		lookbehind: true,
-		alias: "keyword",
+		alias: 'keyword',
 	},
 	interactive: {
-		pattern: simple_form("interactive"),
+		pattern: simple_form('interactive'),
 		lookbehind: true,
-		alias: "keyword",
+		alias: 'keyword',
 	},
 	boolean: {
-		pattern: primitive("(?:t|nil)"),
+		pattern: primitive('(?:t|nil)'),
 		lookbehind: true,
 	},
 	number: {
-		pattern: primitive("[-+]?\\d+(?:\\.\\d*)?"),
+		pattern: primitive('[-+]?\\d+(?:\\.\\d*)?'),
 		lookbehind: true,
 	},
 	defvar: {
-		pattern: new RegExp(par + "def(?:var|const|custom|group)\\s+" + symbol),
+		pattern: new RegExp(par + 'def(?:var|const|custom|group)\\s+' + symbol),
 		lookbehind: true,
 		inside: {
 			keyword: /^def[a-z]+/,
@@ -95,23 +95,23 @@ var language = {
 	defun: {
 		pattern: new RegExp(
 			par +
-				"(?:cl-)?(?:defun\\*?|defmacro)\\s+" +
+				'(?:cl-)?(?:defun\\*?|defmacro)\\s+' +
 				symbol +
-				"\\s+\\([\\s\\S]*?\\)"
+				'\\s+\\([\\s\\S]*?\\)'
 		),
 		lookbehind: true,
 		inside: {
 			keyword: /^(?:cl-)?def\S+/,
 			arguments: null,
 			function: {
-				pattern: new RegExp("(^\\s)" + symbol),
+				pattern: new RegExp('(^\\s)' + symbol),
 				lookbehind: true,
 			},
 			punctuation: /[()]/,
 		},
 	},
 	lambda: {
-		pattern: new RegExp(par + "lambda\\s+\\((?:&?" + symbol + "\\s*)*\\)"),
+		pattern: new RegExp(par + 'lambda\\s+\\((?:&?' + symbol + '\\s*)*\\)'),
 		lookbehind: true,
 		inside: {
 			keyword: /^lambda/,
@@ -134,17 +134,17 @@ var language = {
 			lookbehind: true,
 		},
 	],
-}
+};
 
 var arg = {
-	"lisp-marker": new RegExp(marker),
+	'lisp-marker': new RegExp(marker),
 	rest: {
 		argument: {
 			pattern: new RegExp(symbol),
-			alias: "variable",
+			alias: 'variable',
 		},
 		varform: {
-			pattern: new RegExp(par + symbol + "\\s+\\S[\\s\\S]*" + endpar),
+			pattern: new RegExp(par + symbol + '\\s+\\S[\\s\\S]*' + endpar),
 			lookbehind: true,
 			inside: {
 				string: language.string,
@@ -155,38 +155,38 @@ var arg = {
 			},
 		},
 	},
-}
+};
 
-var forms = "\\S+(?:\\s+\\S+)*"
+var forms = '\\S+(?:\\s+\\S+)*';
 
 var arglist = {
-	pattern: new RegExp(par + "[\\s\\S]*" + endpar),
+	pattern: new RegExp(par + '[\\s\\S]*' + endpar),
 	lookbehind: true,
 	inside: {
-		"rest-vars": {
-			pattern: new RegExp("&(?:rest|body)\\s+" + forms),
+		'rest-vars': {
+			pattern: new RegExp('&(?:rest|body)\\s+' + forms),
 			inside: arg,
 		},
-		"other-marker-vars": {
-			pattern: new RegExp("&(?:optional|aux)\\s+" + forms),
+		'other-marker-vars': {
+			pattern: new RegExp('&(?:optional|aux)\\s+' + forms),
 			inside: arg,
 		},
 		keys: {
-			pattern: new RegExp("&key\\s+" + forms + "(?:\\s+&allow-other-keys)?"),
+			pattern: new RegExp('&key\\s+' + forms + '(?:\\s+&allow-other-keys)?'),
 			inside: arg,
 		},
 		argument: {
 			pattern: new RegExp(symbol),
-			alias: "variable",
+			alias: 'variable',
 		},
 		punctuation: /[()]/,
 	},
-}
+};
 
-language["lambda"].inside.arguments = arglist
-language["defun"].inside.arguments = Prism.util.clone(arglist)
-language["defun"].inside.arguments.inside.sublist = arglist
+language['lambda'].inside.arguments = arglist;
+language['defun'].inside.arguments = Prism.util.clone(arglist);
+language['defun'].inside.arguments.inside.sublist = arglist;
 
-Prism.languages.elisp = language
-Prism.languages.emacs = language
-Prism.languages.lisp = language
+Prism.languages.elisp = language;
+Prism.languages.emacs = language;
+Prism.languages.lisp = language;
