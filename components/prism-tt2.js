47b4cc52de844c86c04c66ab5901a48a6fb19a6c
Guido Flohr <guido.flohr@cantanea.com>
2018-05-03 10:15:17 +0300

    test tt2 keywords

--- components/prism-tt2.js
+++ components/prism-tt2.js
@@ -1,11 +1,17 @@
 (function(Prism) {
 
 	Prism.languages.tt2 = Prism.languages.extend('clike', {
-		'comment': {
+		comment: {
 			pattern: /#.*/,
 			lookbehind: true
 		},
-		'punctuation': /[[\]{},()]/	});
+		punctuation: /[[\]{},()]/,
+		keyword: /\b(?:GET|CALL|SET|DEFAULT|INSERT|INCLUDE|PROCESS|WRAPPER|BLOCK|IF|UNLESS|ELSIF|ELSE|SWITCH|CASE|FOREACH|WHILE|FILTER|USE|MACRO|RAWPERL|PERL|TRY|THROW|CATCH|FINAL|NEXT|LAST|RETURN|STOP|CLEAR|META|TAGS|DEBUG)\b/
+	});
+
+	Prism.languages.insertBefore('tt2', 'delimiter', {
+		comment: /\[%#[^]*?\%\]/
+	});
 
 	Prism.languages.insertBefore('tt2', 'number', {
 		variable: /[a-z][_a-z0-9]*(?:\.(?:\d+|\$?[a-z][_a-z0-9]*))*/i
