ef644969ea60537ea6e9afbeb5c772443a5f5874
Fangzhou Li <riophaelee@gmail.com>
2018-07-04 07:44:07 +0800

    add Vue support

--- components/prism-markup.js
+++ components/prism-markup.js
@@ -54,3 +54,4 @@ Prism.languages.xml = Prism.languages.markup;
 Prism.languages.html = Prism.languages.markup;
 Prism.languages.mathml = Prism.languages.markup;
 Prism.languages.svg = Prism.languages.markup;
+Prism.languages.vue = Prism.languages.markup;
