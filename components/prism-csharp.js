a4eaa2b03d30f22dbb1b6f24ee4dcb4b557fac45
RunDevelopment <mitchi5000.ms@googlemail.com>
2018-07-08 01:50:15 +0200

    Refactoring `true` and `false` are now `boolean` Support for `as` and `is`

--- components/prism-csharp.js
+++ components/prism-csharp.js
@@ -1,119 +1,135 @@
-Prism.languages.csharp = Prism.languages.extend('clike', {
-	'keyword': /\b(?:abstract|add|alias|as|ascending|async|await|base|bool|break|byte|case|catch|char|checked|class|const|continue|decimal|default|delegate|descending|do|double|dynamic|else|enum|event|explicit|extern|false|finally|fixed|float|for|foreach|from|get|global|goto|group|if|implicit|in|int|interface|internal|into|is|join|let|lock|long|nameof|namespace|new|null|object|operator|orderby|out|override|params|partial|private|protected|public|readonly|ref|remove|return|sbyte|sealed|select|set|short|sizeof|stackalloc|static|string|struct|switch|this|throw|true|try|typeof|uint|ulong|unchecked|unsafe|ushort|using|value|var|virtual|void|volatile|where|while|yield)\b/,
-	'string': [
-		{
-			pattern: /@("|')(?:\1\1|\\[\s\S]|(?!\1)[^\\])*\1/,
-			greedy: true
-		},
-		{
-			pattern: /("|')(?:\\.|(?!\1)[^\\\r\n])*?\1/,
-			greedy: true
-		}
-	],
-	'class-name': [
-		{
-			// [Foo]
-			pattern: /((?:^|\W)\[)[A-Z]\w*(?:\.\w+)*\b/,
-			lookbehind: true,
-			inside: {
-				punctuation: /\./
-			}
-		},
-		{
-			// class Foo
-			pattern: /(\b(?:class|enum|interface|struct)\s+)[A-Z]\w*(?:\.\w+)*(?:<[^\r\n=;{]+?>(?:\.\w+)*)?/,
-			lookbehind: true,
-			inside: {
-				punctuation: /[<>(),.:[\]]/
-			}
-		},
-		{
-			// catch(Foo)
-			pattern: /(catch\s+\()[A-Z]\w*(?:\.\w+)*\b/,
-			lookbehind: true,
-			inside: {
-				punctuation: /\./
-			}
-		},
-		{
-			// where Foo
-			pattern: /(\bwhere\s+)[A-Z]\w*\b/,
-			lookbehind: true
-		}
-	],
-	'number': /(?:\b0(?:x[\da-f_]*[\da-f]|b[01_]*[01])|\B\.\d+|\b\d+(?:_+\d+)*\.?\d*)(?:ul|[flu])?\b/i
-});
+(function (Prism) {
 
-Prism.languages.insertBefore('csharp', 'class-name', {
-	'type-expression': {
-		// default(Foo), typeof(Foo<Bar>)
-		pattern: /(\b(?:default|typeof)\(\s*)[A-Z]\w*(?:\.\w+)*(?:<[^\r\n=;{]+?>(?:\.\w+)*)?(?=\s*\))/,
-		lookbehind: true,
-		inside: {
-			keyword: Prism.languages.csharp.keyword,
-			'class-name': {
-				pattern: /\b[A-Z]\w*(?:\.\w+)*\b/,
-				inside: {
-					punctuation: /\./
-				}
-			},
-			punctuation: /[<>(),.:[\]]/
-		}
-	}
-});
+	var keywords = /\b(?:abstract|add|alias|as|ascending|async|await|base|bool|break|byte|case|catch|char|checked|class|const|continue|decimal|default|delegate|descending|do|double|dynamic|else|enum|event|explicit|extern|finally|fixed|float|for|foreach|from|get|global|goto|group|if|implicit|in|int|interface|internal|into|is|join|let|lock|long|nameof|namespace|new|null|object|operator|orderby|out|override|params|partial|private|protected|public|readonly|ref|remove|return|sbyte|sealed|select|set|short|sizeof|stackalloc|static|string|struct|switch|this|throw|try|typeof|uint|ulong|unchecked|unsafe|ushort|using|value|var|virtual|void|volatile|where|while|yield)\b/;
 
-Prism.languages.insertBefore('csharp', 'class-name', {
-	'constructor-invocation': {
-		// new List<Foo<Bar[]>> { }
-		pattern: /(\bnew\s+)[A-Z]\w*(?:\.\w+)*(?:<[^\r\n=;{]+?>(?:\.\w+)*)?\s*(?=[[({])/,
-		lookbehind: true,
-		inside: Prism.languages.csharp['type-expression'].inside
-	},
-	'return-type': {
-		// Foo<Bar> ForBar()
-		pattern: /\b[A-Z]\w*(?:\.\w+)*(?:<[^\r\n=;{]+?>(?:\.\w+)*)?(?:\[\s*(,\s*)*\])?(?=\s+(?:[A-Z]\w*\.)?(?:\w+(?:\s*<[^\r\n=;{]+?>)?\s*(?:[({]|=>)|this\s*\[))/,
-		inside: Prism.languages.csharp['type-expression'].inside
-	},
-	'generic-method': {
-		// foo<Bar>()
-		pattern: /\w+\s*<[^\r\n=;{)]+?>\s*(?=\()/,
-		inside: {
-			function: /^\w+/,
-			'class-name': {
-				pattern: /\b[A-Z]\w*(?:\.\w+)*\b/,
-				inside: {
-					punctuation: /\./
-				}
+	var classNameInside = {
+		'keyword': keywords,
+		'punctuation': /[<>(),.:[\]]/
+	};
+
+	Prism.languages.csharp = Prism.languages.extend('clike', {
+		'keyword': keywords,
+		'string': [
+			{
+				pattern: /@("|')(?:\1\1|\\[\s\S]|(?!\1)[^\\])*\1/,
+				greedy: true
 			},
-			keyword: Prism.languages.csharp.keyword,
-			punctuation: /[<>(),.:[\]]/
-		}
-	},
-	'type-list': {
-		// class Foo<F> : Bar, IList<FooBar>
-		// where F : Bar, IList<int>
-		pattern: /(\b(?:class|interface|struct|enum)\s+\w*(?:\.\w+)*(<[^\r\n=;{]+?>)?(?:\.\w+)*\s*:\s*|\bwhere\s+\w+\s*:\s*)(?:\w+(?:<[^\r\n=;{]+?>)?(?:\s*,)?\s*?)+(?=\s*(?:where|[{;]|=>))/,
-		lookbehind: true,
-		inside: Prism.languages.csharp['type-expression'].inside
-	},
-	'identifier-type': {
-		// (Foo bar, Bar baz, Foo[,,] bay, Foo<Bar, FooBar<Bar>> bax)
-		pattern: /\b[A-Z]\w*(?:\.\w+)*(?:<[^\r\n=;{]+?>(?:\.\w+)*)?(?:\s*\[\s*(?:,\s*)*\])?(?=\s+\w+(?:\s*[=,;:{)]|\s+in))/,
-		inside: Prism.languages.csharp['type-expression'].inside
-	},
-	'preprocessor': {
-		pattern: /(^\s*)#.*/m,
-		lookbehind: true,
-		alias: 'property',
-		inside: {
-			// highlight preprocessor directives as keywords
-			'directive': {
-				pattern: /(\s*#)\b(?:define|elif|else|endif|endregion|error|if|line|pragma|region|undef|warning)\b/,
+			{
+				pattern: /("|')(?:\\.|(?!\1)[^\\\r\n])*?\1/,
+				greedy: true
+			}
+		],
+		'class-name': [
+			{
+				// (Foo bar, Bar baz, Foo[,,] bay, Foo<Bar, FooBar<Bar>> bax)
+				pattern: /\b[A-Z]\w*(?:\.\w+)*(?:<(?:[^>\n=;{}]+|>(?=\s*[,.>?[)]))+>)?(?:\.\w+)*(?:\[\s*(?:,\s*)*\])*(?=\s+\w+(?:\s*[=,;:{)\]]|\s+in))/,
+				inside: classNameInside
+			},
+			{
+				// [Foo]
+				pattern: /((?:^|\W)\[)[A-Z]\w*(?:\.\w+)*\b/,
 				lookbehind: true,
-				alias: 'keyword'
+				inside: {
+					'punctuation': /\./
+				}
+			},
+			{
+				// class Foo<A, B>
+				pattern: /(\b(?:class|enum|interface|struct)\s+)[A-Z]\w*(?:\s*<[^>]+>)?/,
+				lookbehind: true,
+				inside: {
+					'punctuation': /[<>,]/
+				}
+			},
+			{
+				// catch(Foo)
+				pattern: /(\bcatch\s*\()[A-Z]\w*(?:\.\w+)*\b/,
+				lookbehind: true,
+				inside: {
+					'punctuation': /\./
+				}
+			},
+			{
+				// where Foo
+				pattern: /(\bwhere\s+)[A-Z]\w*\b/,
+				lookbehind: true
+			},
+			{
+				// as Foo<A>, is Bar<B>
+				pattern: /(\b(?:is|as)\s+)[A-Z]\w*(?:\.\w+)*(?:<(?:[^>\n=;{}]+|>(?=\s*[,.>?[)]))+>)?(?:\.\w+)*(?:\[\s*(?:,\s*)*\])*/,
+				lookbehind: true,
+				inside: classNameInside
+			}
+		],
+		'number': /(?:\b0(?:x[\da-f_]*[\da-f]|b[01_]*[01])|\B\.\d+|\b\d+(?:_+\d+)*\.?\d*)(?:ul|[flu])?\b/i
+	});
+
+	Prism.languages.insertBefore('csharp', 'class-name', {
+		'type-expression': {
+			// default(Foo), typeof(Foo<Bar>)
+			pattern: /(\b(?:default|typeof)\(\s*)[A-Z]\w*(?:\.\w+)*(?:<(?:[^>\n=;{}]+|>(?=\s*[,.>?[]))+>)?(?:\.\w+)*(?:\[\s*(?:,\s*)*\])*(?=\s*\))/,
+			lookbehind: true,
+			inside: classNameInside,
+			alias: 'class-name'
+		},
+		'constructor-invocation': {
+			// new List<Foo<Bar[]>> { }
+			pattern: /(\bnew\s+)[A-Z]\w*(?:\.\w+)*(?:<(?:[^>\n=;{}]+|>(?=\s*[,.>?[)]))+>)?(?:\.\w+)*(?:\[\s*(?:,\s*)*\])*(?=\s*[[({])/,
+			lookbehind: true,
+			inside: classNameInside,
+			alias: 'class-name'
+		},
+		'return-type': {
+			// Foo<Bar> ForBar(), Foo IFoo.Bar() => 0
+			pattern: /\b[A-Z]\w*(?:\.\w+)*(?:<[^\n=;{]+?>(?:\.\w+)*)?(?:\[\s*(,\s*)*\])?(?=\s+(?:[A-Z]\w*\.)?(?:\w+(?:\s*<[^\n=;{]+?>)?\s*(?:[({]|=>)|this\s*\[))/,
+			inside: classNameInside,
+			alias: 'class-name'
+		},
+		'generic-method': {
+			// foo<Bar>()
+			pattern: /\w+\s*<[^\n=;{)]+?>(?=\s*\()/,
+			inside: {
+				'function': /^\w+/,
+				'class-name': {
+					pattern: /\b[A-Z]\w*(?:\.\w+)*\b/,
+					inside: {
+						'punctuation': /\./
+					}
+				},
+				'keyword': keywords,
+				'punctuation': /[<>(),.:[\]]/
+			}
+		},
+		'type-list': {
+			// ( class Foo<F> ) : Bar, IList<FooBar>
+			// ( where F ) : Bar, IList<int>
+			pattern: /(\b(?:class|interface|struct|enum)\s+[A-Z]\w*(?:\s*<[^>]+>)?\s*:\s*|\bwhere\s+\w+\s*:\s*)(?:\w+(?:<[^\r\n=;{]+?>)?(?:\s*,)?\s*?)+(?=\s*(?:where|[{;]|=>))/,
+			lookbehind: true,
+			inside: {
+				'keyword': keywords,
+				'class-name': {
+					pattern: /\b[A-Z]\w*(?:\.\w+)*\b/,
+					inside: {
+						'punctuation': /\./
+					}
+				},
+				'punctuation': /[<>(),.:[\]]/
+			}
+		},
+		'preprocessor': {
+			pattern: /(^\s*)#.*/m,
+			lookbehind: true,
+			alias: 'property',
+			inside: {
+				// highlight preprocessor directives as keywords
+				'directive': {
+					pattern: /(\s*#)\b(?:define|elif|else|endif|endregion|error|if|line|pragma|region|undef|warning)\b/,
+					lookbehind: true,
+					alias: 'keyword'
+				}
 			}
 		}
-	}
-});
+	});
+}(Prism));
 
 Prism.languages.dotnet = Prism.languages.csharp;
