805124cd93361162f8b67d65c4a9cdbd38574d53
RunDevelopment <mitchi5000.ms@googlemail.com>
2018-07-10 20:47:51 +0200

    New way to match generic parameters Improved type list matching New placeholder-based way to construct patterns

--- components/prism-csharp.js
+++ components/prism-csharp.js
@@ -1,11 +1,43 @@
 (function (Prism) {
 
+	/**
+	 * Replaces all placeholders "<<n>>" of given expression with the n-th replacement (zero based).
+	 *
+	 * Note: Only the flags of the expression are kept. The flags of replacements will be ignored.
+	 *
+	 * Note: This is a simple text based replacement. Be careful when using backreferences!
+	 *
+	 * @param {RegExp} regex the given expression.
+	 * @param {RegExp[]} replacements a list of replacement which can be inserted into the given expression.
+	 * @returns {RegExp} the expression with all placeholders replaced with their corresponding replacements.
+	 * @example replace(/a<<0>>a/i, /(?:b+)/) === /ab+a/i
+	 */
+	function replace(regex) {
+		var replacements = arguments;
+		return RegExp(regex.source.replace(/<<(\d+)>>/g, function (m, index) {
+			return replacements[(+index) + 1].source;
+		}), regex.toString().match(/[gimuy]*$/)[0]);
+	}
+
+
 	var keywords = /\b(?:abstract|add|alias|as|ascending|async|await|base|bool|break|byte|case|catch|char|checked|class|const|continue|decimal|default|delegate|descending|do|double|dynamic|else|enum|event|explicit|extern|finally|fixed|float|for|foreach|from|get|global|goto|group|if|implicit|in|int|interface|internal|into|is|join|let|lock|long|nameof|namespace|new|null|object|operator|orderby|out|override|params|partial|private|protected|public|readonly|ref|remove|return|sbyte|sealed|select|set|short|sizeof|stackalloc|static|string|struct|switch|this|throw|try|typeof|uint|ulong|unchecked|unsafe|ushort|using|value|var|virtual|void|volatile|where|while|yield)\b/;
 
 	var classNameInside = {
 		'keyword': keywords,
 		'punctuation': /[<>(),.:[\]]/
 	};
+
+
+	var genericParameters = /(?:<(?:[^<>]|<(?:[^<>]|<(?:[^<>]|<[^<>]+>)+>)+>)+>)/;
+
+	var className = replace(/(?:[A-Z]\w*(?:\.\w+)*<<0>>?(?:\.\w+)*)/, genericParameters);
+	var classNamePlusArray = replace(/(?:<<0>>)(?:\[\s*(?:,\s*)*\])*/, className);
+	var classNamePlusArrayOrKeyword = replace(/(?:<<0>>|[a-z]+)/, className);
+
+	var methodOrPropertyDeclaration = /(?:\w+(?:\s*<[^<>]+>)?\s*(?:[({]|=>)|this\s*\[)/;
+
+	var typeDeclarationKeywords = /(?:class|enum|interface|struct)/;
+
 
 	Prism.languages.csharp = Prism.languages.extend('clike', {
 		'keyword': keywords,
@@ -30,9 +62,9 @@
 				}
 			},
 			{
-				// Class declarations
+				// Type declarations
 				// class Foo<A, B>
-				pattern: /(\b(?:class|enum|interface|struct)\s+)[A-Z]\w*(?:\s*<[^>]+>)?/,
+				pattern: replace(/(\b<<0>>\s+)[A-Z]\w*(?:\s*<[^<>]+>)?/, typeDeclarationKeywords),
 				lookbehind: true,
 				inside: {
 					'punctuation': /[<>,]/
@@ -58,14 +90,14 @@
 				// Casts and checks via as and is.
 				// as Foo<A>, is Bar<B>
 				// (things like if(a is Foo b) is covered by variable declaration)
-				pattern: /(\b(?:is|as)\s+)[A-Z]\w*(?:\.\w+)*(?:<(?:[^>\n=;{}]+|>(?=\s*(?:\[\s*(?:,\s*)*\]\s*)*[,.>?)]))+?>)?(?:\.\w+)*(?:\[\s*(?:,\s*)*\])*/,
+				pattern: replace(/(\b(?:is|as)\s+)<<0>>/, classNamePlusArray),
 				lookbehind: true,
 				inside: classNameInside
 			},
 			{
 				// Variable, field and parameter declaration
 				// (Foo bar, Bar baz, Foo[,,] bay, Foo<Bar, FooBar<Bar>> bax)
-				pattern: /\b[A-Z]\w*(?:\.\w+)*(?:<(?:[^>\n=;{}]+|>(?=\s*(?:\[\s*(?:,\s*)*\]\s*)*[,.>?)]))+?>)?(?:\.\w+)*(?:\[\s*(?:,\s*)*\])*(?=\s+\w+(?:\s*[=,;:{)\]]|\s+in))/,
+				pattern: replace(/\b<<0>>(?=\s+\w+(?:\s*[=,;:{)\]]|\s+in))/, classNamePlusArray),
 				inside: classNameInside
 			}
 		],
@@ -82,20 +114,26 @@
 		},
 		'return-type': {
 			// Foo<Bar> ForBar(), Foo IFoo.Bar() => 0
-			pattern: /\b[A-Z]\w*(?:\.\w+)*(?:<[^\n=;{]+?>(?:\.\w+)*)?(?:\[\s*(,\s*)*\])?(?=\s+(?:[A-Z]\w*\.)?(?:\w+(?:\s*<[^>]+>)?\s*(?:[({]|=>)|this\s*\[))/,
+			pattern: replace(/\b<<0>>(?=\s+(?:<<1>>\.)?<<2>>)/, classNamePlusArray, className, methodOrPropertyDeclaration),
 			inside: classNameInside,
 			alias: 'class-name'
 		},
 		'constructor-invocation': {
 			// new List<Foo<Bar[]>> { }
-			pattern: /(\bnew\s+)[A-Z]\w*(?:\.\w+)*(?:<(?:[^>\n=;{}]+|>(?=\s*(?:\[\s*(?:,\s*)*\]\s*)*[,.>?)]))+?>)?(?:\.\w+)*(?:\[\s*(?:,\s*)*\])*(?=\s*[[({])/,
+			pattern: replace(/(\bnew\s+)<<0>>(?=\s*[[({])/, classNamePlusArray),
 			lookbehind: true,
+			inside: classNameInside,
+			alias: 'class-name'
+		},
+		'explicit-implementation': {
+			// int IFoo<Foo>.Bar => 0; void IFoo<Foo<Foo>>.Foo<T>();
+			pattern: replace(/\b<<0>>(?=\.<<1>>)/, className, methodOrPropertyDeclaration),
 			inside: classNameInside,
 			alias: 'class-name'
 		},
 		'generic-method': {
 			// foo<Bar>()
-			pattern: /\w+\s*<[^\n=;{(]+?>(?=\s*\()/,
+			pattern: replace(/\w+\s*<<0>>(?=\s*\()/, genericParameters),
 			inside: {
 				'function': /^\w+/,
 				'class-name': {
@@ -112,7 +150,7 @@
 			// The list of types inherited or of generic constraints
 			// ( class Foo<F> ) : Bar, IList<FooBar>
 			// ( where F ) : Bar, IList<int>
-			pattern: /(\b(?:class|interface|struct|enum)\s+[A-Z]\w*(?:\s*<[^>]+>)?\s*:\s*|\bwhere\s+\w+\s*:\s*)(?:\w+(?:<[^\r\n=;{]+?>)?(?:\s*,)?\s*?)+(?=\s*(?:where|[{;]|=>))/,
+			pattern: replace(/\b((?:<<1>>\s+[A-Z]\w*(?:\s*<[^<>]+>)?|where\s+\w+)\s*:\s*)<<0>>(?:\s*,\s*<<0>>)*(?=\s*(?:where|[{;]|=>|$))/, classNamePlusArrayOrKeyword, typeDeclarationKeywords),
 			lookbehind: true,
 			inside: {
 				'keyword': keywords,
