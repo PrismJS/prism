c34fc87a44e14bc112388eed1d6b543fabfdabdd
RunDevelopment <mitchi5000.ms@googlemail.com>
2018-07-13 15:34:38 +0200

    null is now a keyword

--- components/prism-java.js
+++ components/prism-java.js
@@ -1,6 +1,6 @@
 (function (Prism) {
 
-	var keywords = /\b(?:abstract|continue|for|new|switch|assert|default|goto|package|synchronized|boolean|do|if|private|this|break|double|implements|protected|throw|byte|else|import|public|throws|case|enum|instanceof|return|transient|catch|extends|int|short|try|char|final|interface|static|void|class|finally|long|strictfp|volatile|const|float|native|super|while|var)\b/;
+	var keywords = /\b(?:abstract|continue|for|new|switch|assert|default|goto|package|synchronized|boolean|do|if|private|this|break|double|implements|protected|throw|byte|else|import|public|throws|case|enum|instanceof|return|transient|catch|extends|int|short|try|char|final|interface|static|void|class|finally|long|strictfp|volatile|const|float|native|super|while|var|null)\b/;
 
 	Prism.languages.java = Prism.languages.extend('clike', {
 		'class-name': [
