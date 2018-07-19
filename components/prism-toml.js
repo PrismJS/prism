7ba14d8785f01ac3e1f7ff9bef0d86c082531102
RunDevelopment <mitchi5000.ms@googlemail.com>
2018-07-19 17:02:26 +0200

    Added TOML support

---
+++ components/prism-toml.js
@@ -0,0 +1,37 @@
+Prism.languages.toml = {
+	'comment': {
+		pattern: /#.*/,
+		greedy: true
+	},
+	'table': {
+		pattern: /(\[\s*)(?:[\w-]+|'[^'\n\r]*'|"(?:\\.|[^\r\n\\"])*")(?:\s*\.\s*(?:[\w-]+|'[^'\n\r]*'|"(?:\\.|[^\r\n\\"])*"))*(?=\s*\])/,
+		lookbehind: true,
+		greedy: true,
+		alias: 'class-name'
+	},
+	'key': {
+		pattern: /(^\s*|[{,]\s*)(?:[\w-]+|'[^'\n\r]*'|"(?:\\.|[^\r\n\\"])*")(?:\s*\.\s*(?:[\w-]+|'[^'\n\r]*'|"(?:\\.|[^\r\n\\"])*"))*(?=\s*=)/m,
+		lookbehind: true,
+		greedy: true,
+		alias: 'property'
+	},
+	'string': {
+		pattern: /"""(?:\\[\s\S]|[^\\])*?"""|'''[\s\S]*?'''|'[^'\n\r]*'|"(?:\\.|[^\n\r"\\])*"/,
+		greedy: true
+	},
+	'date': [
+		{
+			// Offset Date-Time, Local Date-Time, Local Date
+			pattern: /\d{4}-\d{2}-\d{2}(?:[T\s]\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}:\d{2})?)?/i,
+			alias: 'number'
+		},
+		{
+			// Local Time
+			pattern: /\d{2}:\d{2}:\d{2}(?:\.\d+)?/i,
+			alias: 'number'
+		}
+	],
+	'number': /(?:\b0(?:x[\da-zA-Z]+(?:_[\da-zA-Z]+)*|o[0-7]+(?:_[0-7]+)*|b[10]+(?:_[10]+)*))\b|[-+]?\d+(?:_\d+)*(?:\.\d+(?:_\d+)*)?(?:[eE][+-]?\d+)?\b|[-+]?(?:inf|nan)\b/,
+	'boolean': /\b(?:true|false)\b/,
+	'punctuation': /[.,=[\]{}]/
+};
