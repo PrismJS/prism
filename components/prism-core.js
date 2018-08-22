abc28c1bd10308d28b119675dee6aa7178edbc2e
RunDevelopment <mitchi5000.ms@googlemail.com>
2018-08-22 22:37:59 +0200

    Added doc

--- components/prism-core.js
+++ components/prism-core.js
@@ -459,10 +459,12 @@ var _ = _self.Prism = {
 	patterns: {
 
 		/**
-		 * asd
-		 * @param {string|RegExp} basePattern
-		 * @param {Object.<string, string|RegExp>} replacements
-		 * @returns {RegExp}
+		 * Replaces all placeholders (`<<name>>`) in `basePattern` with `replacements[name]`.
+		 * @param {string|RegExp} basePattern the pattern in which all placeholder are to be replaced. Placeholders are
+		 * not allowed to be inside character sets and cannot be preceded by a unescaped backslash.
+		 * @param {Object.<string, string|RegExp>} replacements the name-replacement-pairs. Replacements are not allowed
+		 * to contain backreferences and capturing groups.
+		 * @returns {RegExp} the base pattern with all placeholders replaced.
 		 */
 		build: function build(basePattern, replacements) {
 			var placeholder = /<<([\w-]+)>>/g;
