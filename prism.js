d33d259c5599813f1d45fe4b1d51c09b76fffef7
Michael Schmidt <mitchi5000.ms@googlemail.com>
2018-12-02 00:02:57 +0100

    Ignore duplicates in insertBefore (#1628)
    Fix #1525 and implement option number 2.

--- prism.js
+++ prism.js
@@ -101,7 +101,7 @@ var _ = _self.Prism = {
 		/**
 		 * Insert a token before another token in a language literal
 		 * As this needs to recreate the object (we cannot actually insert before keys in object literals),
-		 * we cannot just provide an object, we need anobject and a key.
+		 * we cannot just provide an object, we need an object and a key.
 		 * @param inside The key (or language id) of the parent
 		 * @param before The key to insert before.
 		 * @param insert Object with the key/value pairs to insert
@@ -113,20 +113,20 @@ var _ = _self.Prism = {
 			var ret = {};
 
 			for (var token in grammar) {
-
 				if (grammar.hasOwnProperty(token)) {
 
 					if (token == before) {
-
 						for (var newToken in insert) {
-
 							if (insert.hasOwnProperty(newToken)) {
 								ret[newToken] = insert[newToken];
 							}
 						}
 					}
 
-					ret[token] = grammar[token];
+					// Do not insert token which also occur in insert. See #1525
+					if (!insert.hasOwnProperty(token)) {
+						ret[token] = grammar[token];
+					}
 				}
 			}
 
