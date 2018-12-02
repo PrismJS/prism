686abbf1ec558d64c624c68a5e5eae930eeda2fb
Michael Schmidt <mitchi5000.ms@googlemail.com>
2018-12-02 14:44:14 +0100

    PHPDoc improvements

--- components/prism-phpdoc.js
+++ components/prism-phpdoc.js
@@ -4,7 +4,7 @@
 
 	Prism.languages.phpdoc = Prism.languages.extend('javadoclike', {
 		'parameter': {
-			pattern: RegExp('(' + /@(?:global|param|property(?:-read|-write)?|var)\s+/.source + '(?:' + typeExpression + '\\s+)?)' + /\$\w+/.source),
+			pattern: RegExp('(@(?:global|param|property(?:-read|-write)?|var)\\s+(?:' + typeExpression + '\\s+)?)\\$\\w+'),
 			lookbehind: true
 		}
 	});
@@ -12,7 +12,7 @@
 	Prism.languages.insertBefore('phpdoc', 'keyword', {
 		'class-name': [
 			{
-				pattern: RegExp(/(@(?:global|package|param|property(?:-read|-write)?|return|subpackage|throws|var)\s+)/.source + typeExpression),
+				pattern: RegExp('(@(?:global|package|param|property(?:-read|-write)?|return|subpackage|throws|var)\\s+)' + typeExpression),
 				lookbehind: true,
 				inside: {
 					'keyword': /\b(?:callback|resource|boolean|integer|double|object|string|array|false|float|mixed|bool|null|self|true|void|int)\b/,
