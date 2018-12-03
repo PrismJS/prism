5fcee96675103f16fc4668501aee502194d449a2
Rex Zeng <rex@rexskz.info>
2018-12-03 20:55:22 +0800

    Identify CSS units and variables (#1450)
    Assume CSS unit is a word or `%` after a number, CSS variable is a word inside `var()`.
    
    `%` is not recognized as a part of number, but the leading `-` is.
    
    When using minus operator in `calc` function, we must type a space in both sides of `-`
    (value like `calc(100%-5px)` is wrong), so if we met a pattern like `-[\d.]` in a CSS value
    (not in selector, rule or variable), it must be the start of a negative number.

--- components/prism-css-extras.js
+++ components/prism-css-extras.js
@@ -10,7 +10,19 @@ Prism.languages.css.selector = {
 };
 
 Prism.languages.insertBefore('css', 'function', {
+	'variable': {
+		pattern: /(var\()[^)]+(?=\))/,
+		lookbehind: true
+	},
+	'operator': {
+		pattern: /(\s)[+\-*\/](?=\s)/,
+		lookbehind: true
+	},
 	'hexcode': /#[\da-f]{3,8}/i,
 	'entity': /\\[\da-f]{1,8}/i,
-	'number': /[\d%.]+/
+	'unit': {
+		pattern: /(\d)(?:%|[a-z]+)/,
+		lookbehind: true
+	},
+	'number': /-?[\d.]+/
 });
