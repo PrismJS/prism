f8b2249d33e67db171fad12e634c2b7f74953a43
Outsider <outsideris@gmail.com>
2018-12-17 04:10:50 +0900

    apply code reviews
    Signed-off-by: Outsider <outsideris@gmail.com>

--- components/prism-hcl.js
+++ components/prism-hcl.js
@@ -2,73 +2,64 @@ Prism.languages.hcl = {
 	'comment': /(?:\/\/|#).*|\/\*[\s\S]*?(?:\*\/|$)/,
 	'keyword': [
 		{
-			pattern: /(resource|data)\s+([\w-]+|"[\w-]+")(?=\s+"[\w-]+"\s+{)/i,
+			pattern: /(?:resource|data)\s+(?:[\w-]+|"[\w-]+")(?=\s+"[\w-]+"\s+{)/i,
 			inside: {
 				'type': [
 					{
-						pattern: /(resource|data|\s+)([\w-]+|"[\w-]+")/i,
+						pattern: /(resource|data|\s+)(?:[\w-]+|"[\w-]+")/i,
 						lookbehind: true,
 						alias: 'variable'
 					}
-                ]
+				]
 			}
 		},
 		{
-			pattern: /(provider|provisioner|variable|output|module|backend)\s+(")?([\w-]+)(")?\s+(?={)/i,
+			pattern: /(?:provider|provisioner|variable|output|module|backend)\s+"?[\w-]+"?\s+(?={)/i,
 			inside: {
 				'type': [
 					{
-						pattern: /(provider|provisioner|variable|output|module|backend)\s+(")?([\w-]+)(")?\s+/i,
+						pattern: /(provider|provisioner|variable|output|module|backend)\s+"?[\w-]+"?\s+/i,
 						lookbehind: true,
 						alias: 'variable'
 					}
-                ]
+				]
 			}
 		},
 		{
-			pattern: /([\w-]+)(?=\s+{)/
+			pattern: /[\w-]+(?=\s+{)/
 		}
 	],
 	'interpolation': [
 		{
-			pattern: /\${([\w[\](+)\-*%^"',?=:/\s]+\.?)+}/i,
+			pattern: /\${(?:[\w[\](+)\-*%^"',?=:/\s\\]+\.?)+}/i,
 			inside: {
 				'type': [
 					{
-						pattern: /((terraform|var|self|count|module|path|data|local)\.)[\w\*]+/i,
+						pattern: /((?:terraform|var|self|count|module|path|data|local)\.)[\w\*]+/i,
 						lookbehind: true,
 						alias: 'variable'
 					}
 				],
-				'keyword': /(terraform|var|self|count|module|path|data|local)/i,
-				'operator': /\$\{|[}.]/,
-				'function': [
-					{
-						pattern: /[\w]+(?=\()/,
-						alias: 'variable'
-					}
-				],
-				'string': /".*"/,
-				'punctuation': /[!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~]/,
+				'keyword': /(?:terraform|var|self|count|module|path|data|local)/i,
+				'function': /\w+(?=\()/,
+				'string': /"(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*"/,
+				'punctuation': /[!"\$#%&'()*+,.\/;<=>@\[\\\]^`{|}~]/,
 				'number': /-?\d+\.?\d*/,
 				'others': [
 					{
 						pattern: /[\w\*]+/,
 						alias: 'string'
 					}
-				],
-			},
+				]
+			}
 		}
 	],
 	'property': [
-		{
-			pattern: /[\w-\.]+(?=\s*=)/i,
-		},
-		{
-			pattern: /"[\w-\.]+"(?=\s*:)/i
-		}
+		/[^\\\r\n]+(?=\s*=)/,
+		/"[^\\\r\n]+"(?=\s*:)/
 	],
-	'string': /"(?:[^\n\r\\"]|\\.)*"/,
-	'number': /(?:\d+e[+-]?\d+|0[0-7]+|0x[\da-f]+|\d+\.?\d*)/i,
-	'boolean': /\b(?:true|false)\b/i
+	'string': /"(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*"/,
+	'number': /(?:\d+.?\d*e[+-]?\d+|0[0-7]+|0x[\da-f]+|\d+\.?\d*)/i,
+	'boolean': /\b(?:true|false)\b/i,
+	'punctuation': /[=\[\]]/,
 };
