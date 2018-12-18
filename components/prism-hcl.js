65d5c83d575286c6cffbcd43f08e8b9f6d74ae62
Outsider <outsideris@gmail.com>
2018-12-19 00:36:56 +0900

    remove one item array and fix capturing group unused
    Signed-off-by: Outsider <outsideris@gmail.com>

--- components/prism-hcl.js
+++ components/prism-hcl.js
@@ -4,62 +4,52 @@ Prism.languages.hcl = {
 		{
 			pattern: /(?:resource|data)\s+(?:[\w-]+|"[\w-]+")(?=\s+"[\w-]+"\s+{)/i,
 			inside: {
-				'type': [
-					{
-						pattern: /(resource|data|\s+)(?:[\w-]+|"[\w-]+")/i,
-						lookbehind: true,
-						alias: 'variable'
-					}
-				]
+				'type': {
+					pattern: /(resource|data|\s+)(?:[\w-]+|"[\w-]+")/i,
+					lookbehind: true,
+					alias: 'variable'
+				}
 			}
 		},
 		{
 			pattern: /(?:provider|provisioner|variable|output|module|backend)\s+"?[\w-]+"?\s+(?={)/i,
 			inside: {
-				'type': [
-					{
-						pattern: /(provider|provisioner|variable|output|module|backend)\s+"?[\w-]+"?\s+/i,
-						lookbehind: true,
-						alias: 'variable'
-					}
-				]
+				'type': {
+					pattern: /(provider|provisioner|variable|output|module|backend)\s+"?[\w-]+"?\s+/i,
+					lookbehind: true,
+					alias: 'variable'
+				}
 			}
 		},
 		{
 			pattern: /[\w-]+(?=\s+{)/
 		}
 	],
-	'interpolation': [
-		{
-			pattern: /\${(?:[\w[\](+)\-*%^"',?=:/\s\\]+\.?)+}/i,
-			inside: {
-				'type': [
-					{
-						pattern: /((?:terraform|var|self|count|module|path|data|local)\.)[\w\*]+/i,
-						lookbehind: true,
-						alias: 'variable'
-					}
-				],
-				'keyword': /terraform|var|self|count|module|path|data|local/i,
-				'function': /\w+(?=\()/,
-				'string': /"(?:\\[\s\S]|[^\\"])*"/,
-				'punctuation': /[!"\$#%&'()*+,.\/;<=>@\[\\\]^`{|}~]/,
-				'number': /-?\d+\.?\d*/,
-				'others': [
-					{
-						pattern: /[\w\*]+/,
-						alias: 'string'
-					}
-				]
+	'interpolation': {
+		pattern: /\${(?:[\w[\](+)\-*%^"',?=:/\s\\]+\.?)+}/i,
+		inside: {
+			'type': {
+				pattern: /((?:terraform|var|self|count|module|path|data|local)\.)[\w\*]+/i,
+				lookbehind: true,
+				alias: 'variable'
+			},
+			'keyword': /terraform|var|self|count|module|path|data|local/i,
+			'function': /\w+(?=\()/,
+			'string': /"(?:\\[\s\S]|[^\\"])*"/,
+			'punctuation': /[!"\$#%&'()*+,.\/;<=>@\[\\\]^`{|}~]/,
+			'number': /-?\d+\.?\d*/,
+			'others': {
+				pattern: /[\w\*]+/,
+				alias: 'string'
 			}
 		}
-	],
+	},
 	'property': [
 		/[\w-\.]+(?=\s*=)/,
 		/"(?:\\[\s\S]|[^\\"])+"(?=\s*[:=])/,
 	],
 	'string': /"(?:\\[\s\S]|[^\\"])*"/,
 	'number': /0x[\da-f]+|\d+\.?\d*(?:e[+-]?\d+)?/i,
-	'boolean': /\b(true|false)\b/i,
+	'boolean': /\b(?:true|false)\b/i,
 	'punctuation': /[=\[\]{}]/,
 };
