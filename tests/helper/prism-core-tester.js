fde47f2780975d7a42a24ad08f99f8a1b4e9a62c
Michael Schmidt <mitchi5000.ms@googlemail.com>
2019-01-06 12:35:20 +0100

    Improved doc & flag testing

--- tests/helper/prism-core-tester.js
+++ tests/helper/prism-core-tester.js
@@ -8,11 +8,11 @@
 	 * 2. Placeholders cannot be inside character sets. E.g. `foo[<<bar>>]`.
 	 *
 	 * `replacements`:
-	 * 1. There has to a replacement for each placeholder. E.g. `build(/<<bar>>/, {})`.
-	 * 2. A replacement's flags without the `g` flag have to be a subset of the flags of `basePattern`.
+	 * 1. There has to be a replacement for each placeholder. E.g. `build(/<<bar>>/, {})`.
+	 * 2. A replacement's flags (without the `g` and `y` flag) have to be a subset of the flags of `basePattern`.
 	 *    E.g. `build(/foo<<0>>/, [ /[a-z]/i ])`
 	 * 3. A replacement cannot contain backreferences. E.g. `build(/(foo)\1<<0>>/, [ /(bar)\1/ ])`
-	 * 3. A replacement cannot contain capturing groups. E.g. `build(/<<0>>(foo)\1/, [ /(bar)\1/ ])`
+	 * 4. A replacement cannot contain capturing groups. E.g. `build(/<<0>>(foo)\1/, [ /(bar)\1/ ])`
 	 *
 	 * __Note:__ All strings are assumed to represent patterns without flags.
 	 *
@@ -23,6 +23,21 @@
 	 * @param {string} flags The flags of `basePattern`.
 	 */
 	Prism.patterns.build.test = function buildTest(basePattern, replacements, placeholder, source, flags) {
+		/**
+		 * Sorts and filters the given flags, so that only the ones relevant for the pattern remain.
+		 *
+		 * @param {string} flags
+		 * @returns {string}
+		 */
+		function normalizeFlags(flags) {
+			if (!flags) {
+				return '';
+			}
+			return flags.split('').filter(f => 'gy'.indexOf(f) === -1).sort().join('');
+		}
+
+		flags = normalizeFlags(flags);
+
 
 		// test the base pattern
 
@@ -67,15 +82,15 @@
 			if (repFlags === undefined) {
 				repFlags = replacement.exec ? replacement.toString().match(/[igmuy]*$/)[0] : '';
 			}
+			repFlags = normalizeFlags(repFlags);
 
-			// the replacement's flags have to be a subset of the base pattern's ones. (excluding g)
-			for (var i = repFlags.length - 1; i >= 0; i++) {
-				var f = repFlags[i];
-				if (f !== 'g' && flags.indexOf(f) < 0) {
+			// the replacement's flags have to be a subset of the base pattern's ones.
+			repFlags.split('').forEach(function (f) {
+				if (flags.indexOf(f) < 0) {
 					throw new Error('The ' + f + ' flag is present in replacement "' + name +
 						'" but in its base pattern ' + basePattern);
 				}
-			}
+			});
 
 			// source
 			replacement = '' + (replacement.source || replacement);
