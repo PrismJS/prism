ceb190cebf21907723deeb89f3cfcae10573a995
Stephan Max <hello@stephanmax.com>
2019-01-15 12:49:57 +0100

    Added HDL (Hardware Description Language) support

---
+++ components/prism-hdl.js
@@ -0,0 +1,18 @@
+Prism.languages.hdl = {
+	'comment': [
+		{
+			pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
+			lookbehind: true
+		},
+		{
+			pattern: /(^|[^\\:])\/\/.*/,
+			lookbehind: true,
+			greedy: true
+		}
+	],
+	'keyword': /\b(?:CHIP|IN|OUT|PARTS|BUILTIN|CLOCKED)\b/,
+	'boolean': /\b(?:true|false)\b/,
+	'number': /\b\d+\b/i,
+	'operator': /=/,
+	'punctuation': /[{}[\];(),.:]/
+};
