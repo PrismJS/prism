03cf85b024244137776e05129d9072127cf4a2b6
Michael Schmidt <mitchi5000.ms@googlemail.com>
2019-05-18 20:56:47 +0200

    Added support for nested bold and italic tokens

--- components/prism-markdown.js
+++ components/prism-markdown.js
@@ -1,227 +1,250 @@
-Prism.languages.markdown = Prism.languages.extend('markup', {});
-Prism.languages.insertBefore('markdown', 'prolog', {
-	'blockquote': {
-		// > ...
-		pattern: /^>(?:[\t ]*>)*/m,
-		alias: 'punctuation'
-	},
-	'code': [
-		{
-			// Prefixed by 4 spaces or 1 tab
-			pattern: /^(?: {4}|\t).+/m,
-			alias: 'keyword'
+(function (Prism) {
+
+	// Allow only one line break
+	var inner = /\\.|[^\\\n\r_]|(?:\r?\n|\r)(?!\r?\n|\r)/.source;
+
+	// both bold and italic allow one nested instance of the other
+	var bold_ = /__(?:<inner>|_(?:<inner>)+_)+__/.source.replace(/<inner>/g, inner);
+	var italic_ = /_(?:<inner>|__(?:<inner>)+__)+_/.source.replace(/<inner>/g, inner);
+
+	Prism.languages.markdown = Prism.languages.extend('markup', {});
+	Prism.languages.insertBefore('markdown', 'prolog', {
+		'blockquote': {
+			// > ...
+			pattern: /^>(?:[\t ]*>)*/m,
+			alias: 'punctuation'
 		},
-		{
-			// `code`
-			// ``code``
-			pattern: /``.+?``|`[^`\n]+`/,
-			alias: 'keyword'
+		'code': [
+			{
+				// Prefixed by 4 spaces or 1 tab
+				pattern: /^(?: {4}|\t).+/m,
+				alias: 'keyword'
+			},
+			{
+				// `code`
+				// ``code``
+				pattern: /``.+?``|`[^`\n]+`/,
+				alias: 'keyword'
+			},
+			{
+				// ```optional language
+				// code block
+				// ```
+				pattern: /^```[\s\S]*?^```$/m,
+				greedy: true,
+				inside: {
+					'code-block': {
+						pattern: /^(```.*(?:\r?\n|\r))[\s\S]+?(?=(?:\r?\n|\r)^```$)/m,
+						lookbehind: true
+					},
+					'code-language': {
+						pattern: /^(```).+/,
+						lookbehind: true
+					},
+					'punctuation': /```/
+				}
+			}
+		],
+		'title': [
+			{
+				// title 1
+				// =======
+
+				// title 2
+				// -------
+				pattern: /\S.*(?:\r?\n|\r)(?:==+|--+)/,
+				alias: 'important',
+				inside: {
+					punctuation: /==+$|--+$/
+				}
+			},
+			{
+				// # title 1
+				// ###### title 6
+				pattern: /(^\s*)#+.+/m,
+				lookbehind: true,
+				alias: 'important',
+				inside: {
+					punctuation: /^#+|#+$/
+				}
+			}
+		],
+		'hr': {
+			// ***
+			// ---
+			// * * *
+			// -----------
+			pattern: /(^\s*)([*-])(?:[\t ]*\2){2,}(?=\s*$)/m,
+			lookbehind: true,
+			alias: 'punctuation'
 		},
-		{
-			// ```optional language
-			// code block
-			// ```
-			pattern: /^```[\s\S]*?^```$/m,
+		'list': {
+			// * item
+			// + item
+			// - item
+			// 1. item
+			pattern: /(^\s*)(?:[*+-]|\d+\.)(?=[\t ].)/m,
+			lookbehind: true,
+			alias: 'punctuation'
+		},
+		'url-reference': {
+			// [id]: http://example.com "Optional title"
+			// [id]: http://example.com 'Optional title'
+			// [id]: http://example.com (Optional title)
+			// [id]: <http://example.com> "Optional title"
+			pattern: /!?\[[^\]]+\]:[\t ]+(?:\S+|<(?:\\.|[^>\\])+>)(?:[\t ]+(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\)))?/,
+			inside: {
+				'variable': {
+					pattern: /^(!?\[)[^\]]+/,
+					lookbehind: true
+				},
+				'string': /(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\))$/,
+				'punctuation': /^[\[\]!:]|[<>]/
+			},
+			alias: 'url'
+		},
+		'bold': {
+			// **strong**
+			// __strong__
+
+			pattern: RegExp('((?:^|[^\\\\])(?:\\\\{2})*)(?:' + bold_ + '|' + bold_.replace(/_/g, '\\*') + ')'),
+			lookbehind: true,
 			greedy: true,
 			inside: {
-				'code-block': {
-					pattern: /^(```.*(?:\r?\n|\r))[\s\S]+?(?=(?:\r?\n|\r)^```$)/m,
-					lookbehind: true
+				'content': {
+					pattern: /(^..)[\s\S]+(?=..$)/,
+					lookbehind: true,
+					inside: {} // see below
 				},
-				'code-language': {
-					pattern: /^(```).+/,
-					lookbehind: true
-				},
-				'punctuation': /```/
-			}
-		}
-	],
-	'title': [
-		{
-			// title 1
-			// =======
-
-			// title 2
-			// -------
-			pattern: /\S.*(?:\r?\n|\r)(?:==+|--+)/,
-			alias: 'important',
-			inside: {
-				punctuation: /==+$|--+$/
+				'punctuation': /\*\*|__/
 			}
 		},
-		{
-			// # title 1
-			// ###### title 6
-			pattern: /(^\s*)#+.+/m,
+		'italic': {
+			// *em*
+			// _em_
+
+			pattern: RegExp('((?:^|[^\\\\])(?:\\\\{2})*)(?:' + italic_ + '|' + italic_.replace(/_/g, '\\*') + ')'),
 			lookbehind: true,
-			alias: 'important',
+			greedy: true,
 			inside: {
-				punctuation: /^#+|#+$/
+				'content': {
+					pattern: /(^.)[\s\S]+(?=.$)/,
+					lookbehind: true,
+					inside: {} // see below
+				},
+				'punctuation': /[*_]/
 			}
-		}
-	],
-	'hr': {
-		// ***
-		// ---
-		// * * *
-		// -----------
-		pattern: /(^\s*)([*-])(?:[\t ]*\2){2,}(?=\s*$)/m,
-		lookbehind: true,
-		alias: 'punctuation'
-	},
-	'list': {
-		// * item
-		// + item
-		// - item
-		// 1. item
-		pattern: /(^\s*)(?:[*+-]|\d+\.)(?=[\t ].)/m,
-		lookbehind: true,
-		alias: 'punctuation'
-	},
-	'url-reference': {
-		// [id]: http://example.com "Optional title"
-		// [id]: http://example.com 'Optional title'
-		// [id]: http://example.com (Optional title)
-		// [id]: <http://example.com> "Optional title"
-		pattern: /!?\[[^\]]+\]:[\t ]+(?:\S+|<(?:\\.|[^>\\])+>)(?:[\t ]+(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\)))?/,
-		inside: {
-			'variable': {
-				pattern: /^(!?\[)[^\]]+/,
-				lookbehind: true
-			},
-			'string': /(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\))$/,
-			'punctuation': /^[\[\]!:]|[<>]/
 		},
-		alias: 'url'
-	},
-	'bold': {
-		// **strong**
-		// __strong__
+		'strike': {
+			// ~~strike through~~
+			// ~strike~
 
-		// Allow only one line break
-		pattern: /(^|[^\\])(\*\*|__)(?:(?:\r?\n|\r)(?!\r?\n|\r)|.)+?\2/,
-		lookbehind: true,
-		greedy: true,
-		inside: {
-			'punctuation': /^\*\*|^__|\*\*$|__$/
-		}
-	},
-	'italic': {
-		// *em*
-		// _em_
-
-		// Allow only one line break
-		pattern: /(^|[^\\])([*_])(?:(?:\r?\n|\r)(?!\r?\n|\r)|.)+?\2/,
-		lookbehind: true,
-		greedy: true,
-		inside: {
-			'punctuation': /^[*_]|[*_]$/
-		}
-	},
-	'strike': {
-		// ~~strike through~~
-		// ~strike~
-
-		// Allow only one line break
-		pattern: /(^|[^\\])(~~?)(?:(?:\r?\n|\r)(?!\r?\n|\r)|.)+?\2/,
-		lookbehind: true,
-		greedy: true,
-		inside: {
-			'punctuation': /^~~?|~~?$/
-		}
-	},
-	'url': {
-		// [example](http://example.com "Optional title")
-		// [example] [id]
-		pattern: /!?\[[^\]]+\](?:\([^\s)]+(?:[\t ]+"(?:\\.|[^"\\])*")?\)| ?\[[^\]\n]*\])/,
-		inside: {
-			'variable': {
-				pattern: /(!?\[)[^\]]+(?=\]$)/,
-				lookbehind: true
-			},
-			'string': {
-				pattern: /"(?:\\.|[^"\\])*"(?=\)$)/
+			pattern: RegExp(/((?:^|[^\\])(?:\\{2})*)(~~?)(?:<inner>|_)+?\2/.source.replace(/<inner>/g, inner)),
+			lookbehind: true,
+			greedy: true,
+			inside: {
+				'content': {
+					pattern: /(^~~?)[\s\S]+(?=\1$)/,
+					lookbehind: true,
+					inside: {} // see below
+				},
+				'punctuation': /~~?/
 			}
-		}
-	}
-});
-
-['bold', 'italic', 'strike'].forEach(function (token) {
-	['url', 'bold', 'italic', 'strike'].forEach(function (inside) {
-		if (token !== inside) {
-			Prism.languages.markdown[token].inside[inside] = Prism.languages.markdown[inside];
-		}
-	});
-});
-
-Prism.hooks.add('after-tokenize', function (env) {
-	if (env.language !== 'markdown' && env.language !== 'md') {
-		return;
-	}
-
-	function walkTokens(tokens) {
-		if (!tokens || typeof tokens === 'string') {
-			return;
-		}
-
-		for (var i = 0, l = tokens.length; i < l; i++) {
-			var token = tokens[i];
-
-			if (token.type !== 'code') {
-				walkTokens(token.content);
-				continue;
-			}
-
-			var codeLang = token.content[1];
-			var codeBlock = token.content[3];
-
-			if (codeLang && codeBlock &&
-				codeLang.type === 'code-language' && codeBlock.type === 'code-block' &&
-				typeof codeLang.content === 'string') {
-
-				// this might be a language that Prism does not support
-				var alias = 'language-' + codeLang.content.trim().split(/\s+/)[0].toLowerCase();
-
-				// add alias
-				if (!codeBlock.alias) {
-					codeBlock.alias = [alias];
-				} else if (typeof codeBlock.alias === 'string') {
-					codeBlock.alias = [codeBlock.alias, alias];
-				} else {
-					codeBlock.alias.push(alias);
+		},
+		'url': {
+			// [example](http://example.com "Optional title")
+			// [example] [id]
+			pattern: /!?\[[^\]]+\](?:\([^\s)]+(?:[\t ]+"(?:\\.|[^"\\])*")?\)| ?\[[^\]\n]*\])/,
+			inside: {
+				'variable': {
+					pattern: /(!?\[)[^\]]+(?=\]$)/,
+					lookbehind: true
+				},
+				'string': {
+					pattern: /"(?:\\.|[^"\\])*"(?=\)$)/
 				}
 			}
 		}
-	}
+	});
 
-	walkTokens(env.tokens);
-});
+	['bold', 'italic', 'strike'].forEach(function (token) {
+		['url', 'bold', 'italic', 'strike'].forEach(function (inside) {
+			if (token !== inside) {
+				Prism.languages.markdown[token].inside.content.inside[inside] = Prism.languages.markdown[inside];
+			}
+		});
+	});
 
-Prism.hooks.add('wrap', function (env) {
-	if (env.type !== 'code-block') {
-		return;
-	}
-
-	var codeLang = '';
-	for (var i = 0, l = env.classes.length; i < l; i++) {
-		var cls = env.classes[i];
-		var match = /language-(.+)/.exec(cls);
-		if (match) {
-			codeLang = match[1];
-			break;
+	Prism.hooks.add('after-tokenize', function (env) {
+		if (env.language !== 'markdown' && env.language !== 'md') {
+			return;
 		}
-	}
 
-	var grammar = Prism.languages[codeLang];
+		function walkTokens(tokens) {
+			if (!tokens || typeof tokens === 'string') {
+				return;
+			}
 
-	if (!grammar) {
-		return;
-	}
+			for (var i = 0, l = tokens.length; i < l; i++) {
+				var token = tokens[i];
 
-	// reverse Prism.util.encode
-	var code = env.content.replace(/&lt;/g, '<').replace(/&amp;/g, '&');
+				if (token.type !== 'code') {
+					walkTokens(token.content);
+					continue;
+				}
 
-	env.content = Prism.highlight(code, grammar, codeLang);
-});
+				var codeLang = token.content[1];
+				var codeBlock = token.content[3];
 
-Prism.languages.md = Prism.languages.markdown;
+				if (codeLang && codeBlock &&
+					codeLang.type === 'code-language' && codeBlock.type === 'code-block' &&
+					typeof codeLang.content === 'string') {
+
+					// this might be a language that Prism does not support
+					var alias = 'language-' + codeLang.content.trim().split(/\s+/)[0].toLowerCase();
+
+					// add alias
+					if (!codeBlock.alias) {
+						codeBlock.alias = [alias];
+					} else if (typeof codeBlock.alias === 'string') {
+						codeBlock.alias = [codeBlock.alias, alias];
+					} else {
+						codeBlock.alias.push(alias);
+					}
+				}
+			}
+		}
+
+		walkTokens(env.tokens);
+	});
+
+	Prism.hooks.add('wrap', function (env) {
+		if (env.type !== 'code-block') {
+			return;
+		}
+
+		var codeLang = '';
+		for (var i = 0, l = env.classes.length; i < l; i++) {
+			var cls = env.classes[i];
+			var match = /language-(.+)/.exec(cls);
+			if (match) {
+				codeLang = match[1];
+				break;
+			}
+		}
+
+		var grammar = Prism.languages[codeLang];
+
+		if (!grammar) {
+			return;
+		}
+
+		// reverse Prism.util.encode
+		var code = env.content.replace(/&lt;/g, '<').replace(/&amp;/g, '&');
+
+		env.content = Prism.highlight(code, grammar, codeLang);
+	});
+
+	Prism.languages.md = Prism.languages.markdown;
+
+}(Prism));
