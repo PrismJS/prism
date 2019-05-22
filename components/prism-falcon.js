18ab000aa8fcbb2f59c409165d21bf94c29b3f48
QFish <qfish.cn@gmail.com>
2019-05-22 22:56:32 +0800

    Update components/prism-falcon.js
    Co-Authored-By: Michael Schmidt <mitchi5000.ms@googlemail.com>

--- components/prism-falcon.js
+++ components/prism-falcon.js
@@ -8,7 +8,7 @@ Prism.languages.falcon = Prism.languages.extend('javascript', {
 	'builtin': /\b(?:get|post|put|delete|head|byte|int|int8|int16|int32|int64|uint|uint8|uint64|int_t|int8_t|int16_t|int32_t|int64_t|uint_t|uint8_t|uint64_t|long|Long|float|Float|double|Double|boolean|Boolean|bool|Bool|string|String|binary|string|Function|any|number|boolean|Array|symbol|console|Promise|unknown|never)\b/,
 });
 
-Prism.languages.falcon['class-name'][0].pattern = /(\b(?:enum|struct|class|interface|extends|implements|instanceof|new)\s+)[\w.\\]+/;
+Prism.languages.falcon['class-name'][0].pattern = /(\b(?:class|enum|extends|implements|instanceof|interface|new|struct)\s+)[\w.\\]+/;
 
 Prism.languages.falcon['keyword'].push({
 	pattern: /(^|[^.])\b(?:enum|struct)\b/,
