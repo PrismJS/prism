ae16e58faef6c38ce971ded3c92007d7a3eaaeca
RunDevelopment <mitchi5000.ms@googlemail.com>
2019-07-11 16:33:01 +0200

    Unescaped markup tests + refactoring

--- plugins/unescaped-markup/prism-unescaped-markup.js
+++ plugins/unescaped-markup/prism-unescaped-markup.js
@@ -1,44 +1,45 @@
 (function () {
 
-	if (typeof self === 'undefined' || !self.Prism || !self.document || !Prism.languages.markup) {
+	if (typeof self === 'undefined' || !self.Prism || !self.document) {
 		return;
 	}
 
 	Prism.plugins.UnescapedMarkup = true;
 
 	Prism.hooks.add('before-highlightall', function (env) {
-		env.selector += ", [class*='lang-'] script[type='text/plain'], [class*='language-'] script[type='text/plain']" +
-		                ", script[type='text/plain'][class*='lang-'], script[type='text/plain'][class*='language-']";
+		env.selector += ', [class*="lang-"] script[type="text/plain"], [class*="language-"] script[type="text/plain"]' +
+			', script[type="text/plain"][class*="lang-"], script[type="text/plain"][class*="language-"]';
 	});
 
 	Prism.hooks.add('before-sanity-check', function (env) {
-		if ((env.element.matches || env.element.msMatchesSelector).call(env.element, "script[type='text/plain']")) {
-			var code = document.createElement("code");
-			var pre = document.createElement("pre");
+		/** @type {HTMLElement} */
+		var element = env.element;
 
-			pre.className = code.className = env.element.className;
+		if ((element.matches || element.msMatchesSelector).call(element, 'script[type="text/plain"]')) {
+			var code = document.createElement('code');
+			var pre = document.createElement('pre');
 
-			if (env.element.dataset) {
-				Object.keys(env.element.dataset).forEach(function (key) {
-					if (Object.prototype.hasOwnProperty.call(env.element.dataset, key)) {
-						pre.dataset[key] = env.element.dataset[key];
+			pre.className = code.className = element.className;
+
+			if (element.dataset) {
+				Object.keys(element.dataset).forEach(function (key) {
+					if (Object.prototype.hasOwnProperty.call(element.dataset, key)) {
+						pre.dataset[key] = element.dataset[key];
 					}
 				});
 			}
 
-			env.code = env.code.replace(/&lt;\/script(>|&gt;)/gi, "</scri" + "pt>");
-			code.textContent = env.code;
+			code.textContent = env.code = env.code.replace(/&lt;\/script(>|&gt;)/gi, '</scri' + 'pt>');
 
 			pre.appendChild(code);
-			env.element.parentNode.replaceChild(pre, env.element);
+			element.parentNode.replaceChild(pre, element);
 			env.element = code;
-			return;
+
+		} else if (!env.code && (element.matches || element.msMatchesSelector).call(element, 'pre > code') &&
+			element.firstChild && element.firstChild.nodeName == '#comment') {
+			// <pre><code><!-- actual code --></code></pre>
+			element.textContent = env.code = element.firstChild.textContent;
 		}
 
-		var pre = env.element.parentNode;
-		if (!env.code && pre && pre.nodeName.toLowerCase() == 'pre' &&
-				env.element.childNodes.length && env.element.childNodes[0].nodeName == "#comment") {
-			env.element.textContent = env.code = env.element.childNodes[0].textContent;
-		}
 	});
 }());
