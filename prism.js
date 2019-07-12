3bf52c01c8614d88548ddc178b9c00961f98ca84
RunDevelopment <mitchi5000.ms@googlemail.com>
2019-07-13 00:38:15 +0200

    Fixed build

--- prism.js
+++ prism.js
@@ -869,137 +869,107 @@ Prism.languages.js = Prism.languages.javascript;
 ********************************************** */
 
 (function () {
-	if (typeof self === 'undefined' || !self.Prism || !self.document) {
+	if (typeof self === 'undefined' || !self.Prism || !self.document || !document.querySelector) {
 		return;
 	}
 
-	var Prism = window.Prism;
+	/**
+	 * @param {Element} [container=document]
+	 */
+	self.Prism.fileHighlight = function(container) {
+		container = container || document;
 
-	var LOADING_MESSAGE = 'Loading…';
-	var FAILURE_MESSAGE = '✖ Error {status} while fetching file: {message}';
-	var FAILURE_EMPTY_MESSAGE = '✖ Error: File does not exist or is empty';
+		var Extensions = {
+			'js': 'javascript',
+			'py': 'python',
+			'rb': 'ruby',
+			'ps1': 'powershell',
+			'psm1': 'powershell',
+			'sh': 'bash',
+			'bat': 'batch',
+			'h': 'c',
+			'tex': 'latex'
+		};
 
-	var EXTENSIONS = {
-		'js': 'javascript',
-		'py': 'python',
-		'rb': 'ruby',
-		'ps1': 'powershell',
-		'psm1': 'powershell',
-		'sh': 'bash',
-		'bat': 'batch',
-		'h': 'c',
-		'tex': 'latex'
-	};
-
-	var ATTR_LOADING = 'data-src-loading';
-	var ATTR_LOADED = 'data-src-loaded';
-	var ATTR_FAILED = 'data-src-failed';
-	var SELECTOR = 'pre[data-src]:not([' + ATTR_LOADED + ']):not([' + ATTR_LOADING + ']):empty';
-
-	var lang = /\blang(?:uage)?-([\w-]+)\b/i;
-
-
-	Prism.hooks.add('before-highlightall', function (env) {
-		env.selector += ', ' + SELECTOR;
-	});
-
-	Prism.hooks.add('before-sanity-check', function (env) {
-		/** @type {HTMLPreElement} */
-		var pre = env.element;
-		if (pre.matches(SELECTOR)) {
-			env.code = ''; // fast-path the whole thing and go to complete
-
-			pre.setAttribute(ATTR_LOADING, ''); // mark as loading
-
-			// add code element with loading message
-			var code = pre.appendChild(document.createElement('CODE'));
-			code.textContent = LOADING_MESSAGE;
-
-			var src = pre.getAttribute('data-src');
-
-			var language = env.language;
-			if (language === 'none') {
-				// the language might be 'none' because there is no language set;
-				// in this case, we want to use the extension as the language
-				var extension = (/\.(\w+)$/.exec(src) || [, 'none'])[1];
-				language = EXTENSIONS[extension] || extension;
+		Array.prototype.slice.call(container.querySelectorAll('pre[data-src]')).forEach(function (pre) {
+			// ignore if already loaded
+			if (pre.hasAttribute('data-src-loaded')) {
+				return;
 			}
 
-			// TODO: Preload language using Autoloader
+			// load current
+			var src = pre.getAttribute('data-src');
 
-			// set language classes
+			var language, parent = pre;
+			var lang = /\blang(?:uage)?-([\w-]+)\b/i;
+			while (parent && !lang.test(parent.className)) {
+				parent = parent.parentNode;
+			}
+
+			if (parent) {
+				language = (pre.className.match(lang) || [, ''])[1];
+			}
+
+			if (!language) {
+				var extension = (src.match(/\.(\w+)$/) || [, ''])[1];
+				language = Extensions[extension] || extension;
+			}
+
+			var code = document.createElement('code');
 			code.className = 'language-' + language;
-			pre.className = (pre.className.replace(lang, ' ') + ' language-' + language).replace(/\s+/g, ' ');
 
-			// load file
+			pre.textContent = '';
+
+			code.textContent = 'Loading…';
+
+			pre.appendChild(code);
+
 			var xhr = new XMLHttpRequest();
+
 			xhr.open('GET', src, true);
+
 			xhr.onreadystatechange = function () {
 				if (xhr.readyState == 4) {
+
 					if (xhr.status < 400 && xhr.responseText) {
-						// mark as loaded
-						pre.removeAttribute(ATTR_LOADING);
-						pre.setAttribute(ATTR_LOADED, '');
-
-						// highlight code
 						code.textContent = xhr.responseText;
+
 						Prism.highlightElement(code);
-
-					} else {
-						// mark as failed
-						pre.removeAttribute(ATTR_LOADING);
-						pre.setAttribute(ATTR_FAILED, '');
-
-						if (xhr.status >= 400) {
-							code.textContent = FAILURE_MESSAGE.replace('{status}', xhr.status).replace('{message}', xhr.statusText);
-						} else {
-							code.textContent = FAILURE_EMPTY_MESSAGE;
-						}
+						// mark as loaded
+						pre.setAttribute('data-src-loaded', '');
+					}
+					else if (xhr.status >= 400) {
+						code.textContent = '✖ Error ' + xhr.status + ' while fetching file: ' + xhr.statusText;
+					}
+					else {
+						code.textContent = '✖ Error: File does not exist or is empty';
 					}
 				}
 			};
+
 			xhr.send(null);
-		}
-	});
-
-
-	// register download file plugin
-	if (Prism.plugins.toolbar) {
-		Prism.plugins.toolbar.registerButton('download-file', function (env) {
-			var pre = env.element.parentNode;
-			if (!pre || !/^pre$/i.test(pre.nodeName) || !pre.hasAttribute('data-src') || !pre.hasAttribute('data-download-link')) {
-				return;
-			}
-			var src = pre.getAttribute('data-src');
-			var a = document.createElement('a');
-			a.textContent = pre.getAttribute('data-download-link-label') || 'Download';
-			a.setAttribute('download', '');
-			a.href = src;
-			return a;
 		});
-	}
 
-	Prism.plugins.fileHighlight = {
-		/**
-		 * Executes the File Highlight plugin for all matching `pre` elements under the given container.
-		 *
-		 * Note: Elements which are already loaded or currently loading will not be touched by this method.
-		 *
-		 * @param {Element | Document} [container=document]
-		 */
-		highlight: function highlight(container) {
-			var elements = (container || document).querySelectorAll(SELECTOR);
-
-			for (var i = 0, element; element = elements[i++];) {
-				Prism.highlightElement(element);
-			}
+		if (Prism.plugins.toolbar) {
+			Prism.plugins.toolbar.registerButton('download-file', function (env) {
+				var pre = env.element.parentNode;
+				if (!pre || !/pre/i.test(pre.nodeName) || !pre.hasAttribute('data-src') || !pre.hasAttribute('data-download-link')) {
+					return;
+				}
+				var src = pre.getAttribute('data-src');
+				var a = document.createElement('a');
+				a.textContent = pre.getAttribute('data-download-link-label') || 'Download';
+				a.setAttribute('download', '');
+				a.href = src;
+				return a;
+			});
 		}
+
 	};
 
-	/** @deprecated Use `Prism.plugins.fileHighlight.highlight` instead. */
-	Prism.fileHighlight = function () {
-		console.warn('Prism.fileHighlight is deprecated. Use `Prism.plugins.fileHighlight.highlight` instead.');
-		Prism.plugins.fileHighlight.highlight.apply(this, arguments);
-	}
+	document.addEventListener('DOMContentLoaded', function () {
+		// execute inside handler, for dropping Event as argument
+		self.Prism.fileHighlight();
+	});
 
 })();
