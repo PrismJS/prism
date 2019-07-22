70f78061cd90bc5453fbd88f731a77f5c5bb5fdf
RunDevelopment <mitchi5000.ms@googlemail.com>
2019-07-23 01:43:45 +0200

    Take peer dependencies into consideration

--- tests/pattern-tests.js
+++ tests/pattern-tests.js
@@ -84,108 +84,122 @@ for (const lang in languages) {
 	}
 
 	describe(`Patterns of '${lang}'`, function () {
-
 		const Prism = PrismLoader.createInstance(lang);
+		testPatterns(Prism);
+	});
 
-		/**
-		 * Invokes the given function on every pattern in `Prism.languages`.
-		 *
-		 * @param {(values: CallbackValue) => void} callback
-		 *
-		 * @typedef CallbackValue
-		 * @property {RegExp} pattern
-		 * @property {LiteralAST} ast
-		 * @property {string} tokenPath
-		 * @property {string} name
-		 * @property {any} parent
-		 * @property {{ key: string, value: any }[]} path
-		 */
-		function forEachPattern(callback) {
-			BFS(Prism.languages, path => {
-				const { key, value } = path[path.length - 1];
+	/** @type {undefined | string | string[]} */
+	let peerDeps = languages[lang].peerDependencies;
+	peerDeps = !peerDeps ? [] : (Array.isArray(peerDeps) ? peerDeps : [peerDeps]);
 
-				let tokenPath = '<languages>';
-				for (const { key } of path) {
-					if (!key) {
-						// do nothing
-					} else if (/^\d+$/.test(key)) {
-						tokenPath += `[${key}]`;
-					} else if (/^[a-z]\w*$/i.test(key)) {
-						tokenPath += `.${key}`;
-					} else {
-						tokenPath += `[${JSON.stringify(key)}]`;
-					}
-				}
-
-				if (Object.prototype.toString.call(value) == '[object RegExp]') {
-					let ast;
-					try {
-						ast = parseRegex(value);
-					} catch (error) {
-						throw new SyntaxError(`Invalid RegExp at ${tokenPath}\n\n${error.message}`);
-					}
-
-					callback({
-						pattern: value,
-						ast,
-						tokenPath,
-						name: key,
-						parent: path.length > 1 ? path[path.length - 2].value : undefined,
-						path
-					});
-				}
-			});
-		}
-
-
-		it('- should not match the empty string', function () {
-			forEachPattern(({ pattern, tokenPath }) => {
-				// test for empty string
-				assert.notMatch('', pattern, `Token ${tokenPath}: ${pattern} should not match the empty string.`);
-			});
+	if (peerDeps.length > 0) {
+		describe(`Patterns of '${lang}' + peer dependencies '${peerDeps.join("', '")}'`, function () {
+			const Prism = PrismLoader.createInstance([...peerDeps, lang]);
+			testPatterns(Prism);
 		});
+	}
+}
 
-		it('- should have a capturing group if lookbehind is set to true', function () {
-			forEachPattern(({ ast, tokenPath, name, parent }) => {
-				const lookbehind = name === 'pattern' && parent.lookbehind;
+function testPatterns(Prism) {
 
-				if (lookbehind) {
-					let hasCapturingGroup = false;
-					visitRegExpAST(ast.pattern, {
-						onCapturingGroupEnter() {
-							hasCapturingGroup = true;
-						}
-					});
+	/**
+	 * Invokes the given function on every pattern in `Prism.languages`.
+	 *
+	 * @param {(values: CallbackValue) => void} callback
+	 *
+	 * @typedef CallbackValue
+	 * @property {RegExp} pattern
+	 * @property {LiteralAST} ast
+	 * @property {string} tokenPath
+	 * @property {string} name
+	 * @property {any} parent
+	 * @property {{ key: string, value: any }[]} path
+	 */
+	function forEachPattern(callback) {
+		BFS(Prism.languages, path => {
+			const { key, value } = path[path.length - 1];
 
-					if (!hasCapturingGroup) {
-						assert.fail(`Token ${tokenPath}: The pattern is set to 'lookbehind: true' but does not have a capturing group.`);
-					}
+			let tokenPath = '<languages>';
+			for (const { key } of path) {
+				if (!key) {
+					// do nothing
+				} else if (/^\d+$/.test(key)) {
+					tokenPath += `[${key}]`;
+				} else if (/^[a-z]\w*$/i.test(key)) {
+					tokenPath += `.${key}`;
+				} else {
+					tokenPath += `[${JSON.stringify(key)}]`;
 				}
-			});
-		});
+			}
 
-		it('- should not have unused capturing groups', function () {
-			const errors = [];
+			if (Object.prototype.toString.call(value) == '[object RegExp]') {
+				let ast;
+				try {
+					ast = parseRegex(value);
+				} catch (error) {
+					throw new SyntaxError(`Invalid RegExp at ${tokenPath}\n\n${error.message}`);
+				}
 
-			forEachPattern(({ ast, tokenPath, name, parent }) => {
-				const lookbehind = name === 'pattern' && parent.lookbehind;
-
-				let first = true;
-				visitRegExpAST(ast.pattern, {
-					onCapturingGroupEnter(node) {
-						if (node.references.length === 0 && !(lookbehind && first)) {
-							errors.push(`Token ${tokenPath}: Unused capturing group ${node.raw}`);
-						}
-
-						first = false;
-					}
+				callback({
+					pattern: value,
+					ast,
+					tokenPath,
+					name: key,
+					parent: path.length > 1 ? path[path.length - 2].value : undefined,
+					path
 				});
-			});
-
-			if (errors.length > 0) {
-				assert.fail(errors.join('\n\n'));
 			}
 		});
+	}
 
+
+	it('- should not match the empty string', function () {
+		forEachPattern(({ pattern, tokenPath }) => {
+			// test for empty string
+			assert.notMatch('', pattern, `Token ${tokenPath}: ${pattern} should not match the empty string.`);
+		});
 	});
+
+	it('- should have a capturing group if lookbehind is set to true', function () {
+		forEachPattern(({ ast, tokenPath, name, parent }) => {
+			const lookbehind = name === 'pattern' && parent.lookbehind;
+
+			if (lookbehind) {
+				let hasCapturingGroup = false;
+				visitRegExpAST(ast.pattern, {
+					onCapturingGroupEnter() {
+						hasCapturingGroup = true;
+					}
+				});
+
+				if (!hasCapturingGroup) {
+					assert.fail(`Token ${tokenPath}: The pattern is set to 'lookbehind: true' but does not have a capturing group.`);
+				}
+			}
+		});
+	});
+
+	it('- should not have unused capturing groups', function () {
+		const errors = [];
+
+		forEachPattern(({ ast, tokenPath, name, parent }) => {
+			const lookbehind = name === 'pattern' && parent.lookbehind;
+
+			let first = true;
+			visitRegExpAST(ast.pattern, {
+				onCapturingGroupEnter(node) {
+					if (node.references.length === 0 && !(lookbehind && first)) {
+						errors.push(`Token ${tokenPath}: Unused capturing group ${node.raw}`);
+					}
+
+					first = false;
+				}
+			});
+		});
+
+		if (errors.length > 0) {
+			assert.fail(errors.join('\n\n'));
+		}
+	});
+
 }
