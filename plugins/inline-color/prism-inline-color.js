3800ffaab07a9907cf5304135fc79bea63e27af5
RunDevelopment <mitchi5000.ms@googlemail.com>
2019-08-14 12:00:13 +0200

    Improvements

--- plugins/inline-color/prism-inline-color.js
+++ plugins/inline-color/prism-inline-color.js
@@ -4,25 +4,17 @@
 		return;
 	}
 
-	// Copied from markup language definition
+	// Copied from the markup language definition
 	var HTML_TAG = /<\/?(?!\d)[^\s>\/=$<%]+(?:\s(?:\s*[^\s>\/=]+(?:\s*=\s*(?:"[^"]*"|'[^']*'|[^\s'">=]+(?=[\s>]))|(?=[\s/>])))+)?\s*\/?>/g;
 
 	/**
-	 * @typedef Color A normalized RGBA color.
-	 * @property {number} r The red channel of the color. `0 <= r <= 1`.
-	 * @property {number} g The green channel of the color. `0 <= g <= 1`.
-	 * @property {number} b The blue channel of the color. `0 <= b <= 1`.
-	 * @property {number} a The alpha channel of the color. `0 <= a <= 1`.
-	 */
-
-	/**
-	 * Parses the given hexadecimal representation and returns the parsed color.
+	 * Parses the given hexadecimal representation and returns the parsed RGBA color.
 	 *
 	 * If the format of the given string is invalid, `undefined` will be returned.
 	 * Valid formats are: `RGB`, `RGBA`, `RRGGBB`, and `RRGGBBAA`.
 	 *
 	 * @param {string} hex
-	 * @returns {Color | undefined}
+	 * @returns {string | undefined}
 	 */
 	function parseHexColor(hex) {
 		// the regex explained: In first lookahead we check whether the string is valid and then we use the
@@ -33,12 +25,13 @@
 		var scale = hex.length <= 4 ? 1 / 15 : 1 / 255;
 
 		if (match) {
-			return {
-				r: parseInt(match[1], 16) * scale,
-				g: parseInt(match[2], 16) * scale,
-				b: parseInt(match[3], 16) * scale,
-				a: match[4] === undefined ? 1 : (parseInt(match[4], 16) * scale)
-			};
+			var rgb = match.slice(1, 4).map(function (c) {
+				return String(Math.round(parseInt(c, 16) * scale * 255));
+			}).join(',');
+
+			var alpha = match[4] === undefined ? '1' : (parseInt(match[4], 16) * scale).toFixed(3);
+
+			return 'rgba(' + rgb + ',' + alpha + ')';
 		}
 		return undefined;
 	}
@@ -61,12 +54,13 @@
 	 * These parser serve as validators and as a layer of compatibility to support color formats which the browser
 	 * might not support natively.
 	 *
-	 * @type {((value: string) => (Color|string|undefined))[]}
+	 * @type {((value: string) => (string|undefined))[]}
 	 */
 	var parsers = [
 		parseHexColor,
 		validateColor
 	];
+
 
 	Prism.hooks.add('wrap', function (env) {
 		if (env.type === 'color' || env.type === 'hexcode') {
@@ -84,12 +78,7 @@
 				return;
 			}
 
-			// we can't a simple hex because some browsers (Edge, IE11) don't support hex literals with an alpha channel
-			var value = typeof color === 'string' ? color : 'rgba(' + [color.r, color.g, color.b].map(function (c) {
-				return String(Math.round(c * 255));
-			}).join(',') + ',' + color.a + ')';
-
-			var previewElement = '<span class="inline-color" style="background-color:' + value + ';"></span>';
+			var previewElement = '<span class="inline-color" style="background-color:' + color + ';"></span>';
 
 			env.attributes['data-color'] = '';
 			env.content = previewElement + content;
