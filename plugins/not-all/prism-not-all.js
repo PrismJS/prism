0bfb4e78e5f6a4f47608ff6845a6779133fcc9da
RunDevelopment <mitchi5000.ms@googlemail.com>
2019-09-27 14:59:18 +0200

    Added excludeCss and DOM attributes

--- plugins/not-all/prism-not-all.js
+++ plugins/not-all/prism-not-all.js
@@ -4,6 +4,15 @@
 		return;
 	}
 
+	// https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
+	if (!Element.prototype.matches) {
+		Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+	}
+
+	var scripts = document.getElementsByTagName('script');
+	var script = scripts[scripts.length - 1];
+
+
 	/**
 	 * @type {Array<(element: HTMLElement) => boolean>}
 	 */
@@ -39,19 +48,37 @@
 		},
 
 		/**
+		 * Filters the elements of `highlightAll` and `highlightAllUnder` such that only element which do not match the
+		 * given CSS selection will be highlighted.
+		 *
+		 * @param {string} selector
+		 */
+		excludeCss: function (selector) {
+			filters.push(function (element) {
+				return !element.matches(selector);
+			});
+		},
+
+		/**
 		 * Filters the elements of `highlightAll` and `highlightAllUnder` such that only element with a known language
 		 * will be highlighted. All elements with an unset or unknown language will be ignored.
 		 *
 		 * __Note:__ This will effectively disable the AutoLoader plugin.
 		 */
-		filterKnown: false,
-
+		filterKnown: script.hasAttribute('data-filter-known')
 	};
 
 	config.filter(function filterKnown(env) {
 		return !config.filterKnown || typeof Prism.languages[env.language] === 'object';
 	});
 
+	var attr;
+	if (attr = script.getAttribute('data-filter-css')) {
+		config.filterCss(attr);
+	}
+	if (attr = script.getAttribute('data-exclude-css')) {
+		config.excludeCss(attr);
+	}
 
 	Prism.hooks.add('before-highlightall', function (env) {
 		for (var i = 0, l = filters.length; i < l; i++) {
