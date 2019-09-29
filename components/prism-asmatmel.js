f6a820b53dd8792c68faccf45a984fe9466b811a
Michael Earls <me@cerkit.com>
2019-09-29 14:24:11 -0500

    Added additional PORTs and Constants for newer processors
    Added PA01-PZ31, PORTA-PORTZ as well as DDDA01-DDDZ31 support with tests.

--- components/prism-asmatmel.js
+++ components/prism-asmatmel.js
@@ -1,7 +1,7 @@
 Prism.languages.asmatmel = {
 	'comment': /;.*/,
 	'constant' : {
-		pattern: /P[A-L][0-7]|PORT[A-L]|DDR[A-L]|DD[A-L][0-7]/
+		pattern: /\b(?:P[A-Z]3[0-1])\b|\b(?:P[A-Z][0-2][0-9])\b|PORT[A-Z]|DDR[A-Z]|\b(?:DD[A-Z]3[0-1])\b|\b(?:DD[A-Z][0-2][0-9])\b/
 	},
 	'directive': {
 		pattern: /\.\w+(?= )/,
