c4e430c6a8b237aa8e66d6dd42dd26fd9993d6f3
Rob Galbraith <rob@Carter.home>
2019-11-19 15:42:36 +0000

    Add causejs to language definitions

---
+++ components/prism-causejs.js
@@ -0,0 +1,42 @@
+(function (Prism) {
+
+	var variable = /\{\$([\w\-\.\#:\@\|\$\{\}]+)\}|\{\@([\w\-\.\#:\@\|\$\{\}]+)\}|\{\|[-\w\#:\@\|\$\{\}]+\}/i;
+
+	Prism.languages.causejs = {
+		'comment': /\/\*[\s\S]*?\*\//,
+		'atrule': {
+			pattern: /@media [\s\S]*?(?:;|(?=\s*\{))/,
+			inside: {
+				'rule': /@[\w-]+/
+				// See rest below
+			}
+		},
+		'attr-value': {	// Using "selector" clashes with other ones, so this is instead of "selector".
+			pattern: RegExp('(^|[\r\n\t]+)?(?![;\}\s\r\n\t]+)(#|\.|a-zA-Z)?[~#\?a-zA-Z0-9\-\\[\\]_\.\:\,\r\n\"\=\(\) \.\{\}\@\$\|]+(?=( )?\{(?![\$\@\|]+))'),
+			inside: {
+				'variable': RegExp(variable.source),
+				'event': {
+					pattern: /:[\w]+(,| )/,
+					alias: 'selector'
+				}
+			}
+		},
+		'string': {
+			pattern: /("|')(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
+			inside: {
+				'variable': RegExp(variable.source)
+			}
+		},
+		'property': /[-_a-z\xA0-\uFFFF][-\w\xA0-\uFFFF]*(?=\s*:)/i,
+		'variable': {
+			pattern: variable,
+			greedy: true
+		},
+		'function': /[\w]+(?=\()/i,
+		'important': /after [0-9]+(ms|s)?/i,
+		'punctuation': /[(){};:,]/
+	};
+
+	Prism.languages.causejs['atrule'].inside.rest = Prism.languages.causejs;
+
+}(Prism));
