1ffe936fc68959c8ac79fe52d77ba38c4f19bc0d
Michael Schmidt <mitchi5000.ms@googlemail.com>
2020-01-04 18:14:30 +0100

    Added benchmark suite

---
+++ benchmark/benchmark.js
@@ -0,0 +1,361 @@
+const fs = require('fs');
+const path = require('path');
+const { argv } = require('yargs');
+const Benchmark = require('benchmark');
+const simpleGit = require('simple-git/promise');
+const { parseLanguageNames } = require('../tests/helper/test-case');
+
+
+doBenchmark(getConfig());
+
+/**
+ * @param {import("./config").Config} config
+ */
+async function doBenchmark(config) {
+	const cases = getCases(config);
+	const totalNumberOfCaseFiles = cases.reduce((a, c) => a + c.files.length, 0);
+	const candidates = await getCandidates(config);
+	const maxCandidateNameLength = candidates.reduce((a, c) => Math.max(a, c.name.length), 0);
+
+	console.log(`Found ${cases.length} cases with ${totalNumberOfCaseFiles} files in total.`);
+	console.log(`Test ${candidates.length} candidates on ${config.options.testFunction}`);
+	const ert = candidates.length * totalNumberOfCaseFiles * config.options.maxTime;
+	console.log(`Estimated duration: ${Math.floor(ert / 60)}m ${Math.floor(ert % 60)}s`);
+
+	/**
+	 * @type {Summary[]}
+	 *
+	 * @typedef {{ best: number; worst: number }} Summary
+	 */
+	const totalSummary = Array.from({ length: candidates.length }).map(() => ({ best: 0, worst: 0 }));
+
+	for (const $case of cases) {
+
+		console.log();
+		console.log(`\x1b[90m${'-'.repeat(60)}\x1b[0m`);
+		console.log();
+		if ($case.id !== $case.language) {
+			console.log(`${$case.id} (${$case.language})`);
+		} else {
+			console.log($case.id);
+		}
+		console.log();
+
+		// prepare candidates
+		const warmupCode = fs.readFileSync($case.files[0].path, 'utf8');
+		/** @type {[string, (code: string) => void][]} */
+		const candidateFunctions = candidates.map(({ name, setup }) => {
+			const fn = setup($case.language, $case.languages);
+			fn(warmupCode); // warmup
+			return [name, fn];
+		});
+
+
+		// bench all files
+		for (const caseFile of $case.files) {
+			console.log(`  ${caseFile.name} \x1b[90m(${Math.round(caseFile.size / 1024)} kB)\x1b[0m`);
+
+			const code = fs.readFileSync(caseFile.path, 'utf8');
+
+			const results = measureCandidates(candidateFunctions.map(([name, fn]) => [name, () => fn(code)]), {
+				maxTime: config.options.maxTime,
+				minSamples: 1,
+				delay: 0,
+			});
+
+			const min = results.reduce((a, c) => Math.min(a, c.stats.mean), Infinity);
+			const max = results.reduce((a, c) => Math.max(a, c.stats.mean), -Infinity);
+			const minIndex = results.findIndex(x => x.stats.mean === min);
+			const maxIndex = results.findIndex(x => x.stats.mean === max);
+
+			totalSummary[minIndex].best++;
+			totalSummary[maxIndex].worst++;
+
+			const best = getBest(results);
+			const worst = getWorst(results);
+
+			results.forEach(r => {
+				const mean = (r.stats.mean * 1000).toFixed(2).padStart(8) + 'ms';
+				const r_moe = (100 * r.stats.moe / r.stats.mean).toFixed(0).padStart(3) + '%'
+
+				const color = r === best ? '\x1b[32m' : r === worst ? '\x1b[31m' : '\x1b[0m';
+
+				console.log(`  \x1b[90m| ${color}${r.name.padEnd(maxCandidateNameLength, " ")} ${mean} ±${r_moe} ${r.stats.sample.length}smp\x1b[0m`);
+			});
+		}
+	}
+
+	// total summary
+	console.log();
+	console.log(`\x1b[90m${'-'.repeat(60)}\x1b[0m`);
+	console.log();
+	console.log('summary');
+	console.log(`${' '.repeat(maxCandidateNameLength + 2)}  \x1b[90mbest  worst\x1b[0m`);
+
+	totalSummary.forEach((s, i) => {
+		console.log(`  ${candidates[i].name.padEnd(maxCandidateNameLength, " ")}  ${String(s.best).padStart('best'.length)}  ${String(s.worst).padStart('worst'.length)}`);
+	});
+}
+
+function getConfig() {
+	const base = require('./config.js');
+
+	if (typeof argv.testFunction === 'string') {
+		// @ts-ignore
+		base.options.testFunction = argv.testFunction;
+	}
+	if (typeof argv.maxTime === 'number') {
+		base.options.maxTime = argv.maxTime;
+	}
+	if (typeof argv.language === 'string') {
+		base.options.language = argv.language;
+	}
+
+	return base;
+}
+
+
+/**
+ * @param {import("./config").Config} config
+ */
+function getCases(config) {
+	/**
+	 * @type {Map<string, FileInfo>}
+	 *
+	 * @typedef {{ name: string, path: string, size: number }} FileInfo
+	 * */
+	const files = new Map();
+	fs.readdirSync(path.join(__dirname, 'files')).forEach(file => {
+		const p = path.join(__dirname, 'files', file);
+		const stat = fs.statSync(p);
+		if (stat.isFile()) {
+			files.set(file, {
+				name: file,
+				path: p,
+				size: stat.size
+			});
+		}
+	});
+
+	/** @type {Map<string, Set<FileInfo>>} */
+	const map = new Map();
+
+	/**
+	 * @param {T[] | T | undefined | null} value
+	 * @param {(value: T, index: number) => void} callback
+	 * @template T
+	 */
+	function forEach(value, callback) {
+		if (Array.isArray(value)) {
+			value.forEach(callback);
+		} else if (value != undefined) {
+			callback(value, 0);
+		}
+	}
+
+	function addToMap(id) {
+		if (map.has(id)) {
+			return;
+		}
+		if (!(id in config.cases)) {
+			throw new Error(`Unknown case "${id}"`);
+		}
+
+		const caseEntry = config.cases[id];
+
+		/** @type {Set<FileInfo>} */
+		const caseFiles = new Set();
+
+		forEach(caseEntry.extends, extId => {
+			addToMap(extId);
+			map.get(extId).forEach(fi => caseFiles.add(fi));
+		});
+
+		forEach(caseEntry.files, file => {
+			const fi = files.get(file);
+			if (!fi) {
+				throw new Error(`Unknown file "${file}" in "${id}"`);
+			}
+			caseFiles.add(fi);
+		});
+
+		map.set(id, caseFiles);
+	}
+	Object.keys(config.cases).forEach(addToMap);
+
+	let cases = [...map].map(([id, files]) => {
+		const parsed = parseLanguageNames(id);
+		return {
+			language: parsed.mainLanguage,
+			languages: parsed.languages,
+			id,
+			files: [...files].sort((a, b) => a.name.localeCompare(b.name))
+		};
+	}).filter(c => {
+		const language = config.options.language;
+		if (!language) {
+			return true; // no language(s) defined
+		}
+		const languages = Array.isArray(language) ? language : language.split(/,/g);
+
+		return languages.some(l => c.languages.indexOf(l) >= 0);
+	});
+
+	if (config.options.language) {
+		const set = new Set(config.options.language.split(/,/g));
+		cases = cases.filter(c => {
+			return c.languages.some(l => set.has(l));
+		});
+	}
+
+	return cases.sort((a, b) => a.id.localeCompare(b.id));
+}
+
+/**
+ * @param {Iterable<[string, () => void]>} candidates
+ * @param {import("benchmark").Options} [options]
+ * @returns {Result[]}
+ *
+ * @typedef {{ name: string, stats: import("benchmark").Stats }} Result
+ */
+function measureCandidates(candidates, options) {
+	const suite = new Benchmark.Suite("temp name");
+
+	for (const [name, fn] of candidates) {
+		suite.add(name, fn, options);
+	}
+
+	/** @type {Result[]} */
+	const results = [];
+
+	suite.on('cycle', event => {
+		results.push({
+			name: event.target.name,
+			stats: event.target.stats
+		});
+	}).run();
+
+	return results;
+}
+
+/**
+ * @param {Result[]} results
+ * @returns {Result | null}
+ */
+function getBest(results) {
+	if (results.length >= 2) {
+		const sorted = [...results].sort((a, b) => a.stats.mean - b.stats.mean);
+		const best = sorted[0].stats;
+		const secondBest = sorted[1].stats;
+
+		// basically, it's only the best if the two means plus their moe are disjoint
+		if (best.mean + best.moe + secondBest.moe < secondBest.mean) {
+			return sorted[0];
+		}
+	}
+
+	return null;
+}
+/**
+ * @param {Result[]} results
+ * @returns {Result | null}
+ */
+function getWorst(results) {
+	if (results.length >= 2) {
+		const sorted = [...results].sort((a, b) => b.stats.mean - a.stats.mean);
+		const worst = sorted[0].stats;
+		const secondWorst = sorted[1].stats;
+
+		// basically, it's only the best if the two means plus their moe are disjoint
+		if (worst.mean - worst.moe - secondWorst.moe > secondWorst.mean) {
+			return sorted[0];
+		}
+	}
+
+	return null;
+}
+
+/**
+ * Create a new test function from the given Prism instance.
+ *
+ * @param {any} Prism
+ * @param {string} mainLanguage
+ * @param {string} testFunction
+ * @returns {(code: string) => void}
+ */
+function createTestFunction(Prism, mainLanguage, testFunction) {
+	if (testFunction === 'tokenize') {
+		return (code) => {
+			Prism.tokenize(code, Prism.languages[mainLanguage]);
+		};
+	} else if (testFunction === 'highlight') {
+		return (code) => {
+			Prism.highlight(code, Prism.languages[mainLanguage], mainLanguage);
+		};
+	} else {
+		throw new Error(`Unknown test function "${testFunction}"`);
+	}
+
+}
+
+/**
+ * @param {import("./config").Config} config
+ * @returns {Promise<Candidate[]>}
+ *
+ * @typedef Candidate
+ * @property {string} name
+ * @property {(mainLanguage: string, languages: string[]) => (code: string) => void} setup
+ */
+async function getCandidates(config) {
+	/** @type {Candidate[]} */
+	const candidates = [];
+
+	// local
+	const localPrismLoader = require('../tests/helper/prism-loader');
+	candidates.push({
+		name: 'local',
+		setup(mainLanguage, languages) {
+			const Prism = localPrismLoader.createInstance(languages);
+			return createTestFunction(Prism, mainLanguage, config.options.testFunction);
+		}
+	});
+
+	// remotes
+
+	// prepare base directory
+	const remoteBaseDir = path.join(__dirname, 'remotes');
+	if (!fs.existsSync(remoteBaseDir)) {
+		fs.mkdirSync(remoteBaseDir);
+	}
+
+	const baseGit = simpleGit(remoteBaseDir);
+
+	for (const remote of config.remotes) {
+		const user = /[^/]+(?=\/prism.git)/.exec(remote.repo);
+		const branch = remote.branch || 'master';
+		const remoteName = `${user}@${branch}`;
+		const remoteDir = path.join(remoteBaseDir, `${user}@${branch}`);
+
+		let remoteGit;
+		if (!fs.existsSync(remoteDir)) {
+			console.log(`Cloning ${remote.repo}`);
+			await baseGit.clone(remote.repo, remoteName);
+			remoteGit = simpleGit(remoteDir);
+		} else {
+			remoteGit = simpleGit(remoteDir);
+			await remoteGit.fetch('origin', branch); // get latest version of branch
+		}
+		await remoteGit.checkout(branch); // switch to branch
+
+		const remotePrismLoader = require(path.join(remoteDir, 'tests/helper/prism-loader'));
+		candidates.push({
+			name: remoteName,
+			setup(mainLanguage, languages) {
+				const Prism = remotePrismLoader.createInstance(languages);
+				return createTestFunction(Prism, mainLanguage, config.options.testFunction);
+			}
+		});
+	}
+
+	return candidates;
+}
---
+++ benchmark/config.js
@@ -0,0 +1,71 @@
+/** @type {Config}
+ *
+ * @typedef Config
+ * @property {ConfigOptions} options
+ * @property {ConfigRemote[]} remotes
+ * @property {Object<string, ConfigCase>} cases
+ *
+ * @typedef ConfigOptions
+ * @property {'tokenize' | 'highlight'} testFunction
+ * @property {number} maxTime in seconds
+ * @property {string} [language] An optional comma separated list of languages than, if defined, will be the only
+ * languages for which the benchmark will be run.
+ *
+ * @typedef ConfigRemote
+ * @property {string} repo
+ * @property {string} [branch='master']
+ *
+ * @typedef ConfigCase
+ * @property {string | string[]} [extends]
+ * @property {string | string[]} [files]
+*/
+const config = {
+	options: {
+		testFunction: 'tokenize',
+		maxTime: 3
+	},
+
+	remotes: [
+		/**
+		 * This will checkout a specific branch from a given repo.
+		 *
+		 * If no branch is specified, the master branch will be used.
+		 */
+
+		{
+			repo: 'https://github.com/PrismJS/prism.git'
+		},
+	],
+
+	cases: {
+		'css': {
+			files: [
+				'style.css'
+			]
+		},
+		'css!+css-extras': { extends: 'css' },
+		'javascript': {
+			extends: 'json',
+			files: [
+				'prism.js',
+				'prism.min.js',
+				'utopia.js'
+			]
+		},
+		'json': {
+			files: [
+				'components.json',
+				'package-lock.json'
+			]
+		},
+		'markup': {
+			files: [
+				'download.html',
+				'index.html'
+			]
+		},
+		'markup!+css+javascript': { extends: 'markup' }
+	}
+};
+
+module.exports = config;
