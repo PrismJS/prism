0ecc7f5cb76da0bbb8f115b5393ae96e0f6549ff
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-02-17 12:51:24 +0100

    Fixed function name spaces

--- components/prism-c.js
+++ components/prism-c.js
@@ -4,7 +4,7 @@ Prism.languages.c = Prism.languages.extend('clike', {
 		lookbehind: true
 	},
 	'keyword': /\b(?:__attribute__|_Alignas|_Alignof|_Atomic|_Bool|_Complex|_Generic|_Imaginary|_Noreturn|_Static_assert|_Thread_local|asm|typeof|inline|auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)\b/,
-	'function': /[a-z_]\w*\s*(?=\()/i,
+	'function': /[a-z_]\w*(?=\s*\()/i,
 	'operator': />>=?|<<=?|->|([-+&|:])\1|[?:~]|[-+*/%&|^!=<>]=?/,
 	'number': /(?:\b0x(?:[\da-f]+\.?[\da-f]*|\.[\da-f]+)(?:p[+-]?\d+)?|(?:\b\d+\.?\d*|\B\.\d+)(?:e[+-]?\d+)?)[ful]*/i
 });
