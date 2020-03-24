4f32ba3bfec562c8487d9caea560c70e9f5c1183
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-03-24 11:53:24 +0100

    Scheme: Improvements

--- components/prism-scheme.js
+++ components/prism-scheme.js
@@ -1,12 +1,21 @@
 Prism.languages.scheme = {
 	'comment': /;.*/,
 	'string': {
-		pattern: /"(?:[^"\\]|\\.)*"|'[^()#'\s]+/,
+		pattern: /"(?:[^"\\]|\\.)*"/,
+		greedy: true
+	},
+	'symbol': {
+		pattern: /'[^()#'\s]+/,
 		greedy: true
 	},
 	'character': {
-		pattern: /#\\(?:[ux][a-fA-F\d]+|[a-zA-Z]+|\S)/,
+		pattern: /#\\(?:[ux][a-fA-F\d]+|[-a-zA-Z]+|\S)/,
+		greedy: true,
 		alias: 'string'
+	},
+	'lambda-parameter': {
+		pattern: /(\(lambda\s+\()[^()'\s]+/,
+		lookbehind: true
 	},
 	'keyword': {
 		pattern: /(\()(?:define(?:-syntax|-library|-values)?|(?:case-)?lambda|let(?:\*|rec)?(?:-values)?|else|if|cond|begin|delay(?:-force)?|parameterize|guard|set!|(?:quasi-)?quote|syntax-rules)(?=[()\s])/,
@@ -17,7 +26,7 @@ Prism.languages.scheme = {
 		lookbehind: true
 	},
 	'number': {
-		pattern: /([\s()])[-+]?(?:\d+\/\d+|\d*\.?\d+(?:\s*[-+]\s*\d*\.?\d+i)?)\b/,
+		pattern: /([\s()])[-+]?(?:\d+\/\d+|\d*\.?\d+(?:\s*[-+]\s*\d*\.?\d+i)?)(?=[\s()]|$)/,
 		lookbehind: true
 	},
 	'boolean': /#[tf]/,
@@ -26,7 +35,7 @@ Prism.languages.scheme = {
 		lookbehind: true
 	},
 	'function': {
-		pattern: /(\()[^()'\s]+(?=[()\s)]|$)/,
+		pattern: /(\()[^()'\s]+(?=[()\s]|$)/,
 		lookbehind: true
 	},
 	'punctuation': /[()']/
