b10e90a115f9abca2cc137bd25f893f480133d7d
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-05-05 12:26:09 +0200

    Added regex & improved string and operator

--- components/prism-julia.js
+++ components/prism-julia.js
@@ -3,14 +3,23 @@ Prism.languages.julia = {
 		pattern: /(^|[^\\])#.*/,
 		lookbehind: true
 	},
+	'regex': {
+		// https://docs.julialang.org/en/v1/manual/strings/#Regular-Expressions-1
+		pattern: /r"(?:\\.|[^"\\\r\n])*"[imsx]{0,4}/,
+		greedy: true
+	},
 	'string': {
-		pattern: /("""|''')[\s\S]+?\1|("|')(?:\\.|(?!\2)[^\\\r\n])*\2/,
+		// https://docs.julialang.org/en/v1/manual/strings/#man-characters-1
+		// https://docs.julialang.org/en/v1/manual/strings/#String-Basics-1
+		// https://docs.julialang.org/en/v1/manual/strings/#non-standard-string-literals-1
+		pattern: /"""[\s\S]+?"""|\w*"(?:\\.|[^"\\\r\n])*"|'(?:\\[^\r\n][^'\r\n]*|[^\\\r\n])'/,
 		greedy: true
 	},
 	'keyword': /\b(?:abstract|baremodule|begin|bitstype|break|catch|ccall|const|continue|do|else|elseif|end|export|finally|for|function|global|if|immutable|import|importall|in|let|local|macro|module|print|println|quote|return|struct|try|type|typealias|using|while)\b/,
 	'boolean': /\b(?:true|false)\b/,
 	'number': /(?:\b(?=\d)|\B(?=\.))(?:0[box])?(?:[\da-f]+\.?\d*|\.\d+)(?:[efp][+-]?\d+)?j?/i,
-	'operator': /[-+*^%÷&$\\]=?|\/[\/=]?|!=?=?|\|[=>]?|<(?:<=?|[=:])?|>(?:=|>>?=?)?|==?=?|[~≠≤≥]/,
+	// https://docs.julialang.org/en/v1/manual/mathematical-operations/
+	'operator': /[-+*^%÷⊻&$\\]=?|\/[\/=]?|!=?=?|\|[=>]?|<(?:<=?|[=:])?|>(?:=|>>?=?)?|==?=?|[~≠≤≥]/,
 	'punctuation': /[{}[\];(),.:]/,
 	'constant': /\b(?:(?:NaN|Inf)(?:16|32|64)?)\b/
 };
