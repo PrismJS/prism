d4d83e18842dc4c640cfef2e7146f4a6c0c4d26a
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-05-05 13:36:57 +0200

    Added support for adjoint operator

--- components/prism-julia.js
+++ components/prism-julia.js
@@ -12,14 +12,15 @@ Prism.languages.julia = {
 		// https://docs.julialang.org/en/v1/manual/strings/#man-characters-1
 		// https://docs.julialang.org/en/v1/manual/strings/#String-Basics-1
 		// https://docs.julialang.org/en/v1/manual/strings/#non-standard-string-literals-1
-		pattern: /"""[\s\S]+?"""|\w*"(?:\\.|[^"\\\r\n])*"|'(?:\\[^\r\n][^'\r\n]*|[^\\\r\n])'/,
+		pattern: /"""[\s\S]+?"""|\w*"(?:\\.|[^"\\\r\n])*"|(^|[^\w'])'(?:\\[^\r\n][^'\r\n]*|[^\\\r\n])'/,
+		lookbehind: true,
 		greedy: true
 	},
 	'keyword': /\b(?:abstract|baremodule|begin|bitstype|break|catch|ccall|const|continue|do|else|elseif|end|export|finally|for|function|global|if|immutable|import|importall|in|let|local|macro|module|print|println|quote|return|struct|try|type|typealias|using|while)\b/,
 	'boolean': /\b(?:true|false)\b/,
 	'number': /(?:\b(?=\d)|\B(?=\.))(?:0[box])?(?:[\da-f]+\.?\d*|\.\d+)(?:[efp][+-]?\d+)?j?/i,
 	// https://docs.julialang.org/en/v1/manual/mathematical-operations/
-	'operator': /[-+*^%÷⊻&$\\]=?|\/[\/=]?|!=?=?|\|[=>]?|<(?:<=?|[=:])?|>(?:=|>>?=?)?|==?=?|[~≠≤≥]/,
+	'operator': /[-+*^%÷⊻&$\\]=?|\/[\/=]?|!=?=?|\|[=>]?|<(?:<=?|[=:])?|>(?:=|>>?=?)?|==?=?|[~≠≤≥']/,
 	'punctuation': /[{}[\];(),.:]/,
 	'constant': /\b(?:(?:NaN|Inf)(?:16|32|64)?)\b/
 };
