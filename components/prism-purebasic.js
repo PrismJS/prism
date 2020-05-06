96705a0b3542cd65b3e553df0c7c57c0bea7d50c
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-05-06 23:39:00 +0200

    Removed token + minor improvements

--- components/prism-purebasic.js
+++ components/prism-purebasic.js
@@ -12,8 +12,7 @@ Prism.languages.purebasic = Prism.languages.extend('clike', {
 	'keyword': /\b(?:declarecdll|declaredll|compilerselect|compilercase|compilerdefault|compilerendselect|compilererror|enableexplicit|disableexplicit|not|and|or|xor|calldebugger|debuglevel|enabledebugger|disabledebugger|restore|read|includepath|includebinary|threaded|runtime|with|endwith|structureunion|endstructureunion|align|newlist|newmap|interface|endinterface|extends|enumeration|endenumeration|swap|foreach|continue|fakereturn|goto|gosub|return|break|module|endmodule|declaremodule|enddeclaremodule|declare|declarec|prototype|prototypec|enableasm|disableasm|dim|redim|data|datasection|enddatasection|to|procedurereturn|debug|default|case|select|endselect|as|import|endimport|importc|compilerif|compilerelse|compilerendif|compilerelseif|end|structure|endstructure|while|wend|for|next|step|if|else|elseif|endif|repeat|until|procedure|proceduredll|procedurec|procedurecdll|endprocedure|protected|shared|static|global|define|includefile|xincludefile|macro|endmacro)\b/i,
 	'function': /\b\w+(?:\.\w+)?\s*(?=\()/,
 	'number': /(?:\$[\da-f]+|\b-?\d*\.?\d+(?:e[+-]?\d+)?)\b/i,
-	'operator': /@\*\w+|(?:@|\?)\w+|\*\w+|-[>-]?|\+\+?|!=?|<<?=?|>>?=?|==?|&&?|\|?\||[~^%?*/@]/,
-	'important': /\*\w+\b/,
+	'operator': /(?:@\*?|\?|\*)\w+|-[>-]?|\+\+?|!=?|<<?=?|>>?=?|==?|&&?|\|?\||[~^%?*/@]/
 });
 
 Prism.languages.insertBefore('purebasic', 'keyword', {
@@ -21,6 +20,7 @@ Prism.languages.insertBefore('purebasic', 'keyword', {
 	'asm': {
 		pattern: /(^\s*)!.*/m,
 		lookbehind: true,
+		alias: 'tag',
 		inside: {
 			'comment': /;.*/,
 			'string': {
@@ -57,13 +57,10 @@ Prism.languages.insertBefore('purebasic', 'keyword', {
 				/(?:extern|extern|global)[^;\r\n]*/i,
 				/(?:CPU|FLOAT|DEFAULT).*/
 			],
-			'register': {
-				pattern: /\b(?:st\d|[xyz]mm\d\d?|[cdt]r\d|r\d\d?[bwd]?|[er]?[abcd]x|[abcd][hl]|[er]?(?:bp|sp|si|di)|[cdefgs]s|mm\d+)\b/i
-			},
+			'register': /\b(?:st\d|[xyz]mm\d\d?|[cdt]r\d|r\d\d?[bwd]?|[er]?[abcd]x|[abcd][hl]|[er]?(?:bp|sp|si|di)|[cdefgs]s|mm\d+)\b/i,
 			'number': /(?:\b|-|(?=\$))(?:0[hx][\da-f]*\.?[\da-f]+(?:p[+-]?\d+)?|\d[\da-f]+[hx]|\$\d[\da-f]*|0[oq][0-7]+|[0-7]+[oq]|0[by][01]+|[01]+[by]|0[dt]\d+|\d*\.?\d+(?:\.?e[+-]?\d+)?[dt]?)\b/i,
 			'operator': /[\[\]*+\-/%<>=&|$!,.:]/
-		},
-		alias: 'tag'
+		}
 	}
 });
 
