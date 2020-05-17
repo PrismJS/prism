bb68352ed01f87bd15cc11b9ffab3d60df1a2127
Valtteri Laitinen <dev@valtlai.fi>
2020-05-17 22:52:16 +0300

    Add new aliases
    * atom and rss for markup
    * webmanifest for json

--- components/prism-markup.js
+++ components/prism-markup.js
@@ -100,6 +100,8 @@ Object.defineProperty(Prism.languages.markup.tag, 'addInlined', {
 Prism.languages.html = Prism.languages.markup;
 Prism.languages.mathml = Prism.languages.markup;
 Prism.languages.svg = Prism.languages.markup;
+Prism.languages.atom = Prism.languages.markup;
+Prism.languages.rss = Prism.languages.markup;
 
 Prism.languages.xml = Prism.languages.extend('markup', {});
 Prism.languages.ssml = Prism.languages.xml;
