7ec5cc232ebc8fe88eace076a749bc2103f54267
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2020-06-15 18:41:12 +0100

    jsstacktrace: also match <> in function names because of foo.<anonymous>

--- components/prism-jsstacktrace.js
+++ components/prism-jsstacktrace.js
@@ -19,7 +19,7 @@ Prism.languages.jsstacktrace = {
 			},
 			
 			'function': {
-				pattern: /(at\s+(?:new\s+)?)[_$a-zA-Z\xA0-\uFFFF][.$\w\xA0-\uFFFF]*/,
+				pattern: /(at\s+(?:new\s+)?)[_$a-zA-Z\xA0-\uFFFF][.$\w\xA0-\uFFFF<>]*/,
 				lookbehind: true,
 				inside: {
 					'punctuation': /\./
