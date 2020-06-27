fc0b18db23956df706aec9fb2c32224d51dbe4fd
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-06-27 22:53:55 +0200

    Better explained example

--- components/prism-core.js
+++ components/prism-core.js
@@ -276,9 +276,10 @@ var _ = {
 		 * @public
 		 * @example
 		 * Prism.languages['css-with-colors'] = Prism.languages.extend('css', {
-		 *     // this will overwrite the 'comment' token of CSS at its original position
+		 *     // Prism.languages.css already has a 'comment' token, so this token will overwrite CSS' 'comment' token
+		 *     // at its original position
 		 *     'comment': { ... },
-		 *     // this will be appended
+		 *     // CSS doesn't have a 'color' token, so this token will be appended
 		 *     'color': /\b(?:red|green|blue)\b/
 		 * });
 		 */
