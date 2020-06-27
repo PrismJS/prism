7558f361be651ef1915263fb18464dd069482afa
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-06-27 20:38:11 +0200

    More documentation

--- components/prism-core.js
+++ components/prism-core.js
@@ -261,12 +261,24 @@ var _ = {
 		 * If a token in `redef` also appears in the copied language, then the existing token in the copied language
 		 * will be overwritten at its original position.
 		 *
+		 * ## Best practices
+		 *
+		 * Since the position of overwriting tokens (token in `redef` that overwrite tokens in the copied language)
+		 * doesn't matter, they can technically be in any order. However, this can be confusing to others that trying to
+		 * understand the language definition because, normally, the order of tokens matters in Prism grammars.
+		 *
+		 * Therefore, it is encouraged to order overwriting tokens according to the positions of the overwritten tokens.
+		 * Furthermore, all non-overwriting tokens should be placed after the overwriting ones.
+		 *
 		 * @param {string} id The id of the language to extend. This has to be a key in `Prism.languages`.
 		 * @param {Grammar} redef The new tokens to append.
 		 * @returns {Grammar} The new language created.
 		 * @public
 		 * @example
 		 * Prism.languages['css-with-colors'] = Prism.languages.extend('css', {
+		 *     // this will overwrite the 'comment' token of CSS at its original position
+		 *     'comment': { ... },
+		 *     // this will be appended
 		 *     'color': /\b(?:red|green|blue)\b/
 		 * });
 		 */
@@ -283,27 +295,77 @@ var _ = {
 		/**
 		 * Inserts tokens _before_ another token in a language definition or any other grammar.
 		 *
-		 * As this needs to recreate the object (we cannot actually insert before keys in object literals),
-		 * we cannot just provide an object, we need an object and a key.
+		 * ## Usage
 		 *
-		 * If the grammar of `inside` and `insert` have tokens with the same name, the tokens in `inside` will be ignored.
+		 * This helper method makes it easy to modify existing languages. For example, the CSS language definition
+		 * not only defines CSS highlighting for CSS documents, but also needs to define highlighting for CSS embedded
+		 * in HTML through `<style>` elements. To do this, it needs to modify `Prism.languages.markup` and add the
+		 * appropriate tokens. However, `Prism.languages.markup` is a regular JavaScript object literal, so if you do
+		 * this:
 		 *
-		 * All references of the old object accessible from `Prism.languages` or `insert` will be replace with the new one.
+		 * ```js
+		 * Prism.languages.markup.style = {
+		 *     // token
+		 * };
+		 * ```
 		 *
-		 * @param {string} inside The property of `root` that contains the object to be modified.
+		 * then the `style` token will be added (and processed) at the end. `insertBefore` allows you to insert tokens
+		 * before existing tokens. For the CSS example above, you would use it like this:
 		 *
-		 * This is usually a language id.
+		 * ```js
+		 * Prism.languages.insertBefore('markup', 'cdata', {
+		 *     'style': {
+		 *         // token
+		 *     }
+		 * });
+		 * ```
+		 *
+		 * ## Special cases
+		 *
+		 * If the grammars of `inside` and `insert` have tokens with the same name, the tokens in `inside`'s grammar
+		 * will be ignored.
+		 *
+		 * This behavior can be used to insert tokens after `before`:
+		 *
+		 * ```js
+		 * Prism.languages.insertBefore('markup', 'comment', {
+		 *     'comment': Prism.languages.markup.comment,
+		 *     // tokens after 'comment'
+		 * });
+		 * ```
+		 *
+		 * ## Limitations
+		 *
+		 * The main problem `insertBefore` has to solve is iteration order. Since ES2015, the iteration order for object
+		 * properties is guaranteed to be the insertion order (except for integer keys) but some browsers behave
+		 * differently when keys are deleted and re-inserted. So `insertBefore` can't be implemented by temporarily
+		 * deleting properties which is necessary to insert at arbitrary positions.
+		 *
+		 * To solve this problem, `insertBefore` doesn't actually insert the given tokens into the target object.
+		 * Instead, it will create a new object and replace all references to the target object with the new one. This
+		 * can be done without temporarily deleting properties, so the iteration order is well-defined.
+		 *
+		 * However, only references that can be reached from `Prism.languages` or `insert` will be replaced. I.e. if
+		 * you hold the target object in a variable, then the value of the variable will not change.
+		 *
+		 * ```js
+		 * var oldMarkup = Prism.languages.markup;
+		 * var newMarkup = Prism.languages.insertBefore('markup', 'comment', { ... });
+		 *
+		 * assert(oldMarkup !== Prism.languages.markup);
+		 * assert(newMarkup === Prism.languages.markup);
+		 * ```
+		 *
+		 * @param {string} inside The property of `root` (e.g. a language id in `Prism.languages`) that contains the
+		 * object to be modified.
 		 * @param {string} before The key to insert before.
 		 * @param {Grammar} insert An object containing the key-value pairs to be inserted.
-		 * @param {Object<string, Grammar>} [root] The object containing `inside`, i.e. the object that contains the object that will be modified.
+		 * @param {Object<string, any>} [root] The object containing `inside`, i.e. the object that contains the
+		 * object to be modified.
 		 *
 		 * Defaults to `Prism.languages`.
-		 * @returns {Grammar} The new grammar created.
+		 * @returns {Grammar} The new grammar object.
 		 * @public
-		 * @example
-		 * Prism.languages.insertBefore('markup', 'cdata', {
-		 *     'style': { ... }
-		 * });
 		 */
 		insertBefore: function (inside, before, insert, root) {
 			root = root || /** @type {any} */ (_.languages);
@@ -555,9 +617,17 @@ var _ = {
 	 * @param {Grammar} grammar An object containing the tokens to use.
 	 *
 	 * Usually a language definition like `Prism.languages.markup`.
-	 * @returns {Array<string | Token>} An array of strings, tokens and other arrays.
+	 * @returns {TokenStream} An array of strings and tokens, a token stream.
 	 * @memberof Prism
 	 * @public
+	 * @example
+	 * let code = `var foo = 0;`;
+	 * let tokens = Prism.tokenize(code, Prism.languages.javascript);
+	 * tokens.forEach(token => {
+	 *     if (token instanceof Prism.Token && token.type === 'number') {
+	 *         console.log(`Found numeric literal: ${token.content}`);
+	 *     }
+	 * });
 	 */
 	tokenize: function(text, grammar) {
 		var rest = grammar.rest;
@@ -641,7 +711,7 @@ _self.Prism = _;
  * Creates a new token.
  *
  * @param {string} type See {@link Token#type type}
- * @param {string | Token | Array<string|Token>} content See {@link Token#content content}
+ * @param {string | Token | TokenStream} content See {@link Token#content content}
  * @param {string|string[]} [alias] The alias(es) of the token.
  * @param {string} [matchedStr=""] A copy of the full string this token was created from.
  * @param {boolean} [greedy=false] Whether the pattern that created this token is greedy or not. Will be removed soon.
@@ -656,7 +726,7 @@ function Token(type, content, alias, matchedStr, greedy) {
 	 * This is usually the key of a pattern in a {@link Grammar}.
 	 *
 	 * @member {string}
-	 * @see TokenObject
+	 * @see GrammarToken
 	 * @public
 	 */
 	this.type = type;
@@ -665,7 +735,7 @@ function Token(type, content, alias, matchedStr, greedy) {
 	 *
 	 * This will be a token stream if the pattern matched also defined an `inside` grammar.
 	 *
-	 * @member {string | Token | Array<string|Token>}
+	 * @member {string | Token | TokenStream}
 	 * @public
 	 */
 	this.content = content;
@@ -673,7 +743,7 @@ function Token(type, content, alias, matchedStr, greedy) {
 	 * The alias(es) of the token.
 	 *
 	 * @member {string|string[]}
-	 * @see TokenObject
+	 * @see GrammarToken
 	 * @public
 	 */
 	this.alias = alias;
@@ -682,12 +752,28 @@ function Token(type, content, alias, matchedStr, greedy) {
 }
 
 /**
+ * A token stream is an array of strings and {@link Token Token} objects.
+ *
+ * Token streams have to fulfill a few properties that are assumed by most functions (mostly internal ones) that process
+ * them.
+ *
+ * 1. No adjacent strings.
+ * 2. No empty strings.
+ *
+ *    The only exception here is the token stream that only contains the empty string and nothing else.
+ *
+ * @typedef {Array<string | Token>} TokenStream
+ * @global
+ * @public
+ */
+
+/**
  * Converts the given token or token stream to an HTML representation.
  *
  * The following hooks will be run:
  * 1. `wrap`: On each {@link Token}.
  *
- * @param {string | Token | Array<string|Token>} o The token or token stream to be converted.
+ * @param {string | Token | TokenStream} o The token or token stream to be converted.
  * @param {string} language The name of current language.
  * @returns {string} The HTML representation of the token or token stream.
  * @memberof Token
@@ -1038,7 +1124,7 @@ if (typeof global !== 'undefined') {
 /**
  * The expansion of a simple `RegExp` literal to support additional properties.
  *
- * @typedef TokenObject
+ * @typedef GrammarToken
  * @property {RegExp} pattern The regular expression of the token.
  * @property {boolean} [lookbehind=false] If `true`, then the first capturing group of `pattern` will (effectively)
  * behave as a lookbehind group meaning that the captured text will not be part of the matched text of the new token.
@@ -1052,13 +1138,15 @@ if (typeof global !== 'undefined') {
  *
  * Note: This can cause infinite recursion. Be careful when you embed different languages or even the same language into
  * each another.
+ * @global
  * @public
 */
 
 /**
  * @typedef Grammar
- * @type {Object<string, RegExp | TokenObject | Array<RegExp | TokenObject>>}
+ * @type {Object<string, RegExp | GrammarToken | Array<RegExp | GrammarToken>>}
  * @property {Grammar} [rest] An optional grammar object that will be appended to this grammar.
+ * @global
  * @public
  */
 
@@ -1068,6 +1156,7 @@ if (typeof global !== 'undefined') {
  * @callback HighlightCallback
  * @param {Element} element The element successfully highlighted.
  * @returns {void}
+ * @global
  * @public
 */
 
@@ -1075,5 +1164,6 @@ if (typeof global !== 'undefined') {
  * @callback HookCallback
  * @param {Object<string, any>} env The environment variables of the hook.
  * @returns {void}
+ * @global
  * @public
  */
