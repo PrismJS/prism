7f2db5e8e4dd0791ff0a9f2f9a985bfb9afea4d0
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-08-12 12:58:11 +0200

    Spelling

--- components/prism-stan.js
+++ components/prism-stan.js
@@ -3,7 +3,7 @@
 Prism.languages.stan = {
 	'comment': /\/\/.*|\/\*[\s\S]*?\*\/|#(?!include).*/,
 	'string': {
-		// String literals can contain spaces and any printable ASCII character except " and \
+		// String literals can contain spaces and any printable ASCII characters except for " and \
 		// https://mc-stan.org/docs/2_24/reference-manual/print-statements-section.html#string-literals
 		pattern: /"[\x20\x21\x23-\x5B\x5D-\x7E]*"/,
 		greedy: true
@@ -35,7 +35,7 @@ Prism.languages.stan = {
 	},
 	'keyword': [
 		/\b(?:break|cholesky_factor_corr|cholesky_factor_cov|continue|corr_matrix|cov_matrix|data|else|for|functions|generated|if|in|increment_log_prob|int|matrix|model|ordered|parameters|positive_ordered|print|quantities|real|reject|return|row_vector|simplex|target|transformed|unit_vector|vector|void|while)\b/,
-		// these are function that are known to take another function as their first argument.
+		// these are functions that are known to take another function as their first argument.
 		/\b(?:algebra_solver|integrate_1d|integrate_ode|integrate_ode_bdf|integrate_ode_rk45|map_rect)\b/
 	],
 	'function': /\b[a-z]\w*(?=\s*\()/i,
