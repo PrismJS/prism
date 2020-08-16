7d79e19091d5eaabb81826b2b7d636d983dad742
Barilko Vitaliy <support@softonit.ru>
2020-08-16 16:50:13 +0300

    Update components/prism-bsl.js
    Co-authored-by: Michael Schmidt <mitchi5000.ms@googlemail.com>

--- components/prism-bsl.js
+++ components/prism-bsl.js
@@ -27,7 +27,10 @@ Prism.languages.bsl = {
 			pattern: /\b(?:while|for|new|break|try|except|raise|else|endtry|undefined|function|var|return|endfunction|null|if|elseif|procedure|endprocedure|then|val|export|endif|in|each|true|false|to|do|enddo|true|false)\b/i
 		}
 	],
-	'number': /(?:\b\d+\.?\d*|\B\.\d+)(?:E[+-]?\d+)?/i,
+	'number': {
+		pattern: /(^(?=\d)|[^\w\u0400-\u0484\u0487-\u052f\u1c80-\u1c88\u1d2b\u1d78\u2de0-\u2dff\ua640-\ua69f\ufe2e\ufe2f])(?:\d+\.?\d*|\.\d+)(?:E[+-]?\d+)?/i,
+		lookbehind: true
+	},
 	'operator': [	
 		/<[=]?|>[=]?|[+\-*\/]=?|[\%=]/i,
 		// RU
