4d0d06f6c30f2927f2580786d46ad303b718917b
Sri Harsha Chilakapati <sriharshachilakapati@users.noreply.github.com>
2020-08-30 11:58:23 +0530

    Apply suggestions from code review
    Co-authored-by: Michael Schmidt <mitchi5000.ms@googlemail.com>

--- components/prism-purescript.js
+++ components/prism-purescript.js
@@ -1,11 +1,11 @@
 Prism.languages.purescript = Prism.languages.extend('haskell', {
 	'keyword': /\b(?:ado|case|class|data|derive|do|else|forall|if|in|infixl|infixr|instance|let|module|newtype|of|primitive|then|type|where)\b/,
 
-	'import_statement': {
+	'import-statement': {
 		// The imported or hidden names are not included in this import
 		// statement. This is because we want to highlight those exactly like
 		// we do for the names in the program.
-		pattern: /((?:\r?\n|\r|^)\s*)import\s+(?:[A-Z][\w']*)(?:\.[A-Z][\w']*)*(?:\s+as\s+(?:[A-Z][_a-zA-Z0-9']*)(?:\.[A-Z][\w']*)*)?(?:\s+hiding\b)?/m,
+		pattern: /(^\s*)import\s+[A-Z][\w']*(?:\.[A-Z][\w']*)*(?:\s+as\s+[A-Z][\w']*(?:\.[A-Z][\w']*)*)?(?:\s+hiding\b)?/m,
 		lookbehind: true,
 		inside: {
 			'keyword': /\b(?:import|as|hiding)\b/
@@ -13,7 +13,7 @@ Prism.languages.purescript = Prism.languages.extend('haskell', {
 	},
 
 	// These are builtin functions only. Constructors are highlighted later as a constant.
-	'builtin': /\b(?:when|unless|liftA1|apply|bind|discard|join|ifM|identity|whenM|unlessM|liftM1|ap|compose|otherwise|top|bottom|recip|eq|notEq|degree|div|mod|lcm|gcd|flip|const|map|void|flap|conj|disj|not|mempty|compare|min|max|comparing|clamp|between|sub|negate|append|add|zero|mul|one|show|unit|absurd)\b/,
+	'builtin': /\b(?:absurd|add|ap|append|apply|between|bind|bottom|clamp|compare|comparing|compose|conj|const|degree|discard|disj|div|eq|flap|flip|gcd|identity|ifM|join|lcm|liftA1|liftM1|map|max|mempty|min|mod|mul|negate|not|notEq|one|otherwise|recip|show|sub|top|unit|unless|unlessM|void|when|whenM|zero)\b/,
 });
 
 Prism.languages.purs = Prism.languages.purescript;
