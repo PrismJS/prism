16c1d8fc945a4bcaf3a8f0a155a7b19579c48607
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-08-31 16:11:09 +0200

    Done

--- components/prism-sml.js
+++ components/prism-sml.js
@@ -2,19 +2,6 @@
 // https://people.mpi-sws.org/~rossberg/sml.html
 (function (Prism) {
 
-	/**
-	 * Wraps the given pattern, so that it can't be part of an identifier.
-	 *
-	 * The wrapped pattern requires `lookbehind: true`.
-	 *
-	 * @param {string} source
-	 * @param {string} [flags]
-	 * @returns {RegExp}
-	 */
-	function boundary(source, flags) {
-		return RegExp(/(^|[^\w'.!$%&#+\-/:<=>?@\\~`^|*])/.source + "(?:" + source + ")" + /asd/.source, flags || "");
-	}
-
 	var keywords = /\b(?:abstype|and|andalso|as|case|datatype|do|else|end|eqtype|exception|fn|fun|functor|handle|if|in|include|infix|infixr|let|local|nonfix|of|op|open|orelse|raise|rec|sharing|sig|signature|struct|structure|then|type|val|where|while|with|withtype)\b/i;
 
 	Prism.languages.sml = {
@@ -25,31 +12,42 @@
 			greedy: true
 		},
 
+		'class-name': [
+			{
+				// This is only an approximation since the real grammar is context-free
+				//
+				// Why the main loop so complex?
+				// The main loop is approximately the same as /(?:\s*(?:[*,]|->)\s*<TERMINAL>)*/ which is, obviously, a lot
+				// simpler. The difference is that if a comma is the last iteration of the loop, then the terminal must be
+				// followed by a long identifier.
+				pattern: RegExp(
+					/((?:^|[^:]):\s*)<TERMINAL>(?:\s*(?:(?:\*|->)\s*<TERMINAL>|,\s*<TERMINAL>(?:(?=<NOT-LAST>)|(?!<NOT-LAST>)\s+<LONG-ID>)))*/.source
+						.replace(/<NOT-LAST>/g, function () { return /\s*(?:[*,]|->)/.source; })
+						.replace(/<TERMINAL>/g, function () {
+							return /(?:'[\w']*|<LONG-ID>|\((?:[^()]|\([^()]*\))*\)|\{(?:[^{}]|\{[^{}]*\})*\})(?:\s+<LONG-ID>)*/.source;
+						})
+						.replace(/<LONG-ID>/g, function () { return /(?!<KEYWORD>)[a-z\d_][\w'.]*/.source; })
+						.replace(/<KEYWORD>/g, function () { return keywords.source; }),
+					'i'
+				),
+				lookbehind: true,
+				greedy: true,
+				inside: null // see below
+			},
+			{
+				pattern: /((?:^|[^\w'])(?:datatype|exception|functor|signature|structure|type)\s+)[a-z_][\w'.]*/i,
+				lookbehind: true
+			}
+		],
+		'function': {
+			pattern: /((?:^|[^\w'])fun\s+)[a-z_][\w'.]*/i,
+			lookbehind: true
+		},
+
 		'keyword': keywords,
 		'variable': {
 			pattern: /(^|[^\w'])'[\w']*/,
 			lookbehind: true,
-		},
-		'class-name': {
-			// This is only an approximation since the real grammar is context-free
-			//
-			// Why the main loop so complex?
-			// The main loop is approximately the same as /(?:\s*(?:[*,]|->)\s*<TERMINAL>)*/ which is, obviously, a lot
-			// simpler. The difference is that if a comma is the last iteration of the loop, then the terminal must be
-			// followed by a long identifier.
-			pattern: RegExp(
-				/((?:^|[^:]):\s*)<TERMINAL>(?:\s*(?:(?:\*|->)\s*<TERMINAL>|,\s*<TERMINAL>(?:(?=<NOT-LAST>)|(?!<NOT-LAST>)\s+<LONG-ID>)))*/.source
-					.replace(/<NOT-LAST>/g, function () { return /\s*(?:[*,]|->)/.source; })
-					.replace(/<TERMINAL>/g, function () {
-						return /(?:'[\w']*|<LONG-ID>|\((?:[^()]|\([^()]*\))*\)|\{(?:[^{}]|\{[^{}]*\})*\})(?:\s+<LONG-ID>)*/.source;
-					})
-					.replace(/<LONG-ID>/g, function () { return /(?!<KEYWORD>)[a-z\d_][\w'.]*/.source; })
-					.replace(/<KEYWORD>/g, function () { return keywords.source; }),
-				'i'
-			),
-			lookbehind: true,
-			greedy: true,
-			inside: null // see below
 		},
 
 		'number': /~?\b(?:\d+(?:\.\d+)?(?:e~?\d+)?|0x[\da-f]+)\b/i,
@@ -58,11 +56,12 @@
 			alias: 'constant'
 		},
 
-		'operator': /\.\.\.|=>?|->|[<>]=?|[!+\-*/^#|]/,
+		'boolean': /\b(?:false|true)\b/i,
+		'operator': /\.\.\.|:[>=:]|=>?|->|[<>]=?|[!+\-*/^#|@~]/,
 		'punctuation': /[(){}\[\].:,;]/
 	};
 
-	Prism.languages.sml['class-name'].inside = Prism.languages.sml;
+	Prism.languages.sml['class-name'][0].inside = Prism.languages.sml;
 
 	Prism.languages.smlnj = Prism.languages.sml;
 
