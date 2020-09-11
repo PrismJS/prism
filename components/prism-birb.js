b51c0db60a47b3f5a7535f69235553f0bf3a6f1b
Calamity210 <Calamity210@users.noreply.github.com>
2020-09-11 09:29:17 -0400

    Corrected birb support

--- components/prism-birb.js
+++ components/prism-birb.js
@@ -1,18 +1,17 @@
 Prism.languages.birb = Prism.languages.extend('clike', {
 	'class-name': [
-		/\b[A-Z](?:\w*[a-zA-Z]\w*)?\b/,
-    	/\b[A-Z]\w*(?=\s+\w+\s*[;,=())])/
-    ],
-	'keyword': /\b(?:assert|bool|break|case|class|const|default|double|else|enum|final|follows|for|grab|if|int|nest|next|new|noSeeb|return|static|switch|throw|var|void|while)\b/,
-	'variable': /[a-z_]\w*/,
-	'function': [
-    	Prism.languages.clike.function,
-    	{
-    		pattern: /[a-z_]\w*\(\)/,
-    		greedy: true
-    	}
-    ],
-	'operator': /\+\+|--|&&|\|\||<<=?|>>=?|~(?:\/=?)?|[+\-*\/%&^|=!<>]=?|\?/,
+		/\b[A-Z](?:[\d_]*[a-zA-Z]\w*)?\b/,
+
+		// matches variable and function return types (parameters as well).
+		/\b[A-Z]\w*(?=\s+\w+\s*[;,=()])/
+	],
+	'string': {
+		pattern: /r?("|')(?:\\.|(?!\1)[^\\])*\1/,
+		greedy: true
+	},
+	'keyword': /\b(?:assert|break|case|class|const|default|else|enum|final|follows|for|grab|if|nest|next|new|noSeeb|return|static|switch|throw|var|void|while)\b/,
+	'variable': /\b[a-z_]\??\w*\b/,
+	'operator': /\+\+|--|&&|\|\||<<=?|>>=?|~(?:\/=?)?|[+\-*\/%&^|=!<>:]=?|\?/,
 });
 
 Prism.languages.insertBefore('birb','function',{
@@ -22,4 +21,3 @@ Prism.languages.insertBefore('birb','function',{
 		alias: 'symbol'
 	}
 });
-
