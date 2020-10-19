7e6f02bf91abd9709b14385ea78a527aaea1e8a0
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2020-10-18 22:36:28 -0400

    fixed timeline language definition

--- components/prism-timeline.js
+++ components/prism-timeline.js
@@ -1,8 +1,9 @@
 (function (Prism) {
 	Prism.languages.timeline = {
 		'timepoint': {
-			pattern: /^\s*(?:\.[0-9]+|(?:0|[1-9][0-9]*)(?:\.[0-9]*)?)(?=\s|$)/gm,
+			pattern: /(^\s*)(?:\.\d+|(?:0|[1-9]\d*)(?:\.\d*)?)(?=\s|$)/m,
 			alias: 'number',
+			lookbehind: true
 		},
 	}
 }(Prism))
