7a74497a15dfcf9d89add9baf5b429f3f3af7419
Michael Schmidt <mitchi5000.ms@googlemail.com>
2020-11-04 21:50:43 +0100

    Made Autoloader and Diff Highlight compatible (#2580)

--- plugins/autoloader/prism-autoloader.js
+++ plugins/autoloader/prism-autoloader.js
@@ -460,7 +460,13 @@
 		}
 
 		var deps = getDependencies(element);
-		deps.push(language);
+		if (/^diff-./i.test(language)) {
+			// the "diff-xxxx" format is used by the Diff Highlight plugin
+			deps.push('diff');
+			deps.push(language.substr('diff-'.length));
+		} else {
+			deps.push(language);
+		}
 
 		if (!deps.every(isLoaded)) {
 			// the language or some dependencies aren't loaded
