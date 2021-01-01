af1513fe3f6898df2d2abef70f332b2db3e9dc68
taggon <gonom9@gmail.com>
2021-01-02 02:33:32 +0900

    Fix tests failed with node v10

--- components/prism-v.js
+++ components/prism-v.js
@@ -11,11 +11,11 @@ Prism.languages.v = Prism.languages.extend('clike', {
 			greedy: true,
 			inside: {
 				'interpolation': {
-					pattern: /(?:^|[^\\])(?:\\{2})*\$(?:\{[^{}]*\}|\w+(\.\w+(?:\([^\(\)]*\))?|\[[^\[\]]+\])*)/,
+					pattern: /((?:^|[^\\])(?:\\{2})*)\$(?:\{[^{}]*\}|\w+(?:\.\w+(?:\([^\(\)]*\))?|\[[^\[\]]+\])*)/,
 					lookbehind: true,
 					inside: {
 						'interpolation-variable': {
-							pattern: /^\$\w+(\.\w+(?:\([^\(\)]*\))?|\[[^\[\]]+\])*$/,
+							pattern: /^\$\w+(?:\.\w+(?:\([^\(\)]*\))?|\[[^\[\]]+\])*$/,
 							alias: 'variable'
 						},
 						'interpolation-punctuation': {
@@ -33,9 +33,9 @@ Prism.languages.v = Prism.languages.extend('clike', {
 		lookbehind: true
 	},
 	'keyword': /\b(?:as|asm|assert|atomic|break|const|continue|defer|else|embed|enum|fn|for|__global|go(?:to)?|if|import|in|interface|is|lock|match|module|mut|none|or|pub|return|rlock|select|shared|sizeof|static|struct|type(?:of)?|union|unsafe)\b/,
-	'number': /\b(?:0x[a-f\d]+(_[a-f\d]+)*|0b[01]+(_[01]+)*|0o[0-7]+(_[0-7]+)*|\d+(?:_\d+)*(?:\.\d+(?:_\d+)*)?)\b/i,
+	'number': /\b(?:0x[a-f\d]+(?:_[a-f\d]+)*|0b[01]+(?:_[01]+)*|0o[0-7]+(?:_[0-7]+)*|\d+(?:_\d+)*(?:\.\d+(?:_\d+)*)?)\b/i,
 	'operator': /~|[*\/%^!=]=?|\+[=+]?|-[=-]?|\|[=|]?|&(?:=|&|\^=?)?|>(?:>=?|=)?|<(?:<=?|=|-)?|:=|\.\.\./,
-	'builtin': /\b(?:any(_int|_float)?|bool|byte(ptr)?|charptr|i(8|16|nt|64|128)|rune|size_t|u?string|u(16|32|64|128))\b/
+	'builtin': /\b(?:any(?:_int|_float)?|bool|byte(?:ptr)?|charptr|i(?:8|16|nt|64|128)|rune|size_t|string|u(?:16|32|64|128))\b/
 });
 
 Prism.languages.insertBefore('v', 'function', {
