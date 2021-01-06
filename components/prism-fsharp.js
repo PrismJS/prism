2ecd5109e5a1a3b604df1eb0281c0f16b05a18bf
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-01-06 21:16:17 +0100

    F#: Fixed comment false positive

--- components/prism-fsharp.js
+++ components/prism-fsharp.js
@@ -1,7 +1,7 @@
 Prism.languages.fsharp = Prism.languages.extend('clike', {
 	'comment': [
 		{
-			pattern: /(^|[^\\])\(\*[\s\S]*?\*\)/,
+			pattern: /(^|[^\\])\(\*(?!\))[\s\S]*?\*\)/,
 			lookbehind: true
 		},
 		{
