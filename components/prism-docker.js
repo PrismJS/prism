2f57ef977b4b6de941c5d49a04f4db3300ebb2ef
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-01-14 17:02:55 +0100

    WIP

--- components/prism-docker.js
+++ components/prism-docker.js
@@ -1,14 +1,51 @@
-Prism.languages.docker = {
-	'keyword': {
-		pattern: /(^\s*)(?:ADD|ARG|CMD|COPY|ENTRYPOINT|ENV|EXPOSE|FROM|HEALTHCHECK|LABEL|MAINTAINER|ONBUILD|RUN|SHELL|STOPSIGNAL|USER|VOLUME|WORKDIR)(?=\s)/mi,
-		lookbehind: true
-	},
-	'string': /("|')(?:(?!\1)[^\\\r\n]|\\(?:\r\n|[\s\S]))*\1/,
-	'comment': {
-		pattern: /#.*/,
-		greedy: true
-	},
-	'punctuation': /---|\.\.\.|[:[\]{}\-,|>?]/
-};
+(function (Prism) {
 
-Prism.languages.dockerfile = Prism.languages.docker;
+	var string = /"(?:[^"\\\r\n]|\\(?:\r\n|[\s\S]))*"|'(?:[^'\\\r\n]|\\(?:\r\n|[\s\S]))*'/;
+	var commentRule = {
+		pattern: /(^[ \t]*)#.*/m,
+		lookbehind: true,
+		greedy: true
+	};
+
+	Prism.languages.docker = {
+		'command': {
+			pattern: /(^[ \t]*)(?:ADD|ARG|CMD|COPY|ENTRYPOINT|ENV|EXPOSE|FROM|HEALTHCHECK|LABEL|MAINTAINER|ONBUILD|RUN|SHELL|STOPSIGNAL|USER|VOLUME|WORKDIR)(?=\s)(?:\\.|[^\r\n\\])*(?:\\$(?:\s|#.*$)*(?![\s#])(?:\\.|[^\r\n\\])*)*/mi,
+			lookbehind: true,
+			inside: {
+				'instruction': {
+					pattern: /^\w+|(\s)AS(?=\s)/,
+					lookbehind: true,
+					greedy: true,
+					alias: 'keyword'
+				},
+				'option': {
+					pattern: RegExp(/(^\s+)--[\w-]+=(?:<str>|(?!["'])(?:[^\s\\]|\\.)+)/.source.replace(/<str>/g, function () { return string.source })),
+					lookbehind: true,
+					inside: {
+						'property': /^--[\w-]+/,
+						'attr-value': {
+							pattern: /^(=)[\s\S]+/,
+							lookbehind: true
+						},
+						'punctuation': /=/
+					}
+				},
+				'comment': commentRule,
+				'string': {
+					pattern: string,
+					greedy: true
+				},
+				'variable': /\$(?:\w+|\{[^{}"'\\]*\})/,
+				'operator': /\\$/m,
+				// punctuation for JSON arrays
+				'punctuation': {
+					pattern: /("\s*)/
+				}
+			}
+		},
+		'comment': commentRule
+	};
+
+	Prism.languages.dockerfile = Prism.languages.docker;
+
+}(Prism));
