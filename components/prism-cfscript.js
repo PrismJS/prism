8ae10254fd9611456282a5469bb7fccc2ab718e5
Matthew J Clemente <mjclemente@gmail.com>
2021-02-19 09:32:45 -0500

    Simplify patterns

--- components/prism-cfscript.js
+++ components/prism-cfscript.js
@@ -16,31 +16,19 @@ Prism.languages.cfscript = Prism.languages.extend('clike', {
 			greedy: true
 		}
 	],
-	'keyword': [
-		{
-			pattern: /\b(?:abstract|break|catch|component|continue|default|do|else|extends|final|finally|for|function|if|in|include|package|private|property|public|remote|required|rethrow|return|static|switch|throw|try|var|while|xml)\b(?!\s*\=)/
-		}
-	],
+	'keyword': /\b(?:abstract|break|catch|component|continue|default|do|else|extends|final|finally|for|function|if|in|include|package|private|property|public|remote|required|rethrow|return|static|switch|throw|try|var|while|xml)\b(?!\s*\=)/,
 	'operator': [
-		{
-			pattern: /\+\+|--|&&|\|\||::|=>|[!=]==|<=?|>=?|[-+*/%&|^!=<>]=?|\?(?:\.|:)?|[?:]/
-		},
-		{
-			pattern: /\b(?:and|contains|eq|equal|eqv|gt|gte|imp|is|lt|lte|mod|not|or|xor)\b/
-		}
+		/\+\+|--|&&|\|\||::|=>|[!=]==|<=?|>=?|[-+*/%&|^!=<>]=?|\?(?:\.|:)?|[?:]/,
+		/\b(?:and|contains|eq|equal|eqv|gt|gte|imp|is|lt|lte|mod|not|or|xor)\b/
 	],
-	'scope': [
-		{
-			pattern: /\b(?:application|arguments|cgi|client|cookie|local|session|super|this|variables)\b/,
-			alias: ['global']
-		}
-	],
-	'type': [
-		{
-			pattern: /\b(?:any|array|binary|boolean|date|guid|numeric|query|string|struct|uuid|void|xml)\b/,
-			alias: 'builtin'
-		}
-	]
+	'scope': {
+		pattern: /\b(?:application|arguments|cgi|client|cookie|local|session|super|this|variables)\b/,
+		alias: 'global'
+	},
+	'type': {
+		pattern: /\b(?:any|array|binary|boolean|date|guid|numeric|query|string|struct|uuid|void|xml)\b/,
+		alias: 'builtin'
+	}
 });
 
 Prism.languages.insertBefore('cfscript', 'keyword', {
