fb3e15f004167152dae4dae551077be4c1be2444
Dmitry Surkov <dmitry.surkov@nezaboodka.com>
2021-03-03 19:13:50 +0300

    Add Nevod language regexp patterns.

---
+++ components/prism-nevod.js
@@ -0,0 +1,60 @@
+Prism.languages.nevod = {
+	'comment': /(?:\/\/.*)|(?:\/\*[\s\S]*?(?:\*\/|$))/,
+	'string': {
+		pattern: /("|')[\s\S]*?\1!?\*?/,
+		greedy: true,
+		inside: {
+			modifiers: /!?\*?$/,
+		},
+	},
+	'namespace': {
+		pattern: /@namespace(\s+[a-zA-Z0-9\-.]*)?\s*{/,
+		inside: {
+			keyword: /@namespace\b/,
+			name: /\s+[a-zA-Z0-9\-.]*/,
+			colon: /:$/,
+		},
+	},
+	'pattern': {
+		pattern: /(?:#?[a-zA-Z0-9\-.]+)\s*(?:[({]\s*(?:(?:~\s*)?[a-zA-Z0-9\-.]*\s*(?:,\s*(?:(?:~\s*)?[a-zA-Z0-9\-.]*))*)[)}])?\s*=/,
+		inside: {
+			name: /^#?[a-zA-Z0-9\-.]+/,
+			equal: /=/,
+			attributes: {
+				pattern: /\(.*\)/,
+				inside: {
+					'attr-name': /[a-zA-Z0-9\-.]+/,
+					'attr-hidden-mark': /~/,
+					'attr-punctuation': /[,]/,
+					'attr-bracket': /[()]/,
+				},
+			},
+		},
+	},
+	'search': /#[a-zA-Z0-9\-.]+(\.\*)?\s*(?=;)/,
+	'keyword': /@(require|inside|outside|having|search|pattern|where)\b/,
+	'basic-reference': {
+		pattern: /\b(Word|Punct|Symbol|Space|LineBreak|Start|End|Alpha|AlphaNum|Num|NumAlpha|Blank|WordBreak|Any)(\(.*\)|\b)/,
+		inside: {
+			name: /Word|Punct|Symbol|Space|LineBreak|Start|End/,
+			params: /\(.*\)/,
+		},
+	},
+	'quantifier': /\b\d+(\+|-\d+)?\b/,
+	'conjunction': /&/,
+	'exception': /~/,
+	'optionality': /\?/,
+	'bracket': /[()]/,
+	'square-bracket': /[[\]]/,
+	'curly-bracket': /[{}]/,
+	'semi-colon': /;/,
+	'attr-capture': {
+		pattern: /[a-zA-Z0-9\-.]+\s*:/,
+		inside: {
+			'attr-name': /[a-zA-Z0-9\-.]+/,
+			colon: /:/,
+		},
+	},
+	'punctuation': /[,+_]|\.{2,3}/,
+	'pattern-reference': /\S+/
+}
