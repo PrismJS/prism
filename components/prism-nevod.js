c448cae8ae51f02c731cf87f95d8942525035da8
Dmitry Surkov <dmitry.surkov@nezaboodka.com>
2021-03-04 20:42:32 +0300

    Update Nevod language patterns.

--- components/prism-nevod.js
+++ components/prism-nevod.js
@@ -1,44 +1,54 @@
 Prism.languages.nevod = {
 	'comment': /(?:\/\/.*)|(?:\/\*[\s\S]*?(?:\*\/|$))/,
 	'string': {
-		pattern: /("|')[\s\S]*?\1!?\*?/,
-		greedy: true,
-		inside: {
-			modifiers: /!?\*?$/,
-		},
+	  pattern: /("|')[\s\S]*?\1!?\*?/,
+	  greedy: true,
+	  inside: {
+		'modifiers': /!?\*?$/,
+	  },
 	},
-	'keyword': /@(require|inside|outside|having|search|pattern|where)\b/,
+	'keyword': /@(inside|outside|having|search|where)\b/,
+	'require': /@require\b/,
 	'namespace': {
-		pattern: /@namespace(\s+[a-zA-Z0-9\-.]*)?\s*{/,
-		inside: {
-			keyword: /@namespace\b/,
-			name: /\s+[a-zA-Z0-9\-.]*/,
-			colon: /:$/,
-		},
+	  pattern: /@namespace(\s+[a-zA-Z0-9\-.]*)?\s*{/,
+	  inside: {
+		'keyword': /@namespace\b/,
+		'name': /\b[a-zA-Z0-9\-.]+/
+	  },
 	},
 	'pattern': {
-		pattern: /(?:#?[a-zA-Z0-9\-.]+)\s*(?:[({]\s*(?:(?:~\s*)?[a-zA-Z0-9\-.]*\s*(?:,\s*(?:(?:~\s*)?[a-zA-Z0-9\-.]*))*)[)}])?\s*=/,
-		inside: {
-			name: /^#?[a-zA-Z0-9\-.]+/,
-			equal: /=/,
-			attributes: {
-				pattern: /\(.*\)/,
-				inside: {
-					'attr-name': /[a-zA-Z0-9\-.]+/,
-					'attr-hidden-mark': /~/,
-					'attr-punctuation': /[,]/,
-					'attr-bracket': /[()]/,
-				},
-			},
+	  pattern: /(@pattern\s+)?(?:#?[a-zA-Z0-9\-.]+)\s*(?:[(]\s*(?:(?:~\s*)?[a-zA-Z0-9\-.]*\s*(?:,\s*(?:(?:~\s*)?[a-zA-Z0-9\-.]*))*)[)])?\s*=/,
+	  inside: {
+		'keyword': /@pattern\b/,
+		'name': {
+		  pattern: /^(\s+)?#?[a-zA-Z0-9\-.]+/,
+		  lookbehind: true
 		},
+		'equal': /=/,
+		'attributes': {
+		  pattern: /\(.*\)/,
+		  inside: {
+			'attr-name': /[a-zA-Z0-9\-.]+/,
+			'attr-hidden-mark': /~/,
+			'attr-punctuation': /[,]/,
+			'attr-bracket': /[()]/,
+		  },
+		},
+	  },
 	},
-	'search': /#[a-zA-Z0-9\-.]+(\.\*)?\s*(?=;)/,
+	'search': {
+	  pattern: /(@search\s+|#)[a-zA-Z0-9\-.]+(\.\*)?\s*(?=;)/,
+	  inside: {
+		'keyword': /@search\b/,
+		'name': /#?.*(?=;)/
+	  }
+	},
 	'basic-reference': {
-		pattern: /\b(Word|Punct|Symbol|Space|LineBreak|Start|End|Alpha|AlphaNum|Num|NumAlpha|Blank|WordBreak|Any)(\(.*\)|\b)/,
-		inside: {
-			name: /Word|Punct|Symbol|Space|LineBreak|Start|End/,
-			params: /\(.*\)/,
-		},
+	  pattern: /\b(Word|Punct|Symbol|Space|LineBreak|Start|End|Alpha|AlphaNum|Num|NumAlpha|Blank|WordBreak|Any)(\(.*\)|\b)/,
+	  inside: {
+		name: /.+(?=[(\b])/,
+		params: /\(.*\)/,
+	  },
 	},
 	'quantifier': /\b\d+(\+|-\d+)?\b/,
 	'conjunction': /&/,
@@ -49,12 +59,12 @@ Prism.languages.nevod = {
 	'curly-bracket': /[{}]/,
 	'semi-colon': /;/,
 	'attr-capture': {
-		pattern: /[a-zA-Z0-9\-.]+\s*:/,
-		inside: {
-			'attr-name': /[a-zA-Z0-9\-.]+/,
-			colon: /:/,
-		},
+	  pattern: /[a-zA-Z0-9\-.]+\s*:/,
+	  inside: {
+		'attr-name': /[a-zA-Z0-9\-.]+/,
+		colon: /:/,
+	  },
 	},
 	'punctuation': /[,+_]|\.{2,3}/,
-	'pattern-reference': /\S+/
-}
+	'name': /\S+/
+  }
