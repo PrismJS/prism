ee9bedb25026638b7193822a3a08de6b21abd34d
Dmitry Surkov <dmitry.surkov@nezaboodka.com>
2021-03-05 00:01:10 +0300

    square-bracket -> repetition, curly-bracket -> variation.

--- components/prism-nevod.js
+++ components/prism-nevod.js
@@ -1,70 +1,70 @@
 Prism.languages.nevod = {
 	'comment': /(?:\/\/.*)|(?:\/\*[\s\S]*?(?:\*\/|$))/,
 	'string': {
-	  pattern: /("|')[\s\S]*?\1!?\*?/,
-	  greedy: true,
-	  inside: {
-		'modifiers': /!?\*?$/,
-	  },
+		pattern: /("|')[\s\S]*?\1!?\*?/,
+		greedy: true,
+		inside: {
+			'modifiers': /!?\*?$/,
+		},
 	},
 	'keyword': /@(inside|outside|having|search|where)\b/,
 	'require': /@require\b/,
 	'namespace': {
-	  pattern: /@namespace(\s+[a-zA-Z0-9\-.]*)?\s*{/,
-	  inside: {
-		'keyword': /@namespace\b/,
-		'name': /\b[a-zA-Z0-9\-.]+/
-	  },
+		pattern: /@namespace(\s+[a-zA-Z0-9\-.]*)?\s*{/,
+		inside: {
+			'keyword': /@namespace\b/,
+			'name': /\b[a-zA-Z0-9\-.]+/
+		},
 	},
 	'pattern': {
-	  pattern: /(@pattern\s+)?(?:#?[a-zA-Z0-9\-.]+)\s*(?:[(]\s*(?:(?:~\s*)?[a-zA-Z0-9\-.]*\s*(?:,\s*(?:(?:~\s*)?[a-zA-Z0-9\-.]*))*)[)])?\s*=/,
-	  inside: {
-		'keyword': /@pattern\b/,
-		'name': {
-		  pattern: /^(\s+)?#?[a-zA-Z0-9\-.]+/,
-		  lookbehind: true
+		pattern: /(@pattern\s+)?(?:#?[a-zA-Z0-9\-.]+)\s*(?:[(]\s*(?:(?:~\s*)?[a-zA-Z0-9\-.]*\s*(?:,\s*(?:(?:~\s*)?[a-zA-Z0-9\-.]*))*)[)])?\s*=/,
+		inside: {
+			'keyword': /@pattern\b/,
+			'name': {
+				pattern: /^(\s+)?#?[a-zA-Z0-9\-.]+/,
+				lookbehind: true
+			},
+			'equal': /=/,
+			'attributes': {
+				pattern: /\(.*\)/,
+				inside: {
+					'attr-name': /[a-zA-Z0-9\-.]+/,
+					'attr-hidden-mark': /~/,
+					'attr-punctuation': /[,]/,
+					'attr-bracket': /[()]/,
+				},
+			},
 		},
-		'equal': /=/,
-		'attributes': {
-		  pattern: /\(.*\)/,
-		  inside: {
-			'attr-name': /[a-zA-Z0-9\-.]+/,
-			'attr-hidden-mark': /~/,
-			'attr-punctuation': /[,]/,
-			'attr-bracket': /[()]/,
-		  },
-		},
-	  },
 	},
 	'search': {
-	  pattern: /(@search\s+|#)[a-zA-Z0-9\-.]+(\.\*)?\s*(?=;)/,
-	  inside: {
-		'keyword': /@search\b/,
-		'name': /#?.*(?=;)/
-	  }
+		pattern: /(@search\s+|#)[a-zA-Z0-9\-.]+(\.\*)?\s*(?=;)/,
+		inside: {
+			'keyword': /@search\b/,
+			'name': /#?.*(?=;)/
+		}
 	},
 	'basic-reference': {
-	  pattern: /\b(Word|Punct|Symbol|Space|LineBreak|Start|End|Alpha|AlphaNum|Num|NumAlpha|Blank|WordBreak|Any)(\(.*\)|\b)/,
-	  inside: {
-		name: /.+(?=[(\b])/,
-		params: /\(.*\)/,
-	  },
+		pattern: /\b(Word|Punct|Symbol|Space|LineBreak|Start|End|Alpha|AlphaNum|Num|NumAlpha|Blank|WordBreak|Any)(\(.*\)|\b)/,
+		inside: {
+			'name': /.+(?=[(\b])/,
+			'params': /\(.*\)/,
+		},
 	},
 	'quantifier': /\b\d+(\+|-\d+)?\b/,
 	'conjunction': /&/,
 	'exception': /~/,
 	'optionality': /\?/,
 	'bracket': /[()]/,
-	'square-bracket': /[[\]]/,
-	'curly-bracket': /[{}]/,
+	'repetition': /[[\]]/,
+	'variation': /[{}]/,
 	'semi-colon': /;/,
 	'attr-capture': {
-	  pattern: /[a-zA-Z0-9\-.]+\s*:/,
-	  inside: {
-		'attr-name': /[a-zA-Z0-9\-.]+/,
-		colon: /:/,
-	  },
+		pattern: /[a-zA-Z0-9\-.]+\s*:/,
+		inside: {
+			'attr-name': /[a-zA-Z0-9\-.]+/,
+			'colon': /:/,
+		},
 	},
 	'punctuation': /[,+_]|\.{2,3}/,
 	'name': /\S+/
-  }
+}
