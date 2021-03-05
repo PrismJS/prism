a89058b0ecffdb5832f95a5680f4772d88dc5bcd
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-03-05 17:41:40 +0100

    Copy to clipboard: Removed ClipboardJS dependency

--- plugins/copy-to-clipboard/prism-copy-to-clipboard.js
+++ plugins/copy-to-clipboard/prism-copy-to-clipboard.js
@@ -9,30 +9,73 @@
 		return;
 	}
 
-	var ClipboardJS = window.ClipboardJS || undefined;
-
-	if (!ClipboardJS && typeof require === 'function') {
-		ClipboardJS = require('clipboard');
+	/**
+	 * When the given elements is clicked by the user, the given text will be copied to clipboard.
+	 *
+	 * @param {HTMLElement} element
+	 * @param {CopyInfo} copyInfo
+	 *
+	 * @typedef CopyInfo
+	 * @property {() => string} getText
+	 * @property {() => void} success
+	 * @property {(reason: unknown) => void} error
+	 */
+	function registerClipboard(element, copyInfo) {
+		element.addEventListener('click', function () {
+			copyTextToClipboard(copyInfo);
+		});
 	}
 
-	var callbacks = [];
+	// https://stackoverflow.com/a/30810322/7595472
 
-	if (!ClipboardJS) {
-		var script = document.createElement('script');
-		var head = document.querySelector('head');
+	/** @param {CopyInfo} copyInfo */
+	function fallbackCopyTextToClipboard(copyInfo) {
+		var textArea = document.createElement("textarea");
+		textArea.value = copyInfo.getText();
 
-		script.onload = function () {
-			ClipboardJS = window.ClipboardJS;
+		// Avoid scrolling to bottom
+		textArea.style.top = "0";
+		textArea.style.left = "0";
+		textArea.style.position = "fixed";
 
-			if (ClipboardJS) {
-				while (callbacks.length) {
-					callbacks.pop()();
+		document.body.appendChild(textArea);
+		textArea.focus();
+		textArea.select();
+
+		try {
+			var successful = document.execCommand('copy');
+			setTimeout(function () {
+				if (successful) {
+					copyInfo.success();
+				} else {
+					copyInfo.error();
 				}
-			}
-		};
+			}, 1);
+		} catch (err) {
+			setTimeout(function () {
+				copyInfo.error(err);
+			}, 1);
+		}
 
-		script.src = 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js';
-		head.appendChild(script);
+		document.body.removeChild(textArea);
+	}
+	/** @param {CopyInfo} copyInfo */
+	function copyTextToClipboard(copyInfo) {
+		if (navigator.clipboard) {
+			navigator.clipboard.writeText(copyInfo.getText()).then(copyInfo.success, copyInfo.error);
+		} else {
+			fallbackCopyTextToClipboard(copyInfo);
+		}
+	}
+
+	/**
+	 * Selects the text content of the given element.
+	 *
+	 * @param {Element} element
+	 */
+	function selectElementText(element) {
+		// https://stackoverflow.com/a/20079910/7595472
+		window.getSelection().selectAllChildren(element)
 	}
 
 	/**
@@ -74,32 +117,27 @@
 		linkCopy.textContent = settings['copy'];
 		linkCopy.setAttribute('type', 'button');
 
-		if (!ClipboardJS) {
-			callbacks.push(registerClipboard);
-		} else {
-			registerClipboard();
-		}
-
-		return linkCopy;
-
-		function registerClipboard() {
-			var clip = new ClipboardJS(linkCopy, {
-				'text': function () {
-					return element.textContent;
-				}
-			});
-
-			clip.on('success', function () {
+		registerClipboard(linkCopy, {
+			getText: function () {
+				return element.textContent;
+			},
+			success: function () {
 				linkCopy.textContent = settings['copy-success'];
 
 				resetText();
-			});
-			clip.on('error', function () {
+			},
+			error: function () {
 				linkCopy.textContent = settings['copy-error'];
 
+				setTimeout(function () {
+					selectElementText(element);
+				}, 1);
+
 				resetText();
-			});
-		}
+			}
+		});
+
+		return linkCopy;
 
 		function resetText() {
 			setTimeout(function () {
