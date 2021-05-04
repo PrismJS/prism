5a9cb178c3159eabdcb59486e810cd9c5d8b8a07
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-05-04 18:12:08 +0200

    Idris: Fixed import statement

--- components/prism-idris.js
+++ components/prism-idris.js
@@ -3,11 +3,14 @@ Prism.languages.idris = Prism.languages.extend('haskell', {
 		pattern: /(?:(?:--|\|\|\|).*$|{-[\s\S]*?-})/m,
 	},
 	'keyword': /\b(?:Type|case|class|codata|constructor|corecord|data|do|dsl|else|export|if|implementation|implicit|import|impossible|in|infix|infixl|infixr|instance|interface|let|module|mutual|namespace|of|parameters|partial|postulate|private|proof|public|quoteGoal|record|rewrite|syntax|then|total|using|where|with)\b/,
+	'builtin': undefined
+});
+
+Prism.languages.insertBefore('idris', 'keyword', {
 	'import-statement': {
-		pattern: /(^\s*)import\s+(?:[A-Z][\w']*)(?:\.[A-Z][\w']*)*/m,
+		pattern: /(^\s*import\s+)(?:[A-Z][\w']*)(?:\.[A-Z][\w']*)*/m,
 		lookbehind: true
 	},
-	'builtin': undefined
 });
 
 Prism.languages.idr = Prism.languages.idris;
