3579553e79f0044cbfa568d1edcdd4bd74bdc70d
Jason Kurian <JaKXz@users.noreply.github.com>
2021-05-26 15:30:01 -0400

    specify other code in .liquid as markup

--- components/prism-liquid.js
+++ components/prism-liquid.js
@@ -8,5 +8,10 @@ Prism.languages.liquid = {
 	'function': {
 		pattern: /(^|[\s;|&])(?:append|prepend|capitalize|cycle|cols|increment|decrement|abs|at_least|at_most|ceil|compact|concat|date|default|divided_by|downcase|escape|escape_once|first|floor|join|last|lstrip|map|minus|modulo|newline_to_br|plus|remove|remove_first|replace|replace_first|reverse|round|rstrip|size|slice|sort|sort_natural|split|strip|strip_html|strip_newlines|times|truncate|truncatewords|uniq|upcase|url_decode|url_encode|include|paginate)(?=$|[\s;|&])/,
 		lookbehind: true
+	},
+	'other': {
+		// We want non-blank matches
+		pattern: /\S(?:[\s\S]*\S)?/,
+		inside: Prism.languages.markup
 	}
 };
