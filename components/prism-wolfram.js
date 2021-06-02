fadf27dec2636a7d0f171ea65489b5ed69ea0003
Michael Sollami <1467556+msollami@users.noreply.github.com>
2021-06-02 15:08:46 -0400

    Fixed linting errors

--- components/prism-wolfram.js
+++ components/prism-wolfram.js
@@ -1,27 +1,27 @@
 Prism.languages.wolfram = {
-    'comment': // Allow one level of nesting - note: regex taken from applescipt
-        /\(\*(?:\(\*(?:[^*]|\*(?!\)))*\*\)|(?!\(\*)[\s\S])*?\*\)/,
-    'string': {
-        pattern: /"(?:\\.|[^"\\\r\n])*"/,
-        greedy: true
-    },
-    'keyword': /\b(?:Abs|AbsArg|Accuracy|Block|Do|For|Function|If|Manipulate|Module|Nest|NestList|None|Return|Switch|Table|Which|While)\b/,
-    'context': {
-        pattern: /\w+`+\w*/,
-        alias: 'class-name'
-    },
-    'blank': {
-        pattern: /\b\w+_\b/,
-        alias: 'regex'
-    },
-    'global-variable': {
-        pattern: /\$\w+/,
-        alias: 'variable'
-    },  
-    'boolean': /\b(?:True|False)\b/,
-    'number': /(?:\b(?=\d)|\B(?=\.))(?:0[bo])?(?:(?:\d|0x[\da-f])[\da-f]*(?:\.\d*)?|\.\d+)(?:e[+-]?\d+)?j?\b/i,
-    'operator': /\/\.|\;|\=\.|\^\=|\^\:\=|\:\=|\<\<|\>\>|\<\||\|\>|\:\>|\|\-\>|\-\>|\<\-|\@\@\@|\@\@|\@|\/\@|\=\!\=|\=\=\=|\=\=|\=|\+|\-|\^|\*|\[\/-+%=]=?|\!\=|\*\*?=?|\/\/?=?|<[<=>]?|>[=>]?|[&|^~]/,
-    'punctuation': /[\|{}[\];(),.:]/
+	'comment': // Allow one level of nesting - note: regex taken from applescipt
+		/\(\*(?:\(\*(?:[^*]|\*(?!\)))*\*\)|(?!\(\*)[\s\S])*?\*\)/,
+	'string': {
+		pattern: /"(?:\\.|[^"\\\r\n])*"/,
+		greedy: true
+	},
+	'keyword': /\b(?:Abs|AbsArg|Accuracy|Block|Do|For|Function|If|Manipulate|Module|Nest|NestList|None|Return|Switch|Table|Which|While)\b/,
+	'context': {
+		pattern: /\w+`+\w*/,
+		alias: 'class-name'
+	},
+	'blank': {
+		pattern: /\b\w+_\b/,
+		alias: 'regex'
+	},
+	'global-variable': {
+		pattern: /\$\w+/,
+		alias: 'variable'
+	},
+	'boolean': /\b(?:True|False)\b/,
+	'number': /(?:\b(?=\d)|\B(?=\.))(?:0[bo])?(?:(?:\d|0x[\da-f])[\da-f]*(?:\.\d*)?|\.\d+)(?:e[+-]?\d+)?j?\b/i,
+	'operator': /\/\.|\;|\=\.|\^\=|\^\:\=|\:\=|\<\<|\>\>|\<\||\|\>|\:\>|\|\-\>|\-\>|\<\-|\@\@\@|\@\@|\@|\/\@|\=\!\=|\=\=\=|\=\=|\=|\+|\-|\^|\*|\[\/-+%=]=?|\!\=|\*\*?=?|\/\/?=?|<[<=>]?|>[=>]?|[&|^~]/,
+	'punctuation': /[\|{}[\];(),.:]/
 };
 
 Prism.languages.mathematica = Prism.languages.wolfram;
