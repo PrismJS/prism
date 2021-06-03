6fbafae4eb87bea3cb105c9934972e8b8e17c0b2
Jason Kurian <jason.kurian@shopify.com>
2021-06-02 22:56:51 -0400

    add naive implementation for several new tokens in graphql lang
    - add language flag for patterns tests to isolate one language at a time in development
    - update docs and core build
    
    use builtin regex lookaheads, thanks for the tip @joshgoebel!
    
    remove unnecessary greedy in class-name
    
    apply regex suggestions from code review
    
    Create type-def_feature.test
    
    fix and simplify atom-input pattern
    
    add scalar token pattern
    
    https://graphql.org/learn/schema/#scalar-types

--- components/prism-graphql.js
+++ components/prism-graphql.js
@@ -18,7 +18,16 @@ Prism.languages.graphql = {
 	},
 	'number': /(?:\B-|\b)\d+(?:\.\d+)?(?:e[+-]?\d+)?\b/i,
 	'boolean': /\b(?:true|false)\b/,
+	'variable-input': {
+		pattern: /(\bmutation.*)\$\w+/,
+		alias: 'variable',
+		lookbehind: true
+	},
 	'variable': /\$[a-z_]\w*/i,
+	'atom-input': {
+		pattern: /[A-Z]\w*Input(?=!?.*$)/m,
+		alias: 'class-name'
+	},
 	'directive': {
 		pattern: /@[a-z_]\w*/i,
 		alias: 'function'
@@ -27,8 +36,10 @@ Prism.languages.graphql = {
 		pattern: /[a-z_]\w*(?=\s*(?:\((?:[^()"]|"(?:\\.|[^\\"\r\n])*")*\))?:)/i,
 		greedy: true
 	},
+	'scalar': /\b(?:Boolean|Float|ID|Int|String)\b/,
+	'constant': /\b(?!ID\b)[A-Z][A-Z_\d]*\b/,
 	'class-name': {
-		pattern: /(\b(?:enum|implements|interface|on|scalar|type|union)\s+|&\s*)[a-zA-Z_]\w*/,
+		pattern: /(\b(?:enum|implements|interface|on|scalar|type|union)\s+|&\s*|:\s*|\[)[A-Z_]\w*/,
 		lookbehind: true
 	},
 	'fragment': {
@@ -36,8 +47,25 @@ Prism.languages.graphql = {
 		lookbehind: true,
 		alias: 'function'
 	},
+	'property-mutation': {
+		pattern: /\b(mutation.*$\s+)\b\w+/m,
+		lookbehind: true,
+		greedy: true
+	},
+	'definition-mutation': {
+		pattern: /(\bmutation\s+|\.{3}\s*)[a-zA-Z_]\w*/,
+		lookbehind: true,
+		alias: 'function'
+	},
+	'definition-query': {
+		pattern: /(\bquery\s+|\.{3}\s*)[a-zA-Z_]\w*/,
+		lookbehind: true,
+		alias: 'function'
+	},
 	'keyword': /\b(?:directive|enum|extend|fragment|implements|input|interface|mutation|on|query|repeatable|scalar|schema|subscription|type|union)\b/,
 	'operator': /[!=|&]|\.{3}/,
+	'property-query': /\w+(?=\s*\()/,
+	'object': /\w+(?=\s*{)/,
 	'punctuation': /[!(){}\[\]:=,]/,
-	'constant': /\b(?!ID\b)[A-Z][A-Z_\d]*\b/
+	'property': /\w+/
 };
