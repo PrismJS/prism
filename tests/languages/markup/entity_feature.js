17241874b495e1cd53b98b3fd5919d4abf8cc8ea
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-06-04 14:30:31 +0200

    Converted .JS test cases

--- tests/languages/markup/entity_feature.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	'&amp;': '<span class="token entity named-entity" title="&amp;">&amp;amp;</span>',
-	'&thetasym;': '<span class="token entity named-entity" title="&thetasym;">&amp;thetasym;</span>',
-	'&#65;': '<span class="token entity" title="&#65;">&amp;#65;</span>',
-	'&#x41;': '<span class="token entity" title="&#x41;">&amp;#x41;</span>'
-};
