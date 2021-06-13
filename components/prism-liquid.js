d706be66b8e645ef01e247339247878a011fb205
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-06-13 23:52:15 +0200

    Removed keywords from function list

--- components/prism-liquid.js
+++ components/prism-liquid.js
@@ -9,7 +9,7 @@ Prism.languages.liquid = {
 	},
 	'keyword': /\b(?:as|assign|break|capture|case|comment|contains|continue|decrement|echo|else|elsif|endcapture|endcase|endcomment|endfor|endif|endraw|endtablerow|endunless|for|if|in|include|increment|limit|liquid|offset|range|raw|render|reversed|tablerow|unless|when|with)\b/,
 	'function': {
-		pattern: /(^|[\s;|&])(?:append|prepend|capitalize|cycle|cols|increment|decrement|abs|at_least|at_most|ceil|compact|concat|date|default|divided_by|downcase|escape|escape_once|first|floor|join|last|lstrip|map|minus|modulo|newline_to_br|plus|remove|remove_first|replace|replace_first|reverse|round|rstrip|size|slice|sort|sort_natural|split|strip|strip_html|strip_newlines|times|truncate|truncatewords|uniq|upcase|url_decode|url_encode|include|paginate)(?=$|[\s;:|&])/,
+		pattern: /(^|[\s;|&])(?:append|prepend|capitalize|cycle|cols|abs|at_least|at_most|ceil|compact|concat|date|default|divided_by|downcase|escape|escape_once|first|floor|join|last|lstrip|map|minus|modulo|newline_to_br|plus|remove|remove_first|replace|replace_first|reverse|round|rstrip|size|slice|sort|sort_natural|split|strip|strip_html|strip_newlines|times|truncate|truncatewords|uniq|upcase|url_decode|url_encode|paginate)(?=$|[\s;:|&])/,
 		lookbehind: true
 	},
 	'boolean': /\b(?:true|false)\b/,
