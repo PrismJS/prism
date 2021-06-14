ec8094b71eec902ed48155f4907acd9b2b7e3fe2
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-06-14 14:07:51 +0200

    Updated keywords and operators

--- components/prism-liquid.js
+++ components/prism-liquid.js
@@ -7,9 +7,9 @@ Prism.languages.liquid = {
 		pattern: /"[^"]*"|'[^']*'/,
 		greedy: true
 	},
-	'keyword': /\b(?:as|assign|break|capture|case|comment|continue|cycle|decrement|echo|else|elsif|endcapture|endcase|endcomment|endfor|endif|endraw|endtablerow|endunless|for|if|in|include|increment|limit|liquid|offset|range|raw|render|reversed|tablerow|unless|when|with)\b/,
+	'keyword': /\b(?:as|assign|break|continue|cycle|decrement|echo|else|elsif|(?:end)?(?:capture|case|comment|for|form|if|paginate|style|raw|tablerow|unless)|in|include|increment|limit|liquid|offset|range|render|reversed|section|when|with)\b/,
 	'function': {
-		pattern: /(^|[\s;|&])(?:append|prepend|capitalize|cols|abs|at_least|at_most|ceil|compact|concat|date|default|divided_by|downcase|escape|escape_once|first|floor|join|last|lstrip|map|minus|modulo|newline_to_br|plus|remove|remove_first|replace|replace_first|reverse|round|rstrip|size|slice|sort|sort_natural|split|strip|strip_html|strip_newlines|times|truncate|truncatewords|uniq|upcase|url_decode|url_encode|paginate)(?=$|[\s;:|&])/,
+		pattern: /(^|[\s;|&])(?:append|prepend|capitalize|cols|abs|at_least|at_most|ceil|compact|concat|date|default|divided_by|downcase|escape|escape_once|first|floor|join|last|lstrip|map|minus|modulo|newline_to_br|plus|remove|remove_first|replace|replace_first|reverse|round|rstrip|size|slice|sort|sort_natural|split|strip|strip_html|strip_newlines|times|truncate|truncatewords|uniq|upcase|url_decode|url_encode)(?=$|[\s;:|&])/,
 		lookbehind: true
 	},
 	'boolean': /\b(?:true|false)\b/,
@@ -19,10 +19,7 @@ Prism.languages.liquid = {
 	},
 	// https://github.com/Shopify/liquid/blob/698f5e0d967423e013f6169d9111bd969bd78337/lib/liquid/lexer.rb#L21
 	'number': /\b\d+(?:\.\d+)?\b/,
-	'operator': {
-		pattern: /(^|[^.])(?:\+[+=]?|-[-=]?|!=?|<<?=?|>>?>?=?|==?|&[&=]?|\|[|=]?|\*=?|\/=?|%=?|\^=?|[?:~])|\b(?:and|or|contains(?=\s))\b/m,
-		lookbehind: true
-	},
+	'operator': /[!=]=|<>|[<>]=?|[|?:=-]|\b(?:and|or|contains(?=\s))\b/,
 	'punctuation': /[.,\[\]()]/
 };
 
