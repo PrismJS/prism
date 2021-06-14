fa767781442e91a0d7359501675527243cbbbd99
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-06-14 13:40:59 +0200

    Simplified numbers and changed cycle and cotnains

--- components/prism-liquid.js
+++ components/prism-liquid.js
@@ -7,9 +7,9 @@ Prism.languages.liquid = {
 		pattern: /"[^"]*"|'[^']*'/,
 		greedy: true
 	},
-	'keyword': /\b(?:as|assign|break|capture|case|comment|contains|continue|decrement|echo|else|elsif|endcapture|endcase|endcomment|endfor|endif|endraw|endtablerow|endunless|for|if|in|include|increment|limit|liquid|offset|range|raw|render|reversed|tablerow|unless|when|with)\b/,
+	'keyword': /\b(?:as|assign|break|capture|case|comment|continue|cycle|decrement|echo|else|elsif|endcapture|endcase|endcomment|endfor|endif|endraw|endtablerow|endunless|for|if|in|include|increment|limit|liquid|offset|range|raw|render|reversed|tablerow|unless|when|with)\b/,
 	'function': {
-		pattern: /(^|[\s;|&])(?:append|prepend|capitalize|cycle|cols|abs|at_least|at_most|ceil|compact|concat|date|default|divided_by|downcase|escape|escape_once|first|floor|join|last|lstrip|map|minus|modulo|newline_to_br|plus|remove|remove_first|replace|replace_first|reverse|round|rstrip|size|slice|sort|sort_natural|split|strip|strip_html|strip_newlines|times|truncate|truncatewords|uniq|upcase|url_decode|url_encode|paginate)(?=$|[\s;:|&])/,
+		pattern: /(^|[\s;|&])(?:append|prepend|capitalize|cols|abs|at_least|at_most|ceil|compact|concat|date|default|divided_by|downcase|escape|escape_once|first|floor|join|last|lstrip|map|minus|modulo|newline_to_br|plus|remove|remove_first|replace|replace_first|reverse|round|rstrip|size|slice|sort|sort_natural|split|strip|strip_html|strip_newlines|times|truncate|truncatewords|uniq|upcase|url_decode|url_encode|paginate)(?=$|[\s;:|&])/,
 		lookbehind: true
 	},
 	'boolean': /\b(?:true|false)\b/,
@@ -17,9 +17,10 @@ Prism.languages.liquid = {
 		pattern: /\.\./,
 		alias: 'operator'
 	},
-	'number': /\b0b[01]+\b|\b0x(?:\.[\da-fp-]+|[\da-f]+(?:\.[\da-fp-]+)?)\b|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?[df]?/i,
+	// https://github.com/Shopify/liquid/blob/698f5e0d967423e013f6169d9111bd969bd78337/lib/liquid/lexer.rb#L21
+	'number': /\b\d+(?:\.\d+)?\b/,
 	'operator': {
-		pattern: /(^|[^.])(?:\+[+=]?|-[-=]?|!=?|<<?=?|>>?>?=?|==?|&[&=]?|\|[|=]?|\*=?|\/=?|%=?|\^=?|[?:~])|\b(?:and|or)\b/m,
+		pattern: /(^|[^.])(?:\+[+=]?|-[-=]?|!=?|<<?=?|>>?>?=?|==?|&[&=]?|\|[|=]?|\*=?|\/=?|%=?|\^=?|[?:~])|\b(?:and|or|contains(?=\s))\b/m,
 		lookbehind: true
 	},
 	'punctuation': /[.,\[\]()]/
