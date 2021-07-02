42f14637f46e7e37559567e2f9e23051f0bb37c4
Matilde Park <matilde.park@gmail.com>
2021-07-02 14:29:55 -0400

    components: resolve linting and test failures

--- components/prism-hoon.js
+++ components/prism-hoon.js
@@ -1,42 +1,30 @@
 Prism.languages.hoon = {
-  constant: [
-    {
-      pattern: /(%\.n)|(%\.y)|(%[\w-]*[\w\d]?)/,
-    }
-  ],
-  comment: {
-    pattern: /::.*/,
-    lookahead: true,
-    greedy: true
-  },
-  "function-name": [
-    {
-      pattern: /(\+[-+]  )?([a-z]([a-z0-9-]*[a-z0-9])?)/
-    },
-  ],
-  "class-name": [
-    {
-      pattern: /@([a-z])?([A-Za-z0-9-]*[A-Za-z0-9])?/,
-      lookahead: true,
-      greedy: true
-    },
-    {
-      pattern: /\*/
-    }
-  ],
-  "string": [
-    {
-      pattern: /"([^"])*"/,
-      lookahead: true,
-      greedy: true
-    },
-    {
-      pattern: /'([^'])*'/,
-      lookahead: true,
-      greedy: true
-    }
-  ],
-  "keyword": {
-    pattern: /:_|\.[\^\+\*=\?]|![><:\.=\?!]|=[>|:,\.\-\^<+;/~\*\?]|\?[>|:\.\-\^<\+&~=@!]|\|[\$_%:\.\-\^~\*=@\?]|\+[|\$\+\*]|:[_\-\^\+~\*]|%[_:\.\-\^\+~\*=]|\^[|:\.\-\+&~\*=\?]|\$[|_%:<>\-\^&~@=\?]|;[:<\+;\/~\*=]|~[>|\$_%<\+\/&=\?!]|--|==/
-  }
-}
+	constant: /(?:%\.n)|(?:%\.y)|(?:%[\w-]+\w?)/,
+	comment: {
+		pattern: /::.*/,
+		lookahead: true,
+		greedy: true
+	},
+	'function-name': /(?:\+[-+] {2})?(?:[a-z]([a-z0-9-]*[a-z0-9])?)/,
+	'class-name': [
+		{
+			pattern: /@(?:[a-z])?(?:[A-Za-z0-9-]*[A-Za-z0-9])?/,
+			lookahead: true,
+			greedy: true
+		},
+		/\*/
+	],
+	'string': [
+		{
+			pattern: /"(?:[^"])*"/,
+			lookahead: true,
+			greedy: true
+		},
+		{
+			pattern: /'(?:[^'])*'/,
+			lookahead: true,
+			greedy: true
+		}
+	],
+	'keyword': /:_|\.[\^\+\*=\?]|![><:\.=\?!]|=[>|:,\.\-\^<+;/~\*\?]|\?[>|:\.\-\^<\+&~=@!]|\|[\$_%:\.\-\^~\*=@\?]|\+[|\$\+\*]|:[_\-\^\+~\*]|%[_:\.\-\^\+~\*=]|\^[|:\.\-\+&~\*=\?]|\$[|_%:<>\-\^&~@=\?]|;[:<\+;\/~\*=]|~[>|\$_%<\+\/&=\?!]|--|==/
+};
