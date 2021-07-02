eab307c20bc2113d3695ea41f457711d50d9ba43
Matilde Park <matilde.park@gmail.com>
2021-07-02 15:35:27 -0400

    components/hoon: address @RunDevelopment review
    Discussion in #2978

--- components/prism-hoon.js
+++ components/prism-hoon.js
@@ -1,16 +1,15 @@
 Prism.languages.hoon = {
-	constant: /%(?:\.[ny])|[\w-]+)/,
-	comment: {
+	'constant': /(?:%\.n)|(?:%\.y)|(?:%[\w-]+\w?)/,
+	'comment': {
 		pattern: /::.*/,
 		lookahead: true,
 		greedy: true
 	},
-	'function-name': /(?:\+[-+] {2})?(?:[a-z]([a-z0-9-]*[a-z0-9])?)/,
+	'function': /(?:\+[-+] {2})?(?:[a-z]([a-z0-9-]*[a-z0-9])?)/,
 	'class-name': [
 		{
 			pattern: /@(?:[A-Za-z0-9-]*[A-Za-z0-9])?/,
 			lookahead: true,
-			greedy: true
 		},
 		/\*/
 	],
