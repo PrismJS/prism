42439456dddb68d20a722bf063503b906100c41e
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-08-14 13:37:58 +0200

    Fixed new linting errors

--- components/prism-lisp.js
+++ components/prism-lisp.js
@@ -14,7 +14,7 @@
 
 	// Symbol name. See https://www.gnu.org/software/emacs/manual/html_node/elisp/Symbol-Type.html
 	// & and : are excluded as they are usually used for special purposes
-	var symbol = '[-+*/_~!@$%^=<>{}\\w]+';
+	var symbol = '[-+*/~!@$%^=<>{}\\w]+';
 	// symbol starting with & used in function arguments
 	var marker = '&' + symbol;
 	// Open parenthesis for look-behind
