1b3316a68f713a986dac43a120a669c8764caf39
Darach Ennis <darach@gmail.com>
2021-09-20 21:57:36 +0200

    Add partial interpolation, expand tests, attend to lints
    Signed-off-by: Darach Ennis <darach@gmail.com>

--- components/prism-tremor.js
+++ components/prism-tremor.js
@@ -1,39 +1,44 @@
 // NOTE: Tremor supports recursive nested interpolation in strings
-// but we do support intperpolation in this definition as nested
-// recursive interpolation isn't yet fully supported by prism.
+// and heredocs but we use a partial interpolation method here. The
+// string language rule pattern supports both single and triple double-quote
+// delimited strings.
+//
 
 Prism.languages.tremor = {
 	'comment': {
 		pattern: /(^|[^\\])(?:\/\*[\s\S]*?\*\/|(?:--|\/\/|#).*)/,
 		lookbehind: true
 	},
-   	'heredoc': {
-        pattern: /"""(?:[^"]|"(?!""))+"""/,
-        greedy: true,
-        alias: "string"
+	'string': {
+		pattern: /"""(?:[^"\\\r\n#]|\\(?:\r\n|[\s\S])|#(?!\{)|#\{(?:[^"{}]|\{[^{}]*\}|"(?:[^"\\\r\n]|\\(?:\r\n|[\s\S]))")*\})*"""|"(?:[^"\\\r\n#]|\\(?:\r\n|[\s\S])|#(?!\{)|#\{(?:[^"{}]|\{[^{}]*\}|"(?:[^"\\\r\n]|\\(?:\r\n|[\s\S]))")*\})*"/,
+		greedy: true,
+		inside: {
+			'interpolation': {
+				pattern: /#\{(?:[^"{}]|\{[^{}]*\}|"(?:[^"\\\r\n]|\\(?:\r\n|[\s\S]))")*\}/,
+				inside: {
+					'punctuation': /^#\{|\}$/,
+					'expression': {
+						pattern: /[\s\S]+/,
+						inside: Prism.languages.tremor
+					}
+				}
+			},
+			'string': /[\s\S]+/
+		}
 	},
-   	'string': {
-        pattern: /(^|[^\\])"(?:\\(?:\r\n|[\s\S])|[^"\\\r\n])*"/,
-        lookbehind: true
-        greedy: true,
-        alias: "string"
-	},
-	'function':  /\b[a-z_]\w*(?=\s*(?:::\s*<|\())\b/,
+	'function': /\b[a-z_]\w*(?=\s*(?:::\s*<|\())\b/,
 	'keyword': /\b(?:event|state|select|create|define|deploy|operator|script|connector|pipeline|flow|config|links|connect|to|from|into|with|group|by|args|window|stream|tumbling|sliding|where|having|set|each|emit|drop|const|let|for|match|of|case|when|default|end|patch|insert|update|erase|move|copy|merge|fn|intrinsic|recur|use|as|mod)\b/,
 	'boolean': /\b(?:true|false|null)\b/i,
-    'variable': {
-        pattern: /`[^`]*`|\b[_a-z]\w*\b/,
-        greedy: true
-    },
 	'number': /\b(?:0b[0-1_]*|0x[0-9a-fA-F_]*|\d[0-9_]*(?:\.\d[0-9_]*)?(?:[Ee][+-]?[0-9_]+)?)\b/,
-    'operator': /\b[-+*\/%!^]=?|=[=>]?|&[&=]?|\|[|=]?|<<?=?|>>?=?|\b(?:not|and|or|xor|present|absent)\b/,
-	'punctuation': /[;\[\]()\{\},.]/,
-    'namespace': {
-			pattern: /(?:\b[a-z][a-z_\d]*\s*::\s*)*\b[a-z][a-z_\d]*\s*::(?!\s*<)/,
-			inside: {
-				'punctuation': /::/
-			}
-		},
+	'operator': /[-+*\/%!^]=?|=[=>]?|&[&=]?|\|[|=]?|<<?=?|>>?>?=?|(?:not|and|or|xor|present|absent)\b/,
+	'punctuation': /[;\[\]()\{\},.%#]/,
+	'variable': {
+		pattern: /(?:[a-z](?:[a-z_\d]+\s*)?::|`[^`]+`::)*(?:[a-z][a-z_\d]*|`[^`]+`)/,
+		greedy: true,
+		inside: {
+			'punctuation': /::/
+		}
+	},
 };
 
 Prism.languages.troy = Prism.languages['tremor'];
