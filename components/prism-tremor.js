dba2ff673f627ed952babd48d4f07dcf4c1f76d7
Michael Schmidt <mitchi5000.ms@googlemail.com>
2021-09-21 20:41:39 +0200

    Improved Tremor tokenization
    Signed-off-by: Michael Schmidt <mitchi5000.ms@googlemail.com>

--- components/prism-tremor.js
+++ components/prism-tremor.js
@@ -1,20 +1,55 @@
-// NOTE: Tremor supports recursive nested interpolation in strings
-// and heredocs but we use a partial interpolation method here. The
-// string language rule pattern supports both single and triple double-quote
-// delimited strings.
-//
+(function (Prism) {
 
-Prism.languages.tremor = {
-	'comment': {
-		pattern: /(^|[^\\])(?:\/\*[\s\S]*?\*\/|(?:--|\/\/|#).*)/,
-		lookbehind: true
-	},
-	'string': {
-		pattern: /"""(?:[^"\\\r\n#]|\\(?:\r\n|[\s\S])|#(?!\{)|#\{(?:[^"{}]|\{[^{}]*\}|"(?:[^"\\\r\n]|\\(?:\r\n|[\s\S]))")*\})*"""|"(?:[^"\\\r\n#]|\\(?:\r\n|[\s\S])|#(?!\{)|#\{(?:[^"{}]|\{[^{}]*\}|"(?:[^"\\\r\n]|\\(?:\r\n|[\s\S]))")*\})*"/,
+	Prism.languages.tremor = {
+		'comment': {
+			pattern: /(^|[^\\])(?:\/\*[\s\S]*?\*\/|(?:--|\/\/|#).*)/,
+			lookbehind: true
+		},
+		'interpolated-string': null, // see below
+		'extractor': {
+			pattern: /\b[a-z_]\w*\|(?:[^\r\n\\|]|\\(?:\r\n|[\s\S]))*\|/i,
+			greedy: true,
+			inside: {
+				'function': /^\w+/,
+				'regex': /\|[\s\S]+/,
+			}
+		},
+		'identifier': {
+			pattern: /`[^`]*`/,
+			greedy: true
+		},
+
+		'function': /\b[a-z_]\w*(?=\s*(?:::\s*<|\())\b/,
+
+		'keyword': /\b(?:event|state|select|create|define|deploy|operator|script|connector|pipeline|flow|config|links|connect|to|from|into|with|group|by|args|window|stream|tumbling|sliding|where|having|set|each|emit|drop|const|let|for|match|of|case|when|default|end|patch|insert|update|erase|move|copy|merge|fn|intrinsic|recur|use|as|mod)\b/,
+		'boolean': /\b(?:true|false|null)\b/i,
+
+		'number': /\b(?:0b[0-1_]*|0x[0-9a-fA-F_]*|\d[0-9_]*(?:\.\d[0-9_]*)?(?:[Ee][+-]?[0-9_]+)?)\b/,
+
+		'pattern-punctuation': {
+			pattern: /%(?=[({[])/,
+			alias: 'punctuation'
+		},
+		'operator': /[-+*\/%~!^]=?|=[=>]?|&[&=]?|\|[|=]?|<<?=?|>>?>?=?|(?:not|and|or|xor|present|absent)\b/,
+		'punctuation': /::|[;\[\]()\{\},.:]/,
+	};
+
+	var interpolationPattern = /#\{(?:[^"{}]|\{[^{}]*\}|"(?:[^"\\\r\n]|\\(?:\r\n|[\s\S]))*")*\}/.source;
+
+	Prism.languages.tremor['interpolated-string'] = {
+		pattern: RegExp(
+			/(^|[^\\])/.source +
+			'(?:' +
+			'"""(?:' + /[^"\\#]|\\[\s\S]|"(?!"")|#(?!\{)/.source + '|' + interpolationPattern + ')*"""' +
+			'|' +
+			'"(?:' + /[^"\\\r\n#]|\\(?:\r\n|[\s\S])|#(?!\{)/.source + '|' + interpolationPattern + ')*"' +
+			')'
+		),
+		lookbehind: true,
 		greedy: true,
 		inside: {
 			'interpolation': {
-				pattern: /#\{(?:[^"{}]|\{[^{}]*\}|"(?:[^"\\\r\n]|\\(?:\r\n|[\s\S]))")*\}/,
+				pattern: RegExp(interpolationPattern),
 				inside: {
 					'punctuation': /^#\{|\}$/,
 					'expression': {
@@ -25,21 +60,9 @@ Prism.languages.tremor = {
 			},
 			'string': /[\s\S]+/
 		}
-	},
-	'function': /\b[a-z_]\w*(?=\s*(?:::\s*<|\())\b/,
-	'keyword': /\b(?:event|state|select|create|define|deploy|operator|script|connector|pipeline|flow|config|links|connect|to|from|into|with|group|by|args|window|stream|tumbling|sliding|where|having|set|each|emit|drop|const|let|for|match|of|case|when|default|end|patch|insert|update|erase|move|copy|merge|fn|intrinsic|recur|use|as|mod)\b/,
-	'boolean': /\b(?:true|false|null)\b/i,
-	'number': /\b(?:0b[0-1_]*|0x[0-9a-fA-F_]*|\d[0-9_]*(?:\.\d[0-9_]*)?(?:[Ee][+-]?[0-9_]+)?)\b/,
-	'operator': /[-+*\/%!^]=?|=[=>]?|&[&=]?|\|[|=]?|<<?=?|>>?>?=?|(?:not|and|or|xor|present|absent)\b/,
-	'punctuation': /[;\[\]()\{\},.%#]/,
-	'variable': {
-		pattern: /(?:[a-z](?:[a-z_\d]+\s*)?::|`[^`]+`::)*(?:[a-z][a-z_\d]*|`[^`]+`)/,
-		greedy: true,
-		inside: {
-			'punctuation': /::/
-		}
-	},
-};
+	};
 
-Prism.languages.troy = Prism.languages['tremor'];
-Prism.languages.trickle = Prism.languages['tremor'];
+	Prism.languages.troy = Prism.languages['tremor'];
+	Prism.languages.trickle = Prism.languages['tremor'];
+
+}(Prism));
