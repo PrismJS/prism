f9d0c0624504dd46d8fd07b701a86e32f323fbc5
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-10-02 01:17:09 +0200

    Updated JSDom

--- tests/plugins/keep-markup/test.js
+++ tests/plugins/keep-markup/test.js
@@ -1,82 +1,47 @@
-const { expect } = require('chai');
+const { assert } = require('chai');
 const { reusablePrismDom } = require('../../helper/prism-dom-util');
 
 
 describe('Keep Markup', function () {
-	const { Prism, document, loadPlugins } = reusablePrismDom(this);
-	document.createRange = function () { }; // fake createRange for Keep Markup
-	loadPlugins('keep-markup');
+	const { Prism, document } = reusablePrismDom(this, {
+		plugins: 'keep-markup'
+	});
 
 
-	function execute(code) {
-		const start = [];
-		const end = [];
-		const nodes = [];
-		document.createRange = function () {
-			return {
-				setStart: function (node, offset) {
-					start.push({ node, offset });
-				},
-				setEnd: function (node, offset) {
-					end.push({ node, offset });
-				},
-				extractContents: function () {
-					return document.createTextNode('');
-				},
-				insertNode: function (node) {
-					nodes.push(node);
-				},
-				detach: function () {
-				}
-			};
-		};
+	/**
+	 * @param {string} html
+	 * @param {string} language
+	 */
+	function highlightInElement(html, language = 'none') {
+		const pre = document.createElement('pre');
+		pre.className = `language-${language}`;
+		pre.innerHTML = `<code>${html}</code>`;
 
-		document.body.innerHTML = code;
-		const element = document.body.firstElementChild;
+		Prism.highlightElement(pre);
 
+		return pre.querySelector('code').innerHTML;
+	}
 
-		const beforeHighlight = Prism.hooks.all['before-highlight'][0];
-		const afterHighlight = Prism.hooks.all['after-highlight'][0];
-
-		const env = {
-			element,
-			language: 'javascript'
-		};
-		beforeHighlight(env);
-		afterHighlight(env);
-		return { start, end, nodes };
+	/**
+	 * @param {string} html
+	 * @param {string} language
+	 */
+	function keepMarkup(html, language = 'none') {
+		assert.equal(highlightInElement(html, language), html);
 	}
 
 	it('should keep <span> markup', function () {
-		const result = execute(`<code class="language-none">x<span>a</span>y</code>`);
-		expect(result.start.length).to.equal(1);
-		expect(result.end.length).to.equal(1);
-		expect(result.nodes.length).to.equal(1);
-		expect(result.nodes[0].nodeName).to.equal('SPAN');
+		keepMarkup(`x<span>a</span>y`);
 	});
 	it('should preserve markup order', function () {
-		const result = execute(`<code class="language-none">x<a></a><b></b>y</code>`);
-		expect(result.start.length).to.equal(2);
-		expect(result.start[0].offset).to.equal(0);
-		expect(result.start[0].node.textContent).to.equal('y');
-		expect(result.start[1].offset).to.equal(0);
-		expect(result.start[1].node.textContent).to.equal('y');
-		expect(result.end.length).to.equal(2);
-		expect(result.end[0].offset).to.equal(0);
-		expect(result.end[0].node.textContent).to.equal('y');
-		expect(result.end[1].offset).to.equal(0);
-		expect(result.end[1].node.textContent).to.equal('y');
-		expect(result.nodes.length).to.equal(2);
-		expect(result.nodes[0].nodeName).to.equal('A');
-		expect(result.nodes[1].nodeName).to.equal('B');
+		keepMarkup(`x<a></a><b></b>y`);
 	});
-	it('should keep last <span> markup', function () {
-		const result = execute(`<code class="language-none">xy<span>a</span></code>`);
-		expect(result.start.length).to.equal(1);
-		expect(result.end.length).to.equal(1);
-		expect(result.nodes.length).to.equal(1);
-		expect(result.nodes[0].nodeName).to.equal('SPAN');
+
+	it('should keep last markup', function () {
+		keepMarkup(`xy<span>a</span>`);
+		keepMarkup(`xy<a>a</a>`);
 	});
+
 	// The markup is removed if it's the last element and the element's name is a single letter: a(nchor), b(old), i(talic)...
 	// https://github.com/PrismJS/prism/issues/1618
 	/*
