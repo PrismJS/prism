18e3bc6d6062713b9f6f820652d6ff7e46df08cd
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-12-01 13:07:00 +0100

    PHP extras: Improved `scope` and `this`

--- components/prism-php-extras.js
+++ components/prism-php-extras.js
@@ -1,11 +1,14 @@
 Prism.languages.insertBefore('php', 'variable', {
-	'this': /\$this\b/,
+	'this': {
+		pattern: /\$this\b/,
+		alias: 'keyword'
+	},
 	'global': /\$(?:GLOBALS|HTTP_RAW_POST_DATA|_(?:COOKIE|ENV|FILES|GET|POST|REQUEST|SERVER|SESSION)|argc|argv|http_response_header|php_errormsg)\b/,
 	'scope': {
 		pattern: /\b[\w\\]+::/,
 		inside: {
-			keyword: /parent|self|static/,
-			punctuation: /::|\\/
+			'keyword': /\b(?:parent|self|static)\b/,
+			'punctuation': /::|\\/
 		}
 	}
 });
