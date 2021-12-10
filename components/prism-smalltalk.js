a7bb3001aa21c9914344aef7ccaef67d073e24dd
Michael Schmidt <mitchi5000.ms@googlemail.com>
2021-12-10 14:37:50 +0100

    Smalltalk: Use standard `char` token (#3255)

--- components/prism-smalltalk.js
+++ components/prism-smalltalk.js
@@ -1,10 +1,16 @@
 Prism.languages.smalltalk = {
-	'comment': /"(?:""|[^"])*"/,
-	'character': {
-		pattern: /\$./,
-		alias: 'string'
+	'comment': {
+		pattern: /"(?:""|[^"])*"/,
+		greedy: true
 	},
-	'string': /'(?:''|[^'])*'/,
+	'char': {
+		pattern: /\$./,
+		greedy: true
+	},
+	'string': {
+		pattern: /'(?:''|[^'])*'/,
+		greedy: true
+	},
 	'symbol': /#[\da-z]+|#(?:-|([+\/\\*~<>=@%|&?!])\1?)|#(?=\()/i,
 	'block-arguments': {
 		pattern: /(\[\s*):[^\[|]*\|/,
