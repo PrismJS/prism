2069ab0c4344b608018b9ebe24c4048fb218feae
Michael Schmidt <mitchi5000.ms@googlemail.com>
2021-12-10 14:42:17 +0100

    Rip: Use standard `char` token (#3252)

--- components/prism-rip.js
+++ components/prism-rip.js
@@ -1,5 +1,23 @@
 Prism.languages.rip = {
-	'comment': /#.*/,
+	'comment': {
+		pattern: /#.*/,
+		greedy: true
+	},
+
+	'char': {
+		pattern: /\B`[^\s`'",.:;#\/\\()<>\[\]{}]\b/,
+		greedy: true
+	},
+	'string': {
+		pattern: /("|')(?:\\.|(?!\1)[^\\\r\n])*\1/,
+		greedy: true
+	},
+
+	'regex': {
+		pattern: /(^|[^/])\/(?!\/)(?:\[[^\n\r\]]*\]|\\.|[^/\\\r\n\[])+\/(?=\s*(?:$|[\r\n,.;})]))/,
+		lookbehind: true,
+		greedy: true
+	},
 
 	'keyword': /(?:=>|->)|\b(?:case|catch|class|else|exit|finally|if|raise|return|switch|try)\b/,
 
@@ -11,19 +29,7 @@ Prism.languages.rip = {
 	'time': /\b\d{2}:\d{2}:\d{2}\b/,
 	'datetime': /\b\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\b/,
 
-	'character': /\B`[^\s`'",.:;#\/\\()<>\[\]{}]\b/,
-
-	'regex': {
-		pattern: /(^|[^/])\/(?!\/)(?:\[[^\n\r\]]*\]|\\.|[^/\\\r\n\[])+\/(?=\s*(?:$|[\r\n,.;})]))/,
-		lookbehind: true,
-		greedy: true
-	},
-
 	'symbol': /:[^\d\s`'",.:;#\/\\()<>\[\]{}][^\s`'",.:;#\/\\()<>\[\]{}]*/,
-	'string': {
-		pattern: /("|')(?:\\.|(?!\1)[^\\\r\n])*\1/,
-		greedy: true
-	},
 	'number': /[+-]?\b(?:\d+\.\d+|\d+)\b/,
 
 	'punctuation': /(?:\.{2,3})|[`,.:;=\/\\()<>\[\]{}]/,
