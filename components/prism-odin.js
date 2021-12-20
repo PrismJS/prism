7a292f2339bd4c42c2f8d97fbc020e7696812729
edukisto <52005215+edukisto@users.noreply.github.com>
2021-12-20 05:16:29 +0300

    Make _ a discard variable

--- components/prism-odin.js
+++ components/prism-odin.js
@@ -59,7 +59,12 @@
 
 		'number': /(?:\b0(?:b[01_]+|d[\d_]+|h_*(?:(?:(?:[\dA-Fa-f]_*){8}){1,2}|(?:[\dA-Fa-f]_*){4})|o[0-7_]+|x[\dA-F_a-f]+|z[\dAB_ab]+)\b|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:[Ee][+-]?\d*)?[ijk]?)/,
 
-		'boolean': /\b(?:_|false|nil|true)\b/,
+		'discard': {
+			pattern: /\b_\b/,
+			alias: 'keyword'
+		},
+
+		'boolean': /\b(?:false|nil|true)\b/,
 
 		'keyword': /\b(?:asm|auto_cast|bit_set|break|case|cast|context|continue|defer|distinct|do|dynamic|else|enum|fallthrough|for|foreign|if|import|in|map|matrix|not_in|or_else|or_return|package|proc|return|struct|switch|transmute|typeid|union|using|when|where)\b/,
 
