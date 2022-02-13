25c3b1f8682eff3944742d178703274c5f516a2a
Andreas Huebner <ahue87@gmail.com>
2022-02-13 11:13:09 +0100

    Added cooklang definition incl. tests, examples

---
+++ components/prism-cooklang.js
@@ -0,0 +1,84 @@
+(function (Prism) {
+
+	var ingr_cw_prefix = /(?:[@#][^{@#]+)\{/
+	var ingr_cw_suffix = /\}/
+	var quant1 = /(?:[^}|*%]*[*]?)/
+	var quant_unit = /(?:[^}|*%]+[*]?%[^}]+)/
+	var quant_servings_unit = /(?:(?:[^*}%|]+[|])+[^*}%|]+(?:%[^*|%}]+)?)/
+
+	Prism.languages.cooklang = {
+		'comment': [
+			/\[-[\s\S]*?-\]/, // -- comment
+			/\-{2}[^\r\n]*/,    // [- comment -]
+		],
+		'meta': { // >> key: value
+			'pattern': /\>{2}.*:.*/,
+			'inside': {
+				"property": { // key:
+					'pattern': /(\>{2}\s*)[^:]+/, 
+					'lookbehind': true,
+				}
+			}
+		},
+		'quant': { // @some ingredient{...} #some cookware{...}
+			'pattern': new RegExp(
+				ingr_cw_prefix.source
+				+ "(?:"
+				+ quant1.source + "|"
+				+ quant_unit.source  + "|"
+				+ quant_servings_unit.source
+				+ ")"
+				+ ingr_cw_suffix.source,
+			),
+			'inside': {
+				'variable': { // some ingredient, some cookware
+					pattern: /([@#])[^{@#]+/,
+					lookbehind: true
+				},
+				'amount-group': {
+					'pattern': /\{[^{}]*\}/, // {...}
+					'inside': {
+						'punctuation': /[{}]/,
+						'symbol': {  // unit
+							'pattern': /(%)[^}]+/,
+							'lookbehind': true
+						}, 
+						'operator': /\*%|\*|%|\|/, // already includes not yet officially implemented servings operators
+						'number': /[^{}*%]+/, // amount
+					}
+				},
+				'keyword': /[@#]/,
+			}
+		},
+		'no-quant': { // ingredient, cookware
+			'pattern': /[@#][^{@#\s]+/,
+			'inside': {
+				'keyword': /[@#]/,
+				'variable': /[^{@#]+/
+			}
+		},
+		'timer-group': { // ~timer{...}
+			'pattern': /\~[^{}]*\{\d+%(?:minutes|hours)\}/,
+			'inside': {
+				'variable': { // timer name
+					pattern: /(~)[^{]+/,
+					lookbehind: true
+				},
+				'duration-group': { // {...}
+					'pattern': /\{[^{}]+\}/, 
+					'inside': {
+						'punctuation': /[{}]/,
+						'symbol': { // unit
+							'pattern': /(%)(?:minutes|min|m|hours|hrs|h)/,
+							'lookbehind': true
+						}, 
+						'operator': /%/,
+						'number': /\d+/, // amount
+					}
+				},
+				'keyword': /~/,
+			}
+		}
+	};
+}(Prism));
+	
