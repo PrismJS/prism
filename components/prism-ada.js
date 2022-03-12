221be3f9e78f710d0ede592e6eb8dd11629d0a57
Wei Ting <hoonweiting@gmail.com>
2022-03-12 17:25:50 +0800

    Add `or` keyword

--- components/prism-ada.js
+++ components/prism-ada.js
@@ -10,7 +10,7 @@ Prism.languages.ada = {
 		}
 	],
 	'attr-name': /\b'\w+/,
-	'keyword': /\b(?:abort|abs|abstract|accept|access|aliased|all|and|array|at|begin|body|case|constant|declare|delay|delta|digits|do|else|elsif|end|entry|exception|exit|for|function|generic|goto|if|in|interface|is|limited|loop|mod|new|not|null|of|others|out|overriding|package|pragma|private|procedure|protected|raise|range|record|rem|renames|requeue|return|reverse|select|separate|some|subtype|synchronized|tagged|task|terminate|then|type|until|use|when|while|with|xor)\b/i,
+	'keyword': /\b(?:abort|abs|abstract|accept|access|aliased|all|and|array|at|begin|body|case|constant|declare|delay|delta|digits|do|else|elsif|end|entry|exception|exit|for|function|generic|goto|if|in|interface|is|limited|loop|mod|new|not|null|of|or|others|out|overriding|package|pragma|private|procedure|protected|raise|range|record|rem|renames|requeue|return|reverse|select|separate|some|subtype|synchronized|tagged|task|terminate|then|type|until|use|when|while|with|xor)\b/i,
 	'boolean': /\b(?:false|true)\b/i,
 	'operator': /<[=>]?|>=?|=>?|:=|\/=?|\*\*?|[&+-]/,
 	'punctuation': /\.\.?|[,;():]/,
