aa917cfd5d44bc335635df8c2272c8c4777cad6f
drkameleon <yaniszaf@gmail.com>
2022-03-19 12:11:25 +0100

    added initial definitions

--- components/prism-arturo.js
+++ components/prism-arturo.js
@@ -1,1 +1,16 @@
+Prism.languages.arturo = {
+    'comment': /;.*/,
+
+    'character': {
+        pattern: /`.`/,
+        alias: 'function'
+    },
+
+    'string': {
+        pattern: /"(?:[^"\\]|\\.)*"/,
+        greedy: true,
+        alias: 'function'
+    }
+};
+
 Prism.languages.art = Prism.languages["arturo"];
