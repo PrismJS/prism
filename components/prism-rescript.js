16afdd458d11b1d0aefcb7a2dd81f13ee0fbeb81
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2022-04-15 15:32:14 -0300

    fix tests and added aliases to rescript

--- components/prism-rescript.js
+++ components/prism-rescript.js
@@ -1,7 +1,6 @@
 (function (Prism) {
 	var operator = /\.{3}|:[:=]|\|>|->|=(?:==?|>)?|<=?|>=?|[|^?'#!~`]|[+\-*\/]\.?|\b(?:asr|land|lor|lsl|lsr|lxor|mod)\b/;
 	var className = /(\b[A-Z]\w*)|(@[a-z.]*)|#[A-Za-z]\w*|#\d/;
-	var number = /(?:\b0x(?:[\da-f]+(?:\.[\da-f]*)?|\.[\da-f]+)(?:p[+-]?\d+)?|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?)[ful]{0,4}/i;
 	var keyword = /\b(?:and|as|assert|begin|bool|class|constraint|do|done|downto|else|end|exception|external|float|for|fun|function|if|in|include|inherit|initializer|int|lazy|let|method|module|mutable|new|nonrec|object|of|open|or|private|rec|string|switch|then|to|try|type|val|virtual|when|while|with)\b/;
 
 	Prism.languages.rescript = {
@@ -9,24 +8,7 @@
 			pattern: /\/\/(?:[^\r\n\\]|\\(?:\r\n?|\n|(?![\r\n])))*|\/\*[\s\S]*?(?:\*\/|$)/,
 			greedy: true
 		},
-		'string': {
-			pattern: /("(?:\\(?:\r\n|[\s\S])|[^\\\r\n"])*")|(`(?:\\(?:\r\n|[\s\S])|[^\\\r\n"])*`)/,
-			greedy: true,
-			inside: {
-				'tag': {
-					pattern: /\$\{.*\}/,
-					inside: {
-						'class-name': /[A-Z]\w*/g,
-						'function': /[a-z]\w*/g,
-						'punctuation': /\./g,
-						'operator': operator,
-						'number': number,
-						'keyword': keyword
-					}
-				},
-			}
-		},
-
+		'string': /"(?:\\(?:\r\n|[\s\S])|[^\\\r\n"])*"/,
 		'class-name': className,
 		'function': /[a-zA-Z]\w*(?=\()|(\.)[a-z]\w*/,
 		'number': /(?:\b0x(?:[\da-f]+(?:\.[\da-f]*)?|\.[\da-f]+)(?:p[+-]?\d+)?|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?)[ful]{0,4}/i,
@@ -45,4 +27,31 @@
 		'keyword': keyword,
 		'operator': operator
 	};
+
+	Prism.languages.insertBefore('rescript', 'string', {
+		'template-string': {
+			pattern: /`(?:\\[\s\S]|\$\{(?:[^{}]|\{(?:[^{}]|\{[^}]*\})*\})+\}|(?!\$\{)[^\\`])*`/,
+			greedy: true,
+			inside: {
+				'template-punctuation': {
+					pattern: /^`|`$/,
+					alias: 'string'
+				},
+				'interpolation': {
+					pattern: /((?:^|[^\\])(?:\\{2})*)\$\{(?:[^{}]|\{(?:[^{}]|\{[^}]*\})*\})+\}/,
+					lookbehind: true,
+					inside: {
+						'interpolation-punctuation': {
+							pattern: /^\$\{|\}$/,
+							alias: 'punctuation'
+						},
+						rest: Prism.languages.rescript
+					}
+				},
+				'string': /[\s\S]+/
+			}
+		},
+	});
+
+	Prism.languages.res = Prism.languages.rescript;
 }(Prism));
