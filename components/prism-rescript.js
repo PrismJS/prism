9415642f219f9b61361b4195b0d96ac2c55dd6d2
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2022-04-15 16:21:59 -0300

    adding more tests for rescript

--- components/prism-rescript.js
+++ components/prism-rescript.js
@@ -9,8 +9,12 @@ Prism.languages.rescript = {
 		greedy: true
 	},
 	'class-name': /\b[A-Z]\w*|@[a-z.]*|#[A-Za-z]\w*|#\d/,
-	'function': /[a-zA-Z]\w*(?=\()|(\.)[a-z]\w*/,
+	'function': {
+		pattern: /[a-zA-Z]\w*(?=\()|(\.)[a-z]\w*/,
+		lookbehind: true,
+	},
 	'number': /(?:\b0x(?:[\da-f]+(?:\.[\da-f]*)?|\.[\da-f]+)(?:p[+-]?\d+)?|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?)[ful]{0,4}/i,
+	'boolean': /\b(?:false|true)\b/,
 	'attr-value': /[A-Za-z]\w*(?==)/,
 	'constant': {
 		pattern: /(\btype\s+)[a-z]\w*/,
@@ -24,7 +28,8 @@ Prism.languages.rescript = {
 		},
 	},
 	'keyword': /\b(?:and|as|assert|begin|bool|class|constraint|do|done|downto|else|end|exception|external|float|for|fun|function|if|in|include|inherit|initializer|int|lazy|let|method|module|mutable|new|nonrec|object|of|open|or|private|rec|string|switch|then|to|try|type|val|virtual|when|while|with)\b/,
-	'operator': /\.{3}|:[:=]|\|>|->|=(?:==?|>)?|<=?|>=?|[|^?'#!~`]|[+\-*\/]\.?|\b(?:asr|land|lor|lsl|lsr|lxor|mod)\b/
+	'operator': /\.{3}|:[:=]|\|>|->|=(?:==?|>)?|<=?|>=?|[|^?'#!~`]|[+\-*\/]\.?|\b(?:asr|land|lor|lsl|lsr|lxor|mod)\b/,
+	'punctuation': /[(){}[\],;.]/
 };
 
 Prism.languages.insertBefore('rescript', 'string', {
