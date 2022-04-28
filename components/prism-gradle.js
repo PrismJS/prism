886599280c28edf4166d54cb0d38b9744489b523
Abdelkhalek ZELLAT <ha_zellat@esi.dz>
2022-04-28 18:25:17 +0200

    Finish gradle implementation

--- components/prism-gradle.js
+++ components/prism-gradle.js
@@ -1,62 +1,63 @@
 (function (Prism) {
-
 	var interpolation = {
 		pattern: /((?:^|[^\\$])(?:\\{2})*)\$(?:\w+|\{[^{}]*\})/,
 		lookbehind: true,
 		inside: {
-			'interpolation-punctuation': {
+			"interpolation-punctuation": {
 				pattern: /^\$\{?|\}$/,
-				alias: 'punctuation'
+				alias: "punctuation",
 			},
-			'expression': {
+			expression: {
 				pattern: /[\s\S]+/,
-				inside: null
-			}
-		}
+				inside: null,
+			},
+		},
 	};
 
-	Prism.languages.gradle = Prism.languages.extend('clike', {
-		'string': {
+	Prism.languages.gradle = Prism.languages.extend("clike", {
+		string: {
 			pattern: /'''(?:[^\\]|\\[\s\S])*?'''|'(?:\\.|[^\\'\r\n])*'/,
-			greedy: true
+			greedy: true,
 		},
-		'keyword': /\b(?:abstract|as|assert|boolean|break|byte|case|catch|char|class|const|continue|def|default|do|double|else|enum|extends|final|finally|float|for|goto|if|implements|import|in|instanceof|int|interface|long|native|new|package|private|protected|public|return|short|static|strictfp|super|switch|synchronized|this|throw|throws|trait|transient|try|void|volatile|while|val|tasks|dependencies|plugins|repository|sourceSets)\b/,
-		'number': /\b(?:0b[01_]+|0x[\da-f_]+(?:\.[\da-f_p\-]+)?|[\d_]+(?:\.[\d_]+)?(?:e[+-]?\d+)?)[glidf]?\b/i,
-		'operator': {
-			pattern: /(^|[^.])(?:~|==?~?|\?[.:]?|\*(?:[.=]|\*=?)?|\.[@&]|\.\.<|\.\.(?!\.)|-[-=>]?|\+[+=]?|!=?|<(?:<=?|=>?)?|>(?:>>?=?|=)?|&[&=]?|\|[|=]?|\/=?|\^=?|%=?)/,
-			lookbehind: true
+		keyword:
+			/\b(?:apply|def|dependencies|else|if|implementation|import|plugin|plugins|project|repositories|repository|sourceSets|tasks|val)\b/,
+		number: /\b(?:0b[01_]+|0x[\da-f_]+(?:\.[\da-f_p\-]+)?|[\d_]+(?:\.[\d_]+)?(?:e[+-]?\d+)?)[glidf]?\b/i,
+		operator: {
+			pattern:
+				/(^|[^.])(?:~|==?~?|\?[.:]?|\*(?:[.=]|\*=?)?|\.[@&]|\.\.<|\.\.(?!\.)|-[-=>]?|\+[+=]?|!=?|<(?:<=?|=>?)?|>(?:>>?=?|=)?|&[&=]?|\|[|=]?|\/=?|\^=?|%=?)/,
+			lookbehind: true,
 		},
-		'punctuation': /\.+|[{}[\];(),:$]/
+		punctuation: /\.+|[{}[\];(),:$]/,
 	});
 
-	Prism.languages.insertBefore('gradle', 'string', {
-		'shebang': {
+	Prism.languages.insertBefore("gradle", "string", {
+		shebang: {
 			pattern: /#!.+/,
-			alias: 'comment',
-			greedy: true
+			alias: "comment",
+			greedy: true,
 		},
-		'interpolation-string': {
-			pattern: /"""(?:[^\\]|\\[\s\S])*?"""|(["/])(?:\\.|(?!\1)[^\\\r\n])*\1|\$\/(?:[^/$]|\$(?:[/$]|(?![/$]))|\/(?!\$))*\/\$/,
+		"interpolation-string": {
+			pattern:
+				/"""(?:[^\\]|\\[\s\S])*?"""|(["/])(?:\\.|(?!\1)[^\\\r\n])*\1|\$\/(?:[^/$]|\$(?:[/$]|(?![/$]))|\/(?!\$))*\/\$/,
 			greedy: true,
 			inside: {
-				'interpolation': interpolation,
-				'string': /[\s\S]+/
-			}
-		}
+				interpolation: interpolation,
+				string: /[\s\S]+/,
+			},
+		},
 	});
 
-	Prism.languages.insertBefore('gradle', 'punctuation', {
-		'spock-block': /\b(?:and|cleanup|expect|given|setup|then|when|where):/
+	Prism.languages.insertBefore("gradle", "punctuation", {
+		"spock-block": /\b(?:and|cleanup|expect|given|setup|then|when|where):/,
 	});
 
-	Prism.languages.insertBefore('gradle', 'function', {
-		'annotation': {
+	Prism.languages.insertBefore("gradle", "function", {
+		annotation: {
 			pattern: /(^|[^.])@\w+/,
 			lookbehind: true,
-			alias: 'punctuation'
-		}
+			alias: "punctuation",
+		},
 	});
 
 	interpolation.inside.expression.inside = Prism.languages.gradle;
-
-}(Prism));
+})(Prism);
