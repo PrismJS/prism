bcb8f4f25d55ef861084915a933cff1531346316
Dr4gonthree <ha.robert.27@gmail.com>
2022-05-17 14:17:11 +0200

    Match built-ins before function calls

--- components/prism-wgsl.js
+++ components/prism-wgsl.js
@@ -14,11 +14,11 @@ Prism.languages.wgsl = {
         alias: 'function',
     },
     'keyword': /\b(?:bitcast|break|case|const|continue|continuing|default|discard|else|enable|fallthrough|fn|for|function|if|let|loop|private|return|storage|switch|type|uniform|var|while|workgroup)\b/,
+    'builtin': /\b(?:array|atomic|bool|i32|u32|f32|i64|u64|f64|vec[2-4]|mat[2-4]x[2-4]|override|ptr|sampler|sampler_comparison|staticAssert|struct|texture_1d|texture_2d|texture_2d_array|texture_3d|texture_cube|texture_cube_array|texture_multisampled_2d|texture_storage_1d|texture_storage_2d|texture_storage_2d_array|texture_storage_3d|texture_depth_2d|texture_depth_2d_array|texture_depth_cube|texture_depth_cube_array|texture_depth_multisampled_2d)\b/,
     'function-calls': {
         pattern: /\b[_a-z]\w*(?=\()/i,
         alias: 'function',
     },
-    'builtin': /\b(?:array|atomic|bool|i32|u32|f32|i64|u64|f64|vec[2-4]|mat[2-4]x[2-4]|override|ptr|sampler|sampler_comparison|staticAssert|struct|texture_1d|texture_2d|texture_2d_array|texture_3d|texture_cube|texture_cube_array|texture_multisampled_2d|texture_storage_1d|texture_storage_2d|texture_storage_2d_array|texture_storage_3d|texture_depth_2d|texture_depth_2d_array|texture_depth_cube|texture_depth_cube_array|texture_depth_multisampled_2d)\b/,
     'class-name': /\b(?:[A-Z][A-Za-z0-9]*)\b/,
     'built-in-values-vertex': {
         pattern: /\b(?:vertex_index|instance_index|position)\b/,
