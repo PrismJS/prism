12c35d1c7f6fa8e2a70485e94a6a08920517b400
Hyyan Abo Fakher <hyyanaf@gmail.com>
2022-07-29 17:46:50 +0200

    Update components/prism-bbj.js
    Co-authored-by: Michael Schmidt <msrd0000@gmail.com>

--- components/prism-bbj.js
+++ components/prism-bbj.js
@@ -10,7 +10,7 @@
 			greedy: true
 		},
 		'number': /(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:E[+-]?\d+)?/i,
-		'keyword': /\b(?:abstract|all|argc|begin|bye|callback|case|chn|class|classend|ctl|day|delete|dom|dread|dsz|else|endif|err|exitto|extends|fi|field|field|for|from|gosub|goto|if|implements|interface|interfaceend|iol|iolist|let|list|load|method|methodend|methodret|on|opts|pfx|private|private|process_events|protected|protected|psz|public|public|read_resource|remove_callback|restore|rev|seterr|setesc|sqlchn|sqlunt|ssn|start|static|static|swend|switch|sys|then|tim|unt|until|void|void|wend|where|while|use)\b/i,
+		'keyword': /\b(?:abstract|all|argc|begin|bye|callback|case|chn|class|classend|ctl|day|delete|dom|dread|dsz|else|endif|err|exitto|extends|fi|field|for|from|gosub|goto|if|implements|interface|interfaceend|iol|iolist|let|list|load|method|methodend|methodret|on|opts|pfx|private|process_events|protected|psz|public|read_resource|remove_callback|restore|rev|seterr|setesc|sqlchn|sqlunt|ssn|start|static|swend|switch|sys|then|tim|unt|until|use|void|wend|where|while)\b/i,
 		'function': /\b\w+(?=\()/,
 		'boolean': /\b(?:BBjAPI\.TRUE|BBjAPI\.FALSE)\b/i,
 		'operator': /<[=>]?|>=?|[+\-*\/^=&]|\b(?:and|not|or|xor)\b/i,
