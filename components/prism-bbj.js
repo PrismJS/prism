28924c09d0d9a28c70052c8a45bb1795efda055c
Hyyan Abo Fakher <hyyanaf@gmail.com>
2022-07-29 17:14:57 +0200

    update BBj keywords list

--- components/prism-bbj.js
+++ components/prism-bbj.js
@@ -10,7 +10,7 @@
 			greedy: true
 		},
 		'number': /(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:E[+-]?\d+)?/i,
-		'keyword': /\b(?:abstract|all|argc|auto|begin|boolean|break|bye|case|catch|char|chn|class|classend|continue|ctl|day|declare|delete|dom|double|dread|dsz|else|endif|err|exitto|extends|fi|field|for|from|gosub|goto|if|implements|interface|interfaceend|iol|iolist|let|list|load|method|methodend|methodret|new|next|on|opts|pfx|private|process_events|protected|psz|public|read_resource|remove_callback|restore|return|rev|seterr|setesc|sqlchn|sqlunt|ssn|start|static|step|strictfp|super|swend|sys|then|this|throws|tim|to|try|unt|until|use|void|volatile|wend|where|while)\b/i,
+		'keyword': /\b(?:abstract|all|argc|begin|bye|callback|case|chn|class|classend|ctl|day|delete|dom|dread|dsz|else|endif|err|exitto|extends|fi|field|field|for|from|gosub|goto|if|implements|interface|interfaceend|iol|iolist|let|list|load|method|methodend|methodret|on|opts|pfx|private|private|process_events|protected|protected|psz|public|public|read_resource|remove_callback|restore|rev|seterr|setesc|sqlchn|sqlunt|ssn|start|static|static|swend|switch|sys|then|tim|unt|until|void|void|wend|where|while|use)\b/i,
 		'function': /\b\w+(?=\()/,
 		'boolean': /\b(?:BBjAPI\.TRUE|BBjAPI\.FALSE)\b/i,
 		'operator': /<[=>]?|>=?|[+\-*\/^=&]|\b(?:and|not|or|xor)\b/i,
