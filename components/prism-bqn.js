19c8c41832322e915819e01065a5df2b0dd49562
Christopher Rodriguez <yewscion@gmail.com>
2022-08-05 11:05:32 -0400

    Fixed First Round of Issues from Pull Request.
    + Added Punctuation Test.
    + Added token test for 𝕣, _𝕣, and _𝕣_ 'special-name' tokens.
    ~ 'comment' token is now greedy.
    ~ 'shebang' token is now greedy and doesn't use '\s'.
    ~ Fixed Punctuation Regexp.
    ~ Formatted using `npm run lint:fix`.
    - Removed '/u' modifier flag from all patterns aside from 'special-name'.

--- components/prism-bqn.js
+++ components/prism-bqn.js
@@ -1,61 +1,65 @@
 Prism.languages.bqn = {
 	'shebang': {
-	pattern: /^#!\s*\/.*/,
-	alias: 'important'
+		pattern: /^#![ \t]*\/.*/,
+		alias: 'important',
+		greedy: true
 	},
-	'comment': /#.*$/m,
+	'comment': {
+		pattern: /#.*/,
+		greedy: true
+	},
 	'string-literal': {
-	pattern: /"(?:\\[\s\S]|\$(?!\()|`[^`]+`|[^"\\`$])*"/,
-	greedy: true,
-	alias: 'string'
+		pattern: /"(?:\\[\s\S]|\$(?!\()|`[^`]+`|[^"\\`$])*"/,
+		greedy: true,
+		alias: 'string'
 	},
 	'character-literal': {
-	pattern: /'.'/,
-	greedy: true,
-	alias: 'string'
+		pattern: /'.'/,
+		greedy: true,
+		alias: 'string'
 	},
 	'function': {
-	pattern: /[•][A-Z0-9_¯.∞π]+[A-Z0-9_¯.∞π]*/i
+		pattern: /•[\w¯.∞π]+[\w¯.∞π]*/i
 	},
 	'dot-notation-on-brackets': {
-		pattern: /{(?=.*}\.)|}\./,
+		pattern: /\{(?=.*\}\.)|\}\./,
 		alias: 'namespace'
 	},
 	'special-name': {
-	pattern: /[𝕨𝕩𝕗𝕘𝕤𝕣𝕎𝕏𝔽𝔾𝕊]|_𝕣[_]?/,
-	alias: 'keyword'
+		pattern: /[𝕨𝕩𝕗𝕘𝕤𝕣𝕎𝕏𝔽𝔾𝕊]|_𝕣[_]?/u,
+		alias: 'keyword'
 	},
 	'dot-notation-on-name': {
-		pattern: /[A-Z_][A-Z_¯∞π0-9]*\./i,
+		pattern: /[A-Z_][\w¯∞π]*\./i,
 		alias: 'namespace'
 	},
 	'word-number-scientific': {
-		pattern: /\d+(?:\.\d+)?[e|E][¯]?\d+/,
+		pattern: /\d+(?:\.\d+)?[e|E]¯?\d+/,
 		alias: 'number'
 	},
 	'word-name': {
-	pattern: /[A-Z_][A-Z_¯∞π0-9]*/i,
-	alias: 'symbol'
+		pattern: /[A-Z_][\w¯∞π]*/i,
+		alias: 'symbol'
 	},
 	'word-number': {
 		pattern: /[¯∞π]?(?:\d*\.?\b\d+(?:e[+¯]?\d+)?|¯|∞|π)(?:j¯?(?:(?:\d+(?:\.\d+)?|\.\d+)(?:e[+¯]?\d+)?|¯|∞|π))?/i,
 		alias: 'number'
 	},
 	'null-literal': {
-	pattern: /[@]/,
-	alias: 'string'
+		pattern: /@/,
+		alias: 'string'
 	},
 	'primitive-functions': {
-	pattern: /[-+×÷⋆√⌊⌈|¬∧∨<>≠=≤≥≡≢⊣⊢⥊∾≍⋈↑↓↕«»⌽⍉/⍋⍒⊏⊑⊐⊒∊⍷⊔!]/u,
-	alias: 'operator'
+		pattern: /[-+×÷⋆√⌊⌈|¬∧∨<>≠=≤≥≡≢⊣⊢⥊∾≍⋈↑↓↕«»⌽⍉/⍋⍒⊏⊑⊐⊒∊⍷⊔!]/,
+		alias: 'operator'
 	},
 	'primitive-1-operators': {
-	pattern: /[`˜˘¨⁼⌜´˝˙]/,
-	alias: 'operator'
+		pattern: /[`˜˘¨⁼⌜´˝˙]/,
+		alias: 'operator'
 	},
 	'primitive-2-operators': {
-	pattern: /[∘⊸⟜○⌾⎉⚇⍟⊘◶⎊]/,
-	alias: 'operator'
+		pattern: /[∘⊸⟜○⌾⎉⚇⍟⊘◶⎊]/,
+		alias: 'operator'
 	},
-	'punctuation': /[←⇐↩(){}⟨⟩[]‿·⋄,.;:?]/
+	'punctuation': /[←⇐↩(){}⟨⟩[\]‿·⋄,.;:?]/
 };
