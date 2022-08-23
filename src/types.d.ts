3471834cb0cf278d736232c494b08ae2f588e2f0
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-08-23 16:37:52 +0200

    Improved LanguageProto type

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,5 +3,5 @@ export interface LanguageProto {
 	require?: LanguageProto | readonly LanguageProto[];
 	optional?: string | readonly string[];
 	alias?: string | readonly string[];
-	grammar: () => Grammar
+	grammar: Grammar | ((arg0: { getLanguage: (id: string) => Grammar, extend: (id: string, ref: Grammar) => Grammar }) => Grammar)
 }
