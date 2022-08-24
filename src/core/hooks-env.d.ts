0404416aa3843c20a87065e37822cddd159a8fd6
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-08-24 14:49:57 +0200

    Added hooks class

---
+++ src/core/hooks-env.d.ts
@@ -0,0 +1,95 @@
+import { Grammar } from "../types";
+import { TokenStream } from "./token";
+
+/**
+ * An interface containing all hooks Prism runs.
+ */
+export interface HookEnvMap {
+	// Prism.highlightAll
+	'before-highlightall': BeforeHighlightAllEnv;
+	'before-all-elements-highlight': BeforeAllElementsHighlightEnv;
+
+	// Prism.highlightElement
+	'before-sanity-check': BeforeSanityCheckEnv;
+	'before-highlight': BeforeHighlightEnv;
+
+	'before-insert': BeforeInsertEnv;
+	'after-highlight': AfterHighlightEnv;
+	'complete': CompleteEnv;
+
+	// Prism.highlight
+	'before-tokenize': BeforeTokenizeEnv;
+	'after-tokenize': AfterTokenizeEnv;
+
+	// stringify
+	'wrap': WrapEnv;
+}
+
+export type HookEnv<Name extends string> = Name extends keyof HookEnvMap ? HookEnvMap[Name] : unknown;
+
+export interface BeforeHighlightAllEnv {
+	container: ParentNode;
+	selector: string;
+	// callback: (element: Element) => void;
+}
+export interface BeforeAllElementsHighlightEnv {
+	container: ParentNode;
+	selector: string;
+	// callback: (element: Element) => void;
+	elements: Element[];
+}
+
+export interface BeforeSanityCheckEnv {
+	element: Element;
+	language: string;
+	grammar: Grammar | undefined;
+	code: string;
+}
+export interface BeforeHighlightEnv {
+	element: Element;
+	language: string;
+	grammar: Grammar | undefined;
+	code: string;
+}
+export interface CompleteEnv {
+	element: Element;
+	language: string;
+	grammar: Grammar | undefined;
+	code: string;
+	highlightedCode?: string;
+}
+export interface BeforeInsertEnv {
+	element: Element;
+	language: string;
+	grammar: Grammar | undefined;
+	code: string;
+	highlightedCode: string;
+}
+export interface AfterHighlightEnv {
+	element: Element;
+	language: string;
+	grammar: Grammar | undefined;
+	code: string;
+	highlightedCode: string;
+}
+
+export interface BeforeTokenizeEnv {
+	code: string;
+	language: string;
+	grammar: Grammar | undefined;
+}
+export interface AfterTokenizeEnv {
+	code: string;
+	language: string;
+	grammar: Grammar;
+	tokens: TokenStream;
+}
+
+export interface WrapEnv {
+	type: string;
+	content: string;
+	tag: string;
+	classes: string[];
+	attributes: Record<string, string>;
+	language: string;
+}
---
+++ src/core/hooks.js
@@ -0,0 +1,66 @@
+export class Hooks {
+	constructor() {
+		/**
+		 * @type {Map<string, ((env: any) => void)[]>}
+		 */
+		this.all = new Map();
+	}
+
+	/**
+	 * Adds the given callback to the list of callbacks for the given hook and returns a function that
+	 * removes the hook again when called.
+	 *
+	 * The callback will be invoked when the hook it is registered for is run.
+	 * Hooks are usually directly run by a highlight function but you can also run hooks yourself.
+	 *
+	 * One callback function can be registered to multiple hooks.
+	 *
+	 * A callback function must not be registered for the same hook multiple times. Doing so will cause
+	 * undefined behavior. However, registering a callback again after removing it is fine.
+	 *
+	 * @param {Name} name The name of the hook.
+	 * @param {(env: import("./hooks-env").HookEnv<Name>) => void} callback The callback function which is given environment variables.
+	 * @returns {() => void}
+	 * @template {string} Name
+	 * @public
+	 */
+	add(name, callback) {
+		let hooks = this.all.get(name);
+		if (hooks === undefined) {
+			hooks = [];
+			this.all.set(name, hooks);
+		}
+		const list = hooks;
+
+		list.push(callback);
+
+		return () => {
+			const index = list.indexOf(callback);
+			if (index !== -1) {
+				list.splice(index, 1);
+			}
+		};
+	}
+
+	/**
+	 * Runs a hook invoking all registered callbacks with the given environment variables.
+	 *
+	 * Callbacks will be invoked synchronously and in the order in which they were registered.
+	 *
+	 * @param {Name} name The name of the hook.
+	 * @param {import("./hooks-env").HookEnv<Name>} env The environment variables of the hook passed to all callbacks registered.
+	 * @template {string} Name
+	 * @public
+	 */
+	run(name, env) {
+		let callbacks = this.all.get(name);
+
+		if (!callbacks || !callbacks.length) {
+			return;
+		}
+
+		for (const callback of callbacks) {
+			callback(env);
+		}
+	}
+}
