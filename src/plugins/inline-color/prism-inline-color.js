8f746868a1bac3b2f2c92de2016b848a40a51cf8
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-08-27 22:36:41 +0200

    Migrated inline color

--- src/plugins/inline-color/prism-inline-color.js
+++ src/plugins/inline-color/prism-inline-color.js
@@ -1,80 +1,76 @@
-import { noop } from '../../shared/util.js';
-import cssExtras from '../css-extras/prism-css-extras.js';
+import cssExtras from '../../languages/prism-css-extras.js';
+
+// Copied from the markup language definition
+const HTML_TAG = /<\/?(?!\d)[^\s>\/=$<%]+(?:\s(?:\s*[^\s>\/=]+(?:\s*=\s*(?:"[^"]*"|'[^']*'|[^\s'">=]+(?=[\s>]))|(?=[\s/>])))+)?\s*\/?>/g;
+
+// a regex to validate hexadecimal colors
+const HEX_COLOR = /^#?((?:[\da-f]){3,4}|(?:[\da-f]{2}){3,4})$/i;
+
+/**
+ * Parses the given hexadecimal representation and returns the parsed RGBA color.
+ *
+ * If the format of the given string is invalid, `undefined` will be returned.
+ * Valid formats are: `RGB`, `RGBA`, `RRGGBB`, and `RRGGBBAA`.
+ *
+ * Hexadecimal colors are parsed because they are not fully supported by older browsers, so converting them to
+ * `rgba` functions improves browser compatibility.
+ *
+ * @param {string} hex
+ * @returns {string | undefined}
+ */
+function parseHexColor(hex) {
+	const match = HEX_COLOR.exec(hex);
+	if (!match) {
+		return undefined;
+	}
+	hex = match[1]; // removes the leading "#"
+
+	// the width and number of channels
+	const channelWidth = hex.length >= 6 ? 2 : 1;
+	const channelCount = hex.length / channelWidth;
+
+	// the scale used to normalize 4bit and 8bit values
+	const scale = channelWidth == 1 ? 1 / 15 : 1 / 255;
+
+	// normalized RGBA channels
+	const channels = [];
+	for (let i = 0; i < channelCount; i++) {
+		const int = parseInt(hex.substr(i * channelWidth, channelWidth), 16);
+		channels.push(int * scale);
+	}
+	if (channelCount == 3) {
+		channels.push(1); // add alpha of 100%
+	}
+
+	// output
+	const rgb = channels.slice(0, 3).map((x) => {
+		return String(Math.round(x * 255));
+	}).join(',');
+	const alpha = String(Number(channels[3].toFixed(3))); // easy way to round 3 decimal places
+
+	return 'rgba(' + rgb + ',' + alpha + ')';
+}
+
+/**
+ * Validates the given Color using the current browser's internal implementation.
+ *
+ * @param {string} color
+ * @returns {string | undefined}
+ */
+function validateColor(color) {
+	if (typeof document === 'undefined') {
+		return undefined;
+	}
+
+	const s = new Option().style;
+	s.color = color;
+	return s.color ? color : undefined;
+}
 
 export default /** @type {import("../../types").PluginProto} */ ({
 	id: 'inline-color',
 	require: cssExtras,
-	plugin(Prism) {
-		return {}; // TODO:
-	},
 	effect(Prism) {
-		if (typeof document === 'undefined') {
-			return noop;
-		}
-
-		// Copied from the markup language definition
-		const HTML_TAG = /<\/?(?!\d)[^\s>\/=$<%]+(?:\s(?:\s*[^\s>\/=]+(?:\s*=\s*(?:"[^"]*"|'[^']*'|[^\s'">=]+(?=[\s>]))|(?=[\s/>])))+)?\s*\/?>/g;
-
-		// a regex to validate hexadecimal colors
-		const HEX_COLOR = /^#?((?:[\da-f]){3,4}|(?:[\da-f]{2}){3,4})$/i;
-
-		/**
-		 * Parses the given hexadecimal representation and returns the parsed RGBA color.
-		 *
-		 * If the format of the given string is invalid, `undefined` will be returned.
-		 * Valid formats are: `RGB`, `RGBA`, `RRGGBB`, and `RRGGBBAA`.
-		 *
-		 * Hexadecimal colors are parsed because they are not fully supported by older browsers, so converting them to
-		 * `rgba` functions improves browser compatibility.
-		 *
-		 * @param {string} hex
-		 * @returns {string | undefined}
-		 */
-		function parseHexColor(hex) {
-			const match = HEX_COLOR.exec(hex);
-			if (!match) {
-				return undefined;
-			}
-			hex = match[1]; // removes the leading "#"
-
-			// the width and number of channels
-			const channelWidth = hex.length >= 6 ? 2 : 1;
-			const channelCount = hex.length / channelWidth;
-
-			// the scale used to normalize 4bit and 8bit values
-			const scale = channelWidth == 1 ? 1 / 15 : 1 / 255;
-
-			// normalized RGBA channels
-			const channels = [];
-			for (let i = 0; i < channelCount; i++) {
-				const int = parseInt(hex.substr(i * channelWidth, channelWidth), 16);
-				channels.push(int * scale);
-			}
-			if (channelCount == 3) {
-				channels.push(1); // add alpha of 100%
-			}
-
-			// output
-			const rgb = channels.slice(0, 3).map((x) => {
-				return String(Math.round(x * 255));
-			}).join(',');
-			const alpha = String(Number(channels[3].toFixed(3))); // easy way to round 3 decimal places
-
-			return 'rgba(' + rgb + ',' + alpha + ')';
-		}
-
-		/**
-		 * Validates the given Color using the current browser's internal implementation.
-		 *
-		 * @param {string} color
-		 * @returns {string | undefined}
-		 */
-		function validateColor(color) {
-			const s = new Option().style;
-			s.color = color;
-			return s.color ? color : undefined;
-		}
-
 		/**
 		 * An array of function which parse a given string representation of a color.
 		 *
@@ -88,9 +84,8 @@ export default /** @type {import("../../types").PluginProto} */ ({
 			validateColor
 		];
 
-
-		Prism.hooks.add('wrap', (env) => {
-			if (env.type === 'color' || env.classes.indexOf('color') >= 0) {
+		return Prism.hooks.add('wrap', (env) => {
+			if (env.type === 'color' || env.classes.includes('color')) {
 				const content = env.content;
 
 				// remove all HTML tags inside
