903d73f95728432a9505a3756b51958f2a2127f8
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-08-27 18:18:41 +0200

    Migrated download button

--- src/plugins/download-button/prism-download-button.js
+++ src/plugins/download-button/prism-download-button.js
@@ -4,20 +4,24 @@ import toolbar from '../toolbar/prism-toolbar.js';
 export default /** @type {import("../../types").PluginProto} */ ({
 	id: 'download-button',
 	require: toolbar,
-	plugin(Prism) {
-		return {}; // TODO:
-	},
 	effect(Prism) {
 		if (typeof document === 'undefined') {
 			return noop;
 		}
 
-		Prism.plugins.toolbar.registerButton('download-file', (env) => {
-			const pre = env.element.parentNode;
-			if (!pre || !/pre/i.test(pre.nodeName) || !pre.hasAttribute('data-src') || !pre.hasAttribute('data-download-link')) {
+		const toolbar = /** @type {import('../toolbar/prism-toolbar.js').Toolbar} */(Prism.plugins.toolbar);
+
+		toolbar.registerButton('download-file', (env) => {
+			const pre = env.element.parentElement;
+			if (!pre || !/pre/i.test(pre.nodeName)) {
 				return;
 			}
-			const src = pre.getAttribute('data-src');
+
+			const src = pre.getAttribute('data-src') || pre.getAttribute('data-download-link');
+			if (!src) {
+				return;
+			}
+
 			const a = document.createElement('a');
 			a.textContent = pre.getAttribute('data-download-link-label') || 'Download';
 			a.setAttribute('download', '');
