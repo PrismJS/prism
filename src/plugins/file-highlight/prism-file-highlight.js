d2d7da057fcb5a665f543583be6e280a74835b40
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-08-27 20:18:05 +0200

    Migrated File Highlight

--- src/plugins/file-highlight/prism-file-highlight.js
+++ src/plugins/file-highlight/prism-file-highlight.js
@@ -1,26 +1,60 @@
+import { setLanguage } from '../../shared/dom-util.js';
 import { noop } from '../../shared/util.js';
+
+const STATUS_ATTR = 'data-src-status';
+const STATUS_LOADING = 'loading';
+const STATUS_LOADED = 'loaded';
+const STATUS_FAILED = 'failed';
+
+const SELECTOR = 'pre[data-src]:not([' + STATUS_ATTR + '="' + STATUS_LOADED + '"])'
+				+ ':not([' + STATUS_ATTR + '="' + STATUS_LOADING + '"])';
+
+export class FileHighlight {
+	/**
+	 * @param {import('../../core/prism.js').Prism} Prism
+	 * @package
+	 */
+	constructor(Prism) {
+		this.Prism = Prism;
+	}
+
+	/**
+	 * Executes the File Highlight plugin for all matching `pre` elements under the given container.
+	 *
+	 * Note: Elements which are already loaded or currently loading will not be touched by this method.
+	 *
+	 * @param {ParentNode} [container=document]
+	 */
+	highlight(container = document) {
+		const elements = container.querySelectorAll(SELECTOR);
+
+		for (const element of elements) {
+			this.Prism.highlightElement(element);
+		}
+	}
+}
 
 export default /** @type {import("../../types").PluginProto} */ ({
 	id: 'file-highlight',
 	plugin(Prism) {
-		return {}; // TODO:
+		return new FileHighlight(Prism);
 	},
 	effect(Prism) {
 		if (typeof document === 'undefined') {
 			return noop;
 		}
 
-		// https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
-		if (!Element.prototype.matches) {
-			Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
-		}
-
-		const LOADING_MESSAGE = 'Loading…';
-		const FAILURE_MESSAGE = function (status, message) {
+		/**
+		 * @param {number} status
+		 * @param {string} message
+		 */
+		const FAILURE_MESSAGE = (status, message) => {
 			return '✖ Error ' + status + ' while fetching file: ' + message;
 		};
+		const LOADING_MESSAGE = 'Loading…';
 		const FAILURE_EMPTY_MESSAGE = '✖ Error: File does not exist or is empty';
 
+		/** @type {Record<string, string | undefined>} */
 		const EXTENSIONS = {
 			'js': 'javascript',
 			'py': 'python',
@@ -32,14 +66,6 @@ export default /** @type {import("../../types").PluginProto} */ ({
 			'h': 'c',
 			'tex': 'latex'
 		};
-
-		const STATUS_ATTR = 'data-src-status';
-		const STATUS_LOADING = 'loading';
-		const STATUS_LOADED = 'loaded';
-		const STATUS_FAILED = 'failed';
-
-		const SELECTOR = 'pre[data-src]:not([' + STATUS_ATTR + '="' + STATUS_LOADED + '"])'
-				+ ':not([' + STATUS_ATTR + '="' + STATUS_LOADING + '"])';
 
 		/**
 		 * Loads the given file.
@@ -99,103 +125,85 @@ export default /** @type {import("../../types").PluginProto} */ ({
 
 		Prism.hooks.add('before-sanity-check', (env) => {
 			const pre = /** @type {HTMLPreElement} */ (env.element);
-			if (pre.matches(SELECTOR)) {
-				env.code = ''; // fast-path the whole thing and go to complete
 
-				pre.setAttribute(STATUS_ATTR, STATUS_LOADING); // mark as loading
-
-				// add code element with loading message
-				const code = pre.appendChild(document.createElement('CODE'));
-				code.textContent = LOADING_MESSAGE;
-
-				const src = pre.getAttribute('data-src');
-
-				let language = env.language;
-				if (language === 'none') {
-					// the language might be 'none' because there is no language set;
-					// in this case, we want to use the extension as the language
-					const extension = (/\.(\w+)$/.exec(src) || [, 'none'])[1];
-					language = EXTENSIONS[extension] || extension;
-				}
-
-				// set language classes
-				Prism.util.setLanguage(code, language);
-				Prism.util.setLanguage(pre, language);
-
-				// preload the language
-				const autoloader = Prism.plugins.autoloader;
-				if (autoloader) {
-					autoloader.loadLanguages(language);
-				}
-
-				// load file
-				loadFile(
-					src,
-					(text) => {
-						// mark as loaded
-						pre.setAttribute(STATUS_ATTR, STATUS_LOADED);
-
-						// handle data-range
-						const range = parseRange(pre.getAttribute('data-range'));
-						if (range) {
-							const lines = text.split(/\r\n?|\n/g);
-
-							// the range is one-based and inclusive on both ends
-							let start = range[0];
-							let end = range[1] == null ? lines.length : range[1];
-
-							if (start < 0) { start += lines.length; }
-							start = Math.max(0, Math.min(start - 1, lines.length));
-							if (end < 0) { end += lines.length; }
-							end = Math.max(0, Math.min(end, lines.length));
-
-							text = lines.slice(start, end).join('\n');
-
-							// add data-start for line numbers
-							if (!pre.hasAttribute('data-start')) {
-								pre.setAttribute('data-start', String(start + 1));
-							}
-						}
-
-						// highlight code
-						code.textContent = text;
-						Prism.highlightElement(code);
-					},
-					(error) => {
-						// mark as failed
-						pre.setAttribute(STATUS_ATTR, STATUS_FAILED);
-
-						code.textContent = error;
-					}
-				);
+			if (!pre.matches(SELECTOR)) {
+				return;
 			}
+
+			const src = pre.getAttribute('data-src');
+			if (!src) {
+				return;
+			}
+
+			env.code = ''; // fast-path the whole thing and go to complete
+
+			pre.setAttribute(STATUS_ATTR, STATUS_LOADING); // mark as loading
+
+			// add code element with loading message
+			const code = pre.appendChild(document.createElement('CODE'));
+			code.textContent = LOADING_MESSAGE;
+
+
+			let language = env.language;
+			if (language === 'none') {
+				// the language might be 'none' because there is no language set;
+				// in this case, we want to use the extension as the language
+				const extension = /\.(\w+)$/.exec(src)?.[1] || 'none';
+				language = EXTENSIONS[extension] || extension;
+			}
+
+			// set language classes
+			setLanguage(code, language);
+			setLanguage(pre, language);
+
+			// preload the language
+			const autoloader = Prism.plugins.autoloader;
+			if (autoloader) {
+				autoloader.loadLanguages(language);
+			}
+
+			// load file
+			loadFile(
+				src,
+				(text) => {
+					// mark as loaded
+					pre.setAttribute(STATUS_ATTR, STATUS_LOADED);
+
+					// handle data-range
+					const range = parseRange(pre.getAttribute('data-range'));
+					if (range) {
+						const lines = text.split(/\r\n?|\n/g);
+
+						// the range is one-based and inclusive on both ends
+						let start = range[0];
+						let end = range[1] == null ? lines.length : range[1];
+
+						if (start < 0) { start += lines.length; }
+						start = Math.max(0, Math.min(start - 1, lines.length));
+						if (end < 0) { end += lines.length; }
+						end = Math.max(0, Math.min(end, lines.length));
+
+						text = lines.slice(start, end).join('\n');
+
+						// add data-start for line numbers
+						if (!pre.hasAttribute('data-start')) {
+							pre.setAttribute('data-start', String(start + 1));
+						}
+					}
+
+					// highlight code
+					code.textContent = text;
+					Prism.highlightElement(code);
+				},
+				(error) => {
+					// mark as failed
+					pre.setAttribute(STATUS_ATTR, STATUS_FAILED);
+
+					code.textContent = error;
+				}
+			);
 		});
 
-		Prism.plugins.fileHighlight = {
-			/**
-			 * Executes the File Highlight plugin for all matching `pre` elements under the given container.
-			 *
-			 * Note: Elements which are already loaded or currently loading will not be touched by this method.
-			 *
-			 * @param {ParentNode} [container=document]
-			 */
-			highlight: function highlight(container) {
-				const elements = (container || document).querySelectorAll(SELECTOR);
 
-				for (var i = 0, element; (element = elements[i++]);) {
-					Prism.highlightElement(element);
-				}
-			}
-		};
-
-		let logged = false;
-		/** @deprecated Use `Prism.plugins.fileHighlight.highlight` instead. */
-		Prism.fileHighlight = function () {
-			if (!logged) {
-				console.warn('Prism.fileHighlight is deprecated. Use `Prism.plugins.fileHighlight.highlight` instead.');
-				logged = true;
-			}
-			Prism.plugins.fileHighlight.highlight.apply(this, arguments);
-		};
 	}
 });
