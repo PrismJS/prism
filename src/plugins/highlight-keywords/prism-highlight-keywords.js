43a0957125495ab48bbd3a3449e12ced3d739593
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-08-27 20:58:52 +0200

    Migrated Highlight keywords

--- src/plugins/highlight-keywords/prism-highlight-keywords.js
+++ src/plugins/highlight-keywords/prism-highlight-keywords.js
@@ -1,12 +1,7 @@
-import { noop } from '../../shared/util.js';
-
 export default /** @type {import("../../types").PluginProto} */ ({
 	id: 'highlight-keywords',
-	plugin(Prism) {
-		return {}; // TODO:
-	},
 	effect(Prism) {
-		Prism.hooks.add('wrap', (env) => {
+		return Prism.hooks.add('wrap', (env) => {
 			if (env.type !== 'keyword') {
 				return;
 			}
