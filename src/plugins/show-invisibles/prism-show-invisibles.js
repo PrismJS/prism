cf202522b79544ff129a2b413ec52cd3ad7c4fc0
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-09-02 14:50:11 +0200

    Extracted token stream modification from SI

--- src/plugins/show-invisibles/prism-show-invisibles.js
+++ src/plugins/show-invisibles/prism-show-invisibles.js
@@ -1,3 +1,5 @@
+import { tokenizeStrings } from '../../shared/tokenize-strings';
+
 export default /** @type {import("../../types").PluginProto<'show-invisibles'>} */ ({
 	id: 'show-invisibles',
 	optional: ['autolinker', 'data-uri-highlight'],
@@ -10,43 +12,8 @@ export default /** @type {import("../../types").PluginProto<'show-invisibles'>} 
 			'space': / /
 		};
 
-		/**
-		 * @param {string} code
-		 */
-		const tokenize = code => {
-			const tokens = Prism.tokenize(code, invisibles);
-			if (tokens.length === 1) {
-				const single = tokens[0];
-				if (typeof single === 'string') {
-					return single;
-				}
-			}
-			return tokens;
-		};
-
-		/**
-		 * @param {import("../../core/token").TokenStream} tokens
-		 */
-		const walkTokens = (tokens) => {
-			for (let i = 0; i < tokens.length; i++) {
-				const token = tokens[i];
-
-				if (typeof token === 'string') {
-					const result = tokenize(token);
-					if (typeof result !== 'string') {
-						tokens.splice(i, 1, ...result);
-						i += result.length - 1;
-					}
-				} else if (typeof token.content === 'string') {
-					token.content = tokenize(token.content);
-				} else {
-					walkTokens(token.content);
-				}
-			}
-		};
-
 		return Prism.hooks.add('after-tokenize', (env) => {
-			walkTokens(env.tokens);
+			tokenizeStrings(env.tokens, code => Prism.tokenize(code, invisibles));
 		});
 	}
 });
