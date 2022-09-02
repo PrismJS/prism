7ce7d1c870a1c0d1b0d38334a45b466c30adbec7
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-09-02 15:55:14 +0200

    Migrated Treeview I split the plugin into a language and a plugin that only provides styles

---
+++ src/plugins/treeview-icons/prism-treeview-icons.js
@@ -0,0 +1,6 @@
+import treeview from '../../languages/prism-treeview';
+
+export default /** @type {import("../../types").PluginProto<'treeview-icons'>} */ ({
+	id: 'treeview-icons',
+	require: treeview
+});
