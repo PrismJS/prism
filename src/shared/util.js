ec3e9722fe9f925f2231f8bb77771b166ada3690
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-09-05 13:42:09 +0200

    Removed unused util function

--- src/shared/util.js
+++ src/shared/util.js
@@ -1,57 +1,4 @@
 /**
- * Returns the name of the type of the given value.
- *
- * @param {unknown} obj
- * @returns {string}
- * @example
- * type(null)      === 'Null'
- * type(undefined) === 'Undefined'
- * type(123)       === 'Number'
- * type('foo')     === 'String'
- * type(true)      === 'Boolean'
- * type([1, 2])    === 'Array'
- * type({})        === 'Object'
- * type(String)    === 'Function'
- * type(/abc+/)    === 'RegExp'
- */
-function getType(obj) {
-	return Object.prototype.toString.call(obj).slice(8, -1);
-}
-
-/**
- * @param {any} obj
- * @returns {obj is import("../types").PlainObject}
- */
-function isPlainObject(obj) {
-	return getType(obj) === 'Object';
-}
-
-/**
- * @param {Record<string | number, any>} obj
- * @param {(this: any, key: string, value: any, type: string) => void} callback
- * @param {string | null} [type]
- * @param {Set<unknown>} [visited]
- */
-export function DFS(obj, callback, type, visited = new Set()) {
-	for (const i in obj) {
-		if (obj.hasOwnProperty(i)) {
-			callback.call(obj, i, obj[i], type || i);
-
-			const property = obj[i];
-			if (!visited.has(property)) {
-				if (isPlainObject(property)) {
-					visited.add(property);
-					DFS(property, callback, null, visited);
-				} else if (Array.isArray(property)) {
-					visited.add(property);
-					DFS(property, callback, i, visited);
-				}
-			}
-		}
-	}
-}
-
-/**
  * Returns a function that caches the result of the given supplier.
  *
  * @param {() => T} supplier
@@ -96,49 +43,6 @@ export function noop() {
  */
 export function isNonNull(value) {
 	return value != null;
-}
-
-/**
- * Creates a deep clone of the given object.
- *
- * The main intended use of this function is to clone language definitions.
- *
- * @param {T} o
- * @param {Map<any, any>} [mapping]
- * @returns {T}
- * @template T
- */
-export function deepClone(o, mapping = new Map()) {
-	const mapped = mapping.get(o);
-	if (mapped) {
-		return mapped;
-	}
-
-	if (isPlainObject(o)) {
-		/** @type {Record<string, unknown>} */
-		const clone = {};
-		mapped.set(o, clone);
-
-		for (const key in o) {
-			if (o.hasOwnProperty(key)) {
-				clone[key] = deepClone(o[key], mapping);
-			}
-		}
-
-		return /** @type {any} */ (clone);
-	} else if (Array.isArray(o)) {
-		/** @type {unknown[]} */
-		const clone = [];
-		mapped.set(o, clone);
-
-		o.forEach((v, i) => {
-			clone[i] = deepClone(v, mapping);
-		});
-
-		return /** @type {any} */ (clone);
-	} else {
-		return o;
-	}
 }
 
 /**
