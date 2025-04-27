051e0e650aae1224e7989b9cde3e93a55fcda002
Lea Verou <lea@verou.me>
2025-04-27 15:37:12 -0400

    Update src/core/highlight.ts
    Co-authored-by: Dmitry Sharabin <dmitrysharabin@gmail.com>

--- src/core/highlight.ts
+++ src/core/highlight.ts
@@ -25,7 +25,7 @@ export function highlight (
 	this: Prism,
 	text: string,
 	language: string,
-	options?: HighlightOptions
+	options: HighlightOptions = {}
 ): string {
 	const prism = this ?? singleton;
 
