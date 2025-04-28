12ca167addc173ea2a8819e8734f4d4c8fe43987
Lea Verou <lea@verou.me>
2025-04-28 18:48:10 -0400

    Update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const hasDOM = typeof document !== 'undefined' && typeof window !== 'undefined';
-const scriptElement: HTMLOrSVGScriptElement | null = document?.currentScript;
+const scriptElement: HTMLOrSVGScriptElement | null = hasDOM && document.currentScript;
 // @ts-ignore
 const globalConfig: Record<string, PrismConfig[keyof PrismConfig] | null> =
 	globalThis.Prism?.constructor?.name === 'Object' ? globalThis.Prism : {};
