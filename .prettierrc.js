44429cac5aa9e2ae56d8adab20195edb1496fd30
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-04-29 16:56:46 +0200

    [Prettier] Switch to a JS config file (#3917)
    This allows us to add comments to settings.
    
    * Use a better package for sorting imports.
    
    * Add a note about an alternative plugin.

---
+++ .prettierrc.js
@@ -0,0 +1,45 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default {
+	plugins: [
+		'@ianvs/prettier-plugin-sort-imports', // Consider https://www.npmjs.com/package/prettier-plugin-organize-imports as an alternative
+		'prettier-plugin-brace-style',
+		'prettier-plugin-space-before-function-paren',
+		'prettier-plugin-merge',
+	],
+	importOrder: [
+		'<BUILTIN_MODULES>', // Node.js built-in modules
+		'<THIRD_PARTY_MODULES>', // Imports not matched by other special words or groups.
+		'^\\.\\./', // Parent imports
+		'^\\./', // Sibling imports
+		'^\\.$', // Index file
+		'<TYPES>', // Type imports
+	],
+	braceStyle: 'stroustrup',
+	arrowParens: 'avoid',
+	bracketSpacing: true,
+	endOfLine: 'auto',
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	useTabs: true,
+	trailingComma: 'es5',
+	quoteProps: 'preserve',
+	printWidth: 100,
+	overrides: [
+		{
+			files: ['*.yml', '*.yaml'],
+			options: {
+				tabWidth: 2,
+			},
+		},
+		{
+			files: '*.css',
+			options: {
+				singleQuote: false,
+			},
+		},
+	],
+};
