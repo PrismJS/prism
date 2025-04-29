42312a5fdfcee71b04305833f5d036c655d07ba7
Lea Verou <lea@verou.me>
2025-04-29 00:14:42 -0400

    Update actionscript.ts

--- src/languages/actionscript.ts
+++ src/languages/actionscript.ts
@@ -1,35 +1,30 @@
-import { insertBefore } from '../shared/language-util';
 import javascript from './javascript';
-import type { GrammarToken, LanguageProto } from '../types';
+import type { LanguageProto } from '../types';
 
 export default {
 	id: 'actionscript',
 	require: javascript,
-	grammar ({ extend }) {
-		const actionscript = extend('javascript', {
+	base: javascript,
+	grammar () {
+		return {
 			'keyword':
 				/\b(?:as|break|case|catch|class|const|default|delete|do|dynamic|each|else|extends|final|finally|for|function|get|if|implements|import|in|include|instanceof|interface|internal|is|namespace|native|new|null|override|package|private|protected|public|return|set|static|super|switch|this|throw|try|typeof|use|var|void|while|with)\b/,
 			'operator': /\+\+|--|(?:[+\-*\/%^]|&&?|\|\|?|<<?|>>?>?|[!=]=?)=?|[~?@]/,
-		});
-
-		const className = actionscript['class-name'] as GrammarToken;
-		className.alias = 'function';
-
-		delete actionscript['doc-comment'];
-
-		// doesn't work with AS because AS is too complex
-		delete actionscript['parameter'];
-		delete actionscript['literal-property'];
-
-		insertBefore(actionscript, 'string', {
-			'xml': {
-				pattern:
-					/(^|[^.])<\/?\w+(?:\s+[^\s>\/=]+=("|')(?:\\[\s\S]|(?!\2)[^\\])*\2)*\s*\/?>/,
-				lookbehind: true,
-				inside: 'markup',
+			// doesn't work with AS because AS is too complex
+			$delete: ['doc-comment', 'parameter', 'literal-property'],
+			$insertBefore: {
+				'string': {
+					'xml': {
+						pattern:
+							/(^|[^.])<\/?\w+(?:\s+[^\s>\/=]+=("|')(?:\\[\s\S]|(?!\2)[^\\])*\2)*\s*\/?>/,
+						lookbehind: true,
+						inside: 'markup',
+					},
+				},
 			},
-		});
-
-		return actionscript;
+			$merge: {
+				'class-name': { alias: 'function' },
+			},
+		};
 	},
 } as LanguageProto<'actionscript'>;
