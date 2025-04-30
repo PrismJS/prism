afdffea775d070c92c242c07173c1f4fef707eb4
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-04-30 14:54:48 +0200

    First pass

--- src/languages/php-extras.ts
+++ src/languages/php-extras.ts
@@ -2,18 +2,23 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'php-extras',
+	extends: 'php',
 	grammar: {
-		'this': {
-			pattern: /\$this\b/,
-			alias: 'keyword',
-		},
-		'global':
-			/\$(?:GLOBALS|HTTP_RAW_POST_DATA|_(?:COOKIE|ENV|FILES|GET|POST|REQUEST|SERVER|SESSION)|argc|argv|http_response_header|php_errormsg)\b/,
-		'scope': {
-			pattern: /\b[\w\\]+::/,
-			inside: {
-				'keyword': /\b(?:parent|self|static)\b/,
-				'punctuation': /::|\\/,
+		$insertBefore: {
+			'variable': {
+				'this': {
+					pattern: /\$this\b/,
+					alias: 'keyword',
+				},
+				'global':
+					/\$(?:GLOBALS|HTTP_RAW_POST_DATA|_(?:COOKIE|ENV|FILES|GET|POST|REQUEST|SERVER|SESSION)|argc|argv|http_response_header|php_errormsg)\b/,
+				'scope': {
+					pattern: /\b[\w\\]+::/,
+					inside: {
+						'keyword': /\b(?:parent|self|static)\b/,
+						'punctuation': /::|\\/,
+					},
+				},
 			},
 		},
 	},
