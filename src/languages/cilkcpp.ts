0f77a5968d58f3b57832f7ee72af95959436a721
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-05-01 16:39:31 +0200

    Update cilkcpp

--- src/languages/cilkcpp.ts
+++ src/languages/cilkcpp.ts
@@ -1,19 +1,21 @@
-import { insertBefore } from '../shared/language-util';
 import cpp from './cpp';
 import type { LanguageProto } from '../types';
 
 export default {
 	id: 'cilkcpp',
 	require: cpp,
+	base: cpp,
 	alias: ['cilk-cpp', 'cilk'],
-	grammar ({ extend }) {
-		const cilkcpp = extend('cpp', {});
-		insertBefore(cilkcpp, 'function', {
-			'parallel-keyword': {
-				pattern: /\bcilk_(?:for|reducer|s(?:cope|pawn|ync))\b/,
-				alias: 'keyword',
+	grammar () {
+		return {
+			$insertBefore: {
+				'function': {
+					'parallel-keyword': {
+						pattern: /\bcilk_(?:for|reducer|s(?:cope|pawn|ync))\b/,
+						alias: 'keyword',
+					},
+				},
 			},
-		});
-		return cilkcpp;
+		};
 	},
 } as LanguageProto<'cilkcpp'>;
