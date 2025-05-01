1deb86799dcf1b949dc6195f905b8b619d8d9038
Lea Verou <lea@verou.me>
2025-04-30 22:59:42 -0400

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -154,6 +154,7 @@ const config = [
 			'@typescript-eslint/no-unsafe-member-access': 'off',
 			'@typescript-eslint/no-unsafe-return': 'off',
 			'@typescript-eslint/no-unsafe-argument': 'off',
+			'@typescript-eslint/no-floating-promises': 'off',
 		},
 	},
 	{
