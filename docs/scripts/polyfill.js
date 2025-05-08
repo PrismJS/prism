e1261c78a9d0ad7c031159a36873864eec14490a
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-05-08 22:21:17 +0200

    Ignore generated docs

--- docs/scripts/polyfill.js
+++
@@ -1,4 +0,0 @@
-//IE Fix, src: https://www.reddit.com/r/programminghorror/comments/6abmcr/nodelist_lacks_foreach_in_internet_explorer/
-if (typeof(NodeList.prototype.forEach)!==typeof(alert)){
-    NodeList.prototype.forEach=Array.prototype.forEach;
-}
