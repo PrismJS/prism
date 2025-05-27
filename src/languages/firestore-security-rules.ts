96a13668d1102f775566b27249e2ce7b8ff685a3
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-05-27 15:01:17 +0200

    Firestore security rules

--- src/languages/firestore-security-rules.ts
+++ src/languages/firestore-security-rules.ts
@@ -1,48 +1,45 @@
-import { insertBefore } from '../util/insert';
 import clike from './clike';
-import type { LanguageProto } from '../types';
+import type { Grammar, LanguageProto } from '../types';
 
 export default {
 	id: 'firestore-security-rules',
-	require: clike,
-	grammar ({ extend }) {
-		const fsr = extend('clike', {
+	base: clike,
+	grammar (): Grammar {
+		return {
 			'comment': /\/\/.*/,
 			'keyword': /\b(?:allow|function|if|match|null|return|rules_version|service)\b/,
 			'operator': /&&|\|\||[<>!=]=?|[-+*/%]|\b(?:in|is)\b/,
-		});
-
-		delete fsr['class-name'];
-
-		insertBefore(fsr, 'keyword', {
-			'path': {
-				pattern:
-					/(^|[\s(),])(?:\/(?:[\w\xA0-\uFFFF]+|\{[\w\xA0-\uFFFF]+(?:=\*\*)?\}|\$\([\w\xA0-\uFFFF.]+\)))+/,
-				lookbehind: true,
-				greedy: true,
-				inside: {
-					'variable': {
-						pattern: /\{[\w\xA0-\uFFFF]+(?:=\*\*)?\}|\$\([\w\xA0-\uFFFF.]+\)/,
+			$insertBefore: {
+				'keyword': {
+					'path': {
+						pattern:
+							/(^|[\s(),])(?:\/(?:[\w\xA0-\uFFFF]+|\{[\w\xA0-\uFFFF]+(?:=\*\*)?\}|\$\([\w\xA0-\uFFFF.]+\)))+/,
+						lookbehind: true,
+						greedy: true,
 						inside: {
-							'operator': /=/,
-							'keyword': /\*\*/,
-							'punctuation': /[.$(){}]/,
+							'variable': {
+								pattern: /\{[\w\xA0-\uFFFF]+(?:=\*\*)?\}|\$\([\w\xA0-\uFFFF.]+\)/,
+								inside: {
+									'operator': /=/,
+									'keyword': /\*\*/,
+									'punctuation': /[.$(){}]/,
+								},
+							},
+							'punctuation': /\//,
 						},
 					},
-					'punctuation': /\//,
+					'method': {
+						// to make the pattern shorter, the actual method names are omitted
+						pattern: /(\ballow\s+)[a-z]+(?:\s*,\s*[a-z]+)*(?=\s*[:;])/,
+						lookbehind: true,
+						alias: 'builtin',
+						inside: {
+							'punctuation': /,/,
+						},
+					},
 				},
 			},
-			'method': {
-				// to make the pattern shorter, the actual method names are omitted
-				pattern: /(\ballow\s+)[a-z]+(?:\s*,\s*[a-z]+)*(?=\s*[:;])/,
-				lookbehind: true,
-				alias: 'builtin',
-				inside: {
-					'punctuation': /,/,
-				},
-			},
-		});
-
-		return fsr;
+			$delete: ['class-name'],
+		};
 	},
 } as LanguageProto<'firestore-security-rules'>;
