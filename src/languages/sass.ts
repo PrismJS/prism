9f99a672e612b3b4863e1f6eeabd35b798e54ea7
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-05-28 17:32:27 +0200

    sass

--- src/languages/sass.ts
+++ src/languages/sass.ts
@@ -1,33 +1,10 @@
-import { insertBefore } from '../util/insert';
 import css from './css';
-import type { LanguageProto } from '../types';
+import type { Grammar, LanguageProto } from '../types';
 
 export default {
 	id: 'sass',
-	require: css,
-	grammar ({ extend }) {
-		const sass = extend('css', {
-			// Sass comments don't need to be closed, only indented
-			'comment': {
-				pattern: /^([ \t]*)\/[\/*].*(?:(?:\r?\n|\r)\1[ \t].+)*/m,
-				lookbehind: true,
-				greedy: true,
-			},
-		});
-
-		insertBefore(sass, 'atrule', {
-			// We want to consume the whole line
-			'atrule-line': {
-				// Includes support for = and + shortcuts
-				pattern: /^(?:[ \t]*)[@+=].+/m,
-				greedy: true,
-				inside: {
-					'atrule': /(?:@[\w-]+|[+=])/,
-				},
-			},
-		});
-		delete sass.atrule;
-
+	base: css,
+	grammar ({ base }) {
 		const variable = /\$[-\w]+|#\{\$[-\w]+\}/;
 		const operator = [
 			/[+*\/%]|[=!]=|<=?|>=?|\b(?:and|not|or)\b/,
@@ -37,50 +14,67 @@ export default {
 			},
 		];
 
-		insertBefore(sass, 'property', {
-			// We want to consume the whole line
-			'variable-line': {
-				pattern: /^[ \t]*\$.+/m,
-				greedy: true,
-				inside: {
-					'punctuation': /:/,
-					'variable': variable,
-					'operator': operator,
-				},
-			},
-			// We want to consume the whole line
-			'property-line': {
-				pattern: /^[ \t]*(?:[^:\s]+ *:.*|:[^:\s].*)/m,
-				greedy: true,
-				inside: {
-					'property': [
-						/[^:\s]+(?=\s*:)/,
-						{
-							pattern: /(:)[^:\s]+/,
-							lookbehind: true,
-						},
-					],
-					'punctuation': /:/,
-					'variable': variable,
-					'operator': operator,
-					'important': sass.important,
-				},
-			},
-		});
-		delete sass.property;
-		delete sass.important;
-
-		// Now that whole lines for other patterns are consumed,
-		// what's left should be selectors
-		insertBefore(sass, 'punctuation', {
-			'selector': {
-				pattern:
-					/^([ \t]*)\S(?:,[^,\r\n]+|[^,\r\n]*)(?:,[^,\r\n]+)*(?:,(?:\r?\n|\r)\1[ \t]+\S(?:,[^,\r\n]+|[^,\r\n]*)(?:,[^,\r\n]+)*)*/m,
+		return {
+			// Sass comments don't need to be closed, only indented
+			'comment': {
+				pattern: /^([ \t]*)\/[\/*].*(?:(?:\r?\n|\r)\1[ \t].+)*/m,
 				lookbehind: true,
 				greedy: true,
 			},
-		});
-
-		return sass;
+			$insertBefore: {
+				'atrule': {
+					// We want to consume the whole line
+					'atrule-line': {
+						// Includes support for = and + shortcuts
+						pattern: /^(?:[ \t]*)[@+=].+/m,
+						greedy: true,
+						inside: {
+							'atrule': /(?:@[\w-]+|[+=])/,
+						},
+					},
+				},
+				'property': {
+					// We want to consume the whole line
+					'variable-line': {
+						pattern: /^[ \t]*\$.+/m,
+						greedy: true,
+						inside: {
+							'punctuation': /:/,
+							'variable': variable,
+							'operator': operator,
+						},
+					},
+					// We want to consume the whole line
+					'property-line': {
+						pattern: /^[ \t]*(?:[^:\s]+ *:.*|:[^:\s].*)/m,
+						greedy: true,
+						inside: {
+							'property': [
+								/[^:\s]+(?=\s*:)/,
+								{
+									pattern: /(:)[^:\s]+/,
+									lookbehind: true,
+								},
+							],
+							'punctuation': /:/,
+							'variable': variable,
+							'operator': operator,
+							'important': base!.important,
+						},
+					},
+				},
+				// Now that whole lines for other patterns are consumed,
+				// what's left should be selectors
+				'punctuation': {
+					'selector': {
+						pattern:
+							/^([ \t]*)\S(?:,[^,\r\n]+|[^,\r\n]*)(?:,[^,\r\n]+)*(?:,(?:\r?\n|\r)\1[ \t]+\S(?:,[^,\r\n]+|[^,\r\n]*)(?:,[^,\r\n]+)*)*/m,
+						lookbehind: true,
+						greedy: true,
+					},
+				},
+			},
+			$delete: ['atrule', 'property', 'important'],
+		} as unknown as Grammar;
 	},
 } as LanguageProto<'sass'>;
