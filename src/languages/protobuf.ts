d2ccfb0aede71ebba8c681c32ef3e5f38986766f
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-05-28 16:22:46 +0200

    protobuf

--- src/languages/protobuf.ts
+++ src/languages/protobuf.ts
@@ -1,15 +1,14 @@
-import { insertBefore } from '../util/insert';
 import clike from './clike';
 import type { LanguageProto } from '../types';
 
 export default {
 	id: 'protobuf',
-	require: clike,
-	grammar ({ extend }) {
+	base: clike,
+	grammar () {
 		const builtinTypes =
 			/\b(?:bool|bytes|double|s?fixed(?:32|64)|float|[su]?int(?:32|64)|string)\b/;
 
-		const protobuf = extend('clike', {
+		return {
 			'class-name': [
 				{
 					pattern: /(\b(?:enum|extend|message|service)\s+)[A-Za-z_]\w*(?=\s*\{)/,
@@ -24,31 +23,30 @@ export default {
 			'keyword':
 				/\b(?:enum|extend|extensions|import|message|oneof|option|optional|package|public|repeated|required|reserved|returns|rpc(?=\s+\w)|service|stream|syntax|to)\b(?!\s*=\s*\d)/,
 			'function': /\b[a-z_]\w*(?=\s*\()/i,
-		});
-
-		insertBefore(protobuf, 'operator', {
-			'map': {
-				pattern: /\bmap<\s*[\w.]+\s*,\s*[\w.]+\s*>(?=\s+[a-z_]\w*\s*[=;])/i,
-				alias: 'class-name',
-				inside: {
-					'punctuation': /[<>.,]/,
+			$insertBefore: {
+				'operator': {
+					'map': {
+						pattern: /\bmap<\s*[\w.]+\s*,\s*[\w.]+\s*>(?=\s+[a-z_]\w*\s*[=;])/i,
+						alias: 'class-name',
+						inside: {
+							'punctuation': /[<>.,]/,
+							'builtin': builtinTypes,
+						},
+					},
 					'builtin': builtinTypes,
+					'positional-class-name': {
+						pattern: /(?:\b|\B\.)[a-z_]\w*(?:\.[a-z_]\w*)*(?=\s+[a-z_]\w*\s*[=;])/i,
+						alias: 'class-name',
+						inside: {
+							'punctuation': /\./,
+						},
+					},
+					'annotation': {
+						pattern: /(\[\s*)[a-z_]\w*(?=\s*=)/i,
+						lookbehind: true,
+					},
 				},
 			},
-			'builtin': builtinTypes,
-			'positional-class-name': {
-				pattern: /(?:\b|\B\.)[a-z_]\w*(?:\.[a-z_]\w*)*(?=\s+[a-z_]\w*\s*[=;])/i,
-				alias: 'class-name',
-				inside: {
-					'punctuation': /\./,
-				},
-			},
-			'annotation': {
-				pattern: /(\[\s*)[a-z_]\w*(?=\s*=)/i,
-				lookbehind: true,
-			},
-		});
-
-		return protobuf;
+		};
 	},
 } as LanguageProto<'protobuf'>;
