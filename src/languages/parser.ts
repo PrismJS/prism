c9953879822a75d856f95f72e66298b319c322ef
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-05-28 17:12:44 +0200

    parser

--- src/languages/parser.ts
+++ src/languages/parser.ts
@@ -1,14 +1,13 @@
-import { insertBefore } from '../util/insert';
 import markup from './markup';
-import type { Grammar, GrammarToken, LanguageProto } from '../types';
+import type { LanguageProto } from '../types';
 
 export default {
 	id: 'parser',
-	require: markup,
-	grammar ({ extend }) {
+	base: markup,
+	grammar ({ base }) {
 		const punctuation = /[\[\](){};]/;
 
-		const parser = extend('markup', {
+		return {
 			'keyword': {
 				pattern:
 					/(^|[^^])(?:\^(?:case|eval|for|if|switch|throw)\b|@(?:BASE|CLASS|GET(?:_DEFAULT)?|OPTIONS|SET_DEFAULT|USE)\b)/,
@@ -37,54 +36,49 @@ export default {
 				alias: 'builtin',
 			},
 			'punctuation': punctuation,
-		});
-
-		insertBefore(parser, 'keyword', {
-			'parser-comment': {
-				pattern: /(\s)#.*/,
-				lookbehind: true,
-				alias: 'comment',
-			},
-			'expression': {
-				// Allow for 3 levels of depth
-				pattern: /(^|[^^])\((?:[^()]|\((?:[^()]|\((?:[^()])*\))*\))*\)/,
-				greedy: true,
-				lookbehind: true,
-				inside: {
-					'string': {
-						pattern: /(^|[^^])(["'])(?:(?!\2)[^^]|\^[\s\S])*\2/,
+			$insertBefore: {
+				'keyword': {
+					'parser-comment': {
+						pattern: /(\s)#.*/,
 						lookbehind: true,
+						alias: 'comment',
 					},
-					'keyword': parser.keyword,
-					'variable': parser.variable,
-					'function': parser.function,
-					'boolean': /\b(?:false|true)\b/,
-					'number': /\b(?:0x[a-f\d]+|\d+(?:\.\d*)?(?:e[+-]?\d+)?)\b/i,
-					'escape': parser.escape,
-					'operator':
-						/[~+*\/\\%]|!(?:\|\|?|=)?|&&?|\|\|?|==|<[<=]?|>[>=]?|-[fd]?|\b(?:def|eq|ge|gt|in|is|le|lt|ne)\b/,
-					'punctuation': punctuation,
+					'expression': {
+						// Allow for 3 levels of depth
+						pattern: /(^|[^^])\((?:[^()]|\((?:[^()]|\((?:[^()])*\))*\))*\)/,
+						greedy: true,
+						lookbehind: true,
+						inside: {
+							'string': {
+								pattern: /(^|[^^])(["'])(?:(?!\2)[^^]|\^[\s\S])*\2/,
+								lookbehind: true,
+							},
+							'keyword': base!.keyword,
+							'variable': base!.variable,
+							'function': base!.function,
+							'boolean': /\b(?:false|true)\b/,
+							'number': /\b(?:0x[a-f\d]+|\d+(?:\.\d*)?(?:e[+-]?\d+)?)\b/i,
+							'escape': base!.escape,
+							'operator':
+								/[~+*\/\\%]|!(?:\|\|?|=)?|&&?|\|\|?|==|<[<=]?|>[>=]?|-[fd]?|\b(?:def|eq|ge|gt|in|is|le|lt|ne)\b/,
+							'punctuation': punctuation,
+						},
+					},
+				},
+				'tag/attr-value': {
+					'punctuation': {
+						'expression': base!.expression,
+						'keyword': base!.keyword,
+						'variable': base!.variable,
+						'function': base!.function,
+						'escape': base!.escape,
+						'parser-punctuation': {
+							pattern: punctuation,
+							alias: 'punctuation',
+						},
+					},
 				},
 			},
-		});
-
-		insertBefore(
-			(((parser['tag'] as GrammarToken).inside as Grammar)['attr-value'] as GrammarToken)
-				.inside as Grammar,
-			'punctuation',
-			{
-				'expression': parser.expression,
-				'keyword': parser.keyword,
-				'variable': parser.variable,
-				'function': parser.function,
-				'escape': parser.escape,
-				'parser-punctuation': {
-					pattern: punctuation,
-					alias: 'punctuation',
-				},
-			}
-		);
-
-		return parser;
+		};
 	},
 } as LanguageProto<'parser'>;
