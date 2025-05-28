6e83d98631e744bc8b95c9efccd134f7e4e85d9d
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-05-28 15:51:59 +0200

    idris

--- src/languages/idris.ts
+++ src/languages/idris.ts
@@ -1,31 +1,29 @@
-import { insertBefore } from '../util/insert';
 import haskell from './haskell';
 import type { LanguageProto } from '../types';
 
 export default {
 	id: 'idris',
-	require: haskell,
+	base: haskell,
 	alias: 'idr',
-	grammar ({ extend }) {
-		const idris = extend('haskell', {
+	grammar () {
+		return {
 			'comment': {
 				pattern: /(?:(?:--|\|\|\|).*$|\{-[\s\S]*?-\})/m,
 			},
 			'keyword':
 				/\b(?:Type|case|class|codata|constructor|corecord|data|do|dsl|else|export|if|implementation|implicit|import|impossible|in|infix|infixl|infixr|instance|interface|let|module|mutual|namespace|of|parameters|partial|postulate|private|proof|public|quoteGoal|record|rewrite|syntax|then|total|using|where|with)\b/,
 			'builtin': undefined,
-		});
-
-		insertBefore(idris, 'keyword', {
-			'import-statement': {
-				pattern: /(^\s*import\s+)(?:[A-Z][\w']*)(?:\.[A-Z][\w']*)*/m,
-				lookbehind: true,
-				inside: {
-					'punctuation': /\./,
+			$insertBefore: {
+				'keyword': {
+					'import-statement': {
+						pattern: /(^\s*import\s+)(?:[A-Z][\w']*)(?:\.[A-Z][\w']*)*/m,
+						lookbehind: true,
+						inside: {
+							'punctuation': /\./,
+						},
+					},
 				},
 			},
-		});
-
-		return idris;
+		};
 	},
 } as LanguageProto<'idris'>;
