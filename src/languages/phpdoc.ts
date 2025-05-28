1b597a20ecf71f76e8cd64e409a6fb081fdbb8f1
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-05-28 16:28:05 +0200

    phpdoc

--- src/languages/phpdoc.ts
+++ src/languages/phpdoc.ts
@@ -1,14 +1,13 @@
-import { insertBefore } from '../util/insert';
 import javadoclike from './javadoclike';
 import type { LanguageProto } from '../types';
 
 export default {
 	id: 'phpdoc',
-	require: javadoclike,
-	grammar ({ extend }) {
+	base: javadoclike,
+	grammar () {
 		const typeExpression = /(?:\b[a-zA-Z]\w*|[|\\[\]])+/.source;
 
-		const phpdoc = extend('javadoclike', {
+		return {
 			'parameter': {
 				pattern: RegExp(
 					'(@(?:global|param|property(?:-read|-write)?|var)\\s+(?:' +
@@ -17,25 +16,24 @@ export default {
 				),
 				lookbehind: true,
 			},
-		});
-
-		insertBefore(phpdoc, 'keyword', {
-			'class-name': [
-				{
-					pattern: RegExp(
-						'(@(?:global|package|param|property(?:-read|-write)?|return|subpackage|throws|var)\\s+)' +
-							typeExpression
-					),
-					lookbehind: true,
-					inside: {
-						'keyword':
-							/\b(?:array|bool|boolean|callback|double|false|float|int|integer|mixed|null|object|resource|self|string|true|void)\b/,
-						'punctuation': /[|\\[\]()]/,
-					},
+			$insertBefore: {
+				'keyword': {
+					'class-name': [
+						{
+							pattern: RegExp(
+								'(@(?:global|package|param|property(?:-read|-write)?|return|subpackage|throws|var)\\s+)' +
+									typeExpression
+							),
+							lookbehind: true,
+							inside: {
+								'keyword':
+									/\b(?:array|bool|boolean|callback|double|false|float|int|integer|mixed|null|object|resource|self|string|true|void)\b/,
+								'punctuation': /[|\\[\]()]/,
+							},
+						},
+					],
 				},
-			],
-		});
-
-		return phpdoc;
+			},
+		};
 	},
 } as LanguageProto<'phpdoc'>;
