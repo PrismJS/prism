ae16a1592ebf410befe6ea426e919b29352737eb
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-05-28 15:08:16 +0200

    javadoc

--- src/languages/javadoc.ts
+++ src/languages/javadoc.ts
@@ -1,4 +1,3 @@
-import { insertBefore } from '../util/insert';
 import java from './java';
 import javadoclike from './javadoclike';
 import markup from './markup';
@@ -6,10 +5,11 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'javadoc',
-	require: [markup, java, javadoclike],
-	grammar ({ extend, getLanguage }) {
-		const java = getLanguage('java');
-		const { tag, entity } = getLanguage('markup');
+	base: javadoclike,
+	require: [markup, java],
+	grammar ({ languages }) {
+		const java = languages.java;
+		const { tag, entity } = languages.markup;
 
 		const codeLinePattern = /(^(?:[\t ]*(?:\*\s*)*))[^*\s].*$/m;
 
@@ -19,85 +19,86 @@ export default {
 			() => memberReference
 		);
 
-		const javadoc = extend('javadoclike', {});
-		insertBefore(javadoc, 'keyword', {
-			'reference': {
-				pattern: RegExp(
-					/(@(?:exception|link|linkplain|see|throws|value)\s+(?:\*\s*)?)/.source +
-						'(?:' +
-						reference +
-						')'
-				),
-				lookbehind: true,
-				inside: {
-					'function': {
-						pattern: /(#\s*)\w+(?=\s*\()/,
+		return {
+			$insertBefore: {
+				'keyword': {
+					'reference': {
+						pattern: RegExp(
+							/(@(?:exception|link|linkplain|see|throws|value)\s+(?:\*\s*)?)/.source +
+								'(?:' +
+								reference +
+								')'
+						),
 						lookbehind: true,
-					},
-					'field': {
-						pattern: /(#\s*)\w+/,
-						lookbehind: true,
-					},
-					'namespace': {
-						pattern: /\b(?:[a-z]\w*\s*\.\s*)+/,
 						inside: {
-							'punctuation': /\./,
+							'function': {
+								pattern: /(#\s*)\w+(?=\s*\()/,
+								lookbehind: true,
+							},
+							'field': {
+								pattern: /(#\s*)\w+/,
+								lookbehind: true,
+							},
+							'namespace': {
+								pattern: /\b(?:[a-z]\w*\s*\.\s*)+/,
+								inside: {
+									'punctuation': /\./,
+								},
+							},
+							'class-name': /\b[A-Z]\w*/,
+							'keyword': java.keyword,
+							'punctuation': /[#()[\],.]/,
 						},
 					},
-					'class-name': /\b[A-Z]\w*/,
-					'keyword': java.keyword,
-					'punctuation': /[#()[\],.]/,
-				},
-			},
-			'class-name': {
-				// @param <T> the first generic type parameter
-				pattern: /(@param\s+)<[A-Z]\w*>/,
-				lookbehind: true,
-				inside: {
-					'punctuation': /[.<>]/,
-				},
-			},
-			'code-section': [
-				{
-					pattern:
-						/(\{@code\s+(?!\s))(?:[^\s{}]|\s+(?![\s}])|\{(?:[^{}]|\{(?:[^{}]|\{(?:[^{}]|\{[^{}]*\})*\})*\})*\})+(?=\s*\})/,
-					lookbehind: true,
-					inside: {
-						'code': {
-							// there can't be any HTML inside of {@code} tags
-							pattern: codeLinePattern,
-							lookbehind: true,
-							inside: 'java',
-							alias: 'language-java',
+					'class-name': {
+						// @param <T> the first generic type parameter
+						pattern: /(@param\s+)<[A-Z]\w*>/,
+						lookbehind: true,
+						inside: {
+							'punctuation': /[.<>]/,
 						},
 					},
-				},
-				{
-					pattern: /(<(code|pre|tt)>(?!<code>)\s*)\S(?:\S|\s+\S)*?(?=\s*<\/\2>)/,
-					lookbehind: true,
-					inside: {
-						'line': {
-							pattern: codeLinePattern,
+					'code-section': [
+						{
+							pattern:
+								/(\{@code\s+(?!\s))(?:[^\s{}]|\s+(?![\s}])|\{(?:[^{}]|\{(?:[^{}]|\{(?:[^{}]|\{[^{}]*\})*\})*\})*\})+(?=\s*\})/,
 							lookbehind: true,
 							inside: {
-								// highlight HTML tags and entities
-								'tag': tag,
-								'entity': entity,
 								'code': {
-									// everything else is Java code
-									pattern: /.+/,
+									// there can't be any HTML inside of {@code} tags
+									pattern: codeLinePattern,
+									lookbehind: true,
 									inside: 'java',
 									alias: 'language-java',
 								},
 							},
 						},
-					},
+						{
+							pattern: /(<(code|pre|tt)>(?!<code>)\s*)\S(?:\S|\s+\S)*?(?=\s*<\/\2>)/,
+							lookbehind: true,
+							inside: {
+								'line': {
+									pattern: codeLinePattern,
+									lookbehind: true,
+									inside: {
+										// highlight HTML tags and entities
+										'tag': tag,
+										'entity': entity,
+										'code': {
+											// everything else is Java code
+											pattern: /.+/,
+											inside: 'java',
+											alias: 'language-java',
+										},
+									},
+								},
+							},
+						},
+					],
+					'tag': tag,
+					'entity': entity,
 				},
-			],
-			'tag': tag,
-			'entity': entity,
-		});
-
-		return javadoc;
+			},
+		};
 	},
 } as LanguageProto<'javadoc'>;
