222481bf867967ecf9952e5ef870e2a6d9a71063
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-05-28 16:12:30 +0200

    scala

--- src/languages/scala.ts
+++ src/languages/scala.ts
@@ -1,12 +1,11 @@
-import { insertBefore } from '../util/insert';
 import java from './java';
-import type { LanguageProto } from '../types';
+import type { Grammar, LanguageProto } from '../types';
 
 export default {
 	id: 'scala',
-	require: java,
-	grammar ({ extend }) {
-		const scala = extend('java', {
+	base: java,
+	grammar () {
+		return {
 			'triple-quoted-string': {
 				pattern: /"""[\s\S]*?"""/,
 				greedy: true,
@@ -22,45 +21,40 @@ export default {
 			'builtin':
 				/\b(?:Any|AnyRef|AnyVal|Boolean|Byte|Char|Double|Float|Int|Long|Nothing|Short|String|Unit)\b/,
 			'symbol': /'[^\d\s\\]\w*/,
-		});
-
-		insertBefore(scala, 'triple-quoted-string', {
-			'string-interpolation': {
-				pattern:
-					/\b[a-z]\w*(?:"""(?:[^$]|\$(?:[^{]|\{(?:[^{}]|\{[^{}]*\})*\}))*?"""|"(?:[^$"\r\n]|\$(?:[^{]|\{(?:[^{}]|\{[^{}]*\})*\}))*")/i,
-				greedy: true,
-				inside: {
-					'id': {
-						pattern: /^\w+/,
-						greedy: true,
-						alias: 'function',
-					},
-					'escape': {
-						pattern: /\\\$"|\$[$"]/,
-						greedy: true,
-						alias: 'symbol',
-					},
-					'interpolation': {
-						pattern: /\$(?:\w+|\{(?:[^{}]|\{[^{}]*\})*\})/,
+			$insertBefore: {
+				'triple-quoted-string': {
+					'string-interpolation': {
+						pattern:
+							/\b[a-z]\w*(?:"""(?:[^$]|\$(?:[^{]|\{(?:[^{}]|\{[^{}]*\})*\}))*?"""|"(?:[^$"\r\n]|\$(?:[^{]|\{(?:[^{}]|\{[^{}]*\})*\}))*")/i,
 						greedy: true,
 						inside: {
-							'punctuation': /^\$\{?|\}$/,
-							'expression': {
-								pattern: /[\s\S]+/,
-								inside: 'scala',
+							'id': {
+								pattern: /^\w+/,
+								greedy: true,
+								alias: 'function',
 							},
+							'escape': {
+								pattern: /\\\$"|\$[$"]/,
+								greedy: true,
+								alias: 'symbol',
+							},
+							'interpolation': {
+								pattern: /\$(?:\w+|\{(?:[^{}]|\{[^{}]*\})*\})/,
+								greedy: true,
+								inside: {
+									'punctuation': /^\$\{?|\}$/,
+									'expression': {
+										pattern: /[\s\S]+/,
+										inside: 'scala',
+									},
+								},
+							},
+							'string': /[\s\S]+/,
 						},
 					},
-					'string': /[\s\S]+/,
 				},
 			},
-		});
-
-		delete scala['doc-comment'];
-		delete scala['class-name'];
-		delete scala['function'];
-		delete scala['constant'];
-
-		return scala;
+			$delete: ['doc-comment', 'class-name', 'function', 'constant'],
+		} as unknown as Grammar;
 	},
 } as LanguageProto<'scala'>;
