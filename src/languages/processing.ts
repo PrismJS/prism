cbd86e2ce93be12350722e3d82aa47b492da4390
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-05-28 16:24:40 +0200

    processing

--- src/languages/processing.ts
+++ src/languages/processing.ts
@@ -1,28 +1,26 @@
-import { insertBefore } from '../util/insert';
 import clike from './clike';
 import type { LanguageProto } from '../types';
 
 export default {
 	id: 'processing',
-	require: clike,
-	grammar ({ extend }) {
-		const processing = extend('clike', {
+	base: clike,
+	grammar () {
+		return {
 			'keyword':
 				/\b(?:break|case|catch|class|continue|default|else|extends|final|for|if|implements|import|new|null|private|public|return|static|super|switch|this|try|void|while)\b/,
 			// Spaces are allowed between function name and parenthesis
 			'function': /\b\w+(?=\s*\()/,
 			'operator': /<[<=]?|>[>=]?|&&?|\|\|?|[%?]|[!=+\-*\/]=?/,
-		});
-
-		insertBefore(processing, 'number', {
-			// Special case: XML is a type
-			'constant': /\b(?!XML\b)[A-Z][A-Z\d_]+\b/,
-			'type': {
-				pattern: /\b(?:boolean|byte|char|color|double|float|int|[A-Z]\w*)\b/,
-				alias: 'class-name',
+			$insertBefore: {
+				'number': {
+					// Special case: XML is a type
+					'constant': /\b(?!XML\b)[A-Z][A-Z\d_]+\b/,
+					'type': {
+						pattern: /\b(?:boolean|byte|char|color|double|float|int|[A-Z]\w*)\b/,
+						alias: 'class-name',
+					},
+				},
 			},
-		});
-
-		return processing;
+		};
 	},
 } as LanguageProto<'processing'>;
