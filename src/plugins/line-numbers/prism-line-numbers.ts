a8813c30a3081d8f1120c4949906a3fb9876e95d
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-14 17:59:24 +0200

    Address feedback: Remove boilerplate

--- src/plugins/line-numbers/prism-line-numbers.ts
+++ src/plugins/line-numbers/prism-line-numbers.ts
@@ -1,6 +1,6 @@
+import Prism from '../../global';
 import { getParentPre, isActive } from '../../shared/dom-util';
 import { isNonNull, noop } from '../../shared/util';
-import { documentReady } from '../../util/async';
 import { combineCallbacks } from '../../util/combine-callbacks';
 import type { PluginProto } from '../../types';
 
@@ -276,11 +276,4 @@ const Self = {
 
 export default Self;
 
-if (typeof document !== 'undefined') {
-	void documentReady().then(() => {
-		const Prism = (globalThis as any).Prism;
-		if (Prism) {
-			Prism.components.add(Self);
-		}
-	});
-}
+Prism.components.add(Self);
