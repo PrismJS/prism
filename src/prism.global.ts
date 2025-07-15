44a202e833eece0e66c2827534130c79303bf515
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-15 08:55:07 +0200

    Fix issue with relative URL in dynamic import
    For now, when using the global bundle, we get the `Uncaught (in promise) TypeError: Failed to resolve module specifier './index.js'. The base URL is about:blank because import() is called from a CORS-cross-origin script.` error.

--- src/prism.global.ts
+++ src/prism.global.ts
@@ -1,2 +1,4 @@
 // In browser and imported via non-ESM
-import('./index.js').then(({ default: prism }) => ((globalThis as any).Prism = prism));
+const url = new URL('./index.js', (globalThis.document.currentScript as HTMLScriptElement).src);
+
+void import(url.toString()).then(({ default: prism }) => ((globalThis as any).Prism = prism));
