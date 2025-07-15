ce31d5f21085125374c5edc92bf574dac4a42a73
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-15 13:25:10 +0200

    Treeview Icons

--- src/plugins/treeview-icons/prism-treeview-icons.ts
+++ src/plugins/treeview-icons/prism-treeview-icons.ts
@@ -1,7 +1,12 @@
+import prism from '../../global';
 import treeview from '../../languages/treeview';
 import type { PluginProto } from '../../types';
 
-export default {
+const Self = {
 	id: 'treeview-icons',
 	require: treeview,
 } as PluginProto<'treeview-icons'>;
+
+export default Self;
+
+prism.components.add(Self);
