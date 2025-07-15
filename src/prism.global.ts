46cb9080effc1405f514ab68bdfe6cfb7033fc7d
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-15 16:50:34 +0200

    Address feedback: Make sure `currentScript` exists

--- src/prism.global.ts
+++ src/prism.global.ts
@@ -1,4 +1,7 @@
-// In browser and imported via non-ESM
-const url = new URL('./index.js', (globalThis.document.currentScript as HTMLScriptElement).src);
+const currentScript = globalThis.document?.currentScript as HTMLScriptElement;
+if (currentScript) {
+	// In browser and imported via non-ESM
+	const url = new URL('./index.js', currentScript.src);
 
-void import(url.toString()).then(({ default: prism }) => ((globalThis as any).Prism = prism));
+	void import(url.toString()).then(({ default: prism }) => ((globalThis as any).Prism = prism));
+}
