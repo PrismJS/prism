361c9f7b1fdf8d393e4411ffc9e5ebbc7c7f7fa9
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-21 20:26:04 +0200

    Transform flow

--- src/languages/flow.ts
+++ src/languages/flow.ts
@@ -5,10 +5,30 @@ import type { GrammarToken, LanguageProto } from '../types';
 
 export default {
 	id: 'flow',
-	require: javascript,
-	grammar ({ extend, getLanguage }) {
-		const javascript = getLanguage('javascript');
-		const flow = extend('javascript', {
+	base: javascript,
+	grammar ({ base }) {
+		insertBefore(base!, 'keyword', {
+			'type': {
+				pattern:
+					/\b(?:[Bb]oolean|Function|[Nn]umber|[Ss]tring|[Ss]ymbol|any|mixed|null|void)\b/,
+				alias: 'class-name',
+			},
+		});
+
+		insertBefore(base!, 'operator', {
+			'flow-punctuation': {
+				pattern: /\{\||\|\}/,
+				alias: 'punctuation',
+			},
+		});
+
+		const fnVariable = base!['function-variable'] as GrammarToken;
+		fnVariable.pattern =
+			/(?!\s)[_$a-z\xA0-\uFFFF](?:(?!\s)[$\w\xA0-\uFFFF])*(?=\s*=\s*(?:function\b|(?:\([^()]*\)(?:\s*:\s*\w+)?|(?!\s)[_$a-z\xA0-\uFFFF](?:(?!\s)[$\w\xA0-\uFFFF])*)\s*=>))/i;
+
+		delete base!['parameter'];
+
+		return {
 			'keyword': [
 				{
 					pattern: /(^|[^$]\b)(?:Class|declare|opaque|type)\b(?!\$)/,
@@ -19,31 +39,8 @@ export default {
 						/(^|[^$]\B)\$(?:Diff|Enum|Exact|Keys|ObjMap|PropertyType|Record|Shape|Subtype|Supertype|await)\b(?!\$)/,
 					lookbehind: true,
 				},
-				...toArray(javascript['keyword']),
+				...toArray(base!.keyword),
 			],
-		});
-
-		insertBefore(flow, 'keyword', {
-			'type': {
-				pattern:
-					/\b(?:[Bb]oolean|Function|[Nn]umber|[Ss]tring|[Ss]ymbol|any|mixed|null|void)\b/,
-				alias: 'class-name',
-			},
-		});
-
-		insertBefore(flow, 'operator', {
-			'flow-punctuation': {
-				pattern: /\{\||\|\}/,
-				alias: 'punctuation',
-			},
-		});
-
-		const fnVariable = flow['function-variable'] as GrammarToken;
-		fnVariable.pattern =
-			/(?!\s)[_$a-z\xA0-\uFFFF](?:(?!\s)[$\w\xA0-\uFFFF])*(?=\s*=\s*(?:function\b|(?:\([^()]*\)(?:\s*:\s*\w+)?|(?!\s)[_$a-z\xA0-\uFFFF](?:(?!\s)[$\w\xA0-\uFFFF])*)\s*=>))/i;
-
-		delete flow['parameter'];
-
-		return flow;
+		};
 	},
 } as LanguageProto<'flow'>;
