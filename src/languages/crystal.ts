e734b468cc3cac02f83347b9f8f327270ea139e3
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-21 20:11:50 +0200

    Transform crystal

--- src/languages/crystal.ts
+++ src/languages/crystal.ts
@@ -5,24 +5,9 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'crystal',
-	require: ruby,
-	grammar ({ extend, getLanguage }) {
-		const ruby = getLanguage('ruby');
-		const crystal = extend('ruby', {
-			'keyword': [
-				/\b(?:__DIR__|__END_LINE__|__FILE__|__LINE__|abstract|alias|annotation|as|asm|begin|break|case|class|def|do|else|elsif|end|ensure|enum|extend|for|fun|if|ifdef|include|instance_sizeof|lib|macro|module|next|of|out|pointerof|private|protected|ptr|require|rescue|return|select|self|sizeof|struct|super|then|type|typeof|undef|uninitialized|union|unless|until|when|while|with|yield)\b/,
-				{
-					pattern: /(\.\s*)(?:is_a|responds_to)\?/,
-					lookbehind: true,
-				},
-			],
-			'number':
-				/\b(?:0b[01_]*[01]|0o[0-7_]*[0-7]|0x[\da-fA-F_]*[\da-fA-F]|(?:\d(?:[\d_]*\d)?)(?:\.[\d_]*\d)?(?:[eE][+-]?[\d_]*\d)?)(?:_(?:[uif](?:8|16|32|64))?)?\b/,
-			'operator': [/->/, ...toArray(ruby.operator)],
-			'punctuation': /[(){}[\].,;\\]/,
-		});
-
-		insertBefore(crystal, 'string-literal', {
+	base: ruby,
+	grammar ({ base }) {
+		insertBefore(base!, 'string-literal', {
 			'attribute': {
 				pattern: /@\[.*?\]/,
 				inside: {
@@ -61,6 +46,18 @@ export default {
 			},
 		});
 
-		return crystal;
+		return {
+			'keyword': [
+				/\b(?:__DIR__|__END_LINE__|__FILE__|__LINE__|abstract|alias|annotation|as|asm|begin|break|case|class|def|do|else|elsif|end|ensure|enum|extend|for|fun|if|ifdef|include|instance_sizeof|lib|macro|module|next|of|out|pointerof|private|protected|ptr|require|rescue|return|select|self|sizeof|struct|super|then|type|typeof|undef|uninitialized|union|unless|until|when|while|with|yield)\b/,
+				{
+					pattern: /(\.\s*)(?:is_a|responds_to)\?/,
+					lookbehind: true,
+				},
+			],
+			'number':
+				/\b(?:0b[01_]*[01]|0o[0-7_]*[0-7]|0x[\da-fA-F_]*[\da-fA-F]|(?:\d(?:[\d_]*\d)?)(?:\.[\d_]*\d)?(?:[eE][+-]?[\d_]*\d)?)(?:_(?:[uif](?:8|16|32|64))?)?\b/,
+			'operator': [/->/, ...toArray(base!.operator)],
+			'punctuation': /[(){}[\].,;\\]/,
+		};
 	},
 } as LanguageProto<'crystal'>;
