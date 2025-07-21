80b08f4d9c0c17ab4cb15515786e9b1b81feb84d
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-21 23:23:55 +0200

    Transform idris

--- src/languages/idris.ts
+++ src/languages/idris.ts
@@ -4,19 +4,10 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'idris',
-	require: haskell,
+	base: haskell,
 	alias: 'idr',
-	grammar ({ extend }) {
-		const idris = extend('haskell', {
-			'comment': {
-				pattern: /(?:(?:--|\|\|\|).*$|\{-[\s\S]*?-\})/m,
-			},
-			'keyword':
-				/\b(?:Type|case|class|codata|constructor|corecord|data|do|dsl|else|export|if|implementation|implicit|import|impossible|in|infix|infixl|infixr|instance|interface|let|module|mutual|namespace|of|parameters|partial|postulate|private|proof|public|quoteGoal|record|rewrite|syntax|then|total|using|where|with)\b/,
-			'builtin': undefined,
-		});
-
-		insertBefore(idris, 'keyword', {
+	grammar ({ base }) {
+		insertBefore(base!, 'keyword', {
 			'import-statement': {
 				pattern: /(^\s*import\s+)(?:[A-Z][\w']*)(?:\.[A-Z][\w']*)*/m,
 				lookbehind: true,
@@ -26,6 +17,13 @@ export default {
 			},
 		});
 
-		return idris;
+		return {
+			'comment': {
+				pattern: /(?:(?:--|\|\|\|).*$|\{-[\s\S]*?-\})/m,
+			},
+			'keyword':
+				/\b(?:Type|case|class|codata|constructor|corecord|data|do|dsl|else|export|if|implementation|implicit|import|impossible|in|infix|infixl|infixr|instance|interface|let|module|mutual|namespace|of|parameters|partial|postulate|private|proof|public|quoteGoal|record|rewrite|syntax|then|total|using|where|with)\b/,
+			'builtin': undefined,
+		};
 	},
 } as LanguageProto<'idris'>;
