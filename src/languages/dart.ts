05650aedbbb5ca9bd7cbf92b89593deb10581a33
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-21 20:20:50 +0200

    Transform dart

--- src/languages/dart.ts
+++ src/languages/dart.ts
@@ -4,8 +4,8 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'dart',
-	require: clike,
-	grammar ({ extend }) {
+	base: clike,
+	grammar ({ base }) {
 		const keywords = [
 			/\b(?:async|sync|yield)\*/,
 			/\b(?:abstract|assert|async|await|break|case|catch|class|const|continue|covariant|default|deferred|do|dynamic|else|enum|export|extends|extension|external|factory|final|finally|for|get|hide|if|implements|import|in|interface|library|mixin|new|null|on|operator|part|rethrow|return|set|show|static|super|switch|sync|this|throw|try|typedef|var|void|while|with|yield)\b/,
@@ -28,23 +28,7 @@ export default {
 			},
 		};
 
-		const dart = extend('clike', {
-			'class-name': [
-				className,
-				{
-					// variables and parameters
-					// this to support class names (or generic parameters) which do not contain a lower case letter (also works for methods)
-					pattern: RegExp(packagePrefix + /[A-Z]\w*(?=\s+\w+\s*[;,=()])/.source),
-					lookbehind: true,
-					inside: className.inside,
-				},
-			],
-			'keyword': keywords,
-			'operator':
-				/\bis!|\b(?:as|is)\b|\+\+|--|&&|\|\||<<=?|>>=?|~(?:\/=?)?|[+\-*\/%&^|=!<>]=?|\?/,
-		});
-
-		insertBefore(dart, 'string', {
+		insertBefore(base!, 'string', {
 			'string-literal': {
 				pattern: /r?(?:("""|''')[\s\S]*?\1|(["'])(?:\\.|(?!\2)[^\\\r\n])*\2(?!\2))/,
 				greedy: true,
@@ -66,14 +50,14 @@ export default {
 			'string': undefined,
 		});
 
-		insertBefore(dart, 'class-name', {
+		insertBefore(base!, 'class-name', {
 			'metadata': {
 				pattern: /@\w+/,
 				alias: 'function',
 			},
 		});
 
-		insertBefore(dart, 'class-name', {
+		insertBefore(base!, 'class-name', {
 			'generics': {
 				pattern: /<(?:[\w\s,.&?]|<(?:[\w\s,.&?]|<(?:[\w\s,.&?]|<[\w\s,.&?]*>)*>)*>)*>/,
 				inside: {
@@ -85,6 +69,20 @@ export default {
 			},
 		});
 
-		return dart;
+		return {
+			'class-name': [
+				className,
+				{
+					// variables and parameters
+					// this to support class names (or generic parameters) which do not contain a lower case letter (also works for methods)
+					pattern: RegExp(packagePrefix + /[A-Z]\w*(?=\s+\w+\s*[;,=()])/.source),
+					lookbehind: true,
+					inside: className.inside,
+				},
+			],
+			'keyword': keywords,
+			'operator':
+				/\bis!|\b(?:as|is)\b|\+\+|--|&&|\|\||<<=?|>>=?|~(?:\/=?)?|[+\-*\/%&^|=!<>]=?|\?/,
+		};
 	},
 } as LanguageProto<'dart'>;
