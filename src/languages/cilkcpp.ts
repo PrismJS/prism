5aee9f222a9ae66547b55dfbdabd147db049414d
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-21 16:28:00 +0200

    Transform cilkcpp

--- src/languages/cilkcpp.ts
+++ src/languages/cilkcpp.ts
@@ -4,16 +4,15 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'cilkcpp',
-	require: cpp,
+	base: cpp,
 	alias: ['cilk-cpp', 'cilk'],
-	grammar ({ extend }) {
-		const cilkcpp = extend('cpp', {});
-		insertBefore(cilkcpp, 'function', {
+	grammar ({ base }) {
+		insertBefore(base!, 'function', {
 			'parallel-keyword': {
 				pattern: /\bcilk_(?:for|reducer|s(?:cope|pawn|ync))\b/,
 				alias: 'keyword',
 			},
 		});
-		return cilkcpp;
+		return {};
 	},
 } as LanguageProto<'cilkcpp'>;
