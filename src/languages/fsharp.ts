0e356904561508f1a30f9883c61a978857eafaf1
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-21 20:55:34 +0200

    Transform fsharp

--- src/languages/fsharp.ts
+++ src/languages/fsharp.ts
@@ -4,10 +4,57 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'fsharp',
-	require: clike,
+	base: clike,
 	optional: 'xml-doc',
-	grammar ({ extend, getOptionalLanguage }) {
-		const fsharp = extend('clike', {
+	grammar ({ base, getOptionalLanguage }) {
+		insertBefore(base!, 'keyword', {
+			'preprocessor': {
+				pattern: /(^[\t ]*)#.*/m,
+				lookbehind: true,
+				alias: 'property',
+				inside: {
+					'directive': {
+						pattern: /(^#)\b(?:else|endif|if|light|line|nowarn)\b/,
+						lookbehind: true,
+						alias: 'keyword',
+					},
+				},
+			},
+		});
+		insertBefore(base!, 'punctuation', {
+			'computation-expression': {
+				pattern: /\b[_a-z]\w*(?=\s*\{)/i,
+				alias: 'keyword',
+			},
+		});
+		insertBefore(base!, 'string', {
+			'annotation': {
+				pattern: /\[<.+?>\]/,
+				greedy: true,
+				inside: {
+					'punctuation': /^\[<|>\]$/,
+					'class-name': {
+						pattern: /^\w+$|(^|;\s*)[A-Z]\w*(?=\()/,
+						lookbehind: true,
+					},
+					'annotation-content': {
+						pattern: /[\s\S]+/,
+						inside: 'fsharp',
+					},
+				},
+			},
+			'char': {
+				pattern:
+					/'(?:[^\\']|\\(?:.|\d{3}|x[a-fA-F\d]{2}|u[a-fA-F\d]{4}|U[a-fA-F\d]{8}))'B?/,
+				greedy: true,
+			},
+		});
+
+		insertBefore(base!, 'comment', {
+			'doc-comment': getOptionalLanguage('xml-doc')?.slash,
+		});
+
+		return {
 			'comment': [
 				{
 					pattern: /(^|[^\\])\(\*(?!\))[\s\S]*?\*\)/,
@@ -43,54 +90,6 @@ export default {
 			],
 			'operator':
 				/([<>~&^])\1\1|([*.:<>&])\2|<-|->|[!=:]=|<?\|{1,3}>?|\??(?:<=|>=|<>|[-+*/%=<>])\??|[!?^&]|~[+~-]|:>|:\?>?/,
-		});
-		insertBefore(fsharp, 'keyword', {
-			'preprocessor': {
-				pattern: /(^[\t ]*)#.*/m,
-				lookbehind: true,
-				alias: 'property',
-				inside: {
-					'directive': {
-						pattern: /(^#)\b(?:else|endif|if|light|line|nowarn)\b/,
-						lookbehind: true,
-						alias: 'keyword',
-					},
-				},
-			},
-		});
-		insertBefore(fsharp, 'punctuation', {
-			'computation-expression': {
-				pattern: /\b[_a-z]\w*(?=\s*\{)/i,
-				alias: 'keyword',
-			},
-		});
-		insertBefore(fsharp, 'string', {
-			'annotation': {
-				pattern: /\[<.+?>\]/,
-				greedy: true,
-				inside: {
-					'punctuation': /^\[<|>\]$/,
-					'class-name': {
-						pattern: /^\w+$|(^|;\s*)[A-Z]\w*(?=\()/,
-						lookbehind: true,
-					},
-					'annotation-content': {
-						pattern: /[\s\S]+/,
-						inside: 'fsharp',
-					},
-				},
-			},
-			'char': {
-				pattern:
-					/'(?:[^\\']|\\(?:.|\d{3}|x[a-fA-F\d]{2}|u[a-fA-F\d]{4}|U[a-fA-F\d]{8}))'B?/,
-				greedy: true,
-			},
-		});
-
-		insertBefore(fsharp, 'comment', {
-			'doc-comment': getOptionalLanguage('xml-doc')?.slash,
-		});
-
-		return fsharp;
+		};
 	},
 } as LanguageProto<'fsharp'>;
