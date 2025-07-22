be7b04f8281349954c58ed3bbefec501e237cbc8
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-22 11:26:50 +0200

    Transform phpdoc

--- src/languages/phpdoc.ts
+++ src/languages/phpdoc.ts
@@ -4,22 +4,11 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'phpdoc',
-	require: javadoclike,
-	grammar ({ extend }) {
+	base: javadoclike,
+	grammar ({ base }) {
 		const typeExpression = /(?:\b[a-zA-Z]\w*|[|\\[\]])+/.source;
 
-		const phpdoc = extend('javadoclike', {
-			'parameter': {
-				pattern: RegExp(
-					'(@(?:global|param|property(?:-read|-write)?|var)\\s+(?:' +
-						typeExpression +
-						'\\s+)?)\\$\\w+'
-				),
-				lookbehind: true,
-			},
-		});
-
-		insertBefore(phpdoc, 'keyword', {
+		insertBefore(base!, 'keyword', {
 			'class-name': [
 				{
 					pattern: RegExp(
@@ -36,6 +25,15 @@ export default {
 			],
 		});
 
-		return phpdoc;
+		return {
+			'parameter': {
+				pattern: RegExp(
+					'(@(?:global|param|property(?:-read|-write)?|var)\\s+(?:' +
+						typeExpression +
+						'\\s+)?)\\$\\w+'
+				),
+				lookbehind: true,
+			},
+		};
 	},
 } as LanguageProto<'phpdoc'>;
