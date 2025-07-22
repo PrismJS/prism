c3efedd1c71eda16ac1b20439a7e0c10ee137279
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-22 11:51:26 +0200

    Transform sparql

--- src/languages/sparql.ts
+++ src/languages/sparql.ts
@@ -4,18 +4,10 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'sparql',
-	require: turtle,
+	base: turtle,
 	alias: 'rq',
-	grammar ({ extend }) {
-		const sparql = extend('turtle', {
-			'boolean': /\b(?:false|true)\b/i,
-			'variable': {
-				pattern: /[?$]\w+/,
-				greedy: true,
-			},
-		});
-
-		insertBefore(sparql, 'punctuation', {
+	grammar ({ base }) {
+		insertBefore(base!, 'punctuation', {
 			'keyword': [
 				/\b(?:A|ADD|ALL|AS|ASC|ASK|BNODE|BY|CLEAR|CONSTRUCT|COPY|CREATE|DATA|DEFAULT|DELETE|DESC|DESCRIBE|DISTINCT|DROP|EXISTS|FILTER|FROM|GROUP|HAVING|INSERT|INTO|LIMIT|LOAD|MINUS|MOVE|NAMED|NOT|NOW|OFFSET|OPTIONAL|ORDER|RAND|REDUCED|SELECT|SEPARATOR|SERVICE|SILENT|STRUUID|UNION|USING|UUID|VALUES|WHERE)\b/i,
 				/\b(?:ABS|AVG|BIND|BOUND|CEIL|COALESCE|CONCAT|CONTAINS|COUNT|DATATYPE|DAY|ENCODE_FOR_URI|FLOOR|GROUP_CONCAT|HOURS|IF|IRI|isBLANK|isIRI|isLITERAL|isNUMERIC|isURI|LANG|LANGMATCHES|LCASE|MAX|MD5|MIN|MINUTES|MONTH|REGEX|REPLACE|ROUND|sameTerm|SAMPLE|SECONDS|SHA1|SHA256|SHA384|SHA512|STR|STRAFTER|STRBEFORE|STRDT|STRENDS|STRLANG|STRLEN|STRSTARTS|SUBSTR|SUM|TIMEZONE|TZ|UCASE|URI|YEAR)\b(?=\s*\()/i,
@@ -23,6 +15,12 @@ export default {
 			],
 		});
 
-		return sparql;
+		return {
+			'boolean': /\b(?:false|true)\b/i,
+			'variable': {
+				pattern: /[?$]\w+/,
+				greedy: true,
+			},
+		};
 	},
 } as LanguageProto<'sparql'>;
