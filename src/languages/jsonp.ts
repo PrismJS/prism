5c987d458e4b4ce5259cddf21175cb605b9d8044
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-22 11:00:29 +0200

    Transform jsonp

--- src/languages/jsonp.ts
+++ src/languages/jsonp.ts
@@ -4,16 +4,14 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'jsonp',
-	require: json,
-	grammar ({ extend }) {
-		const jsonp = extend('json', {
-			'punctuation': /[{}[\]();,.]/,
-		});
-
-		insertBefore(jsonp, 'punctuation', {
+	base: json,
+	grammar ({ base }) {
+		insertBefore(base!, 'punctuation', {
 			'function': /(?!\s)[_$a-zA-Z\xA0-\uFFFF](?:(?!\s)[$\w\xA0-\uFFFF])*(?=\s*\()/,
 		});
 
-		return jsonp;
+		return {
+			'punctuation': /[{}[\]();,.]/,
+		};
 	},
 } as LanguageProto<'jsonp'>;
