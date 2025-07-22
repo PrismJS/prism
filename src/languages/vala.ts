4810c14ea149272d8e72c20bb5d410652f60db67
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-22 12:06:55 +0200

    Transform vala

--- src/languages/vala.ts
+++ src/languages/vala.ts
@@ -4,9 +4,52 @@ import type { Grammar, LanguageProto } from '../types';
 
 export default {
 	id: 'vala',
-	require: clike,
-	grammar ({ extend }) {
-		const vala = extend('clike', {
+	base: clike,
+	grammar ({ base }) {
+		insertBefore(base!, 'string', {
+			'raw-string': {
+				pattern: /"""[\s\S]*?"""/,
+				greedy: true,
+				alias: 'string',
+			},
+			'template-string': {
+				pattern: /@"[\s\S]*?"/,
+				greedy: true,
+				inside: {
+					'interpolation': {
+						pattern: /\$(?:\([^)]*\)|[a-zA-Z]\w*)/,
+						inside: {
+							'delimiter': {
+								pattern: /^\$\(?|\)$/,
+								alias: 'punctuation',
+							},
+							$rest: 'vala',
+						} as unknown as Grammar,
+					},
+					'string': /[\s\S]+/,
+				},
+			},
+		});
+
+		insertBefore(base!, 'keyword', {
+			'regex': {
+				pattern:
+					/\/(?:\[(?:[^\]\\\r\n]|\\.)*\]|\\.|[^/\\\[\r\n])+\/[imsx]{0,4}(?=\s*(?:$|[\r\n,.;})\]]))/,
+				greedy: true,
+				inside: {
+					'regex-source': {
+						pattern: /^(\/)[\s\S]+(?=\/[a-z]*$)/,
+						lookbehind: true,
+						alias: 'language-regex',
+						inside: 'regex',
+					},
+					'regex-delimiter': /^\//,
+					'regex-flags': /^[a-z]+$/,
+				},
+			},
+		});
+
+		return {
 			// Classes copied from csharp
 			'class-name': [
 				{
@@ -50,51 +93,6 @@ export default {
 			'operator': /\+\+|--|&&|\|\||<<=?|>>=?|=>|->|~|[+\-*\/%&^|=!<>]=?|\?\??|\.\.\./,
 			'punctuation': /[{}[\];(),.:]/,
 			'constant': /\b[A-Z0-9_]+\b/,
-		});
-
-		insertBefore(vala, 'string', {
-			'raw-string': {
-				pattern: /"""[\s\S]*?"""/,
-				greedy: true,
-				alias: 'string',
-			},
-			'template-string': {
-				pattern: /@"[\s\S]*?"/,
-				greedy: true,
-				inside: {
-					'interpolation': {
-						pattern: /\$(?:\([^)]*\)|[a-zA-Z]\w*)/,
-						inside: {
-							'delimiter': {
-								pattern: /^\$\(?|\)$/,
-								alias: 'punctuation',
-							},
-							$rest: vala,
-						} as unknown as Grammar,
-					},
-					'string': /[\s\S]+/,
-				},
-			},
-		});
-
-		insertBefore(vala, 'keyword', {
-			'regex': {
-				pattern:
-					/\/(?:\[(?:[^\]\\\r\n]|\\.)*\]|\\.|[^/\\\[\r\n])+\/[imsx]{0,4}(?=\s*(?:$|[\r\n,.;})\]]))/,
-				greedy: true,
-				inside: {
-					'regex-source': {
-						pattern: /^(\/)[\s\S]+(?=\/[a-z]*$)/,
-						lookbehind: true,
-						alias: 'language-regex',
-						inside: 'regex',
-					},
-					'regex-delimiter': /^\//,
-					'regex-flags': /^[a-z]+$/,
-				},
-			},
-		});
-
-		return vala;
+		};
 	},
 } as LanguageProto<'vala'>;
