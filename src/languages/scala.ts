9c01c47f0e9d801c9d4e5876ef6b1e6208b1f1be
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-22 11:46:32 +0200

    Transform scala

--- src/languages/scala.ts
+++ src/languages/scala.ts
@@ -4,27 +4,9 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'scala',
-	require: java,
-	grammar ({ extend }) {
-		const scala = extend('java', {
-			'triple-quoted-string': {
-				pattern: /"""[\s\S]*?"""/,
-				greedy: true,
-				alias: 'string',
-			},
-			'string': {
-				pattern: /("|')(?:\\.|(?!\1)[^\\\r\n])*\1/,
-				greedy: true,
-			},
-			'keyword':
-				/<-|=>|\b(?:abstract|case|catch|class|def|derives|do|else|enum|extends|extension|final|finally|for|forSome|given|if|implicit|import|infix|inline|lazy|match|new|null|object|opaque|open|override|package|private|protected|return|sealed|self|super|this|throw|trait|transparent|try|type|using|val|var|while|with|yield)\b/,
-			'number': /\b0x(?:[\da-f]*\.)?[\da-f]+|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e\d+)?[dfl]?/i,
-			'builtin':
-				/\b(?:Any|AnyRef|AnyVal|Boolean|Byte|Char|Double|Float|Int|Long|Nothing|Short|String|Unit)\b/,
-			'symbol': /'[^\d\s\\]\w*/,
-		});
-
-		insertBefore(scala, 'triple-quoted-string', {
+	base: java,
+	grammar ({ base }) {
+		insertBefore(base!, 'triple-quoted-string', {
 			'string-interpolation': {
 				pattern:
 					/\b[a-z]\w*(?:"""(?:[^$]|\$(?:[^{]|\{(?:[^{}]|\{[^{}]*\})*\}))*?"""|"(?:[^$"\r\n]|\$(?:[^{]|\{(?:[^{}]|\{[^{}]*\})*\}))*")/i,
@@ -56,11 +38,27 @@ export default {
 			},
 		});
 
-		delete scala['doc-comment'];
-		delete scala['class-name'];
-		delete scala['function'];
-		delete scala['constant'];
+		delete base!['doc-comment'];
+		delete base!['class-name'];
+		delete base!['function'];
+		delete base!['constant'];
 
-		return scala;
+		return {
+			'triple-quoted-string': {
+				pattern: /"""[\s\S]*?"""/,
+				greedy: true,
+				alias: 'string',
+			},
+			'string': {
+				pattern: /("|')(?:\\.|(?!\1)[^\\\r\n])*\1/,
+				greedy: true,
+			},
+			'keyword':
+				/<-|=>|\b(?:abstract|case|catch|class|def|derives|do|else|enum|extends|extension|final|finally|for|forSome|given|if|implicit|import|infix|inline|lazy|match|new|null|object|opaque|open|override|package|private|protected|return|sealed|self|super|this|throw|trait|transparent|try|type|using|val|var|while|with|yield)\b/,
+			'number': /\b0x(?:[\da-f]*\.)?[\da-f]+|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e\d+)?[dfl]?/i,
+			'builtin':
+				/\b(?:Any|AnyRef|AnyVal|Boolean|Byte|Char|Double|Float|Int|Long|Nothing|Short|String|Unit)\b/,
+			'symbol': /'[^\d\s\\]\w*/,
+		};
 	},
 } as LanguageProto<'scala'>;
