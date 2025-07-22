ddc5e3f60a3df60e5fbfe3a9e86e440ca6d9a124
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-22 11:18:00 +0200

    Transform objectivec

--- src/languages/objectivec.ts
+++ src/languages/objectivec.ts
@@ -3,10 +3,12 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'objectivec',
-	require: c,
+	base: c,
 	alias: 'objc',
-	grammar ({ extend }) {
-		const objectivec = extend('c', {
+	grammar ({ base }) {
+		delete base!['class-name'];
+
+		return {
 			'string': {
 				pattern: /@?"(?:\\(?:\r\n|[\s\S])|[^"\\\r\n])*"/,
 				greedy: true,
@@ -14,10 +16,6 @@ export default {
 			'keyword':
 				/\b(?:asm|auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|in|inline|int|long|register|return|self|short|signed|sizeof|static|struct|super|switch|typedef|typeof|union|unsigned|void|volatile|while)\b|(?:@interface|@end|@implementation|@protocol|@class|@public|@protected|@private|@property|@try|@catch|@finally|@throw|@synthesize|@dynamic|@selector)\b/,
 			'operator': /-[->]?|\+\+?|!=?|<<?=?|>>?=?|==?|&&?|\|\|?|[~^%?*\/@]/,
-		});
-
-		delete objectivec['class-name'];
-
-		return objectivec;
+		};
 	},
 } as LanguageProto<'objectivec'>;
