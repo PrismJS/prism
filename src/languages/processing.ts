b43b43765af1147d7760f8145a0de1e405f60e3d
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-22 11:30:04 +0200

    Transform processing

--- src/languages/processing.ts
+++ src/languages/processing.ts
@@ -4,17 +4,9 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'processing',
-	require: clike,
-	grammar ({ extend }) {
-		const processing = extend('clike', {
-			'keyword':
-				/\b(?:break|case|catch|class|continue|default|else|extends|final|for|if|implements|import|new|null|private|public|return|static|super|switch|this|try|void|while)\b/,
-			// Spaces are allowed between function name and parenthesis
-			'function': /\b\w+(?=\s*\()/,
-			'operator': /<[<=]?|>[>=]?|&&?|\|\|?|[%?]|[!=+\-*\/]=?/,
-		});
-
-		insertBefore(processing, 'number', {
+	base: clike,
+	grammar ({ base }) {
+		insertBefore(base!, 'number', {
 			// Special case: XML is a type
 			'constant': /\b(?!XML\b)[A-Z][A-Z\d_]+\b/,
 			'type': {
@@ -23,6 +15,12 @@ export default {
 			},
 		});
 
-		return processing;
+		return {
+			'keyword':
+				/\b(?:break|case|catch|class|continue|default|else|extends|final|for|if|implements|import|new|null|private|public|return|static|super|switch|this|try|void|while)\b/,
+			// Spaces are allowed between function name and parenthesis
+			'function': /\b\w+(?=\s*\()/,
+			'operator': /<[<=]?|>[>=]?|&&?|\|\|?|[%?]|[!=+\-*\/]=?/,
+		};
 	},
 } as LanguageProto<'processing'>;
