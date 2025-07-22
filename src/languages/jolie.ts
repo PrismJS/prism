f4f92818b7515b89e33274b79c84e52cf7ec6c70
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-22 10:54:44 +0200

    Transform jolie

--- src/languages/jolie.ts
+++ src/languages/jolie.ts
@@ -4,30 +4,9 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'jolie',
-	require: clike,
-	grammar ({ extend }) {
-		const jolie = extend('clike', {
-			'string': {
-				pattern: /(^|[^\\])"(?:\\[\s\S]|[^"\\])*"/,
-				lookbehind: true,
-				greedy: true,
-			},
-			'class-name': {
-				pattern:
-					/((?:\b(?:as|courier|embed|in|inputPort|outputPort|service)\b|@)[ \t]*)\w+/,
-				lookbehind: true,
-			},
-			'keyword':
-				/\b(?:as|cH|comp|concurrent|constants|courier|cset|csets|default|define|else|embed|embedded|execution|exit|extender|for|foreach|forward|from|global|if|import|in|include|init|inputPort|install|instanceof|interface|is_defined|linkIn|linkOut|main|new|nullProcess|outputPort|over|private|provide|public|scope|sequential|service|single|spawn|synchronized|this|throw|throws|type|undef|until|while|with)\b/,
-			'function': /\b[a-z_]\w*(?=[ \t]*[@(])/i,
-			'number': /(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?l?/i,
-			'operator': /-[-=>]?|\+[+=]?|<[<=]?|[>=*!]=?|&&|\|\||[?\/%^@|]/,
-			'punctuation': /[()[\]{},;.:]/,
-			'builtin':
-				/\b(?:Byte|any|bool|char|double|enum|float|int|length|long|ranges|regex|string|undefined|void)\b/,
-		});
-
-		insertBefore(jolie, 'keyword', {
+	base: clike,
+	grammar ({ base }) {
+		insertBefore(base!, 'keyword', {
 			'aggregates': {
 				pattern:
 					/(\bAggregates\s*:\s*)(?:\w+(?:\s+with\s+\w+)?\s*,\s*)*\w+(?:\s+with\s+\w+)?/,
@@ -53,6 +32,25 @@ export default {
 			},
 		});
 
-		return jolie;
+		return {
+			'string': {
+				pattern: /(^|[^\\])"(?:\\[\s\S]|[^"\\])*"/,
+				lookbehind: true,
+				greedy: true,
+			},
+			'class-name': {
+				pattern:
+					/((?:\b(?:as|courier|embed|in|inputPort|outputPort|service)\b|@)[ \t]*)\w+/,
+				lookbehind: true,
+			},
+			'keyword':
+				/\b(?:as|cH|comp|concurrent|constants|courier|cset|csets|default|define|else|embed|embedded|execution|exit|extender|for|foreach|forward|from|global|if|import|in|include|init|inputPort|install|instanceof|interface|is_defined|linkIn|linkOut|main|new|nullProcess|outputPort|over|private|provide|public|scope|sequential|service|single|spawn|synchronized|this|throw|throws|type|undef|until|while|with)\b/,
+			'function': /\b[a-z_]\w*(?=[ \t]*[@(])/i,
+			'number': /(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?l?/i,
+			'operator': /-[-=>]?|\+[+=]?|<[<=]?|[>=*!]=?|&&|\|\||[?\/%^@|]/,
+			'punctuation': /[()[\]{},;.:]/,
+			'builtin':
+				/\b(?:Byte|any|bool|char|double|enum|float|int|length|long|ranges|regex|string|undefined|void)\b/,
+		};
 	},
 } as LanguageProto<'jolie'>;
