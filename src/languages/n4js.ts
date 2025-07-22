c07c022383070fe05d772f4c1c62c853b1218c05
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-22 11:17:47 +0200

    Transform n4js

--- src/languages/n4js.ts
+++ src/languages/n4js.ts
@@ -4,16 +4,10 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'n4js',
-	require: javascript,
+	base: javascript,
 	alias: 'n4jsd',
-	grammar ({ extend }) {
-		const n4js = extend('javascript', {
-			// Keywords from N4JS language spec: https://numberfour.github.io/n4js/spec/N4JSSpec.html
-			'keyword':
-				/\b(?:Array|any|boolean|break|case|catch|class|const|constructor|continue|debugger|declare|default|delete|do|else|enum|export|extends|false|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|module|new|null|number|package|private|protected|public|return|set|static|string|super|switch|this|throw|true|try|typeof|var|void|while|with|yield)\b/,
-		});
-
-		insertBefore(n4js, 'constant', {
+	grammar ({ base }) {
+		insertBefore(base!, 'constant', {
 			// Annotations in N4JS spec: https://numberfour.github.io/n4js/spec/N4JSSpec.html#_annotations
 			'annotation': {
 				pattern: /@+\w+/,
@@ -21,6 +15,10 @@ export default {
 			},
 		});
 
-		return n4js;
+		return {
+			// Keywords from N4JS language spec: https://numberfour.github.io/n4js/spec/N4JSSpec.html
+			'keyword':
+				/\b(?:Array|any|boolean|break|case|catch|class|const|constructor|continue|debugger|declare|default|delete|do|else|enum|export|extends|false|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|module|new|null|number|package|private|protected|public|return|set|static|string|super|switch|this|throw|true|try|typeof|var|void|while|with|yield)\b/,
+		};
 	},
 } as LanguageProto<'n4js'>;
