f6310188f1eab64aef6e7c15f7e6e594c9385630
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-07-22 11:39:19 +0200

    Transform racket

--- src/languages/racket.ts
+++ src/languages/racket.ts
@@ -4,19 +4,10 @@ import type { LanguageProto } from '../types';
 
 export default {
 	id: 'racket',
-	require: scheme,
+	base: scheme,
 	alias: 'rkt',
-	grammar ({ extend }) {
-		const racket = extend('scheme', {
-			'lambda-parameter': {
-				// the racket lambda syntax is a lot more complex, so we won't even attempt to capture it.
-				// this will just prevent false positives of the `function` pattern
-				pattern: /([(\[]lambda\s+[(\[])[^()\[\]'\s]+/,
-				lookbehind: true,
-			},
-		});
-
-		insertBefore(racket, 'string', {
+	grammar ({ base }) {
+		insertBefore(base!, 'string', {
 			'lang': {
 				pattern: /^#lang.+/m,
 				greedy: true,
@@ -24,6 +15,13 @@ export default {
 			},
 		});
 
-		return racket;
+		return {
+			'lambda-parameter': {
+				// the racket lambda syntax is a lot more complex, so we won't even attempt to capture it.
+				// this will just prevent false positives of the `function` pattern
+				pattern: /([(\[]lambda\s+[(\[])[^()\[\]'\s]+/,
+				lookbehind: true,
+			},
+		};
 	},
 } as LanguageProto<'racket'>;
