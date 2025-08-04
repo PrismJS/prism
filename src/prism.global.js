ab75e4ada8867c19a68b91ccbc188abc2786cf6e
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-08-04 19:28:21 +0200

    Transform `src/prism.global.ts`

---
+++ src/prism.global.js
@@ -0,0 +1,8 @@
+/** @type {HTMLScriptElement | undefined} */
+const currentScript = globalThis.document?.currentScript;
+if (currentScript) {
+	// In browser and imported via non-ESM
+	const url = new URL('./index.js', currentScript.src);
+
+	import(url.toString()).then(({ default: prism }) => (globalThis.Prism = prism));
+}
