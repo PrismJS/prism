716277756c058c1025ce352d06733fd0578b5bc5
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-08-04 17:40:03 +0200

    Transform `src/core/tokenize/tokenize.ts`

---
+++ src/core/tokenize/tokenize.js
@@ -0,0 +1,65 @@
+import { LinkedList } from '../linked-list.js';
+import singleton from '../prism.js';
+import { _matchGrammar } from './match.js';
+import { resolve } from './util.js';
+
+/**
+ * This is the heart of Prism, and the most low-level function you can use. It accepts a string of text as input
+ * and the language definitions to use, and returns an array with the tokenized code.
+ *
+ * When the language definition includes nested tokens, the function is called recursively on each of these tokens.
+ *
+ * This method could be useful in other contexts as well, as a very crude parser.
+ *
+ * @this {Prism}
+ * @param {string} text A string with the code to be highlighted.
+ * @param {Grammar} grammar An object containing the tokens to use.
+ *
+ * Usually a language definition like `Prism.languages.markup`.
+ * @returns {TokenStream} An array of strings and tokens, a token stream.
+ * @example
+ * let code = `var foo = 0;`;
+ * let tokens = Prism.tokenize(code, Prism.getLanguage('javascript'));
+ * tokens.forEach(token => {
+ *     if (token instanceof Token && token.type === 'number') {
+ *         console.log(`Found numeric literal: ${token.content}`);
+ *     }
+ * });
+ */
+export function tokenize (text, grammar) {
+	const prism = this ?? singleton;
+	const customTokenize = grammar.$tokenize;
+	if (customTokenize) {
+		return customTokenize(text, grammar, prism);
+	}
+
+	let restGrammar = resolve(prism.components, grammar.$rest);
+	while (restGrammar) {
+		grammar = { ...grammar, ...restGrammar };
+		restGrammar = resolve(prism.components, restGrammar.$rest);
+	}
+
+	/** @type {LinkedList<string | Token>} */
+	const tokenList = new LinkedList();
+	tokenList.addAfter(tokenList.head, text);
+
+	_matchGrammar.call(prism, text, tokenList, grammar, tokenList.head, 0);
+
+	return tokenList.toArray();
+}
+
+/**
+ * @typedef {import('../../types.d.ts').Grammar} Grammar
+ */
+
+/**
+ * @typedef {import('../classes/token.js').Token} Token
+ */
+
+/**
+ * @typedef {import('../classes/token.js').TokenStream} TokenStream
+ */
+
+/**
+ * @typedef {import('../prism.js').Prism} Prism
+ */
