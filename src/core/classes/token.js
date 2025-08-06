9e8d5476cf11e2bb6010839fcd186a9047c71e22
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-08-06 18:40:23 +0200

    Transform `src/core/classes/token.ts`

---
+++ src/core/classes/token.js
@@ -0,0 +1,113 @@
+export class Token {
+	/**
+	 * The type of the token.
+	 *
+	 * This is usually the key of a pattern in a {@link Grammar}.
+	 *
+	 * @see {@link GrammarToken}
+	 * @type {TokenName}
+	 */
+	type;
+
+	/**
+	 * The strings or tokens contained by this token.
+	 *
+	 * This will be a token stream if the pattern matched also defined an `inside` grammar.
+	 *
+	 * @type {string | TokenStream}
+	 */
+	content;
+
+	/**
+	 * The alias(es) of the token.
+	 *
+	 * @see {@link GrammarToken#alias}
+	 * @type {TokenName | TokenName[] | undefined}
+	 */
+	alias;
+
+	/**
+	 * Length of the full string this token was created from.
+	 *
+	 * Only used internally. The API does not guarantee that this field has any particular value or meaning.
+	 *
+	 * @internal
+	 * @type {number}
+	 */
+	length;
+
+	/**
+	 * Creates a new token.
+	 *
+	 * @param {TokenName} type See {@link Token#type}
+	 * @param {string | TokenStream} content See {@link Token#content}
+	 * @param {TokenName | TokenName[]} [alias] The alias(es) of the token.
+	 * @param {string} [matchedStr=''] A copy of the full string this token was created from.
+	 * @public
+	 */
+	constructor (type, content, alias, matchedStr = '') {
+		this.type = type;
+		this.content = content;
+		this.alias = alias;
+		this.length = matchedStr.length;
+	}
+
+	/**
+	 * Adds the given alias to the list of aliases of this token.
+	 *
+	 * @param {TokenName} alias
+	 * @returns {void}
+	 */
+	addAlias (alias) {
+		let aliases = this.alias;
+		if (!aliases) {
+			this.alias = aliases = [];
+		}
+		else if (!Array.isArray(aliases)) {
+			this.alias = aliases = [aliases];
+		}
+		aliases.push(alias);
+	}
+}
+
+export default Token;
+
+/**
+ * Returns the text content of the given token or token stream.
+ *
+ * @param {string | Token | TokenStream} token
+ * @returns {string}
+ */
+export function getTextContent (token) {
+	if (typeof token === 'string') {
+		return token;
+	}
+	else if (Array.isArray(token)) {
+		return token.map(getTextContent).join('');
+	}
+	else {
+		return getTextContent(token.content);
+	}
+}
+
+/**
+ * @typedef {'atrule' | 'attr-name' | 'attr-value' | 'bold' | 'boolean' | 'builtin' | 'cdata' | 'char' | 'class-name' | 'comment' | 'constant' | 'deleted' | 'doctype' | 'entity' | 'function' | 'important' | 'inserted' | 'italic' | 'keyword' | 'namespace' | 'number' | 'operator' | 'prolog' | 'property' | 'punctuation' | 'regex' | 'selector' | 'string' | 'symbol' | 'tag' | 'url'} StandardTokenName
+ */
+
+/**
+ * @typedef {string | StandardTokenName} TokenName
+ */
+
+/**
+ * A token stream is an array of strings and {@link Token Token} objects.
+ *
+ * Token streams have to fulfill a few properties that are assumed by most functions (mostly internal ones) that process
+ * them.
+ *
+ * 1. No adjacent strings.
+ * 2. No empty strings.
+ *
+ * The only exception here is the token stream that only contains the empty string and nothing else.
+ *
+ * @typedef {(string | Token)[]} TokenStream
+ */
