c8992dbe35c7e25327bc67ef223be54263899b1b
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-08-06 18:09:41 +0200

    Transform `src/shared/languages`

---
+++ src/shared/languages/templating.js
@@ -0,0 +1,203 @@
+import { getTextContent } from '../../core/classes/token.js';
+import { withoutTokenize } from '../../util/language-util.js';
+
+const placeholderPattern = /___PH\d+___/;
+
+/**
+ * @param {number} id
+ * @returns {string}
+ */
+function getPlaceholder (id) {
+	return `___PH${id}___`;
+}
+
+/**
+ * @param {string} code
+ * @param {Grammar | undefined} grammar
+ * @param {Prism} Prism
+ * @returns {BuildPlaceholdersResult}
+ */
+function buildPlaceholders (code, grammar, Prism) {
+	if (!grammar) {
+		return { hostCode: code, tokenStack: [] };
+	}
+
+	const templateTokens = Prism.tokenize(code, grammar);
+	const hasPlaceholderLike = placeholderPattern.test(code);
+
+	let hostCode = '';
+
+	/** @type {TokenStack} */
+	const tokenStack = [];
+
+	let id = 0;
+	for (const token of templateTokens) {
+		if (typeof token === 'string') {
+			hostCode += token;
+		}
+		else if (token.type.startsWith('ignore')) {
+			hostCode += getTextContent(token.content);
+		}
+		else {
+			if (hasPlaceholderLike) {
+				while (code.includes(getPlaceholder(id))) {
+					id++;
+				}
+			}
+
+			tokenStack.push([id, token]);
+			hostCode += getPlaceholder(id);
+			id++;
+		}
+	}
+
+	return { hostCode, tokenStack };
+}
+
+/**
+ * @param {TokenStream} hostTokens
+ * @param {TokenStack} tokenStack
+ */
+function insertIntoHostToken (hostTokens, tokenStack) {
+	let j = 0;
+
+	/**
+	 * @param {TokenStream} tokens
+	 * @returns {TokenStream}
+	 */
+	const walkTokens = tokens => {
+		for (let i = 0; i < tokens.length; i++) {
+			// all placeholders are replaced already
+			if (j >= tokenStack.length) {
+				break;
+			}
+
+			const token = tokens[i];
+			if (typeof token === 'string' || typeof token.content === 'string') {
+				const [id, t] = tokenStack[j];
+				const s = typeof token === 'string' ? token : /** @type {string} */ (token.content);
+				const placeholder = getPlaceholder(id);
+
+				const index = s.indexOf(placeholder);
+				if (index > -1) {
+					++j;
+
+					const before = s.substring(0, index);
+					const middle = t;
+					const after = s.substring(index + placeholder.length);
+
+					/** @type {TokenStream} */
+					const replacement = [];
+					if (before) {
+						replacement.push(before);
+					}
+					replacement.push(middle);
+					if (after) {
+						replacement.push(...walkTokens([after]));
+					}
+
+					if (typeof token === 'string') {
+						tokens.splice(i, 1, ...replacement);
+					}
+					else {
+						token.content = replacement;
+					}
+				}
+			}
+			else {
+				walkTokens(token.content);
+			}
+		}
+
+		return tokens;
+	};
+
+	walkTokens(hostTokens);
+}
+
+/**
+ * @param {GrammarRef} ref
+ * @param {Registry} components
+ * @returns {Grammar | undefined}
+ */
+function resolve (ref, components) {
+	if (!ref) {
+		return undefined;
+	}
+	else if (typeof ref === 'string') {
+		return components.getLanguage(ref);
+	}
+	else {
+		return ref;
+	}
+}
+
+/**
+ * @param {string} code
+ * @param {GrammarRef} hostGrammar
+ * @param {GrammarRef} templateGrammar
+ * @param {Prism} Prism
+ * @returns {TokenStream}
+ */
+export function templating (code, hostGrammar, templateGrammar, Prism) {
+	hostGrammar = resolve(hostGrammar, Prism.components);
+	templateGrammar = resolve(templateGrammar, Prism.components);
+
+	const { hostCode, tokenStack } = buildPlaceholders(code, templateGrammar, Prism);
+
+	const tokens = hostGrammar ? Prism.tokenize(hostCode, hostGrammar) : [hostCode];
+	insertIntoHostToken(tokens, tokenStack);
+	return tokens;
+}
+
+/**
+ * @param {GrammarRef} hostGrammar
+ * @returns {EmbeddedInFunction}
+ */
+export function embeddedIn (hostGrammar) {
+	return (code, templateGrammar, Prism) => {
+		return templating(code, hostGrammar, withoutTokenize(templateGrammar), Prism);
+	};
+}
+
+/**
+ * @typedef {object} BuildPlaceholdersResult
+ * @property {string} hostCode
+ * @property {TokenStack} tokenStack
+ */
+
+/**
+ * @callback EmbeddedInFunction
+ * @param {string} code
+ * @param {Grammar} templateGrammar
+ * @param {Prism} Prism
+ * @returns {TokenStream}
+ */
+
+/**
+ * @typedef {Array<[number, Token]>} TokenStack
+ */
+
+/**
+ * @typedef {Grammar | string | undefined | null} GrammarRef
+ */
+
+/**
+ * @typedef {import('../../core.js').Prism} Prism
+ */
+
+/**
+ * @typedef {import('../../core/classes/token.js').Token} Token
+ */
+
+/**
+ * @typedef {import('../../core/classes/token.js').TokenStream} TokenStream
+ */
+
+/**
+ * @typedef {import('../../core/registry.js').Registry} Registry
+ */
+
+/**
+ * @typedef {import('../../types.d.ts').Grammar} Grammar
+ */
