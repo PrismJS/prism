98c8752300a8c6023c0cd51a841ba789c64a9ec5
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-08-06 22:48:14 +0200

    Transform `src/load-languages.ts`

---
+++ src/load-languages.js
@@ -0,0 +1,72 @@
+import { resolveAlias } from './shared/meta/alias-data.js';
+import { knownLanguages } from './shared/meta/all-languages-data.js';
+import { toArray } from './util/iterables.js';
+
+/**
+ * @param {string} dir
+ * @param {string} file
+ * @returns {string}
+ */
+function pathJoin (dir, file) {
+	return dir.replace(/\/$/, '') + '/' + file;
+}
+
+/** @type {Map<string, Promise>} */
+const importCache = new Map();
+
+/**
+ * @param {string} file
+ * @returns {Promise}
+ */
+function importFile (file) {
+	let promise = importCache.get(file);
+	if (promise === undefined) {
+		promise = import(file);
+		importCache.set(file, promise);
+	}
+	return promise;
+}
+
+/**
+ * Loads the given languages and adds them to the given Prism instance.
+ *
+ * If no languages are provided, __all__ Prism languages will be loaded.
+ *
+ * @param {Prism} Prism
+ * @param {string | string[]} [languages=knownLanguages]
+ * @param {string} [srcPath='.']
+ * @returns {Promise}
+ */
+export async function loadLanguages (Prism, languages = knownLanguages, srcPath = '.') {
+	languages = toArray(languages)
+		.map(resolveAlias)
+		.filter(id => !Prism.components.has(id));
+
+	await Promise.all(
+		languages.map(async id => {
+			try {
+				const path = pathJoin(srcPath, `languages/${id}.js`);
+				const exports = await importFile(path);
+				Prism.components.add(exports.default);
+			}
+			catch (error) {
+				if (!loadLanguages.silent) {
+					console.warn(`Unable to load language ${id}: ${String(error)}`);
+				}
+			}
+		})
+	);
+}
+
+/**
+ * Set this to `true` to prevent all warning messages `loadLanguages` logs.
+ */
+loadLanguages.silent = false;
+
+/**
+ * @typedef {import('./core.js').Prism} Prism
+ */
+
+/**
+ * @typedef {import('./types.d.ts').ComponentProto} ComponentProto
+ */
