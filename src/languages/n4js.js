41eb041fb59cbd2546f0457ab7d744221e6e92a2
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-10-10 18:39:24 +0200

    n4js

--- src/languages/n4js.js
+++ src/languages/n4js.js
@@ -1,4 +1,3 @@
-import { insertBefore } from '../util/language-util.js';
 import javascript from './javascript.js';
 
 /** @type {import('../types.d.ts').LanguageProto<'n4js'>} */
@@ -6,19 +5,19 @@ export default {
 	id: 'n4js',
 	base: javascript,
 	alias: 'n4jsd',
-	grammar ({ base }) {
-		insertBefore(base, 'constant', {
-			// Annotations in N4JS spec: https://numberfour.github.io/n4js/spec/N4JSSpec.html#_annotations
-			'annotation': {
-				pattern: /@+\w+/,
-				alias: 'operator',
-			},
-		});
-
+	grammar () {
 		return {
 			// Keywords from N4JS language spec: https://numberfour.github.io/n4js/spec/N4JSSpec.html
 			'keyword':
 				/\b(?:Array|any|boolean|break|case|catch|class|const|constructor|continue|debugger|declare|default|delete|do|else|enum|export|extends|false|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|module|new|null|number|package|private|protected|public|return|set|static|string|super|switch|this|throw|true|try|typeof|var|void|while|with|yield)\b/,
+			$insert: {
+				// Annotations in N4JS spec: https://numberfour.github.io/n4js/spec/N4JSSpec.html#_annotations
+				'annotation': {
+					$before: 'constant',
+					pattern: /@+\w+/,
+					alias: 'operator',
+				},
+			},
 		};
 	},
 };
