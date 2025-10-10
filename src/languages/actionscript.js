365e5314c1b000fcca848f290ba473b4ee468a06
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-10-10 15:38:34 +0200

    actionscript

--- src/languages/actionscript.js
+++ src/languages/actionscript.js
@@ -1,37 +1,30 @@
-import { insertBefore } from '../util/language-util.js';
 import javascript from './javascript.js';
 
 /** @type {import('../types.d.ts').LanguageProto<'actionscript'>} */
 export default {
 	id: 'actionscript',
 	base: javascript,
-	grammar ({ base }) {
-		const className = /** @type {GrammarToken} */ (base['class-name']);
-		className.alias = 'function';
-
-		delete base['doc-comment'];
-
-		// doesn't work with AS because AS is too complex
-		delete base['parameter'];
-		delete base['literal-property'];
-
-		insertBefore(base, 'string', {
-			'xml': {
-				pattern:
-					/(^|[^.])<\/?\w+(?:\s+[^\s>\/=]+=("|')(?:\\[\s\S]|(?!\2)[^\\])*\2)*\s*\/?>/,
-				lookbehind: true,
-				inside: 'markup',
-			},
-		});
-
+	grammar () {
 		return {
 			'keyword':
 				/\b(?:as|break|case|catch|class|const|default|delete|do|dynamic|each|else|extends|final|finally|for|function|get|if|implements|import|in|include|instanceof|interface|internal|is|namespace|native|new|null|override|package|private|protected|public|return|set|static|super|switch|this|throw|try|typeof|use|var|void|while|with)\b/,
 			'operator': /\+\+|--|(?:[+\-*\/%^]|&&?|\|\|?|<<?|>>?>?|[!=]=?)=?|[~?@]/,
+			// doesn't work with AS because AS is too complex
+			$delete: ['doc-comment', 'parameter', 'literal-property'],
+			$merge: {
+				'class-name': {
+					alias: 'function',
+				},
+			},
+			$insert: {
+				'xml': {
+					$before: 'string',
+					pattern:
+						/(^|[^.])<\/?\w+(?:\s+[^\s>\/=]+=("|')(?:\\[\s\S]|(?!\2)[^\\])*\2)*\s*\/?>/,
+					lookbehind: true,
+					inside: 'markup',
+				},
+			},
 		};
 	},
 };
-
-/**
- * @typedef {import('../types.d.ts').GrammarToken} GrammarToken
- */
