3a63677c575927dce3a09a21b6e7690d50b443eb
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-10-10 16:20:45 +0200

    cilkcpp

--- src/languages/cilkcpp.js
+++ src/languages/cilkcpp.js
@@ -1,4 +1,3 @@
-import { insertBefore } from '../util/language-util.js';
 import cpp from './cpp.js';
 
 /** @type {import('../types.d.ts').LanguageProto<'cilkcpp'>} */
@@ -6,13 +5,15 @@ export default {
 	id: 'cilkcpp',
 	base: cpp,
 	alias: ['cilk-cpp', 'cilk'],
-	grammar ({ base }) {
-		insertBefore(base, 'function', {
-			'parallel-keyword': {
-				pattern: /\bcilk_(?:for|reducer|s(?:cope|pawn|ync))\b/,
-				alias: 'keyword',
+	grammar () {
+		return {
+			$insert: {
+				'parallel-keyword': {
+					$before: 'function',
+					pattern: /\bcilk_(?:for|reducer|s(?:cope|pawn|ync))\b/,
+					alias: 'keyword',
+				},
 			},
-		});
-		return {};
+		};
 	},
 };
