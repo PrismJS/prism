08a7f2aacb8cb999200bf28d1f828466e69d74d4
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-10-10 23:05:36 +0200

    phpdoc

--- src/languages/phpdoc.js
+++ src/languages/phpdoc.js
@@ -1,16 +1,24 @@
-import { insertBefore } from '../util/language-util.js';
 import javadoclike from './javadoclike.js';
 
 /** @type {import('../types.d.ts').LanguageProto<'phpdoc'>} */
 export default {
 	id: 'phpdoc',
 	base: javadoclike,
-	grammar ({ base }) {
+	grammar () {
 		const typeExpression = /(?:\b[a-zA-Z]\w*|[|\\[\]])+/.source;
 
-		insertBefore(base, 'keyword', {
-			'class-name': [
-				{
+		return {
+			'parameter': {
+				pattern: RegExp(
+					'(@(?:global|param|property(?:-read|-write)?|var)\\s+(?:' +
+						typeExpression +
+						'\\s+)?)\\$\\w+'
+				),
+				lookbehind: true,
+			},
+			$insert: {
+				'class-name': {
+					$before: 'keyword',
 					pattern: RegExp(
 						'(@(?:global|package|param|property(?:-read|-write)?|return|subpackage|throws|var)\\s+)' +
 							typeExpression
@@ -22,17 +30,6 @@ export default {
 						'punctuation': /[|\\[\]()]/,
 					},
 				},
-			],
-		});
-
-		return {
-			'parameter': {
-				pattern: RegExp(
-					'(@(?:global|param|property(?:-read|-write)?|var)\\s+(?:' +
-						typeExpression +
-						'\\s+)?)\\$\\w+'
-				),
-				lookbehind: true,
 			},
 		};
 	},
