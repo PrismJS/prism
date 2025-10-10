7d6b304b30dc4901f9c82c7040f2202a61fa08d3
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-10-10 17:40:30 +0200

    jsonp

--- src/languages/jsonp.js
+++ src/languages/jsonp.js
@@ -1,17 +1,17 @@
-import { insertBefore } from '../util/language-util.js';
 import json from './json.js';
 
 /** @type {import('../types.d.ts').LanguageProto<'jsonp'>} */
 export default {
 	id: 'jsonp',
 	base: json,
-	grammar ({ base }) {
-		insertBefore(base, 'punctuation', {
-			'function': /(?!\s)[_$a-zA-Z\xA0-\uFFFF](?:(?!\s)[$\w\xA0-\uFFFF])*(?=\s*\()/,
-		});
-
+	grammar () {
 		return {
 			'punctuation': /[{}[\]();,.]/,
+			$insertBefore: {
+				'punctuation': {
+					'function': /(?!\s)[_$a-zA-Z\xA0-\uFFFF](?:(?!\s)[$\w\xA0-\uFFFF])*(?=\s*\()/,
+				},
+			},
 		};
 	},
 };
