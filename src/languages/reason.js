813e60c45d642e2aea2b5bb4626190cfeab99c23
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-10-13 10:59:57 +0200

    reason

--- src/languages/reason.js
+++ src/languages/reason.js
@@ -1,27 +1,10 @@
-import { insertBefore } from '../util/language-util.js';
 import clike from './clike.js';
 
 /** @type {import('../types.d.ts').LanguageProto<'reason'>} */
 export default {
 	id: 'reason',
 	base: clike,
-	grammar ({ base }) {
-		insertBefore(base, 'class-name', {
-			'char': {
-				pattern: /'(?:\\x[\da-f]{2}|\\o[0-3][0-7][0-7]|\\\d{3}|\\.|[^'\\\r\n])'/,
-				greedy: true,
-			},
-			// Negative look-ahead prevents from matching things like String.capitalize
-			'constructor': /\b[A-Z]\w*\b(?!\s*\.)/,
-			'label': {
-				pattern: /\b[a-z]\w*(?=::)/,
-				alias: 'symbol',
-			},
-		});
-
-		// We can't match functions property, so let's not even try.
-		delete base.function;
-
+	grammar () {
 		return {
 			'string': {
 				pattern: /"(?:\\(?:\r\n|[\s\S])|[^\\\r\n"])*"/,
@@ -33,6 +16,24 @@ export default {
 				/\b(?:and|as|assert|begin|class|constraint|do|done|downto|else|end|exception|external|for|fun|function|functor|if|in|include|inherit|initializer|lazy|let|method|module|mutable|new|nonrec|object|of|open|or|private|rec|sig|struct|switch|then|to|try|type|val|virtual|when|while|with)\b/,
 			'operator':
 				/\.{3}|:[:=]|\|>|->|=(?:==?|>)?|<=?|>=?|[|^?'#!~`]|[+\-*\/]\.?|\b(?:asr|land|lor|lsl|lsr|lxor|mod)\b/,
+
+			// We can't match functions property, so let's not even try.
+			$delete: ['function'],
+
+			$insertBefore: {
+				'class-name': {
+					'char': {
+						pattern: /'(?:\\x[\da-f]{2}|\\o[0-3][0-7][0-7]|\\\d{3}|\\.|[^'\\\r\n])'/,
+						greedy: true,
+					},
+					// Negative look-ahead prevents from matching things like String.capitalize
+					'constructor': /\b[A-Z]\w*\b(?!\s*\.)/,
+					'label': {
+						pattern: /\b[a-z]\w*(?=::)/,
+						alias: 'symbol',
+					},
+				},
+			},
 		};
 	},
 };
