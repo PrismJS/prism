b239288a661c29eed78717ad45e75c380ce47f12
Alexandr Chigrinets <alexandr@wps.su>
2026-03-06 23:28:09 +0200

    Idris: add Idris 2 keywords, builtins, pragmas, and string interpolation

--- src/languages/idris.js
+++ src/languages/idris.js
@@ -10,10 +10,28 @@ export default {
 			'comment': {
 				pattern: /(?:(?:--|\|\|\|).*$|\{-[\s\S]*?-\})/m,
 			},
+			'string': {
+				pattern: /"(?:[^\\"]|\\[\s\S])*"/,
+				greedy: true,
+				inside: {
+					'interpolation': {
+						pattern: /\\\{[^}]*\}/,
+						inside: {
+							'punctuation': /^\\\{|\}$/,
+						},
+					},
+				},
+			},
 			'keyword':
-				/\b(?:Type|case|class|codata|constructor|corecord|data|do|dsl|else|export|if|implementation|implicit|import|impossible|in|infix|infixl|infixr|instance|interface|let|module|mutual|namespace|of|parameters|partial|postulate|private|proof|public|quoteGoal|record|rewrite|syntax|then|total|using|where|with)\b/,
-			'builtin': undefined,
+				/\b(?:auto|autobind|case|class|codata|constructor|corecord|covering|data|default|do|dsl|else|export|failing|forall|if|implementation|implicit|import|impossible|in|infix|infixl|infixr|instance|interface|let|module|mutual|namespace|of|open|parameters|partial|postulate|prefix|private|proof|public|quoteGoal|record|rewrite|syntax|then|total|typebind|using|where|with)\b/,
+			'builtin':
+				/\b(?:Bool|Char|Double|IO|Int|Integer|List|Maybe|Nat|String|Type|Unit|Void)\b/,
 			$insert: {
+				'pragma': {
+					$before: 'keyword',
+					pattern: /%\w+/,
+					alias: 'keyword',
+				},
 				'import-statement': {
 					$before: 'keyword',
 					pattern: /(^\s*import\s+)(?:[A-Z][\w']*)(?:\.[A-Z][\w']*)*/m,
